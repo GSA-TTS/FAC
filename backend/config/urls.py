f591aa595e9e2a09814b73dec42c941b42a87b70
Joe Krzystan <joseph.krzystan@gsa.gov>
2022-04-07 11:08:41 -0400

    Initial URL hookup and exposure of API
    Includes default OpenAPI Schema generation

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,23 @@
+from api import views
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.renderers import JSONOpenAPIRenderer
+from rest_framework.schemas import get_schema_view
+
+api_router = routers.DefaultRouter()
+api_router.register(r'sf-sac', views.SACViewSet)
+
+schema_view = get_schema_view(
+    title='Federal Audit Clearinghouse API',
+    version=settings.API_VERSION,
+    url='http://localhost:8000/api/',
+    renderer_classes=[JSONOpenAPIRenderer]
+)
 
 urlpatterns = [
+    path('api/schema.json', schema_view),
+    path('api/', include(api_router.urls)),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
