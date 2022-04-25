397a1c4219a99dd9baf627795716e7484a8ee8e8
Joe Krzystan <joseph.krzystan@gsa.gov>
2022-04-25 10:18:40 -0400

    Create UserProfiles on User creation
    When a new User instance is created, we use
    Django signals to create their UserProfile.

---
+++ backend/users/signals.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import UserProfile
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.get_or_create(user=instance)
