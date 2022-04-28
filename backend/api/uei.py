a1b542946d6f324c77086a3119285a8dd157234e
NicholasKramer <nick.kramer@gsa.gov>
2022-04-28 12:02:32 -0700

    Updated UEI validation

--- backend/api/uei.py
+++ backend/api/uei.py
@@ -3,15 +3,28 @@ import requests
 from config.settings import SAM_API_URL, SAM_API_KEY
 
 
+def call_sam_api(params: dict) -> tuple[requests.Response, str]:
+    """
+    Call the SAM.gov API and return the response and/or error string
+    """
+    try:
+        return requests.get(SAM_API_URL, params=params, verify=False), None
+
+    except requests.exceptions.Timeout:
+        return None, "SAM.gov API timeout"
+    except requests.exceptions.TooManyRedirects:
+        return None, "SAM.gov API error - too many redirects"
+    except requests.exceptions.RequestException as e:
+        return None, "Unable to make SAM.gov API request, error: " + str(e)
+    except Exception as e:
+        return None, "Uncaught SAM.gov API request exception: " + str(e)
+
+
 def get_uei_info_from_sam_gov(uei):
     """
     This utility function will query sam.gov to determine the status and 
     return information about a provided UEI (or throws an Exception)
     """
-    export = {
-        "valid": False
-    }
-    errors = []
 
     # SAM API Params
     api_params = {
@@ -22,75 +35,53 @@ def get_uei_info_from_sam_gov(uei):
     }
 
     # Call the SAM API
-    try:
-        r = requests.get(SAM_API_URL, params=api_params, verify=False)
+    r, error = call_sam_api(api_params)
+    if not r:
+        return {
+            "valid": False,
+            "errors": [error]
+        }
 
-        # Get the status code
-        if r.status_code == 200:
-            # Load the response json data
-            response = r.json()
+    # Get the status code
+    if r.status_code == 200:
+        # Load the response json data
+        response = r.json()
 
-            # Ensure the UEI exists in SAM.gov
-            if response.get('totalRecords') != 1:
-                errors.append("UEI was not found in SAM.gov")
+        # Ensure the UEI exists in SAM.gov
+        if response.get('totalRecords') != 1:
+            return {
+                "valid": False,
+                "errors": ["UEI was not found in SAM.gov"]
+            }
 
-            # UEI status is active
-            if response.get('entityData') and response.get('entityData')[0].get('entityRegistration').get('ueiStatus').upper() != "ACTIVE":
-                errors.append("UEI is not listed as active from SAM.gov response data")
+        entries = response.get("entityData", [])
+        if len(entries) != 1:
+            return {
+                "valid": False,
+                "errors": ["SAM.gov invalid number of entries"]
+            }
+        entry = entries[0]
+        try:
+            status = entry.get('entityRegistration', {}).get('ueiStatus', "").upper()
+        except AttributeError:
+            return {
+                "valid": False,
+                "errors": ["SAM.gov unexpected JSON shape."]
+            }
 
-        else:
-            # API call error
-            errors.append("SAM.gov API response status code invalid, status code: " + str(r.status_code))
+        if status != "ACTIVE":
+            return {
+                "valid": False,
+                "errors": ["UEI is not listed as active from SAM.gov response data"]
+            }
 
-    except requests.exceptions.Timeout:
-        # Timeout error
-        errors.append("SAM.gov API timeout")
-    except requests.exceptions.TooManyRedirects:
-        # Too many redirects
-        errors.append("SAM.gov API error - too many redirects")
-    except requests.exceptions.RequestException as e:
-        # Catastrophic error
-        errors.append("Unable to make SAM.gov API request, error: " + str(e))
-    except KeyError:
-        errors.append("SAM.gov response key error, unable to validate entityData")
-    except Exception as e:
-        errors.append("Uncaught SAM.gov API request exception: " + str(e))
+        return {
+            "valid": True,
+            "response": response["entityData"][0]
+        }
 
-    # Verify error length < 1
-    if errors:
-        export["errors"] = errors
-        return export
-
-    # Valid UEI and API response
-    export["valid"] = True
-    export["response"] = response['entityData'][0]
-
-    # Return the entity data
-    return export
-
-# sample_response = {
-#     "entityRegistration": {
-#         "samRegistered": "Yes",
-#         "ueiSAM": "ZQGGHJH74DW7",
-#         "entityEFTIndicator": null,
-#         "cageCode": "855J5",
-#         "dodaac": null,
-#         "legalBusinessName": "INTERNATIONAL BUSINESS MACHINES CORPORATION",
-#         "dbaName": null,
-#         "purposeOfRegistrationCode": "Z2",
-#         "purposeOfRegistrationDesc": "All Awards",
-#         "registrationStatus": "Inactive",
-#         "evsSource": "D&B",
-#         "registrationDate": "2018-07-24",
-#         "lastUpdateDate": "2022-03-29",
-#         "registrationExpirationDate": "2022-02-06",
-#         "activationDate": "2020-08-13",
-#         "ueiStatus": "Active",
-#         "ueiExpirationDate": null,
-#         "ueiCreationDate": "2020-05-01",
-#         "publicDisplayFlag": "Y",
-#         "exclusionStatusFlag": "N",
-#         "exclusionURL": null,
-#         "dnbOpenData": null
-#     }
-# }
+    else:
+        return {
+            "valid": False,
+            "errors": ["SAM.gov API response status code invalid, status code: " + str(r.status_code)]
+        }
