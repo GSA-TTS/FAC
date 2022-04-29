57ea185e81e291b07d62edb03aab82f824227592
Joe Krzystan <joseph.krzystan@gsa.gov>
2022-04-29 08:45:30 -0400

    Remediation of Flake8 reported errors
    Cleans up a few unsused imports and whitespace notes

--- backend/audit/validators.py
+++ backend/audit/validators.py
@@ -1,6 +1,6 @@
-from curses.ascii import isalnum
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+
 
 def validate_uei(value):
     """Validates the UEI using the UEI Spec"""
@@ -10,6 +10,7 @@ def validate_uei(value):
     validate_uei_nine_digit_sequences(value)
     return value
 
+
 def validate_uei_alphanumeric(value):
     """The UEI should be alphanumeric"""
     if not value.isalnum():
@@ -17,6 +18,7 @@ def validate_uei_alphanumeric(value):
             _('The UEI should be alphanumeric'),
         )
     return value
+
 
 def validate_uei_valid_chars(value):
     """The letters “O” and “I” are not used to avoid confusion with zero and one."""
@@ -26,8 +28,9 @@ def validate_uei_valid_chars(value):
         )
     return value
 
+
 def validate_uei_leading_char(value):
-    """The first character is not zero to avoid cutting off digits that can occur during data imports, 
+    """The first character is not zero to avoid cutting off digits that can occur during data imports,
     for example, when importing data into spreadsheet programs."""
     if value.startswith('0'):
         raise ValidationError(
@@ -35,8 +38,9 @@ def validate_uei_leading_char(value):
         )
     return value
 
+
 def validate_uei_nine_digit_sequences(value):
-    """Nine-digit sequences are not used in the identifier to avoid collision with the nine-digit 
+    """Nine-digit sequences are not used in the identifier to avoid collision with the nine-digit
     DUNS Number or Taxpayer Identification Number (TIN)."""
     count = 0
     for ch in value:
@@ -44,9 +48,9 @@ def validate_uei_nine_digit_sequences(value):
             count += 1
         else:
             count = 0
-            
+
         if count >= 9:
             raise ValidationError(
-            _('Nine-digit sequences are not used in the identifier to avoid collision with the nine-digit DUNS Number or Taxpayer Identification Number (TIN).'),
-        )
+                _('Nine-digit sequences are not used in the identifier to avoid collision with the nine-digit DUNS Number or Taxpayer Identification Number (TIN).'),
+            )
     return value
