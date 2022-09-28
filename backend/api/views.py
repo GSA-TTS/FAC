88a10d9025e862aca9abdddd916a4038af749bdb
Tim Ballard <1425377+timoballard@users.noreply.github.com>
2022-09-28 14:29:31 -0500

    enable check_object_permissions in view

--- backend/api/views.py
+++ backend/api/views.py
@@ -317,11 +317,12 @@ class SacFederalAwardsView(APIView):
 
         # Note this is a placeholder, so we're not returning anything yet.
 
-        # try:
-        #     sac = SingleAuditChecklist.objects.get(report_id=report_id)
-        # except SingleAuditChecklist.DoesNotExist as e:
-        #     raise Http404() from e
-        # self.check_object_permissions(request, sac)
+        try:
+            sac = SingleAuditChecklist.objects.get(report_id=report_id)
+        except SingleAuditChecklist.DoesNotExist as e:
+            raise Http404() from e
+            
+        self.check_object_permissions(request, sac)
 
         # To do: Get federal awards info here
 
