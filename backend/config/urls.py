c4d4e34f0405c6dc1afe6a5d20458af3223eea3c
Sarah Withee <2601974+geekygirlsarah@users.noreply.github.com>
2022-11-02 13:02:27 -0400

    (wip) Enable static serving, add index.html from 11ty repo. (rest of repo goes in backend/static folder)

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,7 @@
 from api import views
+from api.views import IndexView
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from rest_framework.renderers import JSONOpenAPIRenderer
@@ -15,6 +17,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
     path("api/auth/token", AuthToken.as_view(), name="token"),
     path("api/schema.json", schema_view),
     path("public/api/sac", views.SACViewSet.as_view({"get": "list"}), name="sac-list"),
@@ -65,4 +68,4 @@ urlpatterns = [
         name="schemas",
     ),
     path(settings.ADMIN_URL, admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
