8a89b4dcf3b2ff4d3d957ceba38fc5e38f2f0374
lindsayyoung <lindsayn.young@gsa.gov>
2022-11-02 09:49:37 -0400

    Adding an app for front end content management
    Adding app to settings
    
    First pass at content pages
    
    This creates a cms app so that we can have a clear place for content pages. This will also help with assigning scoped permissions later.
    
    There are 2 example pages to get the structure going. The home page lists the content pages. The content pages are at their own URL.
    
    We would still like to move the base template out of the current app directory and in the template directory above. We can do that as we add the real content.
    
    Moving from a app based template approach to a consolidated, project based approach
    
    (wip) Enable static serving, add index.html from 11ty repo. (rest of repo goes in backend/static folder)
    
    Linting/formatting.

---
+++ backend/cms/models.py
@@ -0,0 +1,32 @@
+# importing django models and users
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# creating an django model class
+class Posts(models.Model):
+    """Basic model for posts."""
+
+    # title field using charfield constraint with unique constraint
+    title = models.CharField(max_length=200, unique=True)
+    # slug field auto populated using title with unique constraint
+    slug = models.SlugField(max_length=200, unique=True)
+    # author field populated using users database
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # and date time fields automatically populated using system time
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    # content field to store our post
+    content = models.TextField()
+    # meta description for SEO benefits and accessibility
+    meta_description = models.CharField(max_length=300, null=True)
+
+    # meta for the class
+    class Meta:
+        """Meta information"""
+
+        ordering = ["-created_on"]
+
+    # used while managing models from terminal
+    def __str__(self):
+        return str(self.title)
---
+++ backend/cms/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # home page
+    path("", views.postslist.as_view(), name="posts"),
+    # route for posts
+    path("<slug:slug>/", views.postdetail.as_view(), name="post_detail"),
+]
