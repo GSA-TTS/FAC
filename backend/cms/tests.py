1323cb10a6ef8a8a276f6d72204ac4f47ae3540f
lindsayyoung <lindsayn.young@gsa.gov>
2022-11-02 09:49:37 -0400

    Adding an app for front end content management

--- backend/cms/tests.py
+++ backend/cms/tests.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 # from django.test import TestCase
+=======
+from django.test import TestCase
+>>>>>>> 6d2e3a1... Adding an app for front end content management
 
 # Create your tests here.
