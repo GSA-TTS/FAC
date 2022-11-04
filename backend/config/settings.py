cc82b5621fa07cdbaf066c4a7264398a3773c64e
lindsayyoung <lindsayn.young@gsa.gov>
2022-11-04 10:27:25 -0400

    Moving from a app based template approach to a consolidated, project based approach

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,9 +97,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
-            str(BASE_DIR.joinpath("templates")),
-        ],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
