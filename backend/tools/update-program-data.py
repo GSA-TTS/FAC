c653e6d33a1ec8d92ea1f1068a16fd130a60812f
Tim Ballard <1425377+timoballard@users.noreply.github.com>
2022-11-07 11:42:23 -0600

    lint

--- backend/tools/update-program-data.py
+++ backend/tools/update-program-data.py
@@ -11,11 +11,8 @@ def extract(input_filename):
         reader = csv.reader(csvfile)
 
         for row in reader:
-            data.append({
-                "program_name": row[0],
-                "program_number": row[1]
-            })
-    
+            data.append({"program_name": row[0], "program_number": row[1]})
+
     return data
 
 
@@ -24,14 +21,13 @@ def transform(program_data):
         "title": "ProgramNumber",
         "type": "string",
         "description": "Program numbers",
-        "anyOf": []
+        "anyOf": [],
     }
 
     for entry in program_data:
-        schema["anyOf"].append({
-            "const": entry["program_number"],
-            "description": entry["program_name"]
-        })
+        schema["anyOf"].append(
+            {"const": entry["program_number"], "description": entry["program_name"]}
+        )
 
     return schema
 
@@ -40,7 +36,7 @@ def load(schema_filename, schema_def):
     with open(schema_filename) as schema_file:
         schema = json.load(schema_file)
 
-        jsonpath_expr = parse("$.\"$defs\".ProgramNumber")
+        jsonpath_expr = parse('$."$defs".ProgramNumber')
         jsonpath_expr.update(schema, schema_def)
 
         return schema
@@ -49,9 +45,24 @@ def load(schema_filename, schema_def):
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--input-filename", type=str, required=True, help="Filename for assistance listings CSV file from SAM.gov")
-    parser.add_argument("--schema-filename", type=str, required=True, help="Filename for the JSON schema to be updated")
-    parser.add_argument("--output-filename", type=str, required=False, help="[Optional] Filename for the updated JSON schema. If not present, schema-filename will be updated in place")
+    parser.add_argument(
+        "--input-filename",
+        type=str,
+        required=True,
+        help="Filename for assistance listings CSV file from SAM.gov",
+    )
+    parser.add_argument(
+        "--schema-filename",
+        type=str,
+        required=True,
+        help="Filename for the JSON schema to be updated",
+    )
+    parser.add_argument(
+        "--output-filename",
+        type=str,
+        required=False,
+        help="[Optional] Filename for the updated JSON schema. If not present, schema-filename will be updated in place",
+    )
 
     args = parser.parse_args()
 
@@ -61,7 +72,7 @@ def main():
 
     output_filename = args.output_filename or args.schema_filename
 
-    with open(output_filename, 'w') as schema_file:
+    with open(output_filename, "w") as schema_file:
         json.dump(updated_schema, schema_file, indent=2)
 
 
