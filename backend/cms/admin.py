fba3b4824d838b4e33ca093dcdf1c89cd79b65f7
Matt Henry <hey@matthenry.fyi>
2022-11-10 14:17:58 -0500

    Configure postcss
    Linting

--- backend/cms/admin.py
+++ backend/cms/admin.py
@@ -8,4 +8,5 @@ class PostAdmin(admin.ModelAdmin):
     fields = ["title", "slug", "author", "meta_description", "content"]
 
 
+
 admin.site.register(Posts, PostAdmin)
--- backend/cms/tests.py
+++ backend/cms/tests.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 # from django.test import TestCase
-=======
-from django.test import TestCase
->>>>>>> 6d2e3a1... Adding an app for front end content management
+
 
 # Create your tests here.
