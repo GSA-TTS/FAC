f94cc43d63074d64c957caae5285d8c03a2fbded
JeanMarie Mariadassou <JeanMarie.Mariadassou@gsa.gov>
2023-01-04 15:00:07 -0800

    Navigate from login page to My Submissions page

--- backend/cms/views.py
+++ backend/cms/views.py
@@ -1,12 +1,16 @@
 from .models import Posts
 from django.views import generic
-
+from django.shortcuts import render
 
 # class based views for posts
-class home(generic.TemplateView):
-    """Basic class for home."""
 
-    template_name = "home.html"
+
+class home(generic.View):
+    def get(self, request, *args, **kwargs):
+        template_name = "home.html"
+        if request.user.is_authenticated:
+            template_name = "audit/my_submissions.html"
+        return render(request, template_name)
 
 
 # class based view for each post
