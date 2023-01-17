74540f7a57775c6a68e6a4bd0af71de13c4f4bb7
lindsayyoung <lindsayn.young@gsa.gov>
2023-01-17 13:32:25 -0500

    Initial data exploration
    - Loading Ricks suggested schema
    - Loaded schema from script one table at a time

---
+++ backend/data_distro/pub_data_load_script.py
@@ -0,0 +1,21 @@
+import csv
+
+from models.py import *
+
+def read_file(file_name):
+    """Simple data loader"""
+    data_to_load = []
+    with open(file_name, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        header_row = reader.pop(0)
+        for row in reader:
+            postion = 0
+            for data in row:
+                data_to_load.append({header_row[postion]: data})
+                postion += 1
+
+def load_
+
+
+
+
