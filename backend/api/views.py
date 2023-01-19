280873c112a69ce5590ae28a144264c4163c86e4
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2023-01-19 11:17:56 -0800

    Make black and flake8 play nicely together (binary operator after linebreak issue).

--- backend/api/views.py
+++ backend/api/views.py
@@ -31,12 +31,13 @@ AUDITEE_INFO_PREVIOUS_STEP_DATA_WE_NEED = [
     "is_usa_based",
 ]
 
+AUDITEE_INFO_STEP_TWO_FIELDS = [
+    "auditee_fiscal_period_start",
+    "auditee_fiscal_period_end",
+]
+
 ACCESS_SUBMISSION_PREVIOUS_STEP_DATA_WE_NEED = (
-    AUDITEE_INFO_PREVIOUS_STEP_DATA_WE_NEED
-    + [
-        "auditee_fiscal_period_start",
-        "auditee_fiscal_period_end",
-    ]
+    AUDITEE_INFO_PREVIOUS_STEP_DATA_WE_NEED + AUDITEE_INFO_STEP_TWO_FIELDS
 )
 
 
