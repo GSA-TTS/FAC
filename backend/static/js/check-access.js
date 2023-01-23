f9b78c3ea099863f4754814529bf375475054aa1
Sarah Withee <2601974+geekygirlsarah@users.noreply.github.com>
2023-01-23 11:31:34 -0500

    (wip) Remove doubled code (FE/BE) to redirect step 3->done

--- backend/static/js/check-access.js
+++ backend/static/js/check-access.js
@@ -1,67 +1,7 @@
 import { checkValidity } from './validate';
-import { queryAPI } from './api';
 
-const SUBMISSION_URL = '/report_submission/accessandsubmission/';
-const NEXT_URL = '../../audit/';
 const FORM = document.forms[0];
 let addedContactNum = 1; // Counter for added contacts
-
-function submitForm() {
-  const formData = serializeFormData(new FormData(FORM));
-
-  let preparedData = {};
-  preparedData['certifying_auditee_contact'] =
-    formData.auditee_certifying_official_email;
-  preparedData['certifying_auditor_contact'] =
-    formData.auditor_certifying_official_email;
-  preparedData['auditee_contacts'] = contactsToArray(
-    formData,
-    'auditee_contacts_email'
-  );
-  preparedData['auditor_contacts'] = contactsToArray(
-    formData,
-    'auditor_contacts_email'
-  );
-
-  queryAPI(
-    SUBMISSION_URL,
-    preparedData,
-    {
-      method: 'POST',
-    },
-    [handleAccessResponse, handleErrorResponse]
-  );
-}
-
-function handleAccessResponse(data) {
-  if (data.report_id) {
-    const nextUrlWithID = NEXT_URL + `${data.report_id}`;
-    window.location.href = NEXT_URL;
-  } else {
-    console.log(data);
-  }
-}
-function handleErrorResponse(e) {
-  console.log('ERROR: Form submission error. ' + e);
-  window.location.href = NEXT_URL;
-}
-
-function serializeFormData(formData) {
-  return Object.fromEntries(formData);
-}
-
-function contactsToArray(formData, keyContains) {
-  const regex = new RegExp(keyContains);
-  let outputArray = [];
-  for (var key in formData) {
-    if (Object.hasOwn(formData, key)) {
-      if (regex.test(key)) {
-        outputArray.push(formData[key]);
-      }
-    }
-  }
-  return outputArray;
-}
 
 function setFormDisabled(shouldDisable) {
   const continueBtn = document.getElementById('create');
@@ -86,7 +26,7 @@ function appendContactField(btnEl) {
   const newInputs = newRow.querySelectorAll('input');
   newInputs.forEach(function (input) {
     input.id = input.id + '-' + addedContactNum;
-    input.name = input.name + '-' + addedContactNum;
+    input.name = input.name; // + '-' + addedContactNum;
   });
 
   const newLabels = newRow.querySelectorAll('label');
@@ -122,7 +62,7 @@ function attachEventHandlers() {
   FORM.addEventListener('submit', (e) => {
     e.preventDefault();
     if (!allResponsesValid()) return;
-    submitForm();
+    FORM.submit();
   });
 
   const fieldsNeedingValidation = Array.from(
