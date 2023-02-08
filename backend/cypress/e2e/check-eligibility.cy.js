b9da90d5f95bfe8bdb41336959ed41c6718c97c1
Matt Henry <hey@matthenry.fyi>
2023-02-08 14:25:35 -0500

    wip move cypress specs over

--- backend/cypress/e2e/check-eligibility.cy.js
+++ backend/cypress/e2e/check-eligibility.cy.js
@@ -1,26 +1,36 @@
 describe('Create New Audit', () => {
-  before(() => {
-    cy.visit('/audit/new/step-1');
+  beforeEach(() => {
+    cy.visit('/report_submission/eligibility/');
   });
 
   describe('A Blank Form', () => {
     it('marks empty responses as invalid', () => {
       cy.get('fieldset.question:invalid').should('have.length', 3);
-    });
 
-    it('will not submit', () => {
       cy.get('.usa-form--large').invoke('submit', (e) => {
         e.preventDefault();
         throw new Error('Form was submitted'); // The test will fail if this error is thrown
       });
 
       cy.get('.usa-button').contains('Continue').click();
-    });
 
-    it('sets focus on the first invalid input', () => {
       cy.get('.usa-button').contains('Continue').click();
       cy.focused().should('have.attr', 'type', 'radio');
     });
+
+    // it('will not submit', () => {
+    //   cy.get('.usa-form--large').invoke('submit', (e) => {
+    //     e.preventDefault();
+    //     throw new Error('Form was submitted'); // The test will fail if this error is thrown
+    //   });
+
+    //   cy.get('.usa-button').contains('Continue').click();
+    // });
+
+    // it('sets focus on the first invalid input', () => {
+    //   cy.get('.usa-button').contains('Continue').click();
+    //   cy.focused().should('have.attr', 'type', 'radio');
+    // });
   });
 
   describe('Validation', () => {
