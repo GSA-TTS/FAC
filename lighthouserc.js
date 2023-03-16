130932369b8900dbe2324eb47f30a8bc1c5172a5
Matt Henry <hey@matthenry.fyi>
2023-03-16 10:49:32 -0400

    configure lighthouse and pa11y

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,13 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:8000/'],
+      url: [
+        'http://localhost:8000/',
+        'http://localhost:8000/report_submission/eligibility',
+        'http://localhost:8000/report_submission/auditeeinfo',
+        'http://localhost:8000/report_submission/accessandsubmission',
+        'http://localhost:8000/audit',
+      ],
     },
     assert: {
       assertions: {
