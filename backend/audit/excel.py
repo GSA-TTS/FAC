47dcf2d9c8c411179e71563aa067ba0157848ae6
Tim Ballard <timothy.ballard@gsa.gov>
2023-03-20 15:32:26 -0500

    proxy calls to pydash

--- backend/audit/excel.py
+++ backend/audit/excel.py
@@ -4,6 +4,11 @@ from openpyxl.cell import Cell
 import pydash
 
 
+def _set_by_path(target_obj, target_path, value):
+    """Set a (potentially nested) field in target_obj using JSONPath-esque dot notation, e.g. parent.child[0].field"""
+    pydash.set_(target_obj, target_path, value)
+
+
 """
 Maps a named Excel cell to a JSON path with a callable that applies the cell value to the target object
 """
@@ -17,27 +22,27 @@ ColumnMapping = dict[str, tuple[str, str, Callable[[Any, Any, Any], Any]]]
 
 def _set_pass_through_entity_name(obj, target, value):
     [
-        pydash.set_(obj, f"{target}[{index}].name", v)
+        _set_by_path(obj, f"{target}[{index}].name", v)
         for index, v in enumerate(value.split("|"))
     ]
 
 
 def _set_pass_through_entity_id(obj, target, value):
     [
-        pydash.set_(obj, f"{target}[{index}].identifying_number", v)
+        _set_by_path(obj, f"{target}[{index}].identifying_number", v)
         for index, v in enumerate(value.split("|"))
     ]
 
 
 federal_awards_field_mapping: FieldMapping = {
-    "auditee_ein": ("FederalAwards.auditee_ein", pydash.set_),
-    "total_amount_expended": ("FederalAwards.total_amount_expended", pydash.set_),
+    "auditee_ein": ("FederalAwards.auditee_ein", _set_by_path),
+    "total_amount_expended": ("FederalAwards.total_amount_expended", _set_by_path),
 }
 
 federal_awards_column_mapping: ColumnMapping = {
-    "amount_expended": ("FederalAwards.federal_awards", "amount_expended", pydash.set_),
-    "cluster_name": ("FederalAwards.federal_awards", "cluster_name", pydash.set_),
-    "direct_award": ("FederalAwards.federal_awards", "direct_award", pydash.set_),
+    "amount_expended": ("FederalAwards.federal_awards", "amount_expended", _set_by_path),
+    "cluster_name": ("FederalAwards.federal_awards", "cluster_name", _set_by_path),
+    "direct_award": ("FederalAwards.federal_awards", "direct_award", _set_by_path),
     "direct_award_pass_through_entity_name": (
         "FederalAwards.federal_awards",
         "direct_award_pass_through_entities",
@@ -51,44 +56,44 @@ federal_awards_column_mapping: ColumnMapping = {
     "federal_award_passed_to_subrecipients": (
         "FederalAwards.federal_awards",
         "federal_award_passed_to_subrecipients",
-        pydash.set_,
+        _set_by_path,
     ),
     "federal_award_passed_to_subrecipients_amount": (
         "FederalAwards.federal_awards",
         "federal_award_passed_to_subrecipients_amount",
-        pydash.set_,
+        _set_by_path,
     ),
     "federal_program_name": (
         "FederalAwards.federal_awards",
         "federal_program_name",
-        pydash.set_,
+        _set_by_path,
     ),
     "loan_balance_at_audit_period_end": (
         "FederalAwards.federal_awards",
         "loan_balance_at_audit_period_end",
-        pydash.set_,
+        _set_by_path,
     ),
     "loan_or_loan_guarantee": (
         "FederalAwards.federal_awards",
         "loan_or_loan_guarantee",
-        pydash.set_,
+        _set_by_path,
     ),
-    "major_program": ("FederalAwards.federal_awards", "major_program", pydash.set_),
+    "major_program": ("FederalAwards.federal_awards", "major_program", _set_by_path),
     "major_program_audit_report_type": (
         "FederalAwards.federal_awards",
         "major_program_audit_report_type",
-        pydash.set_,
+        _set_by_path,
     ),
     "number_of_audit_findings": (
         "FederalAwards.federal_awards",
         "number_of_audit_findings",
-        pydash.set_,
+        _set_by_path,
     ),
-    "program_number": ("FederalAwards.federal_awards", "program_number", pydash.set_),
+    "program_number": ("FederalAwards.federal_awards", "program_number", _set_by_path),
     "state_cluster_name": (
         "FederalAwards.federal_awards",
         "state_cluster_name",
-        pydash.set_,
+        _set_by_path,
     ),
 }
 
