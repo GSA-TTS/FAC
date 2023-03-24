545ddf23c5b9c0dd6b017a54fc13d7afdced3a0c
lindsayyoung <lindsayn.young@gsa.gov>
2023-03-24 13:16:10 -0400

    Adds dbkey to the API View for auditee, these are supposed to be stable over the years

--- backend/data_distro/api_views/basic_views.sql
+++ backend/data_distro/api_views/basic_views.sql
@@ -11,13 +11,20 @@ with auditee as (
     from data_distro_auditee
     where data_distro_auditee.is_public=True
 ),
-gen as(
-    select id as general_id, auditee_id
+gen as (
+    select distinct auditee_id as auditee_id, array_agg(id) as general_id
     from data_distro_general
+    group by auditee_id
+),
+db as(
+    select distinct auditee_id, array_agg(dbkey) as dbkey
+    from data_distro_general
+    group by auditee_id
 )
-select auditee.*, gen.general_id
+select auditee.*, gen.general_id, db.dbkey
 from auditee
 left join gen on gen.auditee_id=auditee.id
+left join db on db.auditee_id=auditee.id
 ;
 
 
