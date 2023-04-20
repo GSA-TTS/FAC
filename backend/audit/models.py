352d801438e3fe8087ad31e1ac105b2b27fee1c2
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-04-20 19:18:17 -0400

    #977 #994 Updated db model for storing data related to FindingsUniformGuidance.xlsx and CorrectiveActionPlan.xlsx

--- backend/audit/models.py
+++ backend/audit/models.py
@@ -10,6 +10,7 @@ from .validators import (
     validate_excel_filename,
     validate_corrective_action_plan_json,
     validate_federal_award_json,
+    validate_findings_uniform_guidance_json,
     validate_general_information_json,
 )
 
@@ -103,9 +104,14 @@ class SingleAuditChecklist(models.Model):
     )
 
     # Corrective Action Plan:
-    # corrective_action_plan = models.JSONField(
-    #     blank=True, null=True, validators=[validate_corrective_action_plan_json]
-    # )
+    corrective_action_plan = models.JSONField(
+        blank=True, null=True, validators=[validate_corrective_action_plan_json]
+    )
+
+    # Findings Uniform Guidance:
+    findings_uniform_guidance = models.JSONField(
+        blank=True, null=True, validators=[validate_findings_uniform_guidance_json]
+    )
 
     @property
     def audit_period_covered(self):
