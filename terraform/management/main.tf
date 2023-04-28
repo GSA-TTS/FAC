16a111c3c3ba53cd18d53ee6f3574d8cefd60b22
Bret Mogilefsky <bmogilefsky@gmail.com>
2023-04-28 12:27:57 -0700

    Revert "Merge pull request #1060 from GSA-TTS/1015-create-egress-spaces"
    This reverts commit 74a405149ffeb7fc04b27d422ebbc6b39003a885, reversing
    changes made to acd0da7472ba90edf1a3d505f036dab53b045ec8.

--- terraform/management/main.tf
+++ terraform/management/main.tf
@@ -8,55 +8,22 @@ data "cloudfoundry_asg" "asgs" {
   name     = each.key
 }
 
-locals {
-  egress_spaces = [for s in local.spaces : "${s}-egress"]
-  all_spaces    = setunion(local.spaces, local.egress_spaces)
-}
-
 # Ensure spaces exist
 resource "cloudfoundry_space" "space" {
-  for_each = toset(local.all_spaces)
+  for_each = toset(local.spaces)
   name     = each.key
   org      = data.cloudfoundry_org.org.id
 
-  # Disallow SSH access in production and production-egress
-  allow_ssh = (each.key != "production" && each.key != "production-egress")
+  # Disallow SSH access in production
+  allow_ssh = each.key != "production"
 
   asgs = [for d in data.cloudfoundry_asg.asgs : d.id]
 }
 
-# Grant the expected people access
 resource "cloudfoundry_space_users" "space_permissions" {
-  for_each   = toset(local.all_spaces)
+  for_each   = toset(local.spaces)
   space      = cloudfoundry_space.space[each.key].id
   developers = local.developers
   managers   = local.managers
 }
 
-### 
-# Ensure a deployer account exists in each space, and that it has the
-# SpaceDeveloper role on the corresponding -egress space
-###
-
-data "cloudfoundry_service" "service-account" {
-  name = "cloud-gov-service-account"
-}
-
-resource "cloudfoundry_service_instance" "space-deployer" {
-  for_each     = toset(local.spaces)
-  name         = "${each.key}-deployer"
-  space        = cloudfoundry_space.space[each.key].id
-  service_plan = "space-deployer"
-}
-
-resource "cloudfoundry_service_key" "space-deployer-key" {
-  for_each         = toset(local.spaces)
-  name             = "${each.key}-deployer-key"
-  service_instance = cloudfoundry_service_instance.space-deployer[each.key].id
-}
-
-resource "cloudfoundry_space_users" "egress-deployer" {
-  for_each   = toset(local.spaces)
-  space      = cloudfoundry_space.space["${each.key}-egress"].id
-  developers = [cloudfoundry_service_key.space-deployer-key[each.key].credentials.username]
-}
