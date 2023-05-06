44cbff6cf7c9413423eb609396e7718a7d702477
Matt Jadud <matthew.jadud@gsa.gov>
2023-05-06 11:05:36 -0400

    Removing unused code.

--- backend/data_distro/management/commands/validate_api.py
+++ backend/data_distro/management/commands/validate_api.py
@@ -17,38 +17,6 @@ from data_distro.management.commands.handle_errors import (
 )
 
 
-def validations(view_name, result_set, schema):
-    failure_count = 0
-    start = time()
-    for obj in result_set:
-        try:
-            validate(instance=obj, schema=schema)
-        except:
-            failure_count += 1
-    if len(result_set) > 0:
-        print(f"loop time: {time() - start} each: {(time() - start)/len(result_set)}")
-    print(f"{view_name} assertion failures: {failure_count}")
-    return failure_count
-
-
-def _test_framing(view_name, schema_file):
-    schema = json.load(open(schema_file))
-    failure_count = 0
-    loop_step = 10000
-    for id_range in map(lambda ndx: (ndx * loop_step, (ndx * loop_step) + loop_step), range(0, 10)):
-        print(id_range)
-        start = time()
-        result_set = (FAC()
-                      .table(view_name)
-                      .limits(id_range[0], id_range[1], loop_step // 2)
-                      .debug()
-                      ).run()
-        print(f"query time: {(time() - start)}")
-        print("result_set length:", len(result_set))
-        failure_count += validations(view_name, result_set, schema)
-    return failure_count
-
-
 class Command(BaseCommand):
     help = """
         Runs validations on the API endpoints.
