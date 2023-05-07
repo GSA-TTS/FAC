5d4a3162e3535b4ade4e9e7bc4173b0ac6b65309
JeanMarie Mariadassou <JeanMarie.Mariadassou@gsa.gov>
2023-05-07 14:39:40 -0700

    Download/Scan images on workflow

--- terraform/shared/modules/env/postgrest.tf
+++ terraform/shared/modules/env/postgrest.tf
@@ -16,7 +16,9 @@ resource "cloudfoundry_service_key" "postgrest" {
 resource "cloudfoundry_app" "postgrest" {
   name         = local.postgrest_name
   space        = data.cloudfoundry_space.apps.id
-  docker_image = "file://${{ GITHUB_WORKSPACE }}/postgrest-image.tar.gz" #specify the path to the downloaded artifact
+  # docker_image = "postgrest/postgrest:v10.1.2"
+  docker_image = "./postgrest-image.tar"
+
   timeout      = 180
   memory       = 128
   disk_quota   = 256
