ed9f97064cac6d83706e97c2c76e20adb9085795
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-05-15 08:08:38 -0400

    #940 Linting fix

--- backend/excel_templates/tests/test_range_union.py
+++ backend/excel_templates/tests/test_range_union.py
@@ -7,18 +7,21 @@ schema_path = "../schemas/sources"
 
 test_table = [
     {
-        "excel": "federal-awards-expended-template-20230425.jsonnet", 
+        "excel": "federal-awards-expended-template-20230425.jsonnet",
         "schema": "FederalAwards.schema.jsonnet",
-        "ignore_in_schema": set([
-            "cluster", 
-            "direct_or_indirect_award", 
-            "loan_or_loan_guarantee",
-            "program",
-            "subrecipients",
-            "entities"
-            ])
+        "ignore_in_schema": set(
+            [
+                "cluster",
+                "direct_or_indirect_award",
+                "loan_or_loan_guarantee",
+                "program",
+                "subrecipients",
+                "entities",
+            ]
+        ),
     },
 ]
+
 
 def jsonnet_sheet_spec_to_json(filename):
     print(os.getcwd())
@@ -26,18 +29,20 @@ def jsonnet_sheet_spec_to_json(filename):
     jobj = json.loads(json_str)
     return jobj
 
+
 def extract_ranges_from_excel_def(filename) -> set:
     jobj = jsonnet_sheet_spec_to_json(filename)
     range_names = set()
-    sheets = jobj['sheets']
+    sheets = jobj["sheets"]
     for sheet in sheets:
         singles = sheet["single_cells"]
-        ranges  = sheet["open_ranges"]
+        ranges = sheet["open_ranges"]
         for s in singles:
             range_names.add(s["range_name"])
         for r in ranges:
             range_names.add(r["range_name"])
     return range_names
+
 
 def recursively_find_properties(obj):
     found = set()
@@ -53,15 +58,21 @@ def recursively_find_properties(obj):
             found.update(recursively_find_properties(item))
     return found
 
+
 def extract_ranges_from_schema(root, filename) -> set:
     jobj = jsonnet_sheet_spec_to_json(filename)
     found = recursively_find_properties(jobj["properties"][root]["properties"])
     return found
-        
+
+
 def test_federal_awards_named_ranges():
     for d in test_table:
-        excel_ranges = extract_ranges_from_excel_def(os.path.join(os.getcwd(), excel_path, d["excel"]))
-        schema_ranges = extract_ranges_from_schema("FederalAwards", os.path.join(os.getcwd(), schema_path, d["schema"]))
+        excel_ranges = extract_ranges_from_excel_def(
+            os.path.join(os.getcwd(), excel_path, d["excel"])
+        )
+        schema_ranges = extract_ranges_from_schema(
+            "FederalAwards", os.path.join(os.getcwd(), schema_path, d["schema"])
+        )
         schema_ranges.difference_update(d["ignore_in_schema"])
         ediff = excel_ranges.difference(schema_ranges)
         sdiff = schema_ranges.difference(excel_ranges)
