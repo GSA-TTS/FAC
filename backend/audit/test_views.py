18f9b30354664ec3c80df6052649911b49e8d776
Tim Ballard <timothy.ballard@gsa.gov>
2023-05-16 09:40:04 -0500

    linter appeasement

--- backend/audit/test_views.py
+++ backend/audit/test_views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponseRedirect
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
