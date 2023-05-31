b844435196009a89135ea8536522535888166262
Alex Steel <alexander.steel@gsa.gov>
2023-05-31 13:23:43 -0400

    Set https proxys to local and add outputs

--- terraform/shared/modules/https-proxy/outputs.tf
+++ terraform/shared/modules/https-proxy/outputs.tf
@@ -1,3 +1,23 @@
 output "https_proxy" {
   value = local.https_proxy
 }
+
+output "domain" {
+  value = local.domain
+}
+
+output "username" {
+  value = local.username
+}
+
+output "password" {
+  value = local.password
+}
+
+output "protocol" {
+  value = local.protocol
+}
+
+output "port" {
+  value = local.port
+}
