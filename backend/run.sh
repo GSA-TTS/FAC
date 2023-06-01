66689c280f844523ea578de0feb54a79a947262c
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2023-06-01 14:22:50 -0700

    Workaround for djlint and fixes for GitHub actions.

--- backend/run.sh
+++ backend/run.sh
@@ -14,7 +14,7 @@ mc alias set myminio "${AWS_S3_PRIVATE_ENDPOINT}" minioadmin minioadmin
 mc mb myminio/gsa-fac-private-s3
 mc admin user svcacct add --access-key="${AWS_PRIVATE_ACCESS_KEY_ID}" --secret-key="${AWS_PRIVATE_SECRET_ACCESS_KEY}" myminio minioadmin
 
-if [[ "${ENV}" == "LOCAL" ]]; then
+if [[ "${ENV}" == "LOCAL" || "${ENV}" == "TESTING" ]]; then
   aws --endpoint-url=http://minio:9000 s3 mb s3://gsa-fac-private-s3
 fi;
 
