5da5a20c18550ae52bd2c97681a16d669e9edd5a
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2023-06-01 13:53:34 -0700

    Switch to MinIO.

--- backend/run.sh
+++ backend/run.sh
@@ -3,14 +3,19 @@
 if [[ -n "${ENV}" ]]; then
   echo "Environment set as: ${ENV}"
 else
-  echo "No environment variable `ENV` is set!"
+  echo "No environment variable ${ENV} is set!"
 fi;
 
-export AWS_ACCESS_KEY_ID=test
-export AWS_SECRET_ACCESS_KEY=test
+export AWS_PRIVATE_ACCESS_KEY_ID=longtest
+export AWS_PRIVATE_SECRET_ACCESS_KEY=longtest
+export AWS_S3_PRIVATE_ENDPOINT="http://minio:9000"
+
+mc alias set myminio "${AWS_S3_PRIVATE_ENDPOINT}" minioadmin minioadmin
+mc mb myminio/gsa-fac-private-s3
+mc admin user svcacct add --access-key="${AWS_PRIVATE_ACCESS_KEY_ID}" --secret-key="${AWS_PRIVATE_SECRET_ACCESS_KEY}" myminio minioadmin
 
 if [[ "${ENV}" == "LOCAL" ]]; then
-  aws --endpoint-url=http://localstack:4566 s3 mb s3://gsa-fac-private-s3
+  aws --endpoint-url=http://minio:9000 s3 mb s3://gsa-fac-private-s3
 fi;
 
 # Migrate first
