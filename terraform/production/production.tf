977cb32bb540421fb163fb6598228485f917354e
Bret Mogilefsky <bmogilefsky@gmail.com>
2023-06-12 15:44:30 -0700

    Set up production app.fac.gov domain service

--- terraform/production/production.tf
+++ terraform/production/production.tf
@@ -5,3 +5,16 @@ module "production" {
   postgrest_image       = var.postgrest_image
   clamav_image          = var.clamav_image
 }
+
+module "domain" {
+  source = "github.com/18f/terraform-cloudgov//domain?ref=v0.6.0"
+
+  cf_org_name    = "gsa-tts-oros-fac"
+  cf_space_name  = "production"
+  app_name_or_id = "gsa-fac"
+  cdn_plan_name  = "domain"
+  domain_name    = "app.fac.gov"
+  depends_on = [
+    module.production
+  ]
+}
