f5636b01c90758165c2773030ef1128b446f01fb
Matt Jadud <matthew.jadud@gsa.gov>
2023-06-14 11:02:46 -0400

    Add support for VSCode debugging
    This does a few things:
    
    1. Fixes some settings regarding minio and clamav so that scanning and
       saving of files works in the local stack. (This may conflict with
    another branch/merge...)
    2. Adds `tblib` for traceback support in the container.
    3. Modifies run.sh so that we can run (optionally) with debugging
       support enabled; set VSCODE_DEBUG in .env (or your environment)
    before your compose to enable.
    
    https://testdriven.io/blog/django-debugging-vs-code/
    https://www.yellowduck.be/posts/debugging-django-vs-code
    https://code.visualstudio.com/docs/python/tutorial-django
    
    I'm sad to say I did the debugger config quickly; we'll want to document
    it for others. `launch.json` gets created by VSCode, and that lets you
    connect the debugger to the containerized stack on 5678. Note that when
    running with the debugger, Django does not start until you start the
    debugger... in other words, the stack comes up, but the web stack sits
    there waiting for you to start the debugger in VSCode before kicking off
    the Django stack.
    
    Pushes against this branch to improve the commit are welcome. This was
    fast work early in the morning to unblock development.

--- backend/run.sh
+++ backend/run.sh
@@ -8,17 +8,26 @@ fi;
 
 export AWS_PRIVATE_ACCESS_KEY_ID=longtest
 export AWS_PRIVATE_SECRET_ACCESS_KEY=longtest
-export AWS_S3_PRIVATE_ENDPOINT="http://minio:9000"
+export AWS_S3_PRIVATE_ENDPOINT="http://minio:9001"
 
 mc alias set myminio "${AWS_S3_PRIVATE_ENDPOINT}" minioadmin minioadmin
 mc mb myminio/gsa-fac-private-s3
 mc admin user svcacct add --access-key="${AWS_PRIVATE_ACCESS_KEY_ID}" --secret-key="${AWS_PRIVATE_SECRET_ACCESS_KEY}" myminio minioadmin
 
 if [[ "${ENV}" == "LOCAL" || "${ENV}" == "TESTING" ]]; then
-  aws --endpoint-url=http://minio:9000 s3 mb s3://gsa-fac-private-s3
+  aws --endpoint-url=${AWS_S3_PRIVATE_ENDPOINT} s3 mb s3://gsa-fac-private-s3
 fi;
 
 # Migrate first
 python manage.py migrate
+
 # Run the build/watch assets + run server at the same time
-npm run dev & python manage.py runserver 0.0.0.0:8000
+if [[ "${VSCODE_DEBUG}" == 1 ]]; then 
+  echo "RUNNING WITH VSCODE DEBUGGING"
+  # FIXME MCJ 20230614 - Add this to the container build.
+  pip install debugpy -t /tmp
+  npm run dev & python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8000
+else
+  echo "RUNNING WITHOUT DEBUGGER"
+  npm run dev & python manage.py runserver 0.0.0.0:8000
+fi;
