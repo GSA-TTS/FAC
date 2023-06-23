6d689dc55f02ad218484f1d1db64eb9dead9868a
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2023-06-22 17:59:25 -0700

    Add ability to handle email addresses to load_fixtures Django command.

--- backend/users/fixtures/user_fixtures.py
+++ backend/users/fixtures/user_fixtures.py
@@ -10,9 +10,12 @@ logger = logging.getLogger(__name__)
 
 # list of users that should be auto-created.
 # username here is the UUID for this person's Login.gov user
+
+test_username = getattr(settings, "TEST_USERNAME", None)
+
 USERS = [
     {
-        "username": settings.TEST_USERNAME,
+        "username": test_username,
     },
     {
         "username": "b276a5b3-2d2a-42a3-a078-ad57a36975d4",
@@ -27,6 +30,9 @@ def load_users():
     User = get_user_model()
     for item_info in USERS:
         username = item_info["username"]
+        if username is None:
+            logger.info("username is None, no TEST_USERNAME in settings")
+            continue
         if not User.objects.filter(username=username).exists():
             # need to make this user
             logger.info("Creating username %s", username)
