f2b7bc5f7ed6e3d65499854854b8cdfc25e27f4b
Matt Jadud <matthew.jadud@gsa.gov>
2023-06-29 06:59:53 -0400

    Fixing linting, also.

--- backend/data_distro/census_raw/repair-sql.py
+++ backend/data_distro/census_raw/repair-sql.py
@@ -2,9 +2,9 @@ import re, sys
 
 EMPTY_STRING = ""
 repls = [
-    (" COLLATE \"USING_NLS_COMP\"", EMPTY_STRING),
+    (' COLLATE "USING_NLS_COMP"', EMPTY_STRING),
     ("VARCHAR2\(.*?\)", "TEXT"),
-    (" DEFAULT COLLATION \"USING_NLS_COMP\"", EMPTY_STRING),
+    (' DEFAULT COLLATION "USING_NLS_COMP"', EMPTY_STRING),
     ("NUMBER\([0-9]+\,[ \t]+0\)", "NUMERIC"),
     ("CHAR\(.*?\)", "TEXT"),
     ("EDITIONABLE", EMPTY_STRING),
@@ -14,9 +14,8 @@ repls = [
     ("FACDISSEM_OWNER\.", EMPTY_STRING),
     ("([a-z]+) ([a-z]{1})", "\\1 AS \\2"),
     ("CREATE TABLE (.*?) \(", "DROP TABLE IF EXISTS \\1;\nCREATE TABLE \\1 ("),
-    ("CREATE TABLE", "CREATE TABLE"),    
+    ("CREATE TABLE", "CREATE TABLE"),
 ]
-
 
 
 if __name__ == "__main__":
@@ -26,7 +25,6 @@ if __name__ == "__main__":
     tables_file_to_be_cleaned = sys.argv[1]
     outp = open(sys.argv[2], "w")
 
-
     newlines = []
     for line in open(tables_file_to_be_cleaned, "r"):
         for pat_repl in repls:
@@ -35,6 +33,5 @@ if __name__ == "__main__":
 
     for line in newlines:
         outp.write(line.strip() + "\n")
-
 
     outp.close()
