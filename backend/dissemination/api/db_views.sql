0809ea74808543c229f414561371785c724cc80c
JeanMarie Mariadassou <JeanMarie.Mariadassou@gsa.gov>
2023-07-04 22:29:16 -0700

    Add api views for remaining tables

--- backend/dissemination/api/db_views.sql
+++ backend/dissemination/api/db_views.sql
@@ -23,21 +23,37 @@ create view api.vw_finding as
           award.award_seq_number, award.federal_agency_prefix, award.federal_award_extension, 
           finding.*
     from dissemination_Finding finding
-    left join dissemination_FederalAward award on award.award_seq_number = finding.award_seq_number
+    left join dissemination_FederalAward award 
+        on award.report_id = finding.report_id 
+        and award.award_seq_number = finding.award_seq_number
     left join dissemination_General gen on award.report_id = gen.report_id
     where gen.is_public=True
-      and gen.report_id = finding.report_id    
 ;
+
 drop view if exists api.vw_finding_text;
 create view api.vw_finding_text as
     select gen.uei, gen.audit_year, 
-          -- award.award_seq_number, award.federal_agency_prefix, award.federal_award_extension, 
           ft.*
     from dissemination_FindingText ft
     left join dissemination_General gen on ft.report_id = gen.report_id
-    -- TODO fix join columns
-    -- left join dissemination_Finding finding on award.award_seq_number = finding.award_seq_number
-    -- left join dissemination_FederalAward award on award.award_seq_number = finding.award_seq_number
+    where gen.is_public=True
+;
+
+drop view if exists api.vw_cap_text;
+create view api.vw_cap_text as
+    select gen.uei, gen.audit_year, 
+          ct.*
+    from dissemination_CAPText ct
+    left join dissemination_General gen on ct.report_id = gen.report_id
+    where gen.is_public=True
+;
+
+drop view if exists api.vw_note;
+create view api.vw_note as
+    select gen.uei, gen.audit_year, 
+          note.*
+    from dissemination_Note note
+    left join dissemination_General gen on note.report_id = gen.report_id
     where gen.is_public=True
 ;
 
