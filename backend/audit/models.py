474410c02db9510139fa9a7f0cc4da9f33d2c0e6
Tim Ballard <1425377+timoballard@users.noreply.github.com>
2023-07-06 13:00:54 -0500

    re-add is_submitted SAC property

--- backend/audit/models.py
+++ backend/audit/models.py
@@ -320,6 +320,10 @@ class SingleAuditChecklist(models.Model, GeneralInformationMixin):  # type: igno
             SingleAuditChecklist.STATUS.CERTIFIED,
             SingleAuditChecklist.STATUS.SUBMITTED,
         ]
+    
+    @property
+    def is_submitted(self):
+        return self.submission_status in [SingleAuditChecklist.STATUS.SUBMITTED]
 
     def get_transition_date(self, status):
         index = self.transition_name.index(status)
