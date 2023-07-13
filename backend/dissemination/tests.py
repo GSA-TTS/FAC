b7a9201dfcef6e322d5164e87037f3d985cc0e2c
Matt Jadud <matthew.jadud@gsa.gov>
2023-07-13 08:18:03 -0400

    Trying to get tests to pass live
    I have no idea why these migrations appeared.
    
    I really, really don't like this branch/code.

--- backend/dissemination/tests.py
+++ backend/dissemination/tests.py
@@ -21,43 +21,43 @@ class APIViewTests(TestCase):
 
     def test_views_returns_data(self):
         pass
-        # uei = "UCL2KRV93"
-        # finding_ref_number = "2023-001"
+        uei = "UCL2KRV93"
+        finding_ref_number = "2023-001"
 
-        # general = baker.make(General, auditee_uei=uei, is_public=True)
-        # general.save()
-        # award = baker.make(FederalAward, report_id=general.report_id)
-        # award.save()
-        # finding_text = baker.make(
-        #     FindingText,
-        #     report_id=general.report_id,
-        #     finding_ref_number=finding_ref_number,
-        # )
-        # finding_text.save()
-        # finding = baker.make(
-        #     Finding,
-        #     report_id=general.report_id,
-        #     finding_ref_number=finding_text.finding_ref_number,
-        #     award_seq_number=award.award_seq_number,
-        # )
-        # finding.save()
+        general = baker.make(General, auditee_uei=uei, is_public=True)
+        general.save()
+        award = baker.make(FederalAward, report_id=general.report_id)
+        award.save()
+        finding_text = baker.make(
+            FindingText,
+            report_id=general.report_id,
+            finding_ref_number=finding_ref_number,
+        )
+        finding_text.save()
+        finding = baker.make(
+            Finding,
+            report_id=general.report_id,
+            finding_ref_number=finding_text.finding_ref_number,
+            award_seq_number=award.award_seq_number,
+        )
+        finding.save()
 
-        # for api_version in api_schemas:
-        #     with connection.cursor() as cursor:
-        #         # TODO: Need a better way. Creating the views here as the data is in a temporary test databasse
-        #         for file in [
-        #             "init_api_db.sql",
-        #             "db_views.sql",
-        #         ]:
-        #             filename = f"dissemination/api/{api_version}/{file}"
-        #             sql = open(filename, "r").read()
-        #             cursor.execute(sql)
+        for api_version in api_schemas:
+            with connection.cursor() as cursor:
+                # TODO: Need a better way. Creating the views here as the data is in a temporary test databasse
+                for file in [
+                    "init_api_db.sql",
+                    "db_views.sql",
+                ]:
+                    filename = f"dissemination/api/{api_version}/{file}"
+                    sql = open(filename, "r").read()
+                    cursor.execute(sql)
 
-        #         cursor.execute(f"SELECT auditee_uei FROM {api_version}.general")
-        #         self.assertEquals(cursor.fetchall()[0][0], uei)
-        #         cursor.execute(f"SELECT auditee_uei FROM {api_version}.federal_award")
-        #         self.assertEquals(cursor.fetchall()[0][0], uei)
-        #         cursor.execute(f"SELECT auditee_uei FROM {api_version}.finding")
-        #         self.assertEquals(cursor.fetchall()[0][0], uei)
-        #         cursor.execute(f"SELECT auditee_uei FROM {api_version}.finding_text")
-        #         self.assertEquals(cursor.fetchall()[0][0], uei)
+                cursor.execute(f"SELECT auditee_uei FROM {api_version}.general")
+                self.assertEquals(cursor.fetchall()[0][0], uei)
+                cursor.execute(f"SELECT auditee_uei FROM {api_version}.federal_award")
+                self.assertEquals(cursor.fetchall()[0][0], uei)
+                cursor.execute(f"SELECT auditee_uei FROM {api_version}.finding")
+                self.assertEquals(cursor.fetchall()[0][0], uei)
+                cursor.execute(f"SELECT auditee_uei FROM {api_version}.finding_text")
+                self.assertEquals(cursor.fetchall()[0][0], uei)
