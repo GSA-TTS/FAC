b981e917a5379493e80eafa9207e11980968246e
Bret Mogilefsky <bmogilefsky@gmail.com>
2023-07-18 00:02:34 -0700

    Populate terraform/shared/config/backend.tfvars during bootstrap

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,19 +1,16 @@
 locals {
-  cf_api_url      = "https://api.fr.cloud.gov"
-  s3_service_name = "fac-terraform-state"
+  cf_api_url             = "https://api.fr.cloud.gov"
+  s3_service_name        = "fac-terraform-state"
+  credentials            = jsondecode(cloudfoundry_service_key.bucket_creds.credentials)
+  populate_creds_locally = true
 }
 
 module "s3" {
   source = "github.com/18f/terraform-cloudgov//s3"
 
-  cf_api_url      = local.cf_api_url
-  cf_user         = var.cf_user
-  cf_password     = var.cf_password
-  cf_org_name     = "gsa-tts-oros-fac"
-  cf_space_name   = "production"
-  s3_service_name = local.s3_service_name
-
-
+  cf_org_name   = "gsa-tts-oros-fac"
+  cf_space_name = "production"
+  name          = local.s3_service_name
 }
 
 resource "cloudfoundry_service_key" "bucket_creds" {
@@ -22,5 +19,21 @@ resource "cloudfoundry_service_key" "bucket_creds" {
 }
 
 output "bucket_credentials" {
-  value = cloudfoundry_service_key.bucket_creds.credentials
+  value     = cloudfoundry_service_key.bucket_creds.credentials
+  sensitive = true
+}
+
+# Populate Terraform state credentials for use during local development
+# Run `init.sh` in any environment directory to configure the backend for that directory
+resource "local_file" "backend.tfvars" {
+  count           = local.populate_creds_locally ? 1 : 0
+  filename        = "${local.path}/../shared/config/backend.tfvars"
+  file_permission = "0600"
+  content         = <<-EOF
+  access_key  = "${local.credentials.access_key_id}"
+  secret_key  = "${local.credentials.secret_access_key}"
+  bucket      = "${local.credentials.bucket}"
+  endpoint    = "${local.credentials.fips_endpoint}"
+  region      = "${local.credentials.region}"
+  EOF
 }
