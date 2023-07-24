b2aeae6f0654c10fa09bf1aca2f0e085f8e6be33
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2023-07-24 12:29:43 -0700

    Error messages for late change Excel uploads (#1589)
    * Add meaningful error message for Excel uploads that happen after a submission should no longer be edited.
    
    * Linter appeasement.
    
    * Linter appeasement.
    
    * display raw errors if type is defined but not handled
    
    ---------
    
    Co-authored-by: Tim Ballard <1425377+timoballard@users.noreply.github.com>

--- backend/static/js/upload-page.js
+++ backend/static/js/upload-page.js
@@ -133,6 +133,11 @@ function attachFileUploadHandler() {
                 info_box.innerHTML = get_error_table(data);
               } else if (data.type === 'error_field') {
                 info_box.innerHTML = `Field Error: ${data.errors}`;
+              } else if (data.type === 'no_late_changes') {
+                info_box.innerHTML =
+                  'Access denied. Further changes to audits that have been marked ready for certification are not permitted.';
+              } else if (data.type) {
+                info_box.innerHTML = `Error: ${data.errors}`;
               } else {
                 throw new Error('Returned error type is missing!');
               }
