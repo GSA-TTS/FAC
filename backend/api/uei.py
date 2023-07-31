a7660b0142e5e8d30dc385ad3e0e1f2289207cc3
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2023-07-31 09:22:19 -0500

    Accept SAM.gov in OpenSSL v3

--- backend/api/uei.py
+++ backend/api/uei.py
@@ -1,7 +1,21 @@
 from typing import Optional
 import requests
+import ssl
+import urllib3
 
 from config.settings import SAM_API_URL, SAM_API_KEY
+
+class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+    '''Transport adapter that allows us to use custom ssl_context.'''
+
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
 
 
 def call_sam_api(
@@ -9,10 +23,18 @@ def call_sam_api(
 ) -> tuple[Optional[requests.Response], Optional[str]]:
     """
     Call the SAM.gov API and return the response and/or error string
+
+	SAM.gov uses a legacy version of SSL that OpenSSL 3 rejects. We specify
+    just in this one spot the magic flag 0x04 corresponding to
+	ssl.OP_LEGACY_SERVER_CONNECT for the SSL context.
     """
     try:
+        ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+        session = requests.session()
+        session.mount('https://', CustomHttpAdapter(ctx))
         return (
-            requests.get(sam_api_url, params=params, headers=headers, timeout=15),
+            session.get(sam_api_url, params=params, headers=headers, timeout=15),
             None,
         )
 
