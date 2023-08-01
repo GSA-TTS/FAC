bb884a4a1a7a6898f5349e409ee588421a41ac7f
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2023-08-01 15:38:47 -0700

    No jq in GitHub Action context.

--- bin/ops/deploy.sh
+++ bin/ops/deploy.sh
@@ -8,11 +8,12 @@ set -e
 
 cfspace=$(cf target |tail -1|cut -d':' -f2|xargs)
 appguid=$(cf app gsa-fac --guid)
-before_sha=$(cf curl /v3/apps/"${appguid}"/relationships/current_droplet | jq -r .data.guid)
+before_sha=$(cf curl "/v3/apps/${appguid}/relationships/current_droplet"|head -n 3|tail -n 1|cut -d':' -f2|xargs)
 echo "${before_sha}"
-cf push -f backend/manifests/manifest-fac.yml --vars-file backend/manifests/vars/vars-"${cfspace}".yml --strategy rolling
-after_sha=$(cf curl /v3/apps/"${appguid}"/relationships/current_droplet | jq -r .data.guid)
+echo cf push -f backend/manifests/manifest-fac.yml --vars-file backend/manifests/vars/vars-"${cfspace}".yml --strategy rolling
+after_sha=$(cf curl "/v3/apps/${appguid}/relationships/current_droplet"|head -n 3|tail -n 1|cut -d':' -f2|xargs)
 echo "${after_sha}"
 if [[ "${before_sha}" == "${after_sha}" ]]; then
+    echo "Failed due to unchanged droplet SHA."
     exit 1
 fi
