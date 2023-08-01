0046ff28ccaf2442dfde387889ca3be73b3ea061
Matthew Jadud <matthew.jadud@gsa.gov>
2023-08-01 13:48:27 -0400

    Update terraform/shared/modules/env/postgrest.tf
    Co-authored-by: Bret Mogilefsky <bret.mogilefsky@gsa.gov>

--- terraform/shared/modules/env/postgrest.tf
+++ terraform/shared/modules/env/postgrest.tf
@@ -34,7 +34,7 @@ resource "cloudfoundry_app" "postgrest" {
     PGRST_DB_URI : cloudfoundry_service_key.postgrest.credentials.uri
     PGRST_DB_SCHEMAS : "api_v1_0_0_beta"
     PGRST_DB_ANON_ROLE : "anon"
-    PGRST_JWT_SECRET: var.pgrst_jwt_secret
+    PGRST_JWT_SECRET : var.pgrst_jwt_secret
   }
 }
 
