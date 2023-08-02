c6b98318300548e485ba3227ec3462b0d3a920e8
Matt Jadud <matthew.jadud@gsa.gov>
2023-08-01 22:31:36 -0400

    Runs through five workbooks.
    This is finding possible errors in validations and/or workbook
    generation.
    
    That can be another ticket/branch. However, for now, what is good is
    that this is letting me run workbooks through Django, see them end up in
    minio, and they go throguh the validation process.
    
    Next will be to see what is required to trigger dissemination.

--- backend/dissemination/management/commands/test_dissemination_pipeline.py
+++ backend/dissemination/management/commands/test_dissemination_pipeline.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from dissemination import api_versions
+
 from model_bakery import baker
 import json
 import os
@@ -7,7 +8,6 @@ import sys
 from django.test import Client
 
 from audit.test_views import (
-    MockHttpResponse,
     _mock_login_and_scan
 )
 
@@ -30,8 +30,9 @@ def test_workbook(filename, section, client):
     xlsx_file = open(filename, 'rb')
     mock_scanfile = _scan_file(xlsx_file)    
     sac = _mock_login_and_scan(client, mock_scanfile)
-
+    # Rewind the file, because _scan_file unwound it.
     xlsx_file.seek(0)
+
     response = client.post(
         reverse(
             f"audit:{section}",
@@ -46,6 +47,18 @@ def test_workbook(filename, section, client):
     tc.assertEqual(response.status_code, 302)
     return True
 
+mapping = {
+    FORM_SECTIONS.ADDITIONAL_UEIS : 'additional-ueis-{0}.xlsx',
+    FORM_SECTIONS.CORRECTIVE_ACTION_PLAN : 'captext-{0}.xlsx',
+    # MCJ FIXME the validation or mapping for secondary auditors is broken.
+    # FORM_SECTIONS.SECONDARY_AUDITORS : 'cpas-{0}.xlsx',
+    FORM_SECTIONS.FEDERAL_AWARDS_EXPENDED : 'federal-awards-{0}.xlsx',
+    FORM_SECTIONS.FINDINGS_UNIFORM_GUIDANCE : 'findings-{0}.xlsx',
+    FORM_SECTIONS.FINDINGS_TEXT : 'findings-text-{0}.xlsx',
+    # The form needs to have single cells formatted LtoR, and 
+    # it appears that there is data not coming through to the XLSX file.
+    # FORM_SECTIONS.NOTES_TO_SEFA : 'notes-{0}.xlsx'
+}
 class Command(BaseCommand):
     help = """
     Creates workbooks for testing from Census data, and then runs it through the app.
@@ -56,7 +69,7 @@ class Command(BaseCommand):
         for dbkey in os.listdir(basepath):
             print(f'Handling {dbkey}')
             client = Client()
-
-            test_workbook(os.path.join(basepath, dbkey, f'federal-awards-{dbkey}.xlsx'), 
-                          FORM_SECTIONS.FEDERAL_AWARDS_EXPENDED, 
-                          client)
+            for section, file in mapping.items():
+                test_workbook(os.path.join(basepath, dbkey, file.format(dbkey)), 
+                            section, 
+                            client)
