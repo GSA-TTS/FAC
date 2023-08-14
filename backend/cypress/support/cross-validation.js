4af28ee4998cdc75beb02bae9d331c94558be920
Phil Dominguez <philip.dominguez@gsa.gov>
2023-08-14 12:50:00 -0400

    Whitespace

--- backend/cypress/support/cross-validation.js
+++ backend/cypress/support/cross-validation.js
@@ -1,8 +1,8 @@
 export function testCrossValidation() {
-    cy.url().should('match', /\/audit\/cross-validation\/[0-9A-Z]{17}/);
-    cy.get(".usa-button").contains("Begin Validation").click();
-    cy.get('.usa-fieldset').contains('Looks good!');
+  cy.url().should('match', /\/audit\/cross-validation\/[0-9A-Z]{17}/);
+  cy.get(".usa-button").contains("Begin Validation").click();
+  cy.get('.usa-fieldset').contains('Looks good!');
 
-    const reportTestId = '2023AUG0001000010';
-    cy.visit(`/audit/submission-progress/${reportTestId}`);
-  }
+  const reportTestId = '2023AUG0001000010';
+  cy.visit(`/audit/submission-progress/${reportTestId}`);
+}
