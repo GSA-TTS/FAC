4f1f351ed64450e36333d38ff2ed28c9dd3cea70
Phil Dominguez <philip.dominguez@gsa.gov>
2023-08-14 10:34:44 -0400

    Adding cross validation test

---
+++ backend/cypress/support/cross-validation.js
@@ -0,0 +1,8 @@
+export function testCrossValidation() {
+    cy.url().should('match', /\/audit\/cross-validation\/[0-9A-Z]{17}/);
+    cy.get(".usa-button").contains("Begin Validation").click();
+    cy.get('.usa-fieldset').contains('Looks good!');
+
+    const reportTestId = '2023AUG0001000010';
+    cy.visit(`/audit/submission-progress/${reportTestId}`);
+  }
