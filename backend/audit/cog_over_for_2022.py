737acde583fe12cac580f9efb6a2da5afa830ee2
gsa-suk <sudha.kumar@gsa.gov>
2023-08-22 09:16:49 -0700

    Calculate cog / over for 2022

--- backend/audit/cog_over_for_2022.py
+++ backend/audit/cog_over_for_2022.py
@@ -12,17 +12,10 @@ REF_YEAR = "2022"
 def cog_over_for_2022():
     print("Starting cog_over_for_2022.")
     print("DATABASE_URL = ", os.getenv("DATABASE_URL"))
+
     engine = sqlalchemy.create_engine(
         os.getenv("DATABASE_URL").replace("postgres", "postgresql", 1)
     )
-        # SELECT gen."DBKEY", gen."EIN", cast(gen."TOTFEDEXPEND" as BIGINT),
-        #         gen."COG_OVER", gen."COGAGENCY", gen."OVERSIGHTAGENCY",
-        #         cfda."CFDA", cast(cfda."AMOUNT" as BIGINT), cfda."DIRECT"
-        # FROM census_gen22 gen, census_cfda22 cfda
-        # WHERE gen."AUDITYEAR" = :ref_year
-        # AND cast(gen."TOTFEDEXPEND" as BIGINT) >= :threshold
-        # AND gen."DBKEY" = cfda."DBKEY"
-        # ORDER BY gen."DBKEY"
     AUDIT_QUERY = """
         SELECT gen."DBKEY", gen."EIN", cast(gen."TOTFEDEXPEND" as BIGINT),
                 gen."COG_OVER", gen."COGAGENCY", gen."OVERSIGHTAGENCY",
@@ -31,83 +24,127 @@ def cog_over_for_2022():
         LEFT JOIN census_cfda22 cfda
         ON gen."DBKEY" = cfda."DBKEY"
         WHERE cast(gen."TOTFEDEXPEND" as BIGINT) >= :threshold
+        AND gen."AUDITYEAR" = :ref_year
         ORDER BY gen."DBKEY"
     """
-    # print(AUDIT_QUERY)
     with engine.connect() as conn:
         result = conn.execute(
             sqlalchemy.text(AUDIT_QUERY), {"ref_year": REF_YEAR, "threshold": COG_LIMIT}
         )
         print("Obtained result from postgres")
         print("result.rowcount = ", result.rowcount)
+
         gens = []
         cfdas = []
-
         for row in result:
-            (DBKEY, EIN, TOTFEDEXPEND, COG_OVER, COGAGENCY, OVERSIGHTAGENCY, CFDA, AMOUNT, DIRECT) = row
-            if (DBKEY, EIN, TOTFEDEXPEND, COG_OVER, COGAGENCY, OVERSIGHTAGENCY) not in gens:
-                gens.append((DBKEY, EIN, TOTFEDEXPEND, COG_OVER, COGAGENCY, OVERSIGHTAGENCY))
+            (
+                DBKEY,
+                EIN,
+                TOTFEDEXPEND,
+                COG_OVER,
+                COGAGENCY,
+                OVERSIGHTAGENCY,
+                CFDA,
+                AMOUNT,
+                DIRECT,
+            ) = row
+            if (
+                DBKEY,
+                EIN,
+                TOTFEDEXPEND,
+                COG_OVER,
+                COGAGENCY,
+                OVERSIGHTAGENCY,
+            ) not in gens:
+                gens.append(
+                    (DBKEY, EIN, TOTFEDEXPEND, COG_OVER, COGAGENCY, OVERSIGHTAGENCY)
+                )
             if (DBKEY, CFDA, AMOUNT, DIRECT) not in cfdas:
                 cfdas.append((DBKEY, CFDA, AMOUNT, DIRECT))
 
     print("len(gens = ", len(gens))
     print("len(cfdas) = ", len(cfdas))
+
     # Generate cog / over assignments for 2022
-    df_calc = pd.DataFrame(columns=['DBKEY', 'EIN', 'COGOVER', 'COGAGENCY', 'OVERAGENCY'])
-    df_2022 = pd.DataFrame(columns=['DBKEY', 'EIN', 'COGOVER', 'COGAGENCY', 'OVERAGENCY'])
-    # num_rows = 0
+    df_calc = pd.DataFrame(
+        columns=["DBKEY", "EIN", "COGOVER", "COGAGENCY", "OVERAGENCY"]
+    )
+    df_2022 = pd.DataFrame(
+        columns=["DBKEY", "EIN", "COGOVER", "COGAGENCY", "OVERAGENCY"]
+    )
+    df_diff = pd.DataFrame(
+        columns=[
+            "DBKEY",
+            "EIN",
+            "COGOVER_CALC",
+            "COGAGENCY_CALC",
+            "OVERAGENCY_CALC",
+            "COGOVER_2022",
+            "COGAGENCY_2022",
+            "OVERAGENCY_2022",
+        ]
+    )
+
+    is_match = True
     for gen in gens:
         df_2022_row = {
-            'DBKEY': gen[0],
-            'EIN': gen[1],
-            'COGOVER': gen[3],
-            'COGAGENCY': gen[4],
-            'OVERAGENCY': gen[5]
+            "DBKEY": gen[0],
+            "EIN": gen[1],
+            "COGOVER": gen[3],
+            "COGAGENCY": gen[4],
+            "OVERAGENCY": gen[5],
         }
         df_2022.loc[len(df_2022)] = df_2022_row
+
         dbkey = gen[0]
         ein = gen[1]
         total_amount_expended = gen[2]
         (total_da_amount_expended, max_total_agency, max_da_agency) = calc_cfda_amounts(
             cfdas=[cfda for cfda in cfdas if cfda[0] == dbkey]
         )
+
         agency = determine_agency(
             total_amount_expended,
             total_da_amount_expended,
             max_total_agency,
             max_da_agency,
         )
+
         oversight_agency = cognizant_agency = None
         if total_amount_expended <= COG_LIMIT:
             oversight_agency = agency
-            cogover = 'O'
+            cogover = "O"
         else:
-            cogover = 'C'
+            cogover = "C"
             cognizant_agency = determine_2019_agency(ein)
             if not cognizant_agency:
                 cognizant_agency = agency
+
         df_calc_row = {
-            'DBKEY': dbkey,
-            'EIN': ein,
-            'COGOVER': cogover,
-            'COGAGENCY': cognizant_agency,
-            'OVERAGENCY': oversight_agency
+            "DBKEY": dbkey,
+            "EIN": ein,
+            "COGOVER": cogover,
+            "COGAGENCY": cognizant_agency,
+            "OVERAGENCY": oversight_agency,
         }
         df_calc.loc[len(df_calc)] = df_calc_row
-        # print(f"dbkey = {dbkey}, ein = {ein}, cogover = {cogover}, cogagency = {cognizant_agency}, overagency = {oversight_agency}")
-        # print(f"From 2022 table: {gen[0]}, ein = {gen[1]}, cogover = {gen[3]}, cogagency = {gen[4]}, overagency = {gen[5]}")
-        # num_rows += 1
-        # if num_rows == 10:
-        #     break;
+
+        if not (df_calc_row == df_2022_row):
+            is_match = False
+            df_diff.loc[len(df_diff)] = {
+                "DBKEY": dbkey,
+                "EIN": ein,
+                "COGOVER_CALC": cogover,
+                "COGAGENCY_CALC": cognizant_agency,
+                "OVERAGENCY_CALC": oversight_agency,
+                "COGOVER_2022": gen[3],
+                "COGAGENCY_2022": gen[4],
+                "OVERAGENCY_2022": gen[5],
+            }
 
     filedir = os.getcwd()
-    df_calc.to_csv(filedir + "/calc_2022.csv")
-    df_2022.to_csv(filedir + "/actual_2022.csv")
+    df_calc.to_csv(os.path.join(filedir, "calc_2022.csv"))
+    df_2022.to_csv(os.path.join(filedir, "actual_2022.csv"))
+    df_diff.to_csv(os.path.join(filedir, "calc_vs_actual_diff.csv"))
 
-
-    # Compare generated cog / over assignment with 2022 public table data
-    # --> compare df_calc and df_2022
-    print("df_calc shape = ", df_calc.shape)
-    print("df_2022 shape = ", df_2022.shape)
-    is_match = df_calc.equals(df_2022)
     return (df_calc.shape[0], is_match)
