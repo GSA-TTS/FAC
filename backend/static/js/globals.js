90280849ff7e4c4a3517a87d9a857edc8fcd689a
James Person <james.person@gsa.gov>
2023-08-22 13:13:09 -0400

    Notes to SEFA Upload Page (#1891)
    * Workbook help links, notes to sefa, checklist changes
    
    * Linting!
    
    * Cypress - test for notes to SEFA upload
    
    * Tests: baker makes notes_to_sefa, checklist phrase
    
    * Linting, JS duplicate key

--- backend/static/js/globals.js
+++ backend/static/js/globals.js
@@ -2,10 +2,11 @@ export const UPLOAD_TIMEOUT = 30000; // 30s
 
 export const UPLOAD_URLS = {
   'federal-awards': 'federal-awards-expended',
+  'notes-to-sefa': 'notes-to-sefa',
   'audit-findings': 'findings-uniform-guidance',
   'audit-findings-text': 'findings-text',
+  'secondary-auditors': 'secondary-auditors',
   'additional-ueis': 'additional-ueis',
   'additional-eins': 'additional-eins',
-  'secondary-auditors': 'secondary-auditors',
   CAP: 'corrective-action-plan',
 };
