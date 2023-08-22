7cbb2538aeb77378522d84451c80a4614090cb85
JeanMarie Mariadassou <jmpm@JMPMs-MBP.local>
2023-08-22 11:31:26 +0200

    Fix linting

--- backend/report_submission/views.py
+++ backend/report_submission/views.py
@@ -175,7 +175,6 @@ class GeneralInformationFormView(LoginRequiredMixin, View):
 
             if form.is_valid():
                 general_information = sac.general_information
-                fields = sorted(general_information.keys())
                 general_information.update(form.cleaned_data)
                 validated = validate_general_information_json(general_information)
                 sac.general_information = validated
