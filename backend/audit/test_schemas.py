66e7ee869bbfeaa44a6a9c2126ea83586662beb6
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-08-23 13:28:06 -0400

    #1814 Updated test case

--- backend/audit/test_schemas.py
+++ backend/audit/test_schemas.py
@@ -448,16 +448,16 @@ class AuditInformationSchemaValidityTest(SimpleTestCase):
 
     def test_bad_sp_framework_opinions(self):
         schema = self.AUDIT_INFO_SCHEMA
-        simple_case = jsoncopy(self.SIMPLE_CASE[0])
+        simple_case = jsoncopy(self.SIMPLE_CASE[1])
 
-        simple_case["sp_framework_opinion"] = "not_a_real_opinion"
+        simple_case["sp_framework_opinions"] = "not_a_real_opinion"
         self.assertRaises(exceptions.ValidationError, validate, simple_case, schema)
 
     def test_bad_sp_framework_basis(self):
         schema = self.AUDIT_INFO_SCHEMA
-        simple_case = jsoncopy(self.SIMPLE_CASE[0])
+        simple_case = jsoncopy(self.SIMPLE_CASE[1])
 
-        simple_case["gaap_results"] = "an_invalid_basis"
+        simple_case["sp_framework_basis"] = "an_invalid_basis"
         self.assertRaises(exceptions.ValidationError, validate, simple_case, schema)
 
     def test_valid_aln_prefixes(self):
