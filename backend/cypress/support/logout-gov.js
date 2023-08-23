771b93f4d69fec05d1feb7bc182c1cc76e0ad9e4
Phil Dominguez <philip.dominguez@gsa.gov>
2023-08-23 14:46:24 -0400

    Cleanup

--- backend/cypress/support/logout-gov.js
+++ backend/cypress/support/logout-gov.js
@@ -1,8 +1,6 @@
-
 export function testLogoutGov() {
   cy.get('button').contains('Menu').click();
   cy.get('button').contains('Sign out').click();
-  // cy.url().should('match', /\/opendid\/logout\/$/);
   cy.origin(
     'https://idp.int.identitysandbox.gov/',
     {},
