caf919fc6ed47ca83421032bae87a5a5cf410a52
Tim Ballard <1425377+timoballard@users.noreply.github.com>
2023-08-23 12:45:36 -0500

    Add django debug toolbar (#1878)
    * add django debug toolbar
    
    * lint
    
    * disable debug toolbar during test runs
    
    * disable debug toolbar in non-LOCAL envs

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -82,3 +82,8 @@ urlpatterns = [
     # Keep last so we can use short urls for content pages like home page etc.
     path("", include("cms.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.ENABLE_DEBUG_TOOLBAR:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
