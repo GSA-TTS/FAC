18532e126692a64d99529e58a35c675a1811f21a
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-08-30 09:52:46 -0400

    Setup data load container
    The purpose of this is to simply utilize jadud's container, targeting
    an env, and load the historical data.

--- backend/run.sh
+++ backend/run.sh
@@ -20,19 +20,35 @@ fi;
 # Migrate first
 python manage.py migrate
 
-python manage.py drop_deprecated_api_schema_and_views && 
+python manage.py drop_deprecated_api_schema_and_views &&
 python manage.py drop_api_views &&
-python manage.py drop_api_schema && 
-python manage.py create_api_schema && 
+python manage.py drop_api_schema &&
+python manage.py create_api_schema &&
 python manage.py create_api_views
 
 if [[ ("${ENV}" == "LOCAL" || "${ENV}" == "STAGING") && "$LOAD_TEST_DATA" == 1 ]];
 then
-  # 175887 162392 
-  for dbkey in 91651 147134 175887; 
+  # 175887 162392
+  for dbkey in 91651 147134 175887;
   do
     python manage.py workbooks_e2e --email workbook.generator@test.fac.gov --dbkey $dbkey &
   done
+fi
+
+username=workbook.generator
+is_user_present=$(psql $DATABASE_URL -AXqtc "select exists(select 1 from public.auth_user where username='${username}')")
+
+echo "${username} exists check: $is_user_present"
+
+if [ $is_user_present == "f" ];
+then
+    pass=`cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 32; echo;`
+    psql $DATABASE_URL -q <<-EOSQL
+    INSERT INTO public.auth_user
+    ("password", last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+    VALUES('${pass}', '2023-08-12 00:00:00.000', false, '${username}', 'Workbook', 'Generator', 'workbook.generator@test.fac.gov', false, false, '2023-08-12 00:00:00.000');
+EOSQL
+    echo "Created user ${username}"
 fi
 
 # Run the build/watch assets + run server at the same time
