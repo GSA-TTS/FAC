3f0b5ee837c8bfa2ee453e977a830a1abe8f2a5f
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-09-01 14:25:32 -0400

    Add comment to script

--- backend/load_data.sh
+++ backend/load_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# The following lines will enable extended glob, and allow us to remove /app, except load_data.sh in the event we want to reduce the task disk quota
 #shopt -s extglob
-#rm -rf /home/vcap/app/fac-historic-public-csvs/!(load_data.sh)
-#ls | grep -xv "create-dumps.sh" | parallel rm /home/vcap/app/fac-historic-public-csvs/
+#rm -rf /home/vcap/app/!(load_data.sh)
 git config --global http.proxy "$https_proxy"
 git clone https://github.com/GSA-TTS/fac-historic-public-csvs.git
 ./fac-historic-public-csvs/create-dumps.sh
