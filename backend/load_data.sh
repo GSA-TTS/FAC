ef61f9704c3a606e798bd4c571990b84b0a76210
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-09-01 14:24:32 -0400

    Move data script into backend/

--- backend/load_data.sh
+++ backend/load_data.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#shopt -s extglob
+#rm -rf /home/vcap/app/fac-historic-public-csvs/!(load_data.sh)
+#ls | grep -xv "create-dumps.sh" | parallel rm /home/vcap/app/fac-historic-public-csvs/
 git config --global http.proxy "$https_proxy"
 git clone https://github.com/GSA-TTS/fac-historic-public-csvs.git
 ./fac-historic-public-csvs/create-dumps.sh
