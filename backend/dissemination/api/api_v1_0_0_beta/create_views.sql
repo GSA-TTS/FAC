a57bd2333831bb69d55fedb40b7d380fd6edf5f0
Matt Jadud <matthew.jadud@gsa.gov>
2023-09-06 08:17:28 -0400

    Fixes
    1. Fixes the API views. The names were wrong in places.
    2. Fixes the order of function and view creation. The views rely on the
       functions existing.
    3. Adds a more verbose run sequence to `run.sh`, which is lifted from
       `.profile`. Really, we should break that out someday.

--- backend/dissemination/api/api_v1_0_0_beta/create_views.sql
+++ backend/dissemination/api/api_v1_0_0_beta/create_views.sql
@@ -2,6 +2,191 @@
 begin;
 
 ---------------------------------------
+-- finding_text
+---------------------------------------
+create view api_v1_0_0_beta.findings_text as
+    select 
+        gen.report_id,
+        gen.auditee_uei,
+        gen.audit_year,
+        ft.finding_ref_number,
+        ft.contains_chart_or_table,
+        ft.finding_text
+    from
+        dissemination_findingtext ft,
+        dissemination_general gen
+    where
+        (ft.report_id = gen.report_id
+         and
+         gen.is_public = true)
+        or (gen.is_public = false and has_tribal_data_access())
+    order by ft.id
+;
+
+---------------------------------------
+-- additional_ueis
+---------------------------------------
+create view api_v1_0_0_beta.additional_ueis as
+    select
+        gen.report_id,
+        gen.auditee_uei,
+        gen.audit_year,
+        ---
+        uei.additional_uei
+    from
+        dissemination_general gen,
+        dissemination_additionaluei uei
+    where
+        (gen.report_id = uei.report_id
+         and
+         gen.is_public = true)
+        or (gen.is_public = false and has_tribal_data_access())
+    order by uei.id
+;
+
+---------------------------------------
+-- finding
+---------------------------------------
+create view api_v1_0_0_beta.findings as
+    select 
+        gen.report_id,
+        gen.auditee_uei,
+        gen.audit_year,
+        finding.award_reference,
+        finding.reference_number,
+        finding.is_material_weakness,
+        finding.is_modified_opinion,
+        finding.is_other_findings,
+        finding.is_other_matters,
+        finding.prior_finding_ref_numbers,
+        finding.is_questioned_costs,
+        finding.is_repeat_finding,
+        finding.is_significant_deficiency,
+        finding.type_requirement
+    from
+        dissemination_finding finding,
+        dissemination_general gen
+    where
+        (finding.report_id = gen.report_id
+         and
+         gen.is_public = true)
+        or (gen.is_public = false and has_tribal_data_access())
+    order by finding.id
+;
+
+---------------------------------------
+-- federal award
+---------------------------------------
+create view api_v1_0_0_beta.federal_awards as
+    select 
+        gen.report_id,
+        gen.auditee_uei,
+        gen.audit_year,
+        ---
+        award.award_reference,
+        award.federal_agency_prefix,
+        award.federal_award_extension,
+        award.additional_award_identification,
+        award.federal_program_name,
+        award.amount_expended,
+        award.cluster_name,
+        award.other_cluster_name,
+        award.state_cluster_name,
+        award.cluster_total,
+        award.federal_program_total,
+        award.is_major,
+        award.is_loan,
+        award.loan_balance,
+        award.is_direct,
+        award.audit_report_type,
+        award.findings_count,
+        award.is_passthrough_award,
+        award.passthrough_amount
+    from
+        dissemination_federalaward award,
+        dissemination_general gen
+    where
+        (award.report_id = gen.report_id
+         and
+         gen.is_public = true)
+        or (gen.is_public = false and has_tribal_data_access())
+    order by award.id
+;
+
+
+---------------------------------------
+-- corrective_action_plan
+---------------------------------------
+create view api_v1_0_0_beta.corrective_action_plans as
+    select 
+        gen.report_id,
+        gen.auditee_uei,
+        gen.audit_year,
+        ---
+        ct.finding_ref_number,
+        ct.contains_chart_or_table,
+        ct.planned_action
+    from
+        dissemination_CAPText ct,
+        dissemination_General gen
+    where
+        (ct.report_id = gen.report_id
+         and
+         gen.is_public = true)
+        or (gen.is_public = false and has_tribal_data_access())
+    order by ct.id
+;
+
+---------------------------------------
+-- notes_to_sefa
+---------------------------------------
+create view api_v1_0_0_beta.notes_to_sefa as
+    select 
+        gen.report_id,
+        gen.auditee_uei,
+        gen.audit_year,
+        ---
+        note.note_title as title,
+        note.accounting_policies,
+        note.is_minimis_rate_used,
+        note.rate_explained,
+        note.content
+    from
+        dissemination_general gen,
+        dissemination_note note
+    where
+        (note.report_id = gen.report_id
+         and
+         gen.is_public = true)
+        or (gen.is_public = false and has_tribal_data_access())
+    order by note.id
+;
+
+---------------------------------------
+-- passthrough
+---------------------------------------
+create view api_v1_0_0_beta.passthrough as
+    select
+        gen.report_id,
+        gen.auditee_uei,
+        gen.audit_year,
+        ---
+        pass.award_reference,
+        pass.passthrough_id,
+        pass.passthrough_name
+    from
+        dissemination_general as gen,
+        dissemination_passthrough as pass
+    where
+        (gen.report_id = pass.report_id
+        and
+        gen.is_public = true)
+        or (gen.is_public = false and has_tribal_data_access())
+    order by pass.id
+;
+
+
+---------------------------------------
 -- general
 ---------------------------------------
 create view api_v1_0_0_beta.general as
@@ -36,34 +221,32 @@ create view api_v1_0_0_beta.general as
         gen.auditor_contact_name,
         gen.auditor_email,
         gen.auditor_firm_name,
-        gen.auditor_foreign_addr,
+        -- gen.auditor_foreign_addr,
         gen.auditor_ein,
         -- agency
         gen.cognizant_agency,
         gen.oversight_agency,
         -- dates
-        gen.initial_date_received,
+        gen.date_created,
         gen.ready_for_certification_date,
         gen.auditor_certified_date,
         gen.auditee_certified_date,
         gen.certified_date,
         gen.submitted_date,
-        gen.auditor_signature_date,
-        gen.auditee_signature_date,
         gen.fy_end_date,
         gen.fy_start_date,
         gen.audit_type,
         gen.gaap_results,
-        gen.sp_framework,
+        gen.sp_framework_basis,
         gen.is_sp_framework_required,
-        gen.sp_framework_auditor_opinion,
-        gen.is_going_concern,
-        gen.is_significant_deficiency,
-        gen.is_material_weakness,
-        gen.is_material_noncompliance,
-        gen.is_duplicate_reports,
+        gen.sp_framework_opinions,
+        gen.is_going_concern_included,
+        gen.is_internal_control_deficiency_disclosed,
+        gen.is_internal_control_material_weakness_disclosed,
+        gen.is_material_noncompliance_disclosed,
+        -- gen.is_duplicate_reports,
         gen.dollar_threshold,
-        gen.is_low_risk,
+        gen.is_low_risk_auditee,
         gen.agencies_with_prior_findings,
         gen.entity_type,
         gen.number_months,
@@ -76,9 +259,10 @@ create view api_v1_0_0_beta.general as
         dissemination_General gen,
         dissemination_FederalAward award
     where 
-        gen.report_id = award.report_id
-        and
-        gen.is_public=true 
+        (gen.report_id = award.report_id
+         and
+         gen.is_public = true)
+         or (gen.is_public = false and has_tribal_data_access()) 
     order by gen.id
     
     -- MCJ When it comes time to enable tribal access, this is what it looks like.
@@ -109,185 +293,33 @@ create view api_v1_0_0_beta.secondary_auditors as
         dissemination_General gen,
         dissemination_SecondaryAuditor sa
     where 
-        sa.report_id = gen.report_id
-        and
-        gen.is_public=True
+        (sa.report_id = gen.report_id
+         and
+         gen.is_public=True)
+        or (gen.is_public=false and has_tribal_data_access())
     order by sa.id
 ;
 
----------------------------------------
--- federal award
----------------------------------------
-create view api_v1_0_0_beta.federal_awards as
-    select 
-        gen.report_id,
-        gen.auditee_uei,
-        gen.audit_year,
-        ---
-        award.award_reference,
-        award.federal_agency_prefix,
-        award.federal_award_extension,
-        award.additional_award_identification,
-        award.federal_program_name,
-        award.amount_expended,
-        award.cluster_name,
-        award.other_cluster_name,
-        award.state_cluster_name,
-        award.cluster_total,
-        award.federal_program_total,
-        award.is_major,
-        award.is_loan,
-        award.loan_balance,
-        award.is_direct,
-        award.mp_audit_report_type,
-        award.findings_count,
-        award.is_passthrough_award,
-        award.passthrough_name,
-        award.passthrough_id,
-        award.passthrough_amount
-    from
-        dissemination_FederalAward award,
-        dissemination_General gen
-    where
-        award.report_id = gen.report_id
-        and
-        gen.is_public=True
-    order by award.id
-;
-
----------------------------------------
--- finding
----------------------------------------
-create view api_v1_0_0_beta.findings as
-    select 
-        gen.report_id,
-        gen.auditee_uei,
-        gen.audit_year,
-        finding.award_reference,
-        finding.finding_ref_number,
-        finding.is_material_weakness,
-        finding.is_modified_opinion,
-        finding.is_other_findings,
-        finding.is_other_non_compliance,
-        finding.prior_finding_ref_numbers,
-        finding.is_questioned_costs,
-        finding.is_repeat_finding,
-        finding.is_significant_deficiency,
-        finding.type_requirement
-    from
-        dissemination_Finding finding,
-        dissemination_General gen
-    where
-        finding.report_id = gen.report_id
-        and
-        gen.is_public = True
-    order by finding.id
-;
-
----------------------------------------
--- finding_text
----------------------------------------
-create view api_v1_0_0_beta.findings_text as
-    select 
-        gen.report_id,
-        gen.auditee_uei,
-        gen.audit_year,
-        ft.finding_ref_number,
-        ft.contains_chart_or_table,
-        ft.finding_text
-    from
-        dissemination_FindingText ft,
-        dissemination_General gen
-    where
-        ft.report_id = gen.report_id
-        and
-        gen.is_public = True
-    order by ft.id
-;
-
----------------------------------------
--- corrective_action_plan
----------------------------------------
-create view api_v1_0_0_beta.corrective_action_plans as
-    select 
-        gen.report_id,
-        gen.auditee_uei,
-        gen.audit_year,
-        ---
-        ct.finding_ref_number,
-        ct.contains_chart_or_table,
-        ct.planned_action
-    from
-        dissemination_CAPText ct,
-        dissemination_General gen
-    where
-        ct.report_id = gen.report_id
-        and
-        gen.is_public = True
-    order by ct.id
-;
-
----------------------------------------
--- notes_to_sefa
----------------------------------------
-create view api_v1_0_0_beta.notes_to_sefa as
-    select 
-        gen.report_id,
-        gen.auditee_uei,
-        gen.audit_year,
-        ---
-        note.note_title as title,
-        note.accounting_policies,
-        note.is_minimis_rate_used,
-        note.rate_explained,
-        note.content
-    from
-        dissemination_General gen,
-        dissemination_Note note
-    where
-        note.report_id = gen.report_id
-        and
-        gen.is_public = True
-    order by note.id
-;
-
-create view api_v1_0_0_beta.additional_ueis as
-    select
-        gen.report_id,
-        gen.auditee_uei,
-        gen.audit_year,
-        ---
-        uei.additional_uei
-    from
-        dissemination_general gen,
-        dissemination_additionaluei uei
-    where
-        gen.report_id = uei.report_id
-        and
-        gen.is_public = True
-    order by uei.id
-;
-
-create view api_v1_0_0_beta.additional_eins as
-    select
-        gen.report_id,
-        gen.auditee_uei,
-        gen.audit_year,
-        ---
-        ein.additional_ein
-    from
-        dissemination_general gen,
-        dissemination_additionalein ein
-    where
-        gen.report_id = ein.report_id
-        and
-        gen.is_public = True
-    order by ein.id
-;
+-- create view api_v1_0_0_beta.additional_eins as
+--     select
+--         gen.report_id,
+--         gen.auditee_uei,
+--         gen.audit_year,
+--         ---
+--         ein.additional_ein
+--     from
+--         dissemination_general gen,
+--         dissemination_additionalein ein
+--     where
+--         gen.report_id = ein.report_id
+--         and
+--         gen.is_public = true
+-- or (gen.is_public = false and has_tribal_data_access())
+--     order by ein.id
+-- ;
 
 
 commit;
 
 notify pgrst,
        'reload schema';
-
--- backend/run.sh
+++ backend/run.sh
@@ -19,9 +19,26 @@ fi;
 
 # Migrate first
 python manage.py migrate
+
+echo 'Starting API schema deprecation' &&
 python manage.py drop_deprecated_api_schema_and_views &&
-  python manage.py drop_api_schema &&
-  python manage.py create_api_schema &&
-  python manage.py create_api_views
+echo 'Finished API schema deprecation' &&
+echo 'Dropping API schema' &&
+python manage.py drop_api_schema &&
+echo 'Finished dropping API schema' &&
+echo 'Starting API schema creation' &&
+python manage.py create_api_schema &&
+echo 'Finished API schema creation' &&
+echo 'Starting migrate' &&
+python manage.py migrate &&
+echo 'Finished migrate' &&
+echo 'Starting API view creation' &&
+python manage.py create_api_views &&
+echo 'Finished view creation'
+
+# python manage.py drop_deprecated_api_schema_and_views &&
+#   python manage.py drop_api_schema &&
+#   python manage.py create_api_schema &&
+#   python manage.py create_api_views
 # Run the build/watch assets + run server at the same time
 npm run dev & python manage.py runserver 0.0.0.0:8000
