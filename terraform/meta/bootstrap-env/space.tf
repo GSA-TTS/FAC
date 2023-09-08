e189038d5d47a1ef23ad286a3da3ce13b8512383
Bret Mogilefsky <bmogilefsky@gmail.com>
2023-09-07 23:15:03 -0700

    Explicitly manage ASGs now that the provider allows for it

--- terraform/meta/bootstrap-env/space.tf
+++ terraform/meta/bootstrap-env/space.tf
@@ -10,21 +10,14 @@ data "cloudfoundry_asg" "asgs" {
 
 # Ensure the space exists and is configured as expected
 resource "cloudfoundry_space" "space" {
-  name = var.name
-  org  = data.cloudfoundry_org.org.id
-
-  # Disallow SSH access for the special name production
+  name      = var.name
+  org       = data.cloudfoundry_org.org.id
   allow_ssh = var.allow_ssh
+}
 
-  asgs = [for d in var.asgs : d]
-
-  # Until the CF provider can properly handle ASGs, we're handling removal of the public_egress ASG manually outside of Terraform.
-  # https://github.com/cloudfoundry-community/terraform-provider-cloudfoundry/issues/405
-  lifecycle {
-    ignore_changes = [
-      asgs,
-    ]
-  }
+resource "cloudfoundry_space_asgs" "space_asgs" {
+  space        = cloudfoundry_space.space.id
+  running_asgs = values(data.cloudfoundry_asg.asgs)[*].id
 }
 
 # Commented out until we are properly addressing the TODO
