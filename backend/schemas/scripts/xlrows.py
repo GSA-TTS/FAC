5476090ad1abcb1a15cc74a05d471f4919c9f424
JeanMarie Mariadassou <jmpm@JMPMs-MBP.local>
2023-09-14 23:07:06 +0200

    Resolve linting issue

--- backend/schemas/scripts/xlrows.py
+++ backend/schemas/scripts/xlrows.py
@@ -1,12 +1,12 @@
 import openpyxl
 import glob
 
-folder_path = '../output/excel/xlsx'
+folder_path = "../output/excel/xlsx"
 
-for filename in glob.glob(f'{folder_path}/*.xlsx'):
-    print(f'File: {filename}')
+for filename in glob.glob(f"{folder_path}/*.xlsx"):
+    print(f"File: {filename}")
     wb = openpyxl.load_workbook(filename)
 
     for sheet in wb._sheets:
         nrows = sheet.max_row
-        print(f'Sheet {sheet.title} has {nrows} rows')
+        print(f"Sheet {sheet.title} has {nrows} rows")
