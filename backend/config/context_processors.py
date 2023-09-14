df26d27878ae369dbdcabe5eeba649e317210a2b
James Person <jperson1@umbc.edu>
2023-09-14 14:06:58 -0400

    What would we do without linting?

--- backend/config/context_processors.py
+++ backend/config/context_processors.py
@@ -1,8 +1,9 @@
-from config import settings # import the settings file
+from config import settings
+
 
 def static_site_url(request):
     """
     Returns the STATIC_SITE_URL. This is added to the context of every template request
     made on the site. Used most frequently for links in the primary nav.
     """
-    return {'STATIC_SITE_URL': settings.STATIC_SITE_URL}
+    return {"STATIC_SITE_URL": settings.STATIC_SITE_URL}
