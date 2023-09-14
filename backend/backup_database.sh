e66b7e028c6227c4deb7618d953b7cdc62f6dcf2
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-09-14 08:44:05 -0400

    rollback

--- backend/backup_database.sh
+++ backend/backup_database.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 export PATH=/home/vcap/deps/0/apt/usr/lib/postgresql/15/bin:$PATH
-date=$(date '+%Y-%m-%d-%H%M')
-python manage.py dbbackup -o "prod-db-backup-$date.dump"
+python manage.py dbbackup
