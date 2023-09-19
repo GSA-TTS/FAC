92eebcb5fbe007a65b2f606e59ea85972bc29d61
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-09-19 11:56:57 -0400

    Fix typo

--- backend/report_submission/context_processors.py
+++ backend/report_submission/context_processors.py
@@ -1,5 +1,5 @@
 def certifiers_emails_must_not_match(request):
     emails_must_not_match = (
-        "The certifying auditor and auditee should not have the same email addressjj"
+        "The certifying auditor and auditee should not have the same email address"
     )
     return {"certifiers_emails_must_not_match": emails_must_not_match}
