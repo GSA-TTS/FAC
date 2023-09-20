d7c3dbf896a5d37422506ab6af1fb865674adfaf
James Person <james.person@gsa.gov>
2023-09-20 16:54:53 -0400

    Header Updates (Links) (#2131)
    * STATIC_SITE_URL to all template requests. Used in nav links.
    
    * What would we do without linting?
    
    * Lint - Two empty lines, not one.
    
    * Update primary nav to match static site
    
    * Header - Remove beta banner

--- backend/config/context_processors.py
+++ backend/config/context_processors.py
@@ -1,6 +1,14 @@
 from config import settings
 
 
+def static_site_url(request):
+    """
+    Returns the STATIC_SITE_URL. This is added to the context of every template request
+    made on the site. Used most frequently for links in the primary nav.
+    """
+    return {"STATIC_SITE_URL": settings.STATIC_SITE_URL}
+
+
 def omb_num_exp_date(request):
     """
     Returns the OMB_NUMBER (str) and OMB_EXP_DATE (str) in template context form.
