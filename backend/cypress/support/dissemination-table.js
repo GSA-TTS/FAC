2a3ddf7f94d350881f0d14f3c05aa5842538ef91
Phil Dominguez <philip.dominguez@gsa.gov>
2023-09-21 14:27:20 -0400

    Adding api key

--- backend/cypress/support/dissemination-table.js
+++ backend/cypress/support/dissemination-table.js
@@ -3,6 +3,7 @@
 */
 
 const API_GOV_JWT = Cypress.env('API_GOV_JWT') || '';
+const API_GOV_KEY = Cypress.env('API_GOV_KEY') || '';
 const API_GOV_URL = Cypress.env('API_GOV_URL');
 
 export function testReportId(reportId, expectedResults) {
@@ -11,6 +12,7 @@ export function testReportId(reportId, expectedResults) {
     url: `${API_GOV_URL}/general`,
     headers: {
         Authorization: `Bearer ${API_GOV_JWT}`,
+        'X-Api-Key': API_GOV_KEY,
     },
     qs: {report_id: `eq.${reportId}`},
     }).should((response) => {
