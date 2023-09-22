61740792c9e4cfb5293e411aa92dc06bfb283fa6
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-09-22 08:40:35 -0400

    Add media backup

--- backend/backup_database.sh
+++ backend/backup_database.sh
@@ -3,3 +3,4 @@
 export PATH=/home/vcap/deps/0/apt/usr/lib/postgresql/15/bin:$PATH
 date=$(date '+%Y-%m-%d-%H%M')
 python manage.py dbbackup -o "prod-db-backup-$date.dump"
+python manage.py mediabackup -o "prod-media-backup-$date.tar"
