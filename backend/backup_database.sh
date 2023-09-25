5e2c92336edc167b3708c1d854ac4e7d500186d8
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-09-25 16:54:57 -0400

    Change filename to match default

--- backend/backup_database.sh
+++ backend/backup_database.sh
@@ -3,5 +3,5 @@
 echo "Environment set as: $1"
 export PATH=/home/vcap/deps/0/apt/usr/lib/postgresql/15/bin:$PATH
 date=$(date '+%Y-%m-%d-%H%M')
-python manage.py dbbackup -o "$1-db-backup-$date.dump"
+python manage.py dbbackup -o "$1-db-backup-$date.psql.bin"
 python manage.py mediabackup -o "$1-media-backup-$date.tar"
