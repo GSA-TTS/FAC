7cbb88c3c6bd699a302707290d042ede66af51f5
JeanMarie Mariadassou <jmpm@JMPMs-MBP.local>
2023-09-28 14:04:05 -0700

    Merge and Lint

--- backend/support/management/commands/fac_s3.py
+++ backend/support/management/commands/fac_s3.py
@@ -7,6 +7,7 @@ from django.core.management.base import BaseCommand
 
 from django.conf import settings
 
+
 class Command(BaseCommand):
     help = """
     Alternative to aws s3 as the cli is not available in production.
@@ -16,48 +17,44 @@ class Command(BaseCommand):
     """
 
     def add_arguments(self, parser):
-        parser.add_argument('bucket_name', type=str, help='The S3 bucket name.')
-        parser.add_argument('--src', help='local file name.')
-        parser.add_argument('--tgt', help='s3 file name.')
-        parser.add_argument('--ls', action='store_true', help='List all files.')
-        parser.add_argument('--cp', action='store_true', help='Copy src to tgt.')
-        parser.add_argument('--rm', action='store_true', help='Delete tgt.')
-
+        parser.add_argument("bucket_name", type=str, help="The S3 bucket name.")
+        parser.add_argument("--src", help="local file name.")
+        parser.add_argument("--tgt", help="s3 file name.")
+        parser.add_argument("--ls", action="store_true", help="List all files.")
+        parser.add_argument("--cp", action="store_true", help="Copy src to tgt.")
+        parser.add_argument("--rm", action="store_true", help="Delete tgt.")
 
     def handle(self, *args, **options):
-        
-        bucket_name = options['bucket_name']
-        src_path = options['src']
-        tgt_path = options['tgt']
+        bucket_name = options["bucket_name"]
+        src_path = options["src"]
+        tgt_path = options["tgt"]
 
         s3_client = boto3.client(
-            's3',
+            "s3",
             aws_access_key_id=settings.AWS_PRIVATE_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_PRIVATE_SECRET_ACCESS_KEY,
-            endpoint_url=settings.AWS_S3_ENDPOINT_URL
+            endpoint_url=settings.AWS_S3_ENDPOINT_URL,
         )
 
-        if options['ls']:
+        if options["ls"]:
             items = s3_client.list_objects(
                 Bucket=bucket_name,
-                Prefix=tgt_path or '',
-                )['Contents']
+                Prefix=tgt_path or "",
+            )["Contents"]
             for item in items:
-                print(item['Key'], item['Size'], item['LastModified'])
+                print(item["Key"], item["Size"], item["LastModified"])
             return
-    
-        if options['cp']:
-            file_path = path.join(settings.BASE_DIR,src_path)
+
+        if options["cp"]:
+            file_path = path.join(settings.BASE_DIR, src_path)
             # with open(file_path, 'rb') as file:
             object_name = tgt_path or os.path.basename(file_path)
             s3_client.upload_file(file_path, bucket_name, object_name)
-            print(f'Copied {file_path} to {bucket_name} {object_name}.')
+            print(f"Copied {file_path} to {bucket_name} {object_name}.")
             return
-        
-        if options['rm']:
+
+        if options["rm"]:
             s3_client.delete_object(
                 Bucket=bucket_name,
                 Key=tgt_path,
             )
-
-
