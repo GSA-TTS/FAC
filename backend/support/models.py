7ecf4b5b5e1dea3143d89ab19b1faf6a35818373
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2023-10-02 13:49:27 -0700

    Update report_id format (#2329)
    * Update report_id format.
    
    * Remove no-longer-needed import of calendar.
    
    * Check in support migration file.
    
    * Change dissemination.audit_date to use fiscal year end date; also remove the default and expect auditee fiscal end date to be present.
    
    * Update to use GSAFAC as source instead of GSA.
    
    * End. Not start. End.
    
    * Naming things is hard.
    
    * Remove stray imports.
    
    * Handle migration clash.

--- backend/support/models.py
+++ backend/support/models.py
@@ -56,7 +56,7 @@ ASSIGNMENT_TYPES = (
 
 
 class CognizantAssignment(models.Model):
-    report_id = models.CharField(max_length=17)
+    report_id = models.CharField()
     cognizant_agency = models.CharField(
         # "Two digit Federal agency prefix of the cognizant agency",
         max_length=2,
