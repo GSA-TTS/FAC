3a5b59e708926bca59b747a9eb53f6210d331fd3
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2023-10-06 12:13:11 -0700

    Add v0 to deprecated table list.
    Add missing SQL files.

---
+++ backend/dissemination/api/api_v1_0_1/drop.sql
@@ -0,0 +1,11 @@
+
+begin;
+
+DROP SCHEMA IF EXISTS api_v1_0_1 CASCADE;
+-- DROP ROLE IF EXISTS authenticator;
+-- DROP ROLE IF EXISTS api_fac_gov;
+
+commit;
+
+notify pgrst,
+       'reload schema';
