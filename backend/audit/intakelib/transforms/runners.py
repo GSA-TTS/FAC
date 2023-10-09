d998f8a6294eae8d4ab4ec7ae8fc1a99973bfbe1
Matt Jadud <matthew.jadud@gsa.gov>
2023-10-09 10:44:24 -0400

    Removing print statements

--- backend/audit/intakelib/transforms/runners.py
+++ backend/audit/intakelib/transforms/runners.py
@@ -1,7 +1,6 @@
 from django.core.exceptions import ValidationError
 import logging
 from copy import deepcopy
-from pprint import pprint
 
 logger = logging.getLogger(__name__)
 
--- backend/audit/intakelib/transforms/xform_replace_seq_numbers_in_notes_to_sefa.py
+++ backend/audit/intakelib/transforms/xform_replace_seq_numbers_in_notes_to_sefa.py
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 
 def replace_seq_numbers_in_notes_to_sefa(ir):
     seq = get_range_by_name(ir, "seq_number")
-    logger.info(seq)
     new_values = list(map(lambda v: v + 1, range(len(seq["values"]))))
     new_ir = replace_range_by_name(ir, "seq_number", new_values)
     return new_ir
