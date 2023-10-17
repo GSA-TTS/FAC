b66659f6cdff248025df88735d0c39fe1da844d1
Tim Ballard <timothy.ballard@gsa.gov>
2023-10-17 08:38:56 -0500

    first pass at pdf downloads via search

--- backend/dissemination/urls.py
+++ backend/dissemination/urls.py
@@ -5,6 +5,7 @@ from dissemination import views
 app_name = "dissemination"
 
 urlpatterns = [
+    path("pdf/<str:report_id>", views.PdfDownloadView.as_view(), name="PdfDownload"),
     path("search", views.Search.as_view(), name="Search"),
     path("summary/<str:report_id>", views.AuditSummaryView.as_view(), name="Summary"),
 ]
