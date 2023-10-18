d7d1c27b39d3bccccb7a8f2baeaa5bcf1c44e041
James Person <james.person@gsa.gov>
2023-10-18 16:00:40 -0400

    Search - Pre-populate Form, Summary Link, Link Icons, Formatting (#2538)
    * URLS - Add a backslash to the search url
    
    * Search - Link to summary, icon size, formatting
    
    * Summary - remove "back" button, formatting.
    
    * Search - prepopulate form after making a search
    
    * Remove usused import
    
    * Don't run methods on empty data, kids
    
    * Search - Move UEI and ALN above the name field
    
    * Djlint reformatting

--- backend/dissemination/urls.py
+++ backend/dissemination/urls.py
@@ -5,6 +5,6 @@ from dissemination import views
 app_name = "dissemination"
 
 urlpatterns = [
-    path("search", views.Search.as_view(), name="Search"),
+    path("search/", views.Search.as_view(), name="Search"),
     path("summary/<str:report_id>", views.AuditSummaryView.as_view(), name="Summary"),
 ]
--- backend/dissemination/views.py
+++ backend/dissemination/views.py
@@ -36,6 +36,11 @@ class Search(View):
             results = search_general(
                 names, uei_or_eins, start_date, end_date, cog_or_oversight, agency_name
             )
+            # Reformat these so the date-picker element in HTML prepopulate
+            if form.cleaned_data["start_date"]:
+                form.cleaned_data["start_date"] = start_date.strftime("%Y-%m-%d")
+            if form.cleaned_data["end_date"]:
+                form.cleaned_data["end_date"] = end_date.strftime("%Y-%m-%d")
 
         return render(request, "search.html", {"form": form, "results": results})
 
