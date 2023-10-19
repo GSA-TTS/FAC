f2302930ed544fafe924c606eb1a3853d0bfb3fb
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-10-19 18:02:30 -0400

    #2504 Added logic to check end period loan balance (#2546)
    Co-authored-by: Dan Swick <2365503+danswick@users.noreply.github.com>

--- backend/audit/intakelib/checks/error_messages.py
+++ backend/audit/intakelib/checks/error_messages.py
@@ -26,5 +26,6 @@ messages = {
     "check_passthrough_name_when_no_direct_n_and_empty_number": "When the award is direct, passthrough number must be empty",
     "check_findings_grid_validation": "The combination of findings <b>{}</b> is not a valid combination under Uniform Guidance",
     "check_eins_are_not_empty": "EIN cannot be empty",
+    "check_loan_balance": "The loan balance is currently set to {}. It should either be a positive number, N/A, or left empty.",
     "check_cardinality_of_passthrough_names_and_ids": "You used a <b>|</b> (bar character) to indicate multiple passthrough names and IDs; you must provide equal numbers of names and IDs. You provided <b>{}</b> name{} and <b>{}</b> ID{}",
 }
