19d2859aa0c9be1358ddb5920e95f4451677b1ae
Matthew Jadud <matthew.jadud@gsa.gov>
2023-10-19 16:19:42 -0400

    Fix for Zendesk 245 re: passthrough IDs and ALNs that are not strings (#2550)
    * Linting.
    
    Passes tests.
    
    * Fixed issue where None was being converted to the string 'None'
    
    ---------
    
    Co-authored-by: Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>

--- backend/audit/intakelib/checks/error_messages.py
+++ backend/audit/intakelib/checks/error_messages.py
@@ -26,4 +26,5 @@ messages = {
     "check_passthrough_name_when_no_direct_n_and_empty_number": "When the award is direct, passthrough number must be empty",
     "check_findings_grid_validation": "The combination of findings <b>{}</b> is not a valid combination under Uniform Guidance",
     "check_eins_are_not_empty": "EIN cannot be empty",
+    "check_cardinality_of_passthrough_names_and_ids": "You used a <b>|</b> (bar character) to indicate multiple passthrough names and IDs; you must provide equal numbers of names and IDs. You provided <b>{}</b> name{} and <b>{}</b> ID{}",
 }
