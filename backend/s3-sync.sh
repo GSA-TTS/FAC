b1cdefa9b0b35cede75869c1814b773113f8b442
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-10-24 09:57:26 -0400

    All commands
    Just need to sort creds now

--- backend/s3-sync.sh
+++ backend/s3-sync.sh
@@ -7,8 +7,10 @@ unzip awscliv2.zip
 export PATH=/home/vcap/app/usr/v2/2.13.28/bin:$PATH
 aws --version
 
-# Get the fac-private-s3 bucket
+# Get the s3 from $VCAP_SERVICES
 export S3CREDS="$(echo $VCAP_SERVICES|jq -r '.s3')"
+
+# Get the fac-private-s3 bucket
 export FACPRIVS3="$(echo $S3CREDS|jq '.[]|select(.name=="fac-private-s3")'|jq '.credentials')"
 export AWS_ACCESS_KEY_ID="$(echo "$FACPRIVS3"|jq -r '.access_key_id')"
 export AWS_SECRET_ACCESS_KEY="$(echo "$FACPRIVS3"|jq -r '.secret_access_key')"
@@ -17,23 +19,42 @@ export URI="$(echo "$FACPRIVS3"|jq -r '.uri')"
 export AWS_DEFAULT_REGION='us-gov-west-1'
 
 # Get the backups bucket
-export FACBACKUPS="$(echo $S3CREDS|jq '.[]|select(.name=="backups")'|jq '.credentials')"
-export AWS_ACCESS_KEY_ID="$(echo "$FACBACKUPS"|jq -r '.access_key_id')"
-export AWS_SECRET_ACCESS_KEY="$(echo "$FACBACKUPS"|jq -r '.secret_access_key')"
-export BACKUPS_BUCKET_NAME="$(echo "$FACBACKUPS"|jq -r '.bucket')"
+# export FACBACKUPS="$(echo $S3CREDS|jq '.[]|select(.name=="backups")'|jq '.credentials')"
+# export AWS_ACCESS_KEY_ID="$(echo "$FACBACKUPS"|jq -r '.access_key_id')"
+# export AWS_SECRET_ACCESS_KEY="$(echo "$FACBACKUPS"|jq -r '.secret_access_key')"
+# export BACKUPS_BUCKET_NAME="$(echo "$FACBACKUPS"|jq -r '.bucket')"
 
 # List contents of root bucket
 aws s3 ls "s3://${BUCKET_NAME}"
 
 # Sync the buckets
-aws s3 sync s3://${BUCKET_NAME} s3://${BACKUPS_BUCKET_NAME} --dryrun
+#aws s3 sync s3://${BUCKET_NAME} s3://${BACKUPS_BUCKET_NAME} --dryrun
+
+##########
+# s3 tar #
+##########
 
 # Grab the s3 tar binary
-#curl -X $https_proxy -L "https://github.com/awslabs/amazon-s3-tar-tool/releases/download/v1.0.14/s3tar-linux-arm64.zip" -o "s3tar-linux-arm64.zip"
-#curl -X $https_proxy -L https://github.com/awslabs/amazon-s3-tar-tool/releases/download/v1.0.14/s3tar-linux-amd64.zip -o "s3tar-linux-amd64.zip"
-#unzip s3tar-linux-arm64.zip
+curl -L "https://github.com/awslabs/amazon-s3-tar-tool/releases/download/v1.0.14/s3tar-linux-amd64.zip" -o "s3tar-linux-amd64.zip"
+unzip s3tar-linux-arm64.zip
+# Create a tar in the source bucket
+./s3tar-linux-amd64 --region $AWS_DEFAULT_REGION -cvf s3://${BUCKET_NAME}/prefix/archive.tar s3://${BUCKET_NAME}
 
-# s3tar create a tar
-#s3tar --region $REGION -cvf s3://bucket/prefix/archive.tar s3://bucket/files/
-# s3tar copy tar to destination
-#s3tar --region $REGION -xvf s3://bucket/prefix/archive.tar -C s3://bucket/destination/
+# This gets us the following:
+# ./s3tar-linux-amd64 --region $AWS_DEFAULT_REGION -cvf s3://${BUCKET_NAME}/prefix/archive.tar s3://${BUCKET_NAME}
+# estimated tar size: 637238272
+# processing 263 Amazon S3 Objects
+# final size 0GB (without tar headers + padding)
+# estimated final size: 637334517 bytes (with headers + padding)
+# multipart part-size: 5242880 bytes
+# Final Object: s3://${BUCKET_NAME}/prefix/archive.tar
+# deleting all intermediate objects
+# Time elapsed: 39.093430244s
+# vcap@630ee638-71b3-4821-7a74-b60e:~$ aws s3 ls "s3://${BUCKET_NAME}/prefix/"
+# 2023-10-24 13:43:52  637268480 archive.tar
+
+# List contents of backups bucket
+aws s3 ls "s3://${BACKUPS_BUCKET_NAME}"
+
+# Share the Tar to dest
+./s3tar-linux-amd64 --region $AWS_DEFAULT_REGION -xvf s3://${BUCKET_NAME}/prefix/archive.tar -C s3://${BACKUPS_BUCKET_NAME}/mediabackups/
