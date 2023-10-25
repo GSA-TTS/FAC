079baa0ec2908a506f4cbe41740863a68e018233
Tim Ballard <timothy.ballard@gsa.gov>
2023-10-25 08:47:08 -0500

    add xlsx download view

--- backend/dissemination/urls.py
+++ backend/dissemination/urls.py
@@ -5,6 +5,11 @@ from dissemination import views
 app_name = "dissemination"
 
 urlpatterns = [
+    path(
+        "xlsx/<str:file_type>/<str:report_id>",
+        views.XlsxDownloadView.as_view(),
+        name="XlsxDownload",
+    ),
     path("pdf/<str:report_id>", views.PdfDownloadView.as_view(), name="PdfDownload"),
     path("search/", views.Search.as_view(), name="Search"),
     path("summary/<str:report_id>", views.AuditSummaryView.as_view(), name="Summary"),
