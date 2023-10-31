72555cf5dc282624324da3e01333c8649409159e
Tim Ballard <1425377+timoballard@users.noreply.github.com>
2023-10-31 10:39:51 -0500

    private bucket uses private default ACL

--- backend/report_submission/storages.py
+++ backend/report_submission/storages.py
@@ -13,4 +13,5 @@ class S3PrivateStorage(S3Boto3Storage):
     bucket_name = settings.AWS_PRIVATE_STORAGE_BUCKET_NAME
     access_key = settings.AWS_PRIVATE_ACCESS_KEY_ID
     secret_key = settings.AWS_PRIVATE_SECRET_ACCESS_KEY
+    default_acl = settings.AWS_PRIVATE_DEFAULT_ACL
     location = ""
