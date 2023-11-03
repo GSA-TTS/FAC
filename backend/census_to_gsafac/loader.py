54ba1df22d1bed2dd7a0f42c1cdd97e130476d09
SudhaUKumar <sudhaukumar@fcoh2j-l9jl9nxr.attlocal.net>
2023-11-03 10:19:34 -0700

    Initial commit

---
+++ backend/census_to_gsafac/loader.py
@@ -0,0 +1,28 @@
+import datetime
+
+
+from audit.models import SingleAuditChecklist
+from .models import ELECAUDITHEADER as Gen
+from .wb_generator import load_historic_data
+
+
+def load_data():
+    SingleAuditChecklist.objects.all().delete
+
+    gens = Gen.objects.all()
+    total_count = error_count = 0
+    for gen in gens:
+        audit_year = gen.AUDITYEAR
+        dbkey = gen.DBKEY
+        result = load_historic_data(audit_year, dbkey)
+        print(result)
+        total_count += 1
+        if len(result["errors"]) > 0:
+            error_count += 1
+            break
+        if total_count % 25 == 0:
+            now = datetime.datetime.now()
+            print(now.strftime("%H:%M"))
+            print(f"{error_count} errors out of {total_count}")
+
+    print(f"{error_count} errors out of {total_count}")
