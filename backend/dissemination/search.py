b6336b01456261613416e13500346bc7b6a837e2
James Person <jperson1@umbc.edu>
2023-11-03 11:43:00 -0400

    Search on ALNs

--- backend/dissemination/search.py
+++ backend/dissemination/search.py
@@ -1,9 +1,10 @@
 from django.db.models import Q
 
-from dissemination.models import General
+from dissemination.models import General, FederalAward
 
 
 def search_general(
+    alns=None,
     names=None,
     uei_or_eins=None,
     start_date=None,
@@ -14,14 +15,11 @@ def search_general(
 ):
     query = Q(is_public=True)
 
-    # TODO: use something like auditee_name__contains
-    # SELECT * WHERE auditee_name LIKE '%SomeString%'
+    if alns:
+        query.add(_get_aln_match_query(alns), Q.AND)
+
     if names:
-        names_match = Q()
-        for name in names:
-            names_match.add(Q(auditee_name__search=name), Q.OR)
-            names_match.add(Q(auditor_firm_name__search=name), Q.OR)
-        query.add(names_match, Q.AND)
+        query.add(_get_names_match_query(names), Q.AND)
 
     if uei_or_eins:
         uei_or_ein_match = Q(
@@ -52,3 +50,46 @@ def search_general(
     results = General.objects.filter(query).order_by("-fac_accepted_date")
 
     return results
+
+
+def _get_aln_match_query(alns):
+    """
+    Create the match query for ALNs. d
+    Takes: A list of (potential) ALNs.
+    Returns: A query object matching on relevant report_ids found in the FederalAward table.
+
+    # ALNs are a little weird, because they are stored per-award in the FederalAward table. To search on ALNs, we:
+    # 1. Split the given ALNs into a list with their prefix and extention.
+    # 2. Search the FederalAward table for awards with matching federal_agency_prefix and federal_award_extension.
+    # 3. Add the report_ids from the identified awards to the search params.
+    """
+    # Split each ALN into [prefix, extention]
+    split_alns = []
+    for aln in alns:
+        split_aln = aln.split(".")
+        if len(split_aln) == 2:
+            split_alns.append(split_aln)
+    # Search for relevant awards
+    report_ids = []
+    for aln_list in split_alns:
+        matching_awards = FederalAward.objects.filter(
+            federal_agency_prefix=aln_list[0], federal_award_extension=aln_list[1]
+        ).values()
+        if matching_awards:
+            report_ids.append(matching_awards[0].get("report_id"))
+    # Add the report_id's from the award search to the full search params
+    alns_match = Q()
+    for report_id in report_ids:
+        alns_match.add(Q(report_id=report_id), Q.OR)
+    return alns_match
+
+
+def _get_names_match_query(names):
+    """
+    Given a list of (potential) names, return the query object that searches auditee and firm names.
+    """
+    names_match = Q()
+    for name in names:
+        names_match.add(Q(auditee_name__search=name), Q.OR)
+        names_match.add(Q(auditor_firm_name__search=name), Q.OR)
+    return names_match
