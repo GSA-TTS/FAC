a49959c98d674c8e55f5bf86a28ffee349e5b863
James Person <jperson1@umbc.edu>
2023-11-03 10:19:55 -0400

    Override form reset with JS to empty the inputs

--- backend/static/js/search-results-pagination.js
+++ backend/static/js/search-results-pagination.js
@@ -6,7 +6,7 @@ const previous_page_link = document.querySelectorAll(
 );
 
 function attachEventHandlers() {
-  // If any pagination links are clicked, set the form element and submit it for a reload
+  // If any pagination links are clicked, set the page form element and submit it for a reload
   pagination_links.forEach((link) => {
     link.addEventListener('click', (e) => {
       e.preventDefault();
@@ -14,7 +14,8 @@ function attachEventHandlers() {
       FORM.submit();
     });
   });
-  // If the next or previous page buttons are clicked, set the form element to be +/- 1 and submit for a reload
+
+  // If the next or previous page buttons are clicked, set the page form element to be +/- 1 and submit for a reload
   if (next_page_link[0]) {
     next_page_link[0].addEventListener('click', (e) => {
       e.preventDefault();
@@ -29,6 +30,35 @@ function attachEventHandlers() {
       FORM.submit();
     });
   }
+
+  // The form resets to the default value, which is whatever the user entered on the last form submission, _not_ fully empty fields.
+  // We cannot just loop over every form element, because buttons and hidden inputs should be handled differently.
+  FORM.addEventListener('reset', (e) => {
+    e.preventDefault();
+    // Empty out textareas
+    var text_areas = document.getElementsByTagName('textarea');
+    Array.from(text_areas).forEach((input) => {
+      input.value = '';
+    });
+    // Uncheck checkboxes
+    var checkboxes = document.querySelectorAll('[type="checkbox"]');
+    Array.from(checkboxes).forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+    // Wipe FAC release dates
+    var start_date = document.getElementById('start-date');
+    var end_date = document.getElementById('end-date');
+    start_date.value = '';
+    end_date.value = '';
+    // Reset Cog/Over dropdown
+    var default_option = document.getElementById('cog_or_oversight--none');
+    default_option.selected = true;
+    // Wipe agency name
+    var agency_name = document.getElementById('agency-name');
+    agency_name.value = '';
+
+    FORM.reset();
+  });
 }
 
 function init() {
