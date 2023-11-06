67bb9a87f4ed1f67ea1e9a467125fcdc43d8c1ba
SudhaUKumar <sudhaukumar@fcoh2j-l9jl9nxr.attlocal.net>
2023-11-06 14:22:28 -0800

    Reformatted with black

--- backend/census_to_gsafac/apps.py
+++ backend/census_to_gsafac/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CensusToGsafacConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'census_to_gsafac'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "census_to_gsafac"
