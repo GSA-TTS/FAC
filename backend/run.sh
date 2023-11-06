5e3e74411ac2ec30969797a012c868eae4abc527
SudhaUKumar <sudhaukumar@fcoh2j-l9jl9nxr.attlocal.net>
2023-11-06 10:52:30 -0800

    Replaced c2g with census_to_gsafac, renamed raw_to_pg.py as csv_to_postgres.py

--- backend/run.sh
+++ backend/run.sh
@@ -14,7 +14,7 @@ if [[ "${ENV}" == "LOCAL" || "${ENV}" == "TESTING" ]]; then
     export AWS_S3_PRIVATE_ENDPOINT="http://minio:9000"
     mc alias set myminio "${AWS_S3_PRIVATE_ENDPOINT}" minioadmin minioadmin
     mc mb myminio/gsa-fac-private-s3
-    mc mb myminio/fac-c2g-s3
+    mc mb myminio/fac-census-to-gsafac-s3
     mc admin user svcacct add --access-key="${AWS_PRIVATE_ACCESS_KEY_ID}" --secret-key="${AWS_PRIVATE_SECRET_ACCESS_KEY}" myminio minioadmin
 fi;
 
