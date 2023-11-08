f951ea268a6991f72532cf6c2daed98281ce38cc
Edward Zapata <edward.zapata@gsa.gov>
2023-11-08 16:06:25 -0500

    refactored csv_to_postgres.py
    Co-authored-by: Purvin Patel <purvinptl@users.noreply.github.com>

--- backend/census_historical_migration/management/commands/csv_to_postgres.py
+++ backend/census_historical_migration/management/commands/csv_to_postgres.py
@@ -10,11 +10,8 @@ from django.conf import settings
 from django.apps import apps
 
 CHUNK_SIZE = 10_000
-
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
-
 census_to_gsafac_models = list(
     apps.get_app_config("census_historical_migration").get_models()
 )
@@ -27,97 +24,94 @@ s3_client = boto3.client(
 )
 census_to_gsafac_bucket_name = settings.AWS_CENSUS_TO_GSAFAC_BUCKET_NAME
 DELIMITER = ","
-
-
 class Command(BaseCommand):
     help = """
         Populate Postgres database from csv files
         Usage:
         manage.py csv_to_postgres --folder <folder_name> --clean <True|False>
     """
-
     def add_arguments(self, parser):
-        parser.add_argument("--folder", help="S3 folder name")
-        parser.add_argument("--clean")
-        parser.add_argument("--sample")
+        parser.add_argument("--folder", help="S3 folder name (required)", type=str)
+        parser.add_argument("--clean", help="Clean the data (default: False)", type=bool, default=False)
+        parser.add_argument("--sample", help="Sample the data (default: False)", type=bool, default=False)
         parser.add_argument("--load")
-
+        parser.add_argument("--chunk-size", help="Chunk size for processing data (default: 10000)", type=int, default=10000)
+    
     def handle(self, *args, **options):
-        if options.get("clean") == "True":
-            self.delete_data()
-            return
-        if options.get("sample") == "True":
-            self.sample_data()
-            return
-
         folder = options.get("folder")
         if not folder:
-            print("Please specify a folder name")
+            self.stderr.write(self.style.ERROR("Please specify a folder name"))
             return
-
-        items = s3_client.list_objects(
-            Bucket=census_to_gsafac_bucket_name,
-            Prefix=folder,
-        )["Contents"]
+        if options.get("clean"):
+            self.delete_data()
+            return
+        if options.get("sample"):
+            self.sample_data()
+            return
+        self.process_csv_files(folder)
+    
+    def process_csv_files(self, folder):
+        items = self.list_s3_objects(census_to_gsafac_bucket_name, folder)
         for item in items:
             if item["Key"].endswith("/"):
                 continue
             model_name = self.get_model_name(item["Key"])
             if model_name:
-                model_obj = census_to_gsafac_models[
-                    census_to_gsafac_model_names.index(model_name)
-                ]
-                file = BytesIO()
-                try:
-                    s3_client.download_fileobj(
-                        Bucket=census_to_gsafac_bucket_name,
-                        Key=item["Key"],
-                        Fileobj=file,
-                    )
-                except ClientError:
-                    logger.error("Could not download {}".format(model_obj))
-                    return
-                print(f"Obtained {model_obj} from S3")
-                self.load_data(file, model_obj)
-
+                model_index = census_to_gsafac_model_names.index(model_name)
+                model_obj = census_to_gsafac_models[model_index]
+                file = self.get_s3_object(census_to_gsafac_bucket_name, item["Key"], model_obj)
+                if file:
+                    self.process_and_load_data(file, model_obj)
         for mdl in census_to_gsafac_models:
             row_count = mdl.objects.all().count()
-            print(f"{row_count} in ", mdl)
-
+            self.stdout.write(f"{row_count} in {mdl}")
+    
     def delete_data(self):
         for mdl in census_to_gsafac_models:
-            print("Deleting ", mdl)
+            self.stdout.write(f"Deleting {mdl}")
             mdl.objects.all().delete()
-
+   
     def sample_data(self):
         for mdl in census_to_gsafac_models:
-            print("Sampling ", mdl)
+            self.stdout.write(f"Sampling {mdl}")
             rows = mdl.objects.all()[:1]
             for row in rows:
                 for col in mdl._meta.fields:
-                    print(f"{col.name}: {getattr(row, col.name)}")
-
+                    self.stdout.write(f"{col.name}: {getattr(row, col.name)}")
+    
+    def list_s3_objects(self, bucket_name, folder):
+        return s3_client.list_objects(Bucket=bucket_name, Prefix=folder)["Contents"]
+    
+    def process_and_load_data(self, file, model_obj):
+        self.stdout.write(f"Obtained {model_obj} from S3")
+        self.load_data(file, model_obj)
+   
+    def get_s3_object(self, bucket_name, key, model_obj):
+        file = BytesIO()
+        try:
+            s3_client.download_fileobj(Bucket=bucket_name, Key=key, Fileobj=file)
+        except ClientError:
+            self.stderr.write(self.style.ERROR(f"Could not download {model_obj}"))
+            return None
+        return file
+    
     def get_model_name(self, name):
-        print("Processing ", name)
+        self.stdout.write(f"Processing {name}")
         file_name = name.split("/")[-1].split(".")[0]
         for model_name in census_to_gsafac_model_names:
             if file_name.lower().startswith(model_name):
-                print("model_name = ", model_name)
+                self.stdout.write(f"model_name = {model_name}")
                 return model_name
-        print("Could not find a matching model for ", name)
+        self.stdout.write(f"Could not find a matching model for {name}")
         return None
-
+   
     def load_data(self, file, model_obj):
-        print("Starting load data to postgres")
+        self.stdout.write(f"Starting load data to postgres")
         file.seek(0)
         rows_loaded = 0
         for df in pd.read_csv(file, iterator=True, chunksize=CHUNK_SIZE):
-            # Each row is a dictionary. The columns are the
-            # correct names for our model. So, this should be a
-            # clean way to load the model from a row.
             for _, row in df.iterrows():
                 obj = model_obj(**row)
                 obj.save()
             rows_loaded += df.shape[0]
-            print(f"Loaded {rows_loaded} rows in ", model_obj)
-        return None
+            self.stdout.write(f"Loaded {rows_loaded} rows in {model_obj}")
