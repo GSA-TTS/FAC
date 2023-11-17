c693788c5bcd5e77dcf77dbad7ca60ab1d125a17
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-11-17 00:53:14 -0500

    #2674 Linting

--- backend/census_historical_migration/base_field_maps.py
+++ backend/census_historical_migration/base_field_maps.py
@@ -2,18 +2,16 @@ from collections import namedtuple as NT
 
 FormFieldInDissem = WorkbookFieldInDissem = 1000
 
-BaseFieldMap = NT("BaseFieldMap", "in_db in_dissem default type")
-
 # This provides a way to map the sheet in the workbook to the
 # column in the DB. It also has a default value and
 # the type of value, so that things can be set correctly
 # before filling in the XLSX workbooks.
 # Define a new named tuple with an additional field
-SheetFieldMap = NT("SheetFieldMap", "in_sheet " + " ".join(BaseFieldMap._fields))
+SheetFieldMap = NT("SheetFieldMap", "in_sheet in_db in_dissem default type")
 
 
 # This provides a way to map fields in the form to the
 # column in the DB. It also has a default value and
 # the type of value, so that things can be set correctly
 # before creating an initial sac.
-FormFieldMap = NT("FormFieldMap", "in_form " + " ".join(BaseFieldMap._fields))
+FormFieldMap = NT("FormFieldMap", "in_form in_db in_dissem default type")
