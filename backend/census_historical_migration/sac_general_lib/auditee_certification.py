ecca68efafdbaa403b355407eec3a8c5e81855fd
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-11-17 18:40:55 -0500

    #2674 Updated certification object to include default fields

--- backend/census_historical_migration/sac_general_lib/auditee_certification.py
+++ backend/census_historical_migration/sac_general_lib/auditee_certification.py
@@ -1,4 +1,5 @@
-# import audit.validators
+import audit.validators
+from datetime import date
 from census_historical_migration.workbooklib.census_models.census import (
     CensusGen22 as Gen,
 )
@@ -7,39 +8,51 @@ from census_historical_migration.sac_general_lib.utils import (
     _create_json_from_db_object,
 )
 
-# FIXME: Do we need this ?
-# auditee_certification_mappings = [
-#     FormFieldMap("has_no_PII", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("has_no_BII", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("meets_2CFR_specifications", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("is_2CFR_compliant", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("is_complete_and_accurate", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("has_engaged_auditor", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("is_issued_and_signed", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("is_FAC_releasable", None, FormFieldInDissem, False, bool),
-# ]
+# The following fields represent checkboxes on the auditee certification form.
+# Since all checkboxes must be checked (meaning all fields are set to True),
+# the default value for these fields is set to True. These fields are not disseminated.
+# They are set to ensure that the record passes validation when saved.
+auditee_certification_mappings = [
+    FormFieldMap("has_no_PII", None, FormFieldInDissem, True, bool),
+    FormFieldMap("has_no_BII", None, FormFieldInDissem, True, bool),
+    FormFieldMap("meets_2CFR_specifications", None, FormFieldInDissem, True, bool),
+    FormFieldMap("is_2CFR_compliant", None, FormFieldInDissem, True, bool),
+    FormFieldMap("is_complete_and_accurate", None, FormFieldInDissem, True, bool),
+    FormFieldMap("has_engaged_auditor", None, FormFieldInDissem, True, bool),
+    FormFieldMap("is_issued_and_signed", None, FormFieldInDissem, True, bool),
+    FormFieldMap("is_FAC_releasable", None, FormFieldInDissem, True, bool),
+]
 
+# auditee_certification_date_signed is not disseminated; it is set to ensure that the record passes validation when saved.
 auditee_signature_mappings = [
     FormFieldMap("auditee_name", "auditeename", FormFieldInDissem, None, str),
     FormFieldMap("auditee_title", "auditeetitle", FormFieldInDissem, None, str),
-    # FIXME: Do we need this ?
-    # FormFieldMap(
-    #     "auditee_certification_date_signed", None, FormFieldInDissem, None, str
-    # ),
+    FormFieldMap(
+        "auditee_certification_date_signed", None, FormFieldInDissem, None, str
+    ),
 ]
 
 
+def _xform_set_certification_date(auditee_certification):
+    """Sets the default auditee certification date to today's date."""
+    auditee_certification["auditee_signature"][
+        "auditee_certification_date_signed"
+    ] = date.today().isoformat()
+    return auditee_certification
+
+
 def _auditee_certification(dbkey):
+    """Generates auditee certification JSON."""
     gobj: Gen = Gen.select().where(Gen.dbkey == dbkey).first()
     certification = {}
-    # certification["auditee_certification"] = _create_json_from_db_object(
-    #     gobj, auditee_certification_mappings
-    # )
+    certification["auditee_certification"] = _create_json_from_db_object(
+        gobj, auditee_certification_mappings
+    )
     certification["auditee_signature"] = _create_json_from_db_object(
         gobj, auditee_signature_mappings
     )
-    # FIXME: Do we need this ?
-    # verify that the created object validates against the schema
-    # audit.validators.validate_auditee_certification_json(certification)
+    certification = _xform_set_certification_date(certification)
+
+    audit.validators.validate_auditee_certification_json(certification)
 
     return certification
--- backend/census_historical_migration/sac_general_lib/auditor_certification.py
+++ backend/census_historical_migration/sac_general_lib/auditor_certification.py
@@ -1,4 +1,5 @@
-# import audit.validators
+import audit.validators
+from datetime import date
 from census_historical_migration.workbooklib.census_models.census import (
     CensusGen22 as Gen,
 )
@@ -7,36 +8,47 @@ from census_historical_migration.sac_general_lib.utils import (
     _create_json_from_db_object,
 )
 
-# FIXME: Do we need this ?
-# auditor_certification_mappings = [
-#     FormFieldMap("is_OMB_limited", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("is_auditee_responsible", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("has_used_auditors_report", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("has_no_auditee_procedures", None, FormFieldInDissem, False, bool),
-#     FormFieldMap("is_FAC_releasable", None, FormFieldInDissem, False, bool),
-# ]
+# The following fields represent checkboxes on the auditor certification form.
+# Since all checkboxes must be checked (meaning all fields are set to True),
+# the default value for these fields is set to True. These fields are not disseminated.
+# They are set to ensure that the record passes validation when saved
+auditor_certification_mappings = [
+    FormFieldMap("is_OMB_limited", None, FormFieldInDissem, True, bool),
+    FormFieldMap("is_auditee_responsible", None, FormFieldInDissem, True, bool),
+    FormFieldMap("has_used_auditors_report", None, FormFieldInDissem, True, bool),
+    FormFieldMap("has_no_auditee_procedures", None, FormFieldInDissem, True, bool),
+    FormFieldMap("is_FAC_releasable", None, FormFieldInDissem, True, bool),
+]
 
+# auditor_certification_date_signed is not disseminated; it is set to ensure that the record passes validation when saved.
 auditor_signature_mappings = [
     FormFieldMap("auditor_name", "cpacontact", FormFieldInDissem, None, str),
     FormFieldMap("auditor_title", "cpatitle", FormFieldInDissem, None, str),
-    # FIXME: Do we need this ?
-    # FormFieldMap(
-    #     "auditor_certification_date_signed", None, FormFieldInDissem, None, str
-    # ),
+    FormFieldMap(
+        "auditor_certification_date_signed", None, FormFieldInDissem, None, str
+    ),
 ]
+
+
+def _xform_set_certification_date(auditor_certification):
+    """Sets the default auditor certification date to current date."""
+    auditor_certification["auditor_signature"][
+        "auditor_certification_date_signed"
+    ] = date.today().isoformat()
+    return auditor_certification
 
 
 def _auditor_certification(dbkey):
     gobj: Gen = Gen.select().where(Gen.dbkey == dbkey).first()
     certification = {}
-    # auditor_certification["auditor_certification"] = _create_json_from_db_object(
-    #     gobj, auditor_certification_mappings
-    # )
+    certification["auditor_certification"] = _create_json_from_db_object(
+        gobj, auditor_certification_mappings
+    )
     certification["auditor_signature"] = _create_json_from_db_object(
         gobj, auditor_signature_mappings
     )
-    # FIXME: Do we need this ?
-    # verify that the created object validates against the schema
-    # audit.validators.validate_auditor_certification_json(certification)
+    certification = _xform_set_certification_date(certification)
+
+    audit.validators.validate_auditor_certification_json(certification)
 
     return certification
