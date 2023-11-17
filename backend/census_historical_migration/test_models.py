e7f3917163fdf47fa8fd061b9cd64b0182e39967
Sudha Kumar <sudha.kumar@gsa.gov>
2023-11-17 15:53:00 -0800

    Initial commit

---
+++ backend/census_historical_migration/test_models.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+from model_bakery import baker
+
+from .models import ELECAUDITHEADER, FAILED_SACS, CHANGE_RECORDS
+
+
+class CensusHistoricalMigrationModelsTestCases(TestCase):
+    def test_can_load_elecauditheader_model(self):
+        gen = ELECAUDITHEADER.objects.all()
+        self.assertIsNotNone(gen)
+        baker.make(ELECAUDITHEADER).save()
+        gen = ELECAUDITHEADER.objects.all()
+        self.assertEquals(len(gen), 1)
+
+    def test_can_load_failed_sacs_model(self):
+        failed_sacs = FAILED_SACS.objects.all()
+        self.assertIsNotNone(failed_sacs)
+        baker.make(FAILED_SACS).save()
+        failed_sacs = FAILED_SACS.objects.all()
+        self.assertEquals(len(failed_sacs), 1)
+
+    def test_can_load_change_records_model(self):
+        change_records = CHANGE_RECORDS.objects.all()
+        self.assertIsNotNone(change_records)
+        baker.make(CHANGE_RECORDS).save()
+        change_records = CHANGE_RECORDS.objects.all()
+        self.assertEquals(len(change_records), 1)
