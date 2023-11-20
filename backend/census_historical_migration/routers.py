837b044534413c2cc77cad1194dc2ba409e1c4b4
Sudha Kumar <sudha.kumar@gsa.gov>
2023-11-20 14:39:13 -0800

    Modified FailedSacs and ChangeRecords table per JMM's request

--- backend/census_historical_migration/routers.py
+++ backend/census_historical_migration/routers.py
@@ -1,12 +1,9 @@
 app_name = "census_to_gsafac"
 db_name = "census-to-gsafac-db"
-fac_db_name = "db"
 
 
 class DBRouter:
     def db_for_read(self, model, **hints):
-        if model == "CHANGE_RECORDS":
-            return fac_db_name
         if model._meta.app_label == app_name:
             return db_name
         return None
