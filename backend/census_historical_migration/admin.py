7b5ed7192b6e137923c1acee5391d1d0c3629c17
Sudha Kumar <sudha.kumar@gsa.gov>
2023-11-20 11:28:04 -0800

    Registering ELECAUDITHEADER, FAILED_SACS, CHANGE_RECORDS models

--- backend/census_historical_migration/admin.py
+++ backend/census_historical_migration/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin  # noqa: F401
 
-# Register your models here.
+from census_historical_migration.models import (
+    ELECAUDITHEADER,
+    FAILED_SACS,
+    CHANGE_RECORDS,
+)
+
+admin.site.register(ELECAUDITHEADER)
+admin.site.register(FAILED_SACS)
+admin.site.register(CHANGE_RECORDS)
