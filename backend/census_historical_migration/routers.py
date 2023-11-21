40906e137a8e7735d5bed7072ee33880d5a11f9a
JeanMarie Mariadassou <JeanMarie.Mariadassou@gsa.gov>
2023-11-21 15:58:19 -0800

    Get config test to pass

--- backend/census_historical_migration/routers.py
+++ backend/census_historical_migration/routers.py
@@ -6,10 +6,12 @@ class DBRouter:
     def db_for_read(self, model, **hints):
         if model._meta.app_label == app_name:
             return db_name
-        return None
+        return "default"
 
     def db_for_write(self, model, **hints):
-        return self.db_for_read(model, hints)
+        if model._meta.app_label == app_name:
+            return db_name
+        return "default"
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label == app_name:
