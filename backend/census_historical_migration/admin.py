7f246b412cf9ba11733d809e4cf6af53055ae449
Sudha Kumar <sudha.kumar@gsa.gov>
2023-11-22 12:05:28 -0800

    Implemented changes per ADR review

--- backend/census_historical_migration/admin.py
+++ backend/census_historical_migration/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin  # noqa: F401
 from census_historical_migration.models import (
     ELECAUDITHEADER,
     ReportMigrationStatus,
+    MigrationErrorDetail,
 )
 
 admin.site.register(ELECAUDITHEADER)
 admin.site.register(ReportMigrationStatus)
+admin.site.register(MigrationErrorDetail)
