09a28fa4680a2fa116e5995e0e2e21127e890664
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-11-27 09:28:02 -0500

    terraform fmt
    Signed-off-by: Alex Steel <130377221+asteel-gsa@users.noreply.github.com>

--- terraform/shared/modules/env/logshipper.tf
+++ terraform/shared/modules/env/logshipper.tf
@@ -5,8 +5,8 @@ locals {
 module "cg-logshipper" {
   source                = "../cg-logshipper"
   name                  = local.logshipper_name
-  cf_org_name           = var.cf_org_name               # gsa-tts-oros-fac
-  cf_space_name         = var.cf_space_name             # eg prod-egress
-  client_space          = var.cf_space_name             # eg prod
+  cf_org_name           = var.cf_org_name   # gsa-tts-oros-fac
+  cf_space_name         = var.cf_space_name # eg prod-egress
+  client_space          = var.cf_space_name # eg prod
   new_relic_license_key = var.new_relic_license_key
 }
