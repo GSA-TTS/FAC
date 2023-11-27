2394727be427c4a60e6ec682b08046bd2d2289cb
Phil Dominguez <philip.dominguez@gsa.gov>
2023-11-27 08:57:14 -0500

    Adding new command

---
+++ backend/census_historical_migration/loader.py
@@ -0,0 +1,29 @@
+from .models import ELECAUDITHEADER as Gen
+from .make_submission import load_historic_data
+
+
+def load_data(audit_year):
+    result_log = {}
+    total_count = error_count = 0
+
+    gens = Gen.objects.filter(AUDITYEAR=audit_year)
+    for gen in gens:
+        dbkey = gen.DBKEY
+        result = load_historic_data(audit_year, dbkey)
+        result_log[(audit_year, dbkey)] = result
+        total_count += 1
+
+        if len(result["errors"]) > 0:
+            error_count += 1
+        if total_count % 5 == 0:
+            print(f"Processed = {total_count}, Errors = {error_count}")
+        if error_count > 5:
+            break
+
+    print("********* Loader Summary ***************")
+
+    for k, v in result_log.items():
+        print(k, v)
+        print("-------------------")
+
+    print(f"{error_count} errors out of {total_count}")
---
+++ backend/census_historical_migration/management/commands/run_migration.py
@@ -0,0 +1,28 @@
+from ...loader import load_data
+
+from django.core.management.base import BaseCommand
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+
+
+class Command(BaseCommand):
+    help = """
+        Migrate from Census tables to GSAFAC tables for a given year
+        Usage:
+        manage.py run_migration --year <audit_year>
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument("--year", help="4-digit Audit Year")
+
+    def handle(self, *args, **options):
+        year = options.get("year")
+        if not year:
+            print("Please specify an audit year")
+            return
+
+        load_data(audit_year=year)
