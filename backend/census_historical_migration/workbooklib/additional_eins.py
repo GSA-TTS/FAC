8a463f73b66aa25191d93a83e0ff28ea8927454e
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-11-27 13:02:26 -0500

    #2873 Updated logic to use Census models

--- backend/census_historical_migration/workbooklib/additional_eins.py
+++ backend/census_historical_migration/workbooklib/additional_eins.py
@@ -1,14 +1,18 @@
+from census_historical_migration.transforms.xform_string_to_string import (
+    string_to_string,
+)
 from census_historical_migration.workbooklib.excel_creation_utils import (
-    set_uei,
+    get_audit_header,
     map_simple_columns,
     generate_dissemination_test_table,
+    set_workbook_uei,
 )
 from census_historical_migration.base_field_maps import (
     SheetFieldMap,
     WorkbookFieldInDissem,
 )
 from census_historical_migration.workbooklib.templates import sections_to_template_paths
-from census_historical_migration.workbooklib.census_models.census import dynamic_import
+from census_historical_migration.models import ELECEINS as Eins
 from audit.fixtures.excel import FORM_SECTIONS
 
 import openpyxl as pyxl
@@ -17,25 +21,49 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
+def xform_remove_trailing_decimal_zero(value):
+    """
+    Removes trailing .0 from a EIN strings.
+    """
+    trimmed_ein = string_to_string(value)
+    if trimmed_ein.endswith(".0"):
+        return trimmed_ein[:-2]
+    return trimmed_ein
+
+
 mappings = [
-    SheetFieldMap("additional_ein", "ein", WorkbookFieldInDissem, None, str),
+    SheetFieldMap(
+        "additional_ein",
+        "EIN",
+        WorkbookFieldInDissem,
+        None,
+        xform_remove_trailing_decimal_zero,
+    ),
 ]
 
 
+def _get_eins(dbkey):
+    return Eins.objects.filter(DBKEY=dbkey)
+
+
 def generate_additional_eins(dbkey, year, outfile):
+    """
+    Generates additional eins workbook for a given dbkey.
+    """
     logger.info(f"--- generate additional eins {dbkey} {year} ---")
-    Gen = dynamic_import("Gen", year)
-    Eins = dynamic_import("Eins", year)
+
     wb = pyxl.load_workbook(sections_to_template_paths[FORM_SECTIONS.ADDITIONAL_EINS])
+    audit_header = get_audit_header(dbkey)
 
-    g = set_uei(Gen, wb, dbkey)
+    set_workbook_uei(wb, audit_header.UEI)
 
-    addl_eins = Eins.select().where(Eins.dbkey == g.dbkey)
+    addl_eins = _get_eins(dbkey)
     map_simple_columns(wb, mappings, addl_eins)
     wb.save(outfile)
-
+    # FIXME - MSHD: The logic below will most likely be removed, see comment in federal_awards.py
     table = generate_dissemination_test_table(
-        Gen, "additional_eins", dbkey, mappings, addl_eins
+        audit_header, "additional_eins", dbkey, mappings, addl_eins
     )
-    table["singletons"]["auditee_uei"] = g.uei
+    table["singletons"]["auditee_uei"] = audit_header.UEI
     return (wb, table)
--- backend/census_historical_migration/workbooklib/additional_ueis.py
+++ backend/census_historical_migration/workbooklib/additional_ueis.py
@@ -1,14 +1,15 @@
 from census_historical_migration.workbooklib.excel_creation_utils import (
-    set_uei,
+    get_audit_header,
     map_simple_columns,
     generate_dissemination_test_table,
+    set_workbook_uei,
 )
 from census_historical_migration.base_field_maps import (
     SheetFieldMap,
     WorkbookFieldInDissem,
 )
 from census_historical_migration.workbooklib.templates import sections_to_template_paths
-from census_historical_migration.workbooklib.census_models.census import dynamic_import
+from census_historical_migration.models import ELECUEIS as Ueis
 from audit.fixtures.excel import FORM_SECTIONS
 
 import openpyxl as pyxl
@@ -19,26 +20,33 @@ logger = logging.getLogger(__name__)
 
 mappings = [
     # FIXME: We have no dissemination nodel for this.
-    SheetFieldMap("additional_uei", "uei", WorkbookFieldInDissem, None, str),
+    SheetFieldMap("additional_uei", "UEI", WorkbookFieldInDissem, None, str),
 ]
 
 
-def generate_additional_ueis(dbkey, year, outfile):
-    logger.info(f"--- generate additional ueis {dbkey} {year} ---")
-    Gen = dynamic_import("Gen", year)
-    wb = pyxl.load_workbook(sections_to_template_paths[FORM_SECTIONS.ADDITIONAL_UEIS])
-    g = set_uei(Gen, wb, dbkey)
-    if int(year) >= 22:
-        Ueis = dynamic_import("Ueis", year)
-        addl_ueis = Ueis.select().where(Ueis.dbkey == g.dbkey)
-        map_simple_columns(wb, mappings, addl_ueis)
+def _get_ueis(dbkey):
+    return Ueis.objects.filter(DBKEY=dbkey)
 
-        table = generate_dissemination_test_table(
-            Gen, "additional_ueis", dbkey, mappings, addl_ueis
-        )
-    else:
-        table = {}
-        table["singletons"] = {}
+
+def generate_additional_ueis(dbkey, year, outfile):
+    """
+    Generates additional ueis workbook for a given dbkey.
+    """
+    logger.info(f"--- generate additional ueis {dbkey} {year} ---")
+
+    wb = pyxl.load_workbook(sections_to_template_paths[FORM_SECTIONS.ADDITIONAL_UEIS])
+    audit_header = get_audit_header(dbkey)
+
+    set_workbook_uei(wb, audit_header.UEI)
+
+    additional_ueis = _get_ueis(dbkey)
+    map_simple_columns(wb, mappings, additional_ueis)
     wb.save(outfile)
-    table["singletons"]["auditee_uei"] = g.uei
+
+    # FIXME - MSHD: The logic below will most likely be removed, see comment in federal_awards.py
+    table = generate_dissemination_test_table(
+        audit_header, "additional_ueis", dbkey, mappings, additional_ueis
+    )
+
+    table["singletons"]["auditee_uei"] = audit_header.UEI
     return (wb, table)
