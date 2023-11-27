718b2772af40117d45c23745b9e3b978cfb8c605
Phil Dominguez <philip.dominguez@gsa.gov>
2023-11-27 14:03:30 -0500

    Teaking user creation

--- backend/census_historical_migration/historic_data_loader.py
+++ backend/census_historical_migration/historic_data_loader.py
@@ -1,16 +1,22 @@
 from .models import ELECAUDITHEADER as Gen
-from .make_submission import load_historic_data
+from .workbooklib.end_to_end_core import run_end_to_end
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 def load_historic_data_for_year(audit_year):
     """Iterates over and processes submissions for the given audit year"""
     result_log = {}
     total_count = error_count = 0
+    user = create_or_get_user()
+    submissions_for_year = Gen.objects.filter(AUDITYEAR=audit_year)
 
-    gens = Gen.objects.filter(AUDITYEAR=audit_year)
-    for gen in gens:
-        dbkey = gen.DBKEY
-        result = load_historic_data(audit_year, dbkey)
+    for submission in submissions_for_year:
+        dbkey = submission.DBKEY
+        # Migrate a single submission
+        result = load_historic_data(audit_year, dbkey, user)
         result_log[(audit_year, dbkey)] = result
         total_count += 1
 
@@ -28,3 +34,28 @@ def load_historic_data_for_year(audit_year):
         print("-------------------")
 
     print(f"{error_count} errors out of {total_count}")
+
+
+def load_historic_data(audit_year, dbkey, user):
+    """Create a submission for the given audit year, dbkey, and user"""
+    result = {"success": [], "errors": []}
+    run_end_to_end(user.email, dbkey, audit_year)
+
+    return result
+
+
+def create_or_get_user():
+    """Returns the default migration user"""
+    user_email = "fac-census-migration-auditee-official@fac.gsa.gov"
+    user_name = "fac-census-migration-auditee-official"
+    user = None
+
+    users = User.objects.filter(email=user_email)
+    if users:
+        user = users.first()
+    else:
+        print("Creating user", user_email, user_name)
+        user = User(username=user_name, email=user_email)
+        user.save()
+
+    return user
