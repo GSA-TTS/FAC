2bcf6fc6dda93cf3793d1acd4fc698ae12a8e766
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-11-27 09:26:37 -0500

    Preliminary logshipper terraform

---
+++ terraform/shared/modules/env/logshipper.tf
@@ -0,0 +1,12 @@
+locals {
+  logshipper_name = "cg-logshipper-${var.cf_space_name}"
+}
+
+module "cg-logshipper" {
+  source                = "../cg-logshipper"
+  name                  = local.logshipper_name
+  cf_org_name           = var.cf_org_name               # gsa-tts-oros-fac
+  cf_space_name         = var.cf_space_name             # eg prod-egress
+  client_space          = var.cf_space_name             # eg prod
+  new_relic_license_key = var.new_relic_license_key
+}
