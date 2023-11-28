2a0c13678f4c80d3ec85188f1a40d2fd3dd81227
Phil Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2023-11-28 08:31:49 -0500

    Command to run census migration by year (#2868)
    * Slicing last 2 year digits for dynamic_import
    
    * Adding new command
    
    * Renaming command to run_migration_for_year
    
    * Renaming load_data to load_historic_data_for_year
    
    * Renaming loader to historic_data_loader
    
    * Lint
    
    * Docstrings
    
    * Teaking user creation
    
    * Utilizing results dict
    
    * Lint
    
    * Fixing historic_data_migrator
    
    ---------
    
    Co-authored-by: Phil Dominguez <“philip.dominguez@gsa.gov”>

---
+++ backend/census_historical_migration/historic_data_loader.py
@@ -0,0 +1,59 @@
+from .models import ELECAUDITHEADER as Gen
+from .workbooklib.end_to_end_core import run_end_to_end
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+def load_historic_data_for_year(audit_year):
+    """Iterates over and processes submissions for the given audit year"""
+    result_log = {}
+    total_count = error_count = 0
+    user = create_or_get_user()
+    submissions_for_year = Gen.objects.filter(AUDITYEAR=audit_year)
+
+    for submission in submissions_for_year:
+        dbkey = submission.DBKEY
+        result = {"success": [], "errors": []}
+
+        try:
+            # Migrate a single submission
+            run_end_to_end(user.email, dbkey, audit_year, result)
+        except Exception as exc:
+            result["errors"].append(f"{exc}")
+
+        result_log[(audit_year, dbkey)] = result
+        total_count += 1
+
+        if len(result["errors"]) > 0:
+            error_count += 1
+        if total_count % 5 == 0:
+            print(f"Processed = {total_count}, Errors = {error_count}")
+        if error_count > 5:
+            break
+
+    print("********* Loader Summary ***************")
+
+    for k, v in result_log.items():
+        print(k, v)
+        print("-------------------")
+
+    print(f"{error_count} errors out of {total_count}")
+
+
+def create_or_get_user():
+    """Returns the default migration user"""
+    user_email = "fac-census-migration-auditee-official@fac.gsa.gov"
+    user_name = "fac-census-migration-auditee-official"
+    user = None
+
+    users = User.objects.filter(email=user_email)
+    if users:
+        user = users.first()
+    else:
+        print("Creating user", user_email, user_name)
+        user = User(username=user_name, email=user_email)
+        user.save()
+
+    return user
