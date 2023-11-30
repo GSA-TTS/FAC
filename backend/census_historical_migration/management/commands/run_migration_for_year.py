b7afe4a1dbfbc5ad5efd2eb9d3ad338880463a84
Phil Dominguez <philip.dominguez@gsa.gov>
2023-11-30 10:36:40 -0500

    Using commma separated dbkeys

--- backend/census_historical_migration/management/commands/run_migration_for_year.py
+++ backend/census_historical_migration/management/commands/run_migration_for_year.py
@@ -3,7 +3,7 @@ from ...historic_data_loader import load_historic_data_for_year
 from django.core.management.base import BaseCommand
 
 import logging
-
+import sys
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
@@ -14,13 +14,12 @@ class Command(BaseCommand):
         Migrate from Census tables to GSAFAC tables for a given year. Optionally, you can
         provide a range of dbkeys that will be iterated over to limit the migration.
         Usage:
-        manage.py run_migration --year <audit_year> --start <start dbkey> --end <end dbkey>
+        manage.py run_migration --year <audit_year> --dbkeys <comma separated dbkeys>
     """
 
     def add_arguments(self, parser):
         parser.add_argument("--year", help="4-digit Audit Year")
-        parser.add_argument("--start", help="Start dbkey", type=int, default=0)
-        parser.add_argument("--end", help="End dbkey", type=int, default=9999)
+        parser.add_argument("--dbkeys", type=str, required=False, default="0, 9999")
 
     def handle(self, *args, **options):
         year = options.get("year")
@@ -28,7 +27,13 @@ class Command(BaseCommand):
             print("Please specify an audit year")
             return
 
-        start_dbkey = options.get("start")
-        end_dbkey = options.get("end")
+        try:
+            dbkeys_str = options["dbkeys"]
+            dbkeys = list(map(lambda d: int(d), dbkeys_str.split(",")))
+            start_dbkey = min(dbkeys)
+            end_dbkey = max(dbkeys)
+        except ValueError as e:
+            logger.error(f"Found a non-integer in dbkeys '{dbkeys_str}'")
+            sys.exit(-1)
 
         load_historic_data_for_year(year, start_dbkey, end_dbkey)
