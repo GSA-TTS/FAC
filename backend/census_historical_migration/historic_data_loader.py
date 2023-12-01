fed0298ee5be92e752bdff624deb1deefb3b4d2c
Phil Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2023-12-01 10:21:06 -0500

    2895: New cmd for paginated census migration (#2905)
    * Creating paginated command
    
    * Updating readme
    
    * Lint
    
    * Using django paginator
    
    * Using page_size instead of batchSize
    
    * Readme tweak
    
    * Readme tweak
    
    * Update backend/census_historical_migration/README.md
    
    ---------
    
    Co-authored-by: Phil Dominguez <“philip.dominguez@gsa.gov”>
    Co-authored-by: Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>

--- backend/census_historical_migration/historic_data_loader.py
+++ backend/census_historical_migration/historic_data_loader.py
@@ -2,36 +2,49 @@ from .models import ELECAUDITHEADER as Gen
 from .workbooklib.end_to_end_core import run_end_to_end
 
 from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
+
 
 User = get_user_model()
 
 
-def load_historic_data_for_year(audit_year):
+def load_historic_data_for_year(audit_year, page_size, pages):
     """Iterates over and processes submissions for the given audit year"""
     result_log = {}
     total_count = error_count = 0
     user = create_or_get_user()
-    submissions_for_year = Gen.objects.filter(AUDITYEAR=audit_year)
+    submissions_for_year = Gen.objects.filter(AUDITYEAR=audit_year).order_by(
+        "ELECAUDITHEADERID"
+    )
+    paginator = Paginator(submissions_for_year, page_size)
 
-    for submission in submissions_for_year:
-        dbkey = submission.DBKEY
-        result = {"success": [], "errors": []}
+    print(f"{submissions_for_year.count()} submissions found for {audit_year}")
 
-        try:
-            # Migrate a single submission
-            run_end_to_end(user, dbkey, audit_year, result)
-        except Exception as exc:
-            result["errors"].append(f"{exc}")
+    for page_number in pages:
+        page = paginator.page(page_number)
+        print(
+            f"Processing page {page_number} with {page.object_list.count()} submissions."
+        )
 
-        result_log[(audit_year, dbkey)] = result
-        total_count += 1
+        for submission in page.object_list:
+            dbkey = submission.DBKEY
+            result = {"success": [], "errors": []}
 
-        if len(result["errors"]) > 0:
-            error_count += 1
-        if total_count % 5 == 0:
-            print(f"Processed = {total_count}, Errors = {error_count}")
-        if error_count > 5:
-            break
+            try:
+                # Migrate a single submission
+                run_end_to_end(user, dbkey, audit_year, result)
+            except Exception as exc:
+                result["errors"].append(f"{exc}")
+
+            result_log[(audit_year, dbkey)] = result
+            total_count += 1
+
+            if len(result["errors"]) > 0:
+                error_count += 1
+            if total_count % 5 == 0:
+                print(f"Processed = {total_count}, Errors = {error_count}")
+            if error_count > 5:
+                break
 
     print("********* Loader Summary ***************")
 
