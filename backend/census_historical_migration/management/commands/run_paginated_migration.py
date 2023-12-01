b29f45ee03dff9beceb9ca101dbacce74ecefa6e
Phil Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2023-12-01 10:21:06 -0500

    2895: New cmd for paginated census migration (#2905)
    * Creating paginated command
    
    * Updating readme
    
    * Lint
    
    * Using django paginator
    
    * Using page_size instead of batchSize
    
    * Readme tweak
    
    * Readme tweak
    
    * Update backend/census_historical_migration/README.md
    
    ---------
    
    Co-authored-by: Phil Dominguez <“philip.dominguez@gsa.gov”>
    Co-authored-by: Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>

---
+++ backend/census_historical_migration/management/commands/run_paginated_migration.py
@@ -0,0 +1,41 @@
+from ...historic_data_loader import load_historic_data_for_year
+
+from django.core.management.base import BaseCommand
+
+import logging
+import sys
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+
+
+class Command(BaseCommand):
+    help = """
+        Migrate from Census tables to GSAFAC tables for a given year using pagination
+        Usage:
+        manage.py run_migration
+            --year <audit year>
+            --pageSize <page size>
+            --pages <comma separated pages>
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument("--year", help="4-digit Audit Year")
+        parser.add_argument("--page_size", type=int, required=False, default=5)
+        parser.add_argument("--pages", type=str, required=False, default="1")
+
+    def handle(self, *args, **options):
+        year = options.get("year")
+        if not year:
+            print("Please specify an audit year")
+            return
+
+        try:
+            pages_str = options["pages"]
+            pages = list(map(lambda d: int(d), pages_str.split(",")))
+        except ValueError:
+            logger.error(f"Found a non-integer in pages '{pages_str}'")
+            sys.exit(-1)
+
+        load_historic_data_for_year(year, options["page_size"], pages)
