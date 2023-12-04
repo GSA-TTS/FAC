87a3297572e6ac089dc49edcf6a58d4a886ac550
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2023-12-04 14:08:06 -0800

    Remove top-level directory from the gitref zip
    cf push requires the top-level of the zip to be the app directory
    
    Signed-off-by: Alex Steel <130377221+asteel-gsa@users.noreply.github.com>

--- terraform/shared/modules/cg-logshipper/prepare-logshipper.sh
+++ terraform/shared/modules/cg-logshipper/prepare-logshipper.sh
@@ -4,17 +4,21 @@
 set -e
 
 popdir=$(pwd)
-
 eval "$(jq -r '@sh "GITREF=\(.gitref)"')"
 
 # Portable construct so this will work everywhere
 # https://unix.stackexchange.com/a/84980
 tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
-cd "$tmpdir"
+cd "${tmpdir}"
 
 # Grab a copy of the zip file for the specified ref
 # https://github.com/GSA-TTS/cg-logshipper/archive/refs/heads/main.zip
-curl -s -L https://github.com/GSA-TTS/cg-logshipper/archive/${GITREF}.zip --output "${popdir}/logshipper.zip"
+curl -s -L https://github.com/GSA-TTS/cg-logshipper/archive/"${GITREF}".zip --output "${tmpdir}/logshipper-dist.zip"
+
+# Remove the leading directory; the .zip needs to have the files at the top
+cd "${tmpdir}" 
+unzip -o "${tmpdir}/logshipper-dist.zip" > /dev/null
+cd "${tmpdir}/cg-logshipper-main" && zip -r -o "${popdir}/logshipper.zip" ./ > /dev/null
 
 # Tell Terraform where to find it
 cat << EOF
