2b36b1c7dcda6ad6db9f46dcead320a7e5b03267
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2023-12-08 08:10:57 -0500

    update newrelic tag

--- terraform/shared/modules/env/newrelic.tf
+++ terraform/shared/modules/env/newrelic.tf
@@ -5,5 +5,5 @@ resource "cloudfoundry_user_provided_service" "credentials" {
     "NEW_RELIC_LICENSE_KEY"   = var.new_relic_license_key
     "NEW_RELIC_LOGS_ENDPOINT" = "https://gov-log-api.newrelic.com/log/v1"
   }
-  tags = ["newrelic"]
+  tags = ["newrelic-creds"]
 }
