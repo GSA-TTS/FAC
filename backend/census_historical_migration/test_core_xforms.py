16c7b6b15e1a4665e65b1a6b99074fd665f32ca8
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-12-12 00:24:00 -0500

    #2842 Added tests to core logic

--- backend/census_historical_migration/test_core_xforms.py
+++ backend/census_historical_migration/test_core_xforms.py
@@ -1,48 +1,194 @@
 from django.test import SimpleTestCase
-from .exception_utils import DataMigrationError
 
-from .workbooklib.notes_to_sefa import xform_is_minimis_rate_used
+from .transforms.xform_string_to_int import (
+    string_to_int,
+)
+from .transforms.xform_string_to_string import string_to_string
+
+from .transforms.xform_string_to_bool import string_to_bool
+
+from .base_field_maps import FormFieldInDissem, FormFieldMap
+
+from .sac_general_lib.utils import (
+    create_json_from_db_object,
+    normalize_year_string_or_exit,
+)
 
 
-class TestXformIsMinimisRateUsed(SimpleTestCase):
-    def test_rate_used(self):
-        """Test that the function returns 'Y' when the rate is used."""
+class TestCreateJsonFromDbObject(SimpleTestCase):
+    """Tests for the create_json_from_db_object function."""
+
+    class MockDBObject:
+        def __init__(self, **kwargs):
+            self.__dict__.update(kwargs)
+
+    mappings = [
+        FormFieldMap(
+            "first_field_name", "IN_DB_FIRST_FIELD", FormFieldInDissem, "", str
+        ),
+        FormFieldMap(
+            "second_field_name", "IN_DB_SECOND_FIELD", FormFieldInDissem, None, bool
+        ),
+        FormFieldMap("third_field_name", None, FormFieldInDissem, "default_value", str),
+    ]
+
+    def test_normal_case(self):
+        """Test that the function returns the correct JSON."""
+        db_obj = self.MockDBObject(IN_DB_FIRST_FIELD="John", IN_DB_SECOND_FIELD="Y")
+        expected_json = {
+            "first_field_name": "John",
+            "second_field_name": True,
+            "third_field_name": "default_value",
+        }
         self.assertEqual(
-            # Ensure extra whitespace is acceptable
-            xform_is_minimis_rate_used("The auditee  used the de minimis cost rate."),
-            "Y",
+            create_json_from_db_object(db_obj, self.mappings), expected_json
         )
 
+    def test_missing_field(self):
+        """
+        Test the function for correct JSON output when a database field is missing and the mapping has no default value.
+        """
+        # In this case, second_field_name is not returned because the mapping for this field has no default value.
+        # and the value of the database field `IN_DB_SECOND_FIELD` is missing.
+        # `third_field_name` is returned because it has a default value of `default_value`
+        db_obj = self.MockDBObject(IN_DB_FIRST_FIELD="John")
+        expected_json = {
+            "first_field_name": "John",
+            "third_field_name": "default_value",
+        }
         self.assertEqual(
-            xform_is_minimis_rate_used(
-                "The School has elected to use the 10-percent de minimis indirect cost rate as allowed under the Uniform Guidance."
-            ),
-            "Y",
+            create_json_from_db_object(db_obj, self.mappings), expected_json
         )
 
-    def test_rate_not_used(self):
-        """Test that the function returns 'N' when the rate is not used."""
+    def test_none_value(self):
+        """Test the function for correct JSON output when a database field has a value of None."""
+        db_obj = self.MockDBObject(IN_DB_FIRST_FIELD=None, IN_DB_SECOND_FIELD=None)
+        # In this case, second_field_name is not returned because the mapping for this field has no default value.
+        # and the value of the database field `IN_DB_SECOND_FIELD` is None.
+        # `third_field_name` is returned because it has a default value of `default_value`
+        # `first_field_name` is returned because it has a default value of `""`.
+        expected_json = {"third_field_name": "default_value"}
         self.assertEqual(
-            xform_is_minimis_rate_used(
-                "The auditee did not use the de minimis cost rate."
-            ),
-            "N",
-        )
-        self.assertEqual(
-            xform_is_minimis_rate_used(
-                "The Board has elected not to use the 10 percent de minimus indirect cost as allowed under the Uniform Guidance."
-            ),
-            "N",
+            create_json_from_db_object(db_obj, self.mappings), expected_json
         )
 
-    def test_ambiguous_or_unclear_raises_exception(self):
-        """Test that the function raises an exception when rate usage is ambiguous or unclear."""
-        with self.assertRaises(DataMigrationError):
-            xform_is_minimis_rate_used(
-                "The information regarding the de minimis rate is not clear."
-            )
+    def test_all_default_values(self):
+        """Test the function for correct JSON output when all database fields have default values."""
+        db_obj = self.MockDBObject()
+        # In this case, second_field_name is not returned because the mapping for this field has no default value.
+        # and the value of the database field `IN_DB_SECOND_FIELD` is None.
+        # `third_field_name` is returned because it has a default value of `default_value`
+        # `first_field_name` is returned because it has a default value of `""`.
+        expected_json = {"first_field_name": "", "third_field_name": "default_value"}
+        self.assertEqual(
+            create_json_from_db_object(db_obj, self.mappings), expected_json
+        )
 
-        with self.assertRaises(DataMigrationError):
-            xform_is_minimis_rate_used(
-                "It is unknown whether the de minimis rate was applied."
-            )
+
+class TestStringToBool(SimpleTestCase):
+    """Tests for the string_to_bool function."""
+
+    def test_valid_boolean_string(self):
+        self.assertTrue(string_to_bool("Y"))
+        self.assertTrue(string_to_bool("y"))
+        self.assertFalse(string_to_bool("N"))
+        self.assertFalse(string_to_bool("n"))
+
+    def test_string_with_spaces(self):
+        self.assertTrue(string_to_bool(" Y "))
+        self.assertFalse(string_to_bool("  n"))
+
+    def test_empty_string(self):
+        with self.assertRaises(ValueError):
+            string_to_bool("")
+
+    def test_non_string_input(self):
+        with self.assertRaises(ValueError):
+            string_to_bool(123)
+        with self.assertRaises(ValueError):
+            string_to_bool(None)
+        with self.assertRaises(ValueError):
+            string_to_bool(["True"])
+        with self.assertRaises(ValueError):
+            string_to_bool(True)
+
+    def test_string_length_more_than_one(self):
+        with self.assertRaises(ValueError):
+            string_to_bool("Yes")
+        with self.assertRaises(ValueError):
+            string_to_bool("No")
+
+    def test_invalid_single_character_string(self):
+        with self.assertRaises(ValueError):
+            string_to_bool("A")
+        with self.assertRaises(ValueError):
+            string_to_bool("Z")
+
+
+class TestStringToInt(SimpleTestCase):
+    """Tests for the string_to_int function."""
+
+    def test_valid_integer_strings(self):
+        self.assertEqual(string_to_int("123"), 123)
+        self.assertEqual(string_to_int("-456"), -456)
+        self.assertEqual(string_to_int("   789   "), 789)
+
+    def test_invalid_strings(self):
+        with self.assertRaises(ValueError):
+            string_to_int("abc")
+        with self.assertRaises(ValueError):
+            string_to_int("123abc")
+        with self.assertRaises(ValueError):
+            string_to_int("12.34")
+
+    def test_empty_string(self):
+        with self.assertRaises(ValueError):
+            string_to_int("")
+
+    def test_non_string_input(self):
+        with self.assertRaises(ValueError):
+            string_to_int(123)
+        with self.assertRaises(ValueError):
+            string_to_int(None)
+        with self.assertRaises(ValueError):
+            string_to_int([123])
+
+
+class TestStringToString(SimpleTestCase):
+    def test_valid_strings(self):
+        self.assertEqual(string_to_string("hello"), "hello")
+        self.assertEqual(string_to_string("  world  "), "world")
+        self.assertEqual(string_to_string("  space both sides  "), "space both sides")
+
+    def test_none_input(self):
+        self.assertEqual(string_to_string(None), "")
+
+    def test_non_string_input(self):
+        with self.assertRaises(ValueError):
+            string_to_string(123)
+        with self.assertRaises(ValueError):
+            string_to_string(True)
+        with self.assertRaises(ValueError):
+            string_to_string([1, 2, 3])
+
+    def test_string_with_only_spaces(self):
+        self.assertEqual(string_to_string("     "), "")
+
+
+class TestNormalizeYearString(SimpleTestCase):
+    def test_valid_short_year(self):
+        self.assertEqual(normalize_year_string_or_exit("21"), "2021")
+        self.assertEqual(normalize_year_string_or_exit("16"), "2016")
+
+    def test_valid_full_year(self):
+        self.assertEqual(normalize_year_string_or_exit("2018"), "2018")
+
+    def test_invalid_year_string(self):
+        with self.assertRaises(SystemExit):
+            normalize_year_string_or_exit("invalid")
+
+    def test_year_out_of_range(self):
+        with self.assertRaises(SystemExit):
+            normalize_year_string_or_exit("15")
+        with self.assertRaises(SystemExit):
+            normalize_year_string_or_exit("2023")
---
+++ backend/census_historical_migration/test_general_information_xforms.py
@@ -0,0 +1,212 @@
+from datetime import datetime, timedelta
+from django.test import SimpleTestCase
+
+from .sac_general_lib.general_information import (
+    AUDIT_TYPE_DICT,
+    PERIOD_DICT,
+    xform_audit_period_covered,
+    xform_audit_type,
+    xform_auditee_fiscal_period_end,
+    xform_auditee_fiscal_period_start,
+    xform_country,
+    xform_entity_type,
+)
+
+
+from .exception_utils import DataMigrationError
+
+
+class TestXformEntityType(SimpleTestCase):
+    def test_valid_phrases(self):
+        """Test that the function returns the correct results when given valid phrases."""
+        self.assertEqual(
+            xform_entity_type("institution of higher education"), "higher-ed"
+        )
+        self.assertEqual(xform_entity_type("nonprofit"), "non-profit")
+        self.assertEqual(xform_entity_type("Non-Profit"), "non-profit")
+        self.assertEqual(xform_entity_type("local government"), "local")
+        self.assertEqual(xform_entity_type("state"), "state")
+        self.assertEqual(xform_entity_type("unknown"), "unknown")
+        self.assertEqual(xform_entity_type("tribe"), "tribal")
+        self.assertEqual(xform_entity_type("tribal"), "tribal")
+
+    def test_valid_phrases_with_extra_whitespace(self):
+        """Test that the function returns the correct results when given valid phrases with extra whitespace."""
+        self.assertEqual(
+            xform_entity_type("institution of higher education "), "higher-ed"
+        )
+        self.assertEqual(xform_entity_type(" nonprofit "), "non-profit")
+        self.assertEqual(xform_entity_type("Non-Profit "), "non-profit")
+        self.assertEqual(xform_entity_type(" local government "), "local")
+        self.assertEqual(xform_entity_type(" state   "), "state")
+
+    def test_valid_phrases_with_extra_whitespace_with_capitalization(self):
+        """Test that the function returns the correct results when given valid phrases with extra whitespace and upper cases."""
+        self.assertEqual(
+            xform_entity_type("Institution of Higher Education "), "higher-ed"
+        )
+        self.assertEqual(xform_entity_type(" Nonprofit "), "non-profit")
+        self.assertEqual(xform_entity_type("Non-Profit "), "non-profit")
+        self.assertEqual(xform_entity_type(" LOCAL Government "), "local")
+        self.assertEqual(xform_entity_type(" STATE   "), "state")
+        self.assertEqual(xform_entity_type(" Tribal   "), "tribal")
+
+    def test_valid_phrases_with_extra_whitespace_with_extra_words_with_capitalization(
+        self,
+    ):
+        """Test that the function returns the correct results when given valid phrases with extra whitespace and extra words."""
+        self.assertEqual(
+            xform_entity_type("Institution of Higher Education (IHE) and Research"),
+            "higher-ed",
+        )
+
+        self.assertEqual(
+            xform_entity_type(" LOCAL GOVERNMENT AND AGENCIES "),
+            "local",
+        )
+        self.assertEqual(
+            xform_entity_type(" STATE and Non-Governmental Organization   "), "state"
+        )
+        self.assertEqual(
+            xform_entity_type(" Indian tribe or Tribal  Organization   "), "tribal"
+        )
+
+    def test_invalid_phrase(self):
+        """Test that the function raises an exception when given an invalid phrase."""
+        with self.assertRaises(DataMigrationError):
+            xform_entity_type("business")
+
+    def test_empty_string(self):
+        """Test that the function raises an exception when given an empty string."""
+        with self.assertRaises(DataMigrationError):
+            xform_entity_type("")
+
+    def test_none_input(self):
+        """Test that the function raises an exception when given None."""
+        with self.assertRaises(DataMigrationError):
+            xform_entity_type(None)
+
+
+class TestXformCountry(SimpleTestCase):
+    class MockAuditHeader:
+        def __init__(self, CPASTATE):
+            self.CPASTATE = CPASTATE
+
+    def setUp(self):
+        self.general_information = {
+            "auditor_country": "",
+        }
+        self.audit_header = self.MockAuditHeader("")
+
+    def test_when_auditor_country_set_to_us(self):
+        """Test that the function returns the correct results when the auditor country is set to US."""
+        self.general_information["auditor_country"] = "US"
+        result = xform_country(self.general_information, self.audit_header)
+        self.assertEqual(result["auditor_country"], "USA")
+
+    def test_when_auditor_country_set_to_usa(self):
+        """Test that the function returns the correct results when the auditor country is set to USA."""
+        self.general_information["auditor_country"] = "USA"
+        result = xform_country(self.general_information, self.audit_header)
+        self.assertEqual(result["auditor_country"], "USA")
+
+    def test_when_auditor_country_set_to_empty_string_and_auditor_state_valid(self):
+        """Test that the function returns the correct results when the auditor country is set to an empty string."""
+        self.general_information["auditor_country"] = ""
+        self.audit_header.CPASTATE = "MA"
+        result = xform_country(self.general_information, self.audit_header)
+        self.assertEqual(result["auditor_country"], "USA")
+
+    def test_when_auditor_country_set_to_empty_string_and_auditor_state_invalid(self):
+        """Test that the function raises an exception when the auditor country is set to an empty string and the auditor state is invalid."""
+        self.general_information["auditor_country"] = ""
+        self.audit_header.CPASTATE = "XX"
+        with self.assertRaises(DataMigrationError):
+            xform_country(self.general_information, self.audit_header)
+
+
+class TestXformAuditeeFiscalPeriodEnd(SimpleTestCase):
+    def setUp(self):
+        self.general_information = {
+            "auditee_fiscal_period_end": "01/31/2021 00:00:00",
+        }
+
+    def test_when_auditee_fiscal_period_end_is_valid(self):
+        """Test that the function returns the correct results when the fiscal period end is valid."""
+        result = xform_auditee_fiscal_period_end(self.general_information)
+        self.assertEqual(result["auditee_fiscal_period_end"], "2021-01-31")
+
+    def test_when_auditee_fiscal_period_end_is_invalid(self):
+        """Test that the function raises an exception when the fiscal period end is invalid."""
+        self.general_information["auditee_fiscal_period_end"] = "01/31/2021"
+        with self.assertRaises(ValueError):
+            xform_auditee_fiscal_period_end(self.general_information)
+        self.general_information["auditee_fiscal_period_end"] = ""
+        with self.assertRaises(DataMigrationError):
+            xform_auditee_fiscal_period_end(self.general_information)
+
+
+class TestXformAuditeeFiscalPeriodStart(SimpleTestCase):
+    def setUp(self):
+        self.general_information = {
+            "auditee_fiscal_period_end": "01/31/2021 00:00:00",
+        }
+
+    def test_when_auditee_fiscal_period_end_is_valid(self):
+        """Test that the function returns the correct results when the fiscal period end is valid."""
+        result = xform_auditee_fiscal_period_start(self.general_information)
+        expected_date = (
+            datetime.strptime(
+                self.general_information["auditee_fiscal_period_end"],
+                "%m/%d/%Y %H:%M:%S",
+            )
+            - timedelta(days=365)
+        ).strftime("%Y-%m-%d")
+        self.assertEqual(result["auditee_fiscal_period_start"], expected_date)
+
+    def test_when_auditee_fiscal_period_end_is_invalid(self):
+        """Test that the function raises an exception when the fiscal period end is invalid."""
+        self.general_information["auditee_fiscal_period_end"] = "01/31/2021"
+        with self.assertRaises(ValueError):
+            xform_auditee_fiscal_period_start(self.general_information)
+        self.general_information["auditee_fiscal_period_end"] = ""
+        with self.assertRaises(ValueError):
+            xform_auditee_fiscal_period_start(self.general_information)
+
+
+class TestXformAuditPeriodCovered(SimpleTestCase):
+    def test_valid_period(self):
+        for key, value in PERIOD_DICT.items():
+            with self.subTest(key=key):
+                general_information = {"audit_period_covered": key}
+                result = xform_audit_period_covered(general_information)
+                self.assertEqual(result["audit_period_covered"], value)
+
+    def test_invalid_period(self):
+        general_information = {"audit_period_covered": "invalid_key"}
+        with self.assertRaises(DataMigrationError):
+            xform_audit_period_covered(general_information)
+
+    def test_missing_period(self):
+        general_information = {}
+        with self.assertRaises(DataMigrationError):
+            xform_audit_period_covered(general_information)
+
+
+class TestXformAuditType(SimpleTestCase):
+    def test_valid_audit_type(self):
+        for key, value in AUDIT_TYPE_DICT.items():
+            with self.subTest(key=key):
+                general_information = {"audit_type": key}
+                result = xform_audit_type(general_information)
+                self.assertEqual(result["audit_type"], value)
+
+    def test_invalid_audit_type(self):
+        general_information = {"audit_type": "invalid_key"}
+        with self.assertRaises(DataMigrationError):
+            xform_audit_type(general_information)
+
+    def test_missing_audit_type(self):
+        general_information = {}
+        with self.assertRaises(DataMigrationError):
+            xform_audit_type(general_information)
