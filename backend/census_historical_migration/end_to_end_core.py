d6ddb27375e7463890e38d8ffc21ac99a186e0a3
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2023-12-22 12:31:46 -0500

    # Bug fix

--- backend/census_historical_migration/end_to_end_core.py
+++ backend/census_historical_migration/end_to_end_core.py
@@ -51,18 +51,16 @@ parser = argparse.ArgumentParser()
 
 
 def step_through_certifications(sac):
-    stati = [
-        SingleAuditChecklist.STATUS.IN_PROGRESS,
-        SingleAuditChecklist.STATUS.READY_FOR_CERTIFICATION,
-        SingleAuditChecklist.STATUS.AUDITOR_CERTIFIED,
-        SingleAuditChecklist.STATUS.AUDITEE_CERTIFIED,
-        SingleAuditChecklist.STATUS.CERTIFIED,
-        SingleAuditChecklist.STATUS.SUBMITTED,
-        SingleAuditChecklist.STATUS.DISSEMINATED,
-    ]
-    for status in stati:
-        sac.transition_name.append(status)
-        sac.transition_date.append(datetime.now(timezone.utc))
+    sac.transition_to_ready_for_certification()
+    sac.transition_to_auditor_certified()
+    sac.transition_to_auditee_certified()
+
+    # FIXME-MSHD: We have no method transition_to_certified()
+    sac.transition_name.append(SingleAuditChecklist.STATUS.CERTIFIED)
+    sac.transition_date.append(datetime.now(timezone.utc))
+
+    sac.transition_to_submitted()
+    sac.transition_to_disseminated()
     sac.save()
 
 
