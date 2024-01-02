4b1194195b01277cc6ba5ae76a1cb112b2d24c44
James Person <jperson1@umbc.edu>
2024-01-02 12:07:26 -0500

    Await the upload response and the redirected page.

--- backend/cypress/support/workbook-uploads.js
+++ backend/cypress/support/workbook-uploads.js
@@ -1,20 +1,35 @@
-// re-usable code for workbook uploads
-
-// testWorkbookUpload('/audit/excel/federal-awards-expended/*', '#file-input-federal-awards-xlsx', 'federal-awards-expended-UPDATE.xlsx')
-// assumes you are on the appropriate upload page already
+/**
+ * Tests an upload page. Assumes Cypress is on the appropriate upload page already.
+ * 1. Define the requests to watch (intercepts)
+ *   "uploadSuccess" - The file upload endpoint. On success, returns a 302 redirect to the homepage.
+ *   "uploadRedirect" - The post-upload 302 redirects to the homepage '/', which then directs to '/audit/'.
+ *   The first intercept cannot follow the redirect, so we watch for both requests. See https://github.com/cypress-io/cypress/issues/24700
+ * 2. Attach a file to upload.
+ * 3. Wait for the upload to complete by waiting for the requests to recieve proper responses.
+ * 4. Ensure the information box updated correctly.
+ * 5. Navigate through to the submission checklist.
+ *
+ * @param {string} interceptUrl The url for the file upload endpoint.
+ * @param {string} uploadSelector The name of the fle upload input element.
+ * @param {string} filename The path and name of the test file to upload.
+ * @param {boolean} will_intercept Whether or not the function will mock the upload.
+ * @return {null} No return value.
+ */
 function testWorkbookUpload(interceptUrl, uploadSelector, filename, will_intercept = true) {
-    cy.intercept(interceptUrl + '*', (req) => {
-      if (will_intercept) {
-        // return a success fixture
-        req.reply({ fixture: 'success-res.json' });
-      } else {
-        // with no intercept, don't intervene
-        req.continue();
-      }
-    }).as('uploadSuccess');
+  cy.intercept(interceptUrl + '*', (req) => {
+    if (will_intercept) {
+      req.reply({ fixture: 'success-res.json' });
+    } else {
+      req.continue();
+    }
+  }).as('uploadSuccess');
+  cy.intercept('/audit/').as('uploadRedirect');
+
   cy.get(uploadSelector).attachFile(filename);
-  // Upload url (POST /audit/excel/workbookname) returns a redirect to "/" on successful upload. So, 302.
-  cy.wait('@uploadSuccess').its('response.statusCode').should('eq', 302);  
+
+  cy.wait('@uploadSuccess').its('response.statusCode').should('eq', 302);
+  cy.wait('@uploadRedirect').its('response.statusCode').should('eq', 200);
+  
   cy.get('#info_box')
     .should(
       'have.text',
@@ -25,6 +40,12 @@ function testWorkbookUpload(interceptUrl, uploadSelector, filename, will_interce
   cy.url().should('match', /\/audit\/submission-progress\/[0-9]{4}-[0-9]{2}-GSAFAC-[0-9]{10}/);
 }
 
+/**
+ * Calls testWorkbookUpload with the appropriate static names - upload URL, file input element name, and sample workbook. 
+ * 
+ * @param {boolean} will_intercept Whether or not testWorkbookUpload function will mock the upload. Defaults to true.
+ * @return {null} No return value.
+ */
 export function testWorkbookFederalAwards(will_intercept = true) {
   testWorkbookUpload(
     '/audit/excel/federal-awards-expended/*',
