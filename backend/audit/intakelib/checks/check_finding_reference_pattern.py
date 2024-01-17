7b26da1624c3743eebe3cfe8c92b76cf32c65f30
Daniel Swick <2365503+danswick@users.noreply.github.com>
2024-01-17 15:38:34 -0800

    add validation and should-fail workbook

---
+++ backend/audit/intakelib/checks/check_finding_reference_pattern.py
@@ -0,0 +1,44 @@
+import logging
+from django.core.exceptions import ValidationError
+import re
+from audit.intakelib.intermediate_representation import (
+    get_range_by_name,
+)
+from audit.intakelib.common import (
+    get_message,
+    build_cell_error_tuple,
+    appears_empty,
+    is_value_marked_na,
+)
+
+logger = logging.getLogger(__name__)
+
+# A version of these regexes also exists in Base.libsonnet
+FINDING_REFERENCE_REGEX = r"^20[1-9][0-9]-[0-9]{3}(,\s*20[1-9][0-9]-[0-9]{3})*$"
+
+
+# DESCRIPTION
+# Prior references should be a comma separated list of 20##-### values
+# TESTED BY
+# has_bad_references.xlsx
+def finding_reference_pattern(ir):
+    references = get_range_by_name(ir, "finding_references")
+    errors = []
+    for index, prior_reference in enumerate(references["values"]):
+        if (
+            not appears_empty(prior_reference)
+            and (not is_value_marked_na(prior_reference))
+            and (not re.match(FINDING_REFERENCE_REGEX, str(prior_reference)))
+        ):
+            errors.append(
+                build_cell_error_tuple(
+                    ir,
+                    references,
+                    index,
+                    get_message("finding_references_invalid"),
+                )
+            )
+
+    if len(errors) > 0:
+        logger.info("Raising a validation error.")
+        raise ValidationError(errors)
