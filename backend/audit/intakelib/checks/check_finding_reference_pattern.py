1b1c29dc5ea4f7bb030d93319c2a307b4fadb3a0
Daniel Swick <2365503+danswick@users.noreply.github.com>
2024-01-17 15:40:19 -0800

    remove some stray priors

--- backend/audit/intakelib/checks/check_finding_reference_pattern.py
+++ backend/audit/intakelib/checks/check_finding_reference_pattern.py
@@ -18,17 +18,18 @@ FINDING_REFERENCE_REGEX = r"^20[1-9][0-9]-[0-9]{3}(,\s*20[1-9][0-9]-[0-9]{3})*$"
 
 
 # DESCRIPTION
-# Prior references should be a comma separated list of 20##-### values
+# Finding references should be in 20##-### format where the first four
+# digits are a year >= 2010.
 # TESTED BY
 # has_bad_references.xlsx
 def finding_reference_pattern(ir):
     references = get_range_by_name(ir, "finding_references")
     errors = []
-    for index, prior_reference in enumerate(references["values"]):
+    for index, reference in enumerate(references["values"]):
         if (
-            not appears_empty(prior_reference)
-            and (not is_value_marked_na(prior_reference))
-            and (not re.match(FINDING_REFERENCE_REGEX, str(prior_reference)))
+            not appears_empty(reference)
+            and (not is_value_marked_na(reference))
+            and (not re.match(FINDING_REFERENCE_REGEX, str(reference)))
         ):
             errors.append(
                 build_cell_error_tuple(
