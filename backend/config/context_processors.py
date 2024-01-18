194a707e2d88ae657861191641e64c49883cbc59
James Person <jperson1@umbc.edu>
2024-01-18 15:59:45 -0500

    Test site banner, test implementation

--- backend/config/context_processors.py
+++ backend/config/context_processors.py
@@ -15,3 +15,10 @@ def omb_num_exp_date(request):
     Displayed as a legal requirement on the header of every page.
     """
     return {"OMB_NUMBER": settings.OMB_NUMBER, "OMB_EXP_DATE": settings.OMB_EXP_DATE}
+
+def current_environment(request):
+    """
+    Returns the OMB_NUMBER (str) and OMB_EXP_DATE (str) in template context form.
+    Displayed as a legal requirement on the header of every page.
+    """
+    return {"ENVIRONMENT": settings.ENVIRONMENT}
