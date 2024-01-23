9f6fc1148e5164b2d32c48b79d07d3089e5be9f8
James Person <jperson1@umbc.edu>
2024-01-23 13:42:42 -0500

    Linting to appease the lint gods.

--- backend/config/context_processors.py
+++ backend/config/context_processors.py
@@ -16,6 +16,7 @@ def omb_num_exp_date(request):
     """
     return {"OMB_NUMBER": settings.OMB_NUMBER, "OMB_EXP_DATE": settings.OMB_EXP_DATE}
 
+
 def current_environment(request):
     """
     Returns the OMB_NUMBER (str) and OMB_EXP_DATE (str) in template context form.
