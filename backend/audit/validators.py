11a267d2049910623cca0db200c8354eb6f0d494
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2024-01-26 14:42:33 -0500

    Using getattr to fix tests

--- backend/audit/validators.py
+++ backend/audit/validators.py
@@ -224,8 +224,8 @@ def validate_general_information_json(value, is_data_migration=True):
 
     try:
         if not is_data_migration and settings.GSA_MIGRATION in [
-            value["auditee_email"],
-            value["auditor_email"],
+            getattr(value, "auditee_email", ""),
+            getattr(value, "auditor_email", ""),
         ]:
             raise JSONSchemaValidationError(
                 f"{settings.GSA_MIGRATION} not permitted outside of migrations"
