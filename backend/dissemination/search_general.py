004015785b8492c77aac3954eecd6cb8cddcd183
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2024-01-26 14:18:15 -0800

    Don't create dict as default argument.

--- backend/dissemination/search_general.py
+++ backend/dissemination/search_general.py
@@ -7,7 +7,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def search_general(params={}):
+def search_general(params=None):
+    params = params or {}
     # Time general reduction
     t0 = time.time()
 
