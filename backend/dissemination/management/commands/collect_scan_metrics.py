8ed1587a3f1d5f1e8c013fff67b9014ea0b9f2f2
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2024-02-22 12:24:01 -0500

    Running scans in parallel

--- backend/dissemination/management/commands/collect_scan_metrics.py
+++ backend/dissemination/management/commands/collect_scan_metrics.py
@@ -3,6 +3,7 @@ import logging
 import requests
 import time
 from io import BytesIO
+from concurrent.futures import ThreadPoolExecutor
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -37,52 +38,27 @@ def _scan_file(file, filepath):
         raise ClamAVError(filepath)
 
 
-def scan_file(file, filepath):
+def scan_file(filepath):
     try:
-        return _scan_file(file, filepath)
+        with open(filepath, "rb") as fh:
+            file = BytesIO(fh.read())
+
+        t1 = time.perf_counter(), time.process_time()
+        _scan_file(file, filepath)
+        t2 = time.perf_counter(), time.process_time()
+
+        return t2[0] - t1[0]
     except Exception as e:
         logger.error(f"SCAN SCAN_FILE {e}")
 
 
-def scan_files_at_path(path, num_to_scan):
-    good_count, bad_count = 0, 0
-    num_scanned = 0
-    total_real_time = 0
-
-    filepaths = glob.glob(path + '*')
+def scan_files_at_path(path, num_to_scan, max_workers):
+    filepaths = glob.glob(path + '*')[:num_to_scan]
     if not filepaths:
         raise Exception(f"No files found at {path}")
 
-    while num_scanned < num_to_scan:
-        # Short circuits scans when we have more files than needed
-        filepaths_to_scan = filepaths[:num_to_scan-num_scanned]
-
-        for filepath in filepaths_to_scan:
-            with open(filepath, "rb") as fh:
-                file = BytesIO(fh.read())
-
-            t1 = time.perf_counter(), time.process_time()
-
-            result = scan_file(file, filepath)
-
-            t2 = time.perf_counter(), time.process_time()
-            real_time = t2[0] - t1[0]
-            total_real_time += real_time
-
-            logger.info(f"File real time: {real_time}")
-
-            if not check_scan_ok(result):
-                logger.error("SCAN revealed potential infection, %s", result)
-                bad_count += 1
-            else:
-                good_count += 1
-
-            num_scanned = good_count + bad_count
-
-    logger.info(f" Real time: {total_real_time} seconds")
-    logger.info(f" Real time average: {total_real_time / num_to_scan} seconds")
-
-    return {"good_count": good_count, "bad_count": bad_count}
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        return list(executor.map(scan_file, filepaths))
 
 
 def is_stringlike(o):
@@ -112,15 +88,18 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--path", type=str, required=True, default=None)
         parser.add_argument("--num_to_scan", type=int, required=False, default=1)
+        parser.add_argument("--num_workers", type=int, required=False, default=1)
         pass
 
     def handle(self, *args, **options):
         path = options["path"]
         num_to_scan = options["num_to_scan"]
-        results = scan_files_at_path(path, num_to_scan)
+        num_workers = options["num_workers"]
 
-        logger.info(
-            "SCAN OK: COUNT passed: %s, failed: %s",
-            results["good_count"],
-            results["bad_count"],
-        )
+        results = scan_files_at_path(path, num_to_scan, num_workers)
+        logger.info(f"Num files: {num_to_scan}")
+        logger.info(f"Num workers: {num_workers}")
+        logger.info(f"Total time: {sum(results)} seconds")
+        logger.info(f"Max time: {max(results)} seconds")
+        logger.info(f"Avg time: {sum(results) / len(results)} seconds")
+
