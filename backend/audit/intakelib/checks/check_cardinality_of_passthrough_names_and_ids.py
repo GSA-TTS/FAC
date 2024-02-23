29416c323a6ee1aea7c881878cf926e76fd75e3c
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2024-02-22 19:09:04 -0500

    #2505 Bug fix

--- backend/audit/intakelib/checks/check_cardinality_of_passthrough_names_and_ids.py
+++ backend/audit/intakelib/checks/check_cardinality_of_passthrough_names_and_ids.py
@@ -11,19 +11,43 @@ logger = logging.getLogger(__name__)
 # If users provide a `|` in the passthrough id number field,
 # we expect multiple names in the name field, too.
 def cardinality_of_passthrough_names_and_ids(ir):
-    passthrough_name = get_range_by_name(ir, "passthrough_name")
-    passthrough_identifying_number = get_range_by_name(
+    passthrough_names = get_range_by_name(ir, "passthrough_name").get("values")
+    passthrough_identifying_numbers = get_range_by_name(
         ir, "passthrough_identifying_number"
-    )
-
+    ).get("values")
+    # This is needed to avoid None values to bypass the check
+    passthrough_names = [value if value else "" for value in passthrough_names]
+    passthrough_identifying_numbers = [
+        value if value else "" for value in passthrough_identifying_numbers
+    ]
     errors = []
 
     for index, (pass_name, pass_id) in enumerate(
-        zip(passthrough_name["values"], passthrough_identifying_number["values"])
+        zip(passthrough_names, passthrough_identifying_numbers)
     ):
-        if (pass_name and pass_id) and ("|" in pass_name or "|" in pass_id):
-            names = pass_name.split("|")
-            ids = pass_id.split("|")
+        if (pass_name or pass_id) and ("|" in pass_name or "|" in pass_id):
+            names = [value.strip() for value in pass_name.split("|")]
+            ids = [value.strip() for value in pass_id.split("|")]
+            if "" in names:
+                errors.append(
+                    build_cell_error_tuple(
+                        ir,
+                        get_range_by_name(ir, "passthrough_name"),
+                        index,
+                        get_message("check_blank_space_in_passthrough_names"),
+                    )
+                )
+            if "" in ids:
+                errors.append(
+                    build_cell_error_tuple(
+                        ir,
+                        get_range_by_name(ir, "passthrough_identifying_number"),
+                        index,
+                        get_message(
+                            "check_blank_space_in_passthrough_identifying_numbers"
+                        ),
+                    )
+                )
             # print(names, ids)
             if len(names) != len(ids):
                 errors.append(
