8410b96437aa11309736fed4ece8f89ef32476e7
Edward Zapata <edward.zapata@gsa.gov>
2024-02-26 12:33:53 -0500

    updated test that checks that unlock cert btn works

--- backend/cypress/support/unlock-cert.js
+++ backend/cypress/support/unlock-cert.js
@@ -1,10 +1,15 @@
+import { testCrossValidation } from "./cross-validation";
 
 // test unlock certification
 export function testUnlock(){
-  cy.get(".usa-button").contains("Unlock").click();
+  cy.get('.usa-button').contains('Unlock').click();
+  cy.get('#continue').contains('Unlock submission').click();
   cy.get('img[alt="FAC.gov"]').should('exist').click();
   cy.url().should('match', /\/audit\//);
-  cy.get('table.usa-table.margin-top-0.pa11y-ignore tbody > tr:last-child').should('contain', 'Unlock')
-  cy.contains('Ready for Certification').click();
+  cy.get('table.usa-table.margin-top-0.pa11y-ignore tbody > tr:last-child .usa-link').contains('In Progress').click();
   cy.url().should('match', /\/audit\/submission-progress\/[0-9]{4}-[0-9]{2}-GSAFAC-[0-9]{10}/);
+  cy.then(() => {
+    cy.get(".usa-link").contains("Pre-submission validation").click();
+    testCrossValidation();
+  });
 }
