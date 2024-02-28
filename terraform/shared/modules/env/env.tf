740ac9c228d3267828e93aca2b7d6e12ddcfa531
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-02-28 03:58:07 -0500

    rollback

--- terraform/shared/modules/env/env.tf
+++ terraform/shared/modules/env/env.tf
@@ -1,12 +1,9 @@
 locals {
   module_versions = {
-    database = "^0.8.0",
-    s3       = "^0.8.0",
-    clamav   = "^0.8.0"
+    database = "^0.x", # major version 0
+    s3       = "^0.x", # major version 0
+    clamav   = "^0.x"  # major version 0
   }
-  database_version = "github.com/18f/terraform-cloudgov//database?ref=v${module.version["database"].target_version}"
-  s3_version       = "github.com/18f/terraform-cloudgov//s3?ref=v${module.version["s3"].target_version}"
-  clamav_version   = "github.com/18f/terraform-cloudgov//clamav?ref=v${module.version["clamav"].target_version}"
 }
 
 module "version" {
@@ -16,7 +13,7 @@ module "version" {
 }
 
 module "database" {
-  source = local.database_version
+  source = "github.com/18f/terraform-cloudgov//database?ref=v${module.version["database"].target_version}"
 
   cf_org_name      = var.cf_org_name
   cf_space_name    = var.cf_space_name
@@ -27,7 +24,7 @@ module "database" {
 }
 
 module "s3-public" {
-  source = local.s3_version
+  source = "github.com/18f/terraform-cloudgov//s3?ref=v${module.version["s3"].target_version}"
 
   cf_org_name      = var.cf_org_name
   cf_space_name    = var.cf_space_name
@@ -38,7 +35,7 @@ module "s3-public" {
 }
 
 module "s3-private" {
-  source = local.s3_version
+  source = "github.com/18f/terraform-cloudgov//s3?ref=v${module.version["s3"].target_version}"
 
   cf_org_name      = var.cf_org_name
   cf_space_name    = var.cf_space_name
