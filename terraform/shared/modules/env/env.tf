8f95df928bd58107aa3389e5e0aa21367ed7c3f3
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-02-28 04:25:46 -0500

    comment out semver

--- terraform/shared/modules/env/env.tf
+++ terraform/shared/modules/env/env.tf
@@ -1,16 +1,16 @@
-locals {
-  module_versions = {
-    database = "^0.x", # major version 0
-    s3       = "^0.x", # major version 0
-    clamav   = "^0.x"  # major version 0
-  }
-}
+# locals {
+#   module_versions = {
+#     database = "^0.x", # major version 0
+#     s3       = "^0.x", # major version 0
+#     clamav   = "^0.x"  # major version 0
+#   }
+# }
 
-module "version" {
-  for_each           = local.module_versions
-  source             = "github.com/18f/terraform-cloudgov//semver"
-  version_constraint = each.value
-}
+# module "version" {
+#   for_each           = local.module_versions
+#   source             = "github.com/18f/terraform-cloudgov//semver"
+#   version_constraint = each.value
+# }
 
 module "database" {
   source = "github.com/18f/terraform-cloudgov//database?ref=v0.8.0"
