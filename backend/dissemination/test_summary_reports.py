998e8b6c084f16b916565d2c54a2322a391c3828
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2024-03-01 15:38:45 -0500

    Comments

--- backend/dissemination/test_summary_reports.py
+++ backend/dissemination/test_summary_reports.py
@@ -16,6 +16,7 @@ import openpyxl as pyxl
 
 class SummaryReportTests(TestCase):
     def test_generate_summary_report_returns_filename(self):
+        """The filename returned should be correctly formatted"""
         general = baker.make(General, _quantity=100)
         report_ids = [g.report_id for g in general]
         filename = generate_summary_report(report_ids)
@@ -24,6 +25,7 @@ class SummaryReportTests(TestCase):
         self.assertTrue(filename.endswith, ".xlsx")
 
     def test_get_tribal_report_ids(self):
+        """The report_ids returned should only belong to tribal audits"""
         public_general = baker.make(General, _quantity=3, is_public=True)
         tribal_general = baker.make(General, _quantity=2, is_public=False)
         public_report_ids = [g.report_id for g in public_general]
@@ -35,6 +37,7 @@ class SummaryReportTests(TestCase):
         )
 
     def test_get_tribal_report_ids_no_tribal(self):
+        """No report_ids should be returned for entirely public audits"""
         public_general = baker.make(General, _quantity=3, is_public=True)
         public_report_ids = [g.report_id for g in public_general]
 
@@ -44,15 +47,17 @@ class SummaryReportTests(TestCase):
         )
 
     def test_get_tribal_report_ids_no_public(self):
+        """report_ids returned should be the same as those given if they're all tribal"""
         tribal_general = baker.make(General, _quantity=2, is_public=False)
         tribal_report_ids = [g.report_id for g in tribal_general]
 
-        self.assertEqual(
-            len(_get_tribal_report_ids(tribal_report_ids)),
-            2,
+        self.assertListEqual(
+            _get_tribal_report_ids(tribal_report_ids),
+            tribal_report_ids,
         )
 
     def test_insert_dissem_coversheet_tribal_can_read(self):
+        """Coversheet should have correct disclaimer for tribal data that can be read"""
         workbook = pyxl.Workbook()
         insert_dissem_coversheet(workbook, True, True)
         coversheet = workbook.get_sheet_by_name("Coversheet")
@@ -60,6 +65,7 @@ class SummaryReportTests(TestCase):
         self.assertEqual(coversheet["B3"].value, can_read_tribal_disclaimer)
 
     def test_insert_dissem_coversheet_tribal_cannot_read(self):
+        """Coversheet should have correct disclaimer for tribal data that cannot be read"""
         workbook = pyxl.Workbook()
         insert_dissem_coversheet(workbook, True, False)
         coversheet = workbook.get_sheet_by_name("Coversheet")
@@ -67,6 +73,7 @@ class SummaryReportTests(TestCase):
         self.assertEqual(coversheet["B3"].value, cannot_read_tribal_disclaimer)
 
     def test_insert_dissem_coversheet_public(self):
+        """Coversheet should have no disclaimer for tribal data if all data is public"""
         workbook = pyxl.Workbook()
         insert_dissem_coversheet(workbook, False, False)
         coversheet = workbook.get_sheet_by_name("Coversheet")
@@ -74,6 +81,10 @@ class SummaryReportTests(TestCase):
         self.assertEqual(coversheet["B3"].value, None)
 
     def _test_gather_report_data_dissemination_helper(self, include_private):
+        """
+        Helper to create public and tribal audits, generate a summary, and check the
+        correct tribal data is omitted
+        """
         # Create public audit with relevant sections
         public_general = baker.make(General, is_public=True)
         public_report_ids = [public_general.report_id]
@@ -111,7 +122,9 @@ class SummaryReportTests(TestCase):
             self.assertEqual(found_tribal, include_private)
 
     def test_gather_report_data_dissemination_exclude_private(self):
+        """Summaries with tribal data and no access"""
         self._test_gather_report_data_dissemination_helper(False)
 
     def test_gather_report_data_dissemination_include_private(self):
+        """Summaries with tribal data and access"""
         self._test_gather_report_data_dissemination_helper(True)
