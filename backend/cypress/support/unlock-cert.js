7714aa8ae57ba8c455798afddab9d60a22e25700
Edward Zapata <99770283+eazapata23@users.noreply.github.com>
2024-03-12 11:35:52 -0400

    modified unlock-cert.js code to grab report id + in progress from table

--- backend/cypress/support/unlock-cert.js
+++ backend/cypress/support/unlock-cert.js
@@ -2,14 +2,20 @@ import { testCrossValidation } from "./cross-validation";
 
 // test unlock certification
 export function testUnlock(){
+  cy.url().then(url => {
+    const reportId = url.split('/').pop();
   cy.get('.usa-button').contains('Unlock').click();
   cy.get('#continue').contains('Unlock submission').click();
   cy.get('img[alt="FAC.gov"]').should('exist').click();
   cy.url().should('match', /\/audit\//);
-  cy.get('table.usa-table.margin-top-0.pa11y-ignore tbody > tr:last-child .usa-link').contains('In Progress').click();
+  cy.contains('td', `${reportId}`)
+  .siblings()
+  .contains('td', 'In Progress')
+  .click()
   cy.url().should('match', /\/audit\/submission-progress\/[0-9]{4}-[0-9]{2}-GSAFAC-[0-9]{10}/);
   cy.then(() => {
     cy.get(".usa-link").contains("Pre-submission validation").click();
     testCrossValidation();
+    });
   });
 }
