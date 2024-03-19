afa685a60b5b4a0f9f2338c7aeca7c5077e4bf65
Tadhg O'Higgins <2626258+tadhg-ohiggins@users.noreply.github.com>
2024-03-19 16:39:16 -0700

    Linter appeasement.

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,8 +1,10 @@
-from psycogreen.gevent import patch_psycopg     # use this if you use gevent workers
+from psycogreen.gevent import patch_psycopg  # use this if you use gevent workers
+
 
 def post_fork(server, worker):
     patch_psycopg()
     worker.log.info("Made Psycopg2 Green")
+
 
 max_requests = 500
 max_requests_jitter = 50
