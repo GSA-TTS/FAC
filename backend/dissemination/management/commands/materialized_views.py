feec4bbf4ba487a2dadb357e83d946f29a9744d8
Daniel Swick <2365503+danswick@users.noreply.github.com>
2024-03-22 13:41:58 -0700

    lint

--- backend/dissemination/management/commands/materialized_views.py
+++ backend/dissemination/management/commands/materialized_views.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from dissemination import api_versions
 
+
 class Command(BaseCommand):
     help = """
     Runs sql scripts  to recreate access tables for the postgrest API.
