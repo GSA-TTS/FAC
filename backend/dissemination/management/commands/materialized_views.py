8fa3228ab406f9daf3d0c2cdb61b1a33859ba9e1
Daniel Swick <2365503+danswick@users.noreply.github.com>
2024-03-22 13:56:11 -0700

    bring refresh back. it's used for a lot of tests.

--- backend/dissemination/management/commands/materialized_views.py
+++ backend/dissemination/management/commands/materialized_views.py
@@ -10,6 +10,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("-c", "--create", action="store_true", default=False)
         parser.add_argument("-d", "--drop", action="store_true", default=False)
+        parser.add_argument("-r", "--refresh", action="store_true", default=False)
 
     def handle(self, *args, **options):
         path = "dissemination/sql"
@@ -17,3 +18,5 @@ class Command(BaseCommand):
             api_versions.exec_sql_at_path(path, "create_materialized_views.sql")
         elif options["drop"]:
             api_versions.exec_sql_at_path(path, "drop_materialized_views.sql")
+        elif options["refresh"]:
+            api_versions.exec_sql_at_path(path, "refresh_materialized_views.sql")
