f076466a6b75006c5f41f8c5620a020a0111c33a
Matthew Jadud <matthew.jadud@gsa.gov>
2024-03-25 18:27:18 -0400

    Search, Advanced Search, Oh My! (#3552)
    * Enabling advanced search
    
    This leaves a basic search in place, and adds an advanced search.
    
    Per https://github.com/GSA-TTS/FAC/issues/3526 for more information and
    documentation at the top of search.html:
    
    All input fields besides UEI/EIN are commented out pending a performance investigation and fix.
    Summary report downloads are also disabled.
    
    ** SUMMARY REPORTS HAVE BEEN RE-ENABLED**
    
    However, this commit missed the SF-SAC on overview pages. TBD.
    
    Alerts have been put in place.
    
    ** ALERTS HAVE BEEN COMMENTED OUT **
    
    To undo this action, complete the following steps.
    Search filters:
        1. Uncomment the search filters
    
    ** SEARCH FILTERS UNCOMMENTED FROM search/advanced **
    
    Warnings:
        1. Remove search-alert-performance.html and its imports.
    
    ** THIS WAS COMMENTED OUT **
    
        2. Remove the warning banner in header.html
    
    ** THIS WAS ALSO COMMENTED OUT **
    
    Summary report downloads:
        1. Uncomment the summary report download button here
    
    ** THIS WAS UNUNCOMMENTED **
    
        2. Uncomment the SF-SAC download button in summary.html
    
    ** THIS WAS NOT DONE - NEXT COMMIT **
    
        3. Remove the forced Http404 in SingleSummaryReportDownloadView (dissemination/views.py)
    
    ** I HAVE NO IDEA IF THIS WAS DONE... TBD **
    
    * Uncommenting the SF-SAC on summary pages
    
    * Correctly flip between basic/advanced
    
    Adds a flag for advanced search.
    
    * Linting results.
    
    * Fix linting
    
    * A fix on the adv search code
    
    And test.
    
    * Updating text
    
    * Update search-alert-info.html
    
    Copy edits to "Basic Search" and "Advanced Search" sentences.
    
    * Enabling single summary report download
    
    * cleanup
    
    * update views for advanced search
    
    * lint
    
    * general params and MP logic
    
    * invert the general params
    
    * invert the inversion
    
    * update test_search
    
    * fix summary download tests in test_views
    
    ---------
    
    Co-authored-by: Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
    Co-authored-by: Laura H <123114420+lauraherring@users.noreply.github.com>
    Co-authored-by: Daniel Swick <2365503+danswick@users.noreply.github.com>

--- backend/dissemination/forms.py
+++ backend/dissemination/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 
-class SearchForm(forms.Form):
+class AdvancedSearchForm(forms.Form):
     # Multiple choice field mappings
     findings_field_mapping = {
         "field_name": [
@@ -122,6 +122,79 @@ class SearchForm(forms.Form):
         if "any" in major_program:
             return ["any"]
         return major_program
+
+    def clean_page(self):
+        """
+        Default page number to one.
+        """
+        return int(self.cleaned_data["page"] or 1)
+
+    def clean_limit(self):
+        """
+        Default page limit to 30.
+        """
+        return int(self.cleaned_data["limit"] or 30)
+
+
+class SearchForm(forms.Form):
+    # Multiple choice field Tuples. "choices" variable in field declaration.
+    AY_choices = (("all_years", "All years"),) + tuple(
+        (x, str(x)) for x in reversed(range(2016, 2024))
+    )
+
+    # Query params
+    entity_name = forms.CharField(required=False)
+    uei_or_ein = forms.CharField(required=False)
+    start_date = forms.DateField(required=False)
+    end_date = forms.DateField(required=False)
+    agency_name = forms.CharField(required=False)
+    audit_year = forms.MultipleChoiceField(
+        choices=AY_choices, initial=[2023], required=False
+    )
+    auditee_state = forms.CharField(required=False)
+
+    # Display params
+    limit = forms.CharField(required=False)
+    page = forms.CharField(required=False)
+    order_by = forms.CharField(required=False)
+    order_direction = forms.CharField(required=False)
+
+    # Variables for cleaning functions
+    text_input_delimiters = [
+        ",",
+        ":",
+        ";",
+        "-",
+        " ",
+    ]
+
+    def clean_entity_name(self):
+        """
+        Clean the name field. We can't trust that separators aren't a part of a name somewhere,
+        so just split on newlines.
+        """
+        text_input = self.cleaned_data["entity_name"]
+        return text_input.splitlines()
+
+    def clean_uei_or_ein(self):
+        """
+        Clean up the UEI/EIN field. Replace common separators with a newline.
+        Split on the newlines. Strip all the resulting elements.
+        """
+        text_input = self.cleaned_data["uei_or_ein"]
+        for delimiter in self.text_input_delimiters:
+            text_input = text_input.replace(delimiter, "\n")
+        text_input = [x.strip() for x in text_input.splitlines()]
+        return text_input
+
+    def clean_audit_year(self):
+        """
+        If "All years" is selected, don't include any years.
+        """
+        audit_year = self.cleaned_data["audit_year"]
+        if "all_years" in audit_year:
+            return []
+        return audit_year
 
     def clean_page(self):
         """
