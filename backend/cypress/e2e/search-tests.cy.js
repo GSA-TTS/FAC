8c8142f7d4386d203351813b9d9d841a1a424562
Edward Zapata <99770283+eazapata23@users.noreply.github.com>
2024-03-26 16:33:57 -0400

    add search test data+pageobjects

---
+++ backend/cypress/e2e/search-tests.cy.js
@@ -0,0 +1,86 @@
+import searchPage from '../pageObjects/searchPage.js';
+
+let search;
+let yearsToCheck;
+let ueiOrein;
+let aln;
+let name;
+let accDate;
+let state;
+let cogOrover;
+let findings;
+let directFunding;
+let majorProgram;
+
+beforeEach(() => {
+  cy.visit('/dissemination/search/');
+  
+  cy.fixture('searchPageData/auditYears.json').then((data) => {
+    yearsToCheck = data.yearsToCheck;
+  })
+
+  cy.fixture('searchPageData/uei-ein.json').then((data) => {
+    ueiOrein = data.ueiOrein;
+  })
+
+  cy.fixture('searchPageData/aln.json').then((data) => {
+    aln = data.aln;
+  })
+
+  cy.fixture('searchPageData/name.json').then((data) => {
+    name = data.name;
+  })
+
+  cy.fixture('searchPageData/accDate.json').then((data) => {
+    accDate = data.accDate;
+  })
+
+  cy.fixture('searchPageData/state.json').then((data) => {
+    state = data.state;
+  })
+
+  cy.fixture('searchPageData/cog-over.json').then((data) => {
+    cogOrover = data.cogOrover;
+  })
+
+  cy.fixture('searchPageData/auditFindings.json').then((data) => {
+    findings = data.findings;
+  })
+
+  cy.fixture('searchPageData/directFunding.json').then((data) => {
+    directFunding = data.directFunding;
+  })
+
+  cy.fixture('searchPageData/majorProgram.json').then((data) => {
+    majorProgram = data.majorProgram;
+  })
+
+  search = new searchPage();
+});
+
+//basic search test
+describe('Test Basic Search Fields', () => {
+    
+    it('checks Audit Years', () => {
+      search.checkAuditYearCheckbox('2023');
+      search.uncheckAuditYearCheckbox('2023');
+      yearsToCheck.forEach((year) => {
+        search.checkAuditYearCheckbox(year);
+      });
+      yearsToCheck.forEach((year) => {
+        search.uncheckAuditYearCheckbox(year);
+      })
+      search.testUEIorEin(ueiOrein);
+      search.testName(name);
+      const [startDate, endDate] = accDate;
+        search.testFACacceptanceDate(startDate, endDate);
+
+        search.testState(state);
+
+        search.testSearchSubmitButton();
+
+
+    });
+
+
+  });
---
+++ backend/cypress/pageObjects/searchPage.js
@@ -0,0 +1,113 @@
+class searchPage 
+{
+constructor() {
+    this.auditYearCheckbox = '#audit-year input[type="checkbox"]';
+    //define a function 'yearCheckboxById' that takes a 'year' argument. Using a dynamic selector.
+    this.yearCheckboxById = (year) => (year.toLowerCase() === 'all_years' ? '#audit-year [id^="audit-year-"]' : `#audit-year-${year}`);
+    this.accordionButton = '.usa-accordion__button';
+    this.ueiOrEinField = '#uei-or-ein';
+    this.alnField = '#aln';
+    this.entityNameField = '#entity-name';
+    this.startDateField = '#start-date';
+    this.endDateField = '#end-date';
+    this.auditeeState = '#auditee_state';
+    this.optionsField = '#options';
+    this.findingsAccordionButton = '.usa-accordion__button:contains("Audit findings")';
+    this.auditFindingsCheckbox = '#usa-checkbox__input[value="${value}"]';
+    this.findingsCheckboxId = (findings) => `.usa-checkbox__input[value="${findings.toLowerCase()}"]`;
+    this.directFundingAccordionButton = '.usa-accordion__button:contains("Direct funding")';
+    this.directFundingCheckbox = '#usa-checkbox__input[value="${value}"]';
+    this.directFundingCheckboxId = (funding) => `.usa-checkbox__input[value="${funding.toLowerCase()}"]`;
+    this.majorProgramAccordionButton = '.usa-accordion__button:contains("Major program")';
+    this.majorProgramRadio = '.usa-radio__input[name="major_program"]';
+    this.searchSubmitButton = 'input.usa-button[type="submit"][value="Search"]';
+
+}
+
+checkAuditYearCheckbox(year){
+    cy.get(this.yearCheckboxById(year)).check({force: true}).should('be.checked').and('have.value', year);
+}
+
+uncheckAuditYearCheckbox(year){
+    cy.get(this.yearCheckboxById(year)).uncheck({force: true}).should('not.be.checked').and('have.value', year);
+}
+
+testUEIorEin(value) {
+   // cy.get(this.accordionButton).contains('UEI or EIN').as('accordionButton').click();
+    cy.get(this.ueiOrEinField).clear().type(value);
+}
+
+testALN(value) {
+    cy.get(this.accordionButton).contains('Assistance Listing Number (formerly CFDA').as('accordionButton').click();
+    cy.get(this.alnField).clear().type(value);
+}
+
+testName(value) {
+    cy.get(this.accordionButton).contains('Name (Entity, Auditee, or Auditor)').as('accordionButton').click();
+    cy.get(this.entityNameField).clear().type(value);
+}
+
+testFACacceptanceDate(startDate, endDate) {
+    cy.get(this.accordionButton).contains('FAC acceptance date').as('accordionButton').click();
+    cy.get(this.startDateField).clear().type(startDate);
+    cy.get(this.endDateField).clear().type(endDate);
+}
+
+testState(value) {
+    cy.get(this.accordionButton).contains('State').as('accordionButton').click();
+    cy.get(this.auditeeState).select(value);
+}
+
+testCogorOver(value) {
+    cy.get(this.accordionButton).contains('Cognizant or Oversight').as('accordionButton').click();
+    cy.get(this.optionsField).select(value);
+}
+
+testCogorOver(value) {
+    cy.get(this.accordionButton).contains('Cognizant or Oversight').as('accordionButton').click();
+    cy.get(this.optionsField).select(value);
+}
+
+openFindingsAccordion() {
+    cy.get(this.findingsAccordionButton).as('accordionButton').click();
+}
+
+checkAuditFindingsCheckbox(findings){
+    cy.get(this.findingsCheckboxId(findings)).check({force: true}).should('be.checked').and('have.value', findings);
+}
+
+uncheckAuditFindingsCheckbox(findings){
+    cy.get(this.findingsCheckboxId(findings)).uncheck({force: true}).should('not.be.checked').and('have.value', findings);
+}
+
+openDirectFundingAccordion() {
+    cy.get(this.directFundingAccordionButton).as('accordionButton').click();
+}
+
+checkDirectFundingCheckbox(funding){
+    cy.get(this.directFundingCheckboxId(funding)).check({force: true}).should('be.checked').and('have.value', funding);
+}
+
+uncheckDirectFundingCheckbox(funding){
+    cy.get(this.directFundingCheckboxId(funding)).uncheck({force: true}).should('not.be.checked').and('have.value', funding);
+}
+
+openMajorProgramAccordion() {
+    cy.get(this.majorProgramAccordionButton).as('accordionButton').click();
+}
+
+checkMajorProgramRadio(value){
+    cy.get(`${this.majorProgramRadio}[value="${value}"]`).check({force: true}).should('be.checked').and('have.value', value);
+}
+
+uncheckMajorProgramRadio(value){
+    cy.get(`${this.majorProgramRadio}[value="${value}"]`).check({force: true}).should('be.checked').and('have.value', value);
+}
+
+testSearchSubmitButton(){
+    cy.get(`${this.searchSubmitButton}:last`).click();
+}
+
+}
+
+export default searchPage;
