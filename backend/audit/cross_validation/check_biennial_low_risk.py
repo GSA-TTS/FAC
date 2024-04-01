457c729eac1d90394470d3f550266ddd9c48c92a
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2024-04-01 08:41:24 -0400

    Lint

--- backend/audit/cross_validation/check_biennial_low_risk.py
+++ backend/audit/cross_validation/check_biennial_low_risk.py
@@ -6,7 +6,9 @@ def check_biennial_low_risk(sac_dict, *_args, **_kwargs):
     Check that both biennial and low risk flags aren't both set.
     """
     all_sections = sac_dict["sf_sac_sections"]
-    audit_period_covered = all_sections["general_information"].get("audit_period_covered")
+    audit_period_covered = all_sections["general_information"].get(
+        "audit_period_covered"
+    )
     is_low_risk_auditee = all_sections["audit_information"].get("is_low_risk_auditee")
 
     if audit_period_covered == "biennial" and is_low_risk_auditee:
