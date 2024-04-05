12c010b65187412716525926f87e3a353f2e92c5
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2024-04-05 09:44:52 -0400

    Adding cross validation spinner and disabling button

---
+++ backend/static/js/cross-validation.js
@@ -0,0 +1,23 @@
+var FORM = document.getElementById('cross-validation');
+
+const validation_button = document.getElementById(`begin-validation`); // <button>
+const loader = document.getElementById(`loader`); // <div>
+
+// On form submission, display the loader and disable the submit button
+function attachValidationHandler() {
+  FORM.addEventListener('submit', (e) => {
+    loader.hidden = false;
+    validation_button.disabled = true;
+  });
+}
+
+// On pageload, attach handlers to monitor the page state
+function attachEventHandlers() {
+  attachValidationHandler();
+}
+
+function init() {
+  attachEventHandlers();
+}
+
+init();
