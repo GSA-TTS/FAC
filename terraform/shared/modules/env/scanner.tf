eb2584e188a880e657d3b588480578caf2e58c11
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-04-24 16:47:39 -0400

    Initial Scanner File Commit
    This is a skeletonized app deployment for the scanner application

---
+++ terraform/shared/modules/env/scanner.tf
@@ -0,0 +1,22 @@
+locals {
+  scanner_name = "fac-scanner"
+}
+
+module "fac-file-scanner" {
+  source            = "../scanner"
+  name              = local.logshipper_name
+  cf_org_name       = var.cf_org_name
+  cf_space_name     = var.cf_space_name
+  scanner_instances = 1
+  scanner_memory    = 256
+  disk_quota        = 256
+}
+
+resource "cloudfoundry_network_policy" "scanner-network-policy" {
+  policy {
+    source_app      = module.scanner.app_id
+    destination_app = module.https-proxy.app_id
+    port            = "61443"
+    protocol        = "tcp"
+  }
+}
---
+++ terraform/shared/modules/scanner/scanner.tf
@@ -0,0 +1,54 @@
+data "cloudfoundry_domain" "internal" {
+  name = "apps.internal"
+}
+
+data "cloudfoundry_space" "scanner_space" {
+  org_name = var.cf_org_name
+  name     = var.cf_space_name
+}
+
+resource "cloudfoundry_route" "scanner_route" {
+  space    = data.cloudfoundry_space.scanner_space.id
+  domain   = data.cloudfoundry_domain.internal.id
+  hostname = "${var.cf_org_name}-${replace(var.cf_space_name, ".", "-")}-${var.name}"
+  # Yields something like: orgname-spacename-name.apps.internal
+}
+
+# data "external" "scannerzip" {
+#   program     = ["/bin/sh", "prepare-scanner.sh"]
+#   working_dir = path.module
+#   query = {
+#     gitref = var.gitref
+#   }
+# }
+
+resource "cloudfoundry_app" "scanner_app" {
+  name       = var.name
+  space      = data.cloudfoundry_space.scanner_space.id
+  buildpacks = ["python_buildpack"]
+  # path       = "${path.module}/${data.external.scannerzip.result.path}"
+  # source_code_hash  = filesha256("${path.module}/${data.external.scannerzip.result.path}")
+  timeout           = 180
+  disk_quota        = var.disk_quota
+  memory            = var.scanner_memory
+  instances         = var.scanner_instances
+  strategy          = "rolling"
+  health_check_type = "process"
+
+  routes {
+    route = cloudfoundry_route.scanner_route.id
+  }
+
+  service_binding {
+    service_instance = module.s3-private.bucket_id
+  }
+
+  service_binding {
+    service_instance = module.database.cloudfoundry_app.instance_id
+  }
+
+  service_binding {
+    service_instance = module.clamav.instance_id
+  }
+}
+
