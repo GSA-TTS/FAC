8c7c67c932102073359be175561fff73e3cebc1d
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-04-25 09:27:17 -0400

    move vars

--- terraform/shared/modules/env/scanner.tf
+++ terraform/shared/modules/env/scanner.tf
@@ -8,12 +8,12 @@ module "fac-file-scanner" {
   cf_org_name       = var.cf_org_name
   cf_space_name     = var.cf_space_name
   https_proxy       = module.https-proxy.https_proxy
-  scanner_instances = 1
-  scanner_memory    = 512
-  disk_quota        = 512
   clamav_id         = module.clamav.app_id
   db_id             = module.database.instance_id
   s3_id             = module.s3-private.bucket_id
+  scanner_instances = 1
+  scanner_memory    = 512
+  disk_quota        = 512
 }
 
 resource "cloudfoundry_network_policy" "scanner-network-policy" {
