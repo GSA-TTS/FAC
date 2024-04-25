9b7143dd1db30e93810f6ce908779c595e231b57
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-04-25 11:03:37 -0400

    Test the id

--- terraform/shared/modules/env/scanner.tf
+++ terraform/shared/modules/env/scanner.tf
@@ -2,27 +2,15 @@ locals {
   scanner_name = "fac-file-scanner"
 }
 
-data "cloudfoundry_app" "app" {
-  name_or_id = local.clam_name
-  space      = var.cf_space_name
-}
-
-data "cloudfoundry_service" "s3" {
-  name = "fac-private-s3"
-}
-
-data "cloudfoundry_service" "rds" {
-  name = "fac-db"
-}
 module "fac-file-scanner" {
   source            = "../scanner"
   name              = local.scanner_name
   cf_org_name       = var.cf_org_name
   cf_space_name     = var.cf_space_name
   https_proxy       = module.https-proxy.https_proxy
-  clamav_id         = data.cloudfoundry_app.app.id
-  db_id             = data.cloudfoundry_service.rds.id
-  s3_id             = data.cloudfoundry_service.s3.id
+  clamav_id         = module.clamav.id
+  db_id             = module.database.id
+  s3_id             = module.s3-private.id
   scanner_instances = 1
   scanner_memory    = 512
   disk_quota        = 512
