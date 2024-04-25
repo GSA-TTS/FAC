008a71a80b2e148371a373a314d43abd215647c2
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-04-25 10:02:13 -0400

    add http endpoint

--- terraform/shared/modules/scanner/scanner.tf
+++ terraform/shared/modules/scanner/scanner.tf
@@ -32,12 +32,13 @@ resource "cloudfoundry_app" "scanner_app" {
   buildpack = "https://github.com/cloudfoundry/python-buildpack"
   path      = "${path.module}/${data.external.scannerzip.result.path}"
   # source_code_hash  = filesha256("${path.module}/${data.external.scannerzip.result.path}")
-  timeout           = 180
-  disk_quota        = var.disk_quota
-  memory            = var.scanner_memory
-  instances         = var.scanner_instances
-  strategy          = "rolling"
-  health_check_type = "http"
+  timeout                    = 180
+  disk_quota                 = var.disk_quota
+  memory                     = var.scanner_memory
+  instances                  = var.scanner_instances
+  strategy                   = "rolling"
+  health_check_type          = "http"
+  health_check_http_endpoint = "localhost:8080"
 
   # service_binding {
   #   service_instance = var.clamav_id
