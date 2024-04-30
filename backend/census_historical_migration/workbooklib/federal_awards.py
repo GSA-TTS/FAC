d1e47d771a370476af632d5247f43952200b37fc
Phil Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2024-04-30 09:41:19 -0400

    Whitespace

--- backend/census_historical_migration/workbooklib/federal_awards.py
+++ backend/census_historical_migration/workbooklib/federal_awards.py
@@ -258,7 +258,6 @@ def xform_constructs_cluster_names(
 
 def xform_program_name(audits):
     """Default missing program_name to GSA_MIGRATION"""
-
     change_records = []
     is_empty_program_name_found = False
 
