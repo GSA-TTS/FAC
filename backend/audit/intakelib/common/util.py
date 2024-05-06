02038de2f362438ec5664eb03eaa19e745e1884e
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2024-05-06 08:54:26 -0400

    Lowercase y/n no longer permitted in is_yes_or_no_or_gsa_migration

--- backend/audit/intakelib/common/util.py
+++ backend/audit/intakelib/common/util.py
@@ -73,7 +73,7 @@ def is_yes_or_no_or_gsa_migration(v):
     # in historic data migration to pass validation.
     value = str(v).strip()
 
-    return value in ["Y", "y", "N", "n", settings.GSA_MIGRATION]
+    return value in ["Y", "N", settings.GSA_MIGRATION]
 
 
 def get_missing_value_errors(ir, range_name, message_key):
