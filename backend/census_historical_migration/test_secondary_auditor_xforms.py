9431a5177764ad947e928238706ed78ca853c4b4
Sudha Kumar <sudha.kumar@gsa.gov>
2024-05-07 14:46:08 -0700

    Code improvement

--- backend/census_historical_migration/test_secondary_auditor_xforms.py
+++ backend/census_historical_migration/test_secondary_auditor_xforms.py
@@ -15,20 +15,32 @@ class TestXformCpaFirmName(SimpleTestCase):
 
     def _mock_secondaryauditor_header(self):
         """Returns a mock secondary_auditor with all necessary fields."""
-        return self.MockSecondaryAuditorHeader(
-            DBKEY="123456789",
-            CPAFIRMNAME="John Doe CPA Firm",
-        )
+        return [
+            self.MockSecondaryAuditorHeader(
+                DBKEY="123456789",
+                CPAFIRMNAME="John Doe CPA Firm",
+            ),
+            self.MockSecondaryAuditorHeader(
+                DBKEY="223456789",
+                CPAFIRMNAME="Jack C CPA Firm",
+            ),
+        ]
 
     def test_valid_cpafirm(self):
-        """Test that the function returns the valid CPAFIRMNAME."""
-        secondary_auditor = self._mock_secondaryauditor_header()
-        result = xform_cpafirmname(secondary_auditor)
-        self.assertEqual(result.CPAFIRMNAME, secondary_auditor.CPAFIRMNAME)
+        """Test that the function does not change the valid CPAFIRMNAME."""
+        secondary_auditors = self._mock_secondaryauditor_header()
+        cpas = secondary_auditors
+        xform_cpafirmname(secondary_auditors)
+        for index in range(len(secondary_auditors)):
+            self.assertEqual(
+                secondary_auditors[index].CPAFIRMNAME, cpas[index].CPAFIRMNAME
+            )
 
     def test_blank_cpafirm(self):
-        """Test that the function returns GSA_MIGRATION for CPAFIRMNAME."""
-        secondary_auditor = self._mock_secondaryauditor_header()
-        secondary_auditor.CPAFIRMNAME = ""
-        result = xform_cpafirmname(secondary_auditor)
-        self.assertEqual(result.CPAFIRMNAME, settings.GSA_MIGRATION)
+        """Test that the function changes blank CPAFIRMNAME to GSA_MIGRATION."""
+        secondary_auditors = self._mock_secondaryauditor_header()
+        secondary_auditors[0].CPAFIRMNAME = ""
+        cpas = secondary_auditors
+        xform_cpafirmname(secondary_auditors)
+        self.assertEqual(secondary_auditors[0].CPAFIRMNAME, settings.GSA_MIGRATION)
+        self.assertEqual(secondary_auditors[1].CPAFIRMNAME, cpas[1].CPAFIRMNAME)
