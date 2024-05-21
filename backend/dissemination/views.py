23c4e22a8916e1bc431eb66fef394c2f376da940
James Person <jperson1@umbc.edu>
2024-05-21 11:24:48 -0400

    Search - add search timing to results count

--- backend/dissemination/views.py
+++ backend/dissemination/views.py
@@ -312,10 +312,12 @@ class Search(View):
             "summary_report_download_limit": SUMMARY_REPORT_DOWNLOAD_LIMIT,
         }
         time_beginning_render = time.time()
+        total_time_ms = int(math.ceil((time_beginning_render - time_starting_post) * 1000))
+        total_time_s = total_time_ms / 1000
         logger.info(
             f"Total time between post and render {int(math.ceil((time_beginning_render - time_starting_post) * 1000))}ms"
         )
-        return render(request, "search.html", context)
+        return render(request, "search.html", context | {"total_time_s": total_time_s})
 
 
 class AuditSummaryView(View):
