9a95972b5bad67caab20be88b8a9ba67277eb6a0
Sudha Kumar <sudha.kumar@gsa.gov>
2024-05-22 15:31:54 -0700

    Added EXTRA_FINDING_REFERENCE_NUMBERS_IN_CAPTEXT

---
+++ backend/census_historical_migration/invalid_migration_tags.py
@@ -0,0 +1,5 @@
+class INVALID_MIGRATION_TAGS:
+    """Constants for invalid migration tags."""
+
+    DUPLICATE_FINDING_REFERENCE_NUMBERS = "duplicate_finding_reference_numbers"
+    EXTRA_FINDING_REFERENCE_NUMBERS_IN_CAPTEXT = "extra_finding_reference_numbers_in_captext"
