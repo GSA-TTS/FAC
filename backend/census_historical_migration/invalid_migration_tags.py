93e9d1efd02777808e4b8aef8c63f2e85d349b64
Sudha Kumar <sudha.kumar@gsa.gov>
2024-05-28 13:40:04 -0700

    Handle extra findings text references

--- backend/census_historical_migration/invalid_migration_tags.py
+++ backend/census_historical_migration/invalid_migration_tags.py
@@ -2,3 +2,9 @@ class INVALID_MIGRATION_TAGS:
     """Constants for invalid migration tags."""
 
     DUPLICATE_FINDING_REFERENCE_NUMBERS = "duplicate_finding_reference_numbers"
+    EXTRA_FINDING_REFERENCE_NUMBERS_IN_CAPTEXT = (
+        "extra_finding_reference_numbers_in_captext"
+    )
+    EXTRA_FINDING_REFERENCE_NUMBERS_IN_FINDINGSTEXT = (
+        "extra_finding_reference_numbers_in_findingstext"
+    )
