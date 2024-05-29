89276e8d7bf9e2d1aa0db032febb2c79718d3d1b
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2024-05-29 07:52:54 -0400

    #3688 Added logic to track invalid federal program total

--- backend/census_historical_migration/invalid_migration_tags.py
+++ backend/census_historical_migration/invalid_migration_tags.py
@@ -1,4 +1,5 @@
 class INVALID_MIGRATION_TAGS:
     """Constants for invalid migration tags."""
 
+    INVALID_FEDERAL_PROGRAM_TOTAL = "invalid_federal_program_total"
     DUPLICATE_FINDING_REFERENCE_NUMBERS = "duplicate_finding_reference_numbers"
