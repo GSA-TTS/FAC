5aabf89959a5522d4927588370e90e2eeac4ec5f
James Person <jperson1@umbc.edu>
2024-05-30 10:22:59 -0400

    Search - `distinct` call compatible with cog/over

--- backend/dissemination/search.py
+++ backend/dissemination/search.py
@@ -37,6 +37,9 @@ logger = logging.getLogger(__name__)
 
 
 def is_advanced_search(params_dict):
+    """
+    Returns True if the 'advanced_search_flag' param is True.
+    """
     return params_dict.get("advanced_search_flag")
 
 
@@ -59,7 +62,6 @@ def search(params):
     if is_advanced_search(params):
         logger.info("search Searching `DisseminationCombined`")
         results = search_general(DisseminationCombined, params)
-        results = _sort_results(results, params)
         results = search_alns(results, params)
         results = search_findings(results, params)
         results = search_direct_funding(results, params)
@@ -69,9 +71,9 @@ def search(params):
     else:
         logger.info("search Searching `General`")
         results = search_general(General, params)
-        results = _sort_results(results, params)
 
-    results = results.distinct("report_id", params.get("order_by", "fac_accepted_date"))
+    results = _sort_results(results, params)
+    results = _make_distinct(results, params)
 
     t1 = time.time()
     report_timing("search", params, t0, t1)
@@ -93,9 +95,12 @@ def _set_general_defaults(params):
 
 
 def _sort_results(results, params):
+    """
+    Append an `.order_by()` to the results based on the 'order_by' and 'order_direction' params.
+    The 'cog_over' input field is split into its appropriate DB fields.
+    """
     # Instead of nesting conditions, we'll prep a string
     # for determining the sort direction.
-
     match params.get("order_direction"):
         case DIRECTION.ascending:
             direction = ""
@@ -114,10 +119,31 @@ def _sort_results(results, params):
             new_results = results.order_by(f"{direction}audit_year")
         case ORDER_BY.cog_over:
             if params.get("order_direction") == DIRECTION.ascending:
-                new_results = results.order_by("cognizant_agency")
+                # Ex. COG-01, COG-99, OVER-01, OVER-99
+                new_results = results.order_by("cognizant_agency", "oversight_agency")
             else:
-                new_results = results.order_by("oversight_agency")
+                # Ex. OVER-01, OVER-99, COG-01, COG-99
+                new_results = results.order_by("oversight_agency", "cognizant_agency")
         case _:
             new_results = results.order_by(f"{direction}fac_accepted_date")
 
     return new_results
+
+
+def _make_distinct(results, params):
+    """
+    Append a `.distinct()` to the results QuerySet, to prevent duplicate results.
+    Rows with duplicate report_ids exist when using AdvancedSearch/DisseminationCombined.
+    """
+    order_by = params.get("order_by", "fac_accepted_date")
+    order_direction = params.get("order_direction", "")
+
+    # cog_over needs to be broken out here in the same order it is broken out in _sort_results().
+    if order_by == "cog_over" and order_direction == "ascending":
+        results = results.distinct("report_id", "cognizant_agency", "oversight_agency")
+    elif order_by == "cog_over":
+        results = results.distinct("report_id", "oversight_agency", "cognizant_agency")
+    else:
+        results = results.distinct("report_id", order_by)
+
+    return results
