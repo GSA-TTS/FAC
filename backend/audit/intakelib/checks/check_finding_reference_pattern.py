6676f281fe0c67c4ec5a47f7c8bb1f78e0084f5e
Sudha Kumar <sudha.kumar@gsa.gov>
2024-06-11 11:08:36 -0700

    Handling blank finding reference number

--- backend/audit/intakelib/checks/check_finding_reference_pattern.py
+++ backend/audit/intakelib/checks/check_finding_reference_pattern.py
@@ -22,13 +22,13 @@ FINDING_REFERENCE_REGEX = r"^[1-2][0-9]{3}-[0-9]{3}(,\s*[1-2][0-9]{3}-[0-9]{3})*
 # digits are a year >= 1900.
 # TESTED BY
 # has_bad_references.xlsx
-def finding_reference_pattern(ir):
+def finding_reference_pattern(ir, is_gsa_migration=False):
     references = get_range_by_name(ir, "reference_number")
     errors = []
     for index, reference in enumerate(references["values"]):
         if (
             not appears_empty(reference)
-            and reference != settings.GSA_MIGRATION
+            and (reference != settings.GSA_MIGRATION and is_gsa_migration)
             and (not re.match(FINDING_REFERENCE_REGEX, str(reference)))
         ):
             errors.append(
