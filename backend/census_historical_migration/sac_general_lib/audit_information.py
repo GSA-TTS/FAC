c0a289e9bd57394bf78247d28303f2d4750b1f74
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2024-06-14 16:59:25 -0400

    Bug fix

--- backend/census_historical_migration/sac_general_lib/audit_information.py
+++ backend/census_historical_migration/sac_general_lib/audit_information.py
@@ -390,4 +390,5 @@ def ace_audit_information(audit_header):
         InvalidRecord.append_invalid_migration_tag(
             INVALID_MIGRATION_TAGS.ACE_AUDIT_REPORT,
         )
+
     return default
