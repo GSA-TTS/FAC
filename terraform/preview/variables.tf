d35f44ef4668d19af2e94388c5d11cc1056921c7
Tim Ballard <1425377+timoballard@users.noreply.github.com>
2024-06-17 12:31:22 -0500

    define nr vars in preview module

--- terraform/preview/variables.tf
+++ terraform/preview/variables.tf
@@ -3,6 +3,16 @@ variable "new_relic_license_key" {
   description = "the license key to use when setting up the New Relic agent"
 }
 
+variable "new_relic_account_id" {
+  type        = number
+  description = "New Relic Account ID"
+}
+
+variable "new_relic_api_key" {
+  type        = string
+  description = "New Relic API key"
+}
+
 variable "pgrst_jwt_secret" {
   type        = string
   description = "the JWT signing secret for validating JWT tokens from api.data.gov"
