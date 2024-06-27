f50762a3c500bb2c9f4bed7c73241dcf81b8fde4
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-06-27 13:59:39 -0400

    Adjust columns for widgets

--- terraform/shared/modules/newrelic/logreview.tf
+++ terraform/shared/modules/newrelic/logreview.tf
@@ -10,8 +10,8 @@ resource "newrelic_one_dashboard" "log_review_dashboard" {
 
       row    = 1
       column = 1
-      width  = 2
-      height = 2
+      width  = 3
+      height = 3
 
       nrql_query {
         query = "From Transaction SELECT count(*) as 'Submissions' WHERE appName = 'gsa-fac-${var.cf_space_name}' and request.method = 'POST' and request.uri like '/audit/submission/%' since 1 week ago"
@@ -23,9 +23,9 @@ resource "newrelic_one_dashboard" "log_review_dashboard" {
 
 
       row    = 1
-      column = 3
-      width  = 2
-      height = 2
+      column = 4
+      width  = 3
+      height = 3
 
       nrql_query {
         query = "FROM Metric SELECT count(*) AS 'Total uploads', average(apm.service.transaction.duration) AS 'Average time (s)' WHERE appName = 'gsa-fac-${var.cf_space_name}' AND path LIKE 'audit.views.views:ExcelFileHandlerView.post'"
@@ -39,9 +39,9 @@ resource "newrelic_one_dashboard" "log_review_dashboard" {
       title = "Single Audit Report Uploads"
 
       row    = 1
-      column = 5
-      width  = 2
-      height = 2
+      column = 7
+      width  = 3
+      height = 3
 
       nrql_query {
         query = "FROM Metric SELECT count(*) AS 'Total uploads', average(apm.service.transaction.duration) AS 'Average time (s)' WHERE appName = 'gsa-fac-${var.cf_space_name}' AND path LIKE 'audit.views.upload_report_view:UploadReportView.post'"
@@ -53,10 +53,10 @@ resource "newrelic_one_dashboard" "log_review_dashboard" {
     widget_line {
       title = "Global Requests (Non-Upload)"
 
-      row    = 1
-      column = 5
-      width  = 2
-      height = 2
+      row    = 2
+      column = 1
+      width  = 3
+      height = 3
 
       nrql_query {
         query = "FROM Metric SELECT count(*) AS 'Total requests', average(apm.service.transaction.duration) * 1000 AS 'Average time (ms)' WHERE appName = 'gsa-fac-${var.cf_space_name}' AND path NOT LIKE '%ExcelFileHandlerView.post' AND path NOT LIKE '%UploadReportView.post'"
@@ -68,9 +68,9 @@ resource "newrelic_one_dashboard" "log_review_dashboard" {
     widget_line {
       title = "500 Error Code"
 
-      row    = 2
+      row    = 3
       column = 1
-      width  = 12
+      width  = 9
       height = 3
 
       nrql_query {
