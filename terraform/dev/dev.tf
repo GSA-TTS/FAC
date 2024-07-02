a6fa997d56a36593228781987a7dfa6b3e8eea5a
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-07-02 13:46:34 -0400

    fix recursive delete

--- terraform/dev/dev.tf
+++ terraform/dev/dev.tf
@@ -13,7 +13,6 @@ module "dev" {
   smtp_proxy_instances  = 1
   clamav_instances      = 1
   clamav_fs_instances   = 1
-  recursive_delete      = true
   json_params = jsonencode(
     {
       "storage" : 50,
