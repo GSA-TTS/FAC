3e35dfb7cdea73905bf6796f890b597f29d9dc4c
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-07-02 13:46:59 -0400

    terraform fmt

--- terraform/shared/modules/cg-logshipper/cg-logshipper.tf
+++ terraform/shared/modules/cg-logshipper/cg-logshipper.tf
@@ -10,11 +10,11 @@ data "cloudfoundry_space" "apps" {
 module "s3-logshipper-storage" {
   source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v1.0.0"
 
-  cf_org_name      = var.cf_org_name
-  cf_space_name    = var.cf_space_name
-  name             = "log-storage"
-  s3_plan_name     = "basic"
-  tags             = ["logshipper-s3"]
+  cf_org_name   = var.cf_org_name
+  cf_space_name = var.cf_space_name
+  name          = "log-storage"
+  s3_plan_name  = "basic"
+  tags          = ["logshipper-s3"]
 }
 
 resource "cloudfoundry_service_key" "logshipper-s3-service-key" {
