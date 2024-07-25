4b99623bb0a7d4f2ee7ef2556e471ee206fe551f
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-07-25 14:36:53 -0400

    Add an informational comment on why the trigger={} was added

--- terraform/shared/modules/cors/cors.tf
+++ terraform/shared/modules/cors/cors.tf
@@ -7,6 +7,10 @@ resource "null_resource" "cors_header" {
     interpreter = ["/bin/bash", "-c"]
     command     = "./cors-script.sh ${var.cf_org_name} ${var.cf_space_name} ${local.script_path}"
   }
+  # https://github.com/hashicorp/terraform/issues/8266#issuecomment-454377049
+  # A clever way to get this to run every time, otherwise we would be relying on
+  # an md5 hash, which, once this goes into the system, will rarely (if ever)
+  # be updated
   triggers = {
     always_run = "${timestamp()}"
   }
