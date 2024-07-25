e82377d451eb64149a288e823b90302cfc7306ee
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-07-25 08:45:56 -0400

    add aws

--- terraform/shared/modules/env/cors-script.sh
+++ terraform/shared/modules/env/cors-script.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+curl -L "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip && rm awscliv2.zip
+./aws/install -i ~/usr -b ~/bin
+/bin/aws --version
+
+
 cf t -o "$1" -s "$2"
 SERVICE_INSTANCE_NAME=fac-public-s3;
 KEY_NAME=fac-public-s3-key;
@@ -10,6 +16,6 @@ export AWS_SECRET_ACCESS_KEY="$(echo "${S3_CREDENTIALS}" | jq -r .secret_access_
 export BUCKET_NAME="$(echo "${S3_CREDENTIALS}" | jq -r .bucket)";
 export AWS_DEFAULT_REGION="$(echo "${S3_CREDENTIALS}" | jq -r '.region')";
 echo "$BUCKET_NAME";
-aws s3api put-bucket-cors --bucket "$BUCKET_NAME" --cors-configuration "$3";
-aws s3api get-bucket-cors --bucket "$BUCKET_NAME";
+/bin/aws s3api put-bucket-cors --bucket "$BUCKET_NAME" --cors-configuration "$3";
+/bin/aws s3api get-bucket-cors --bucket "$BUCKET_NAME";
 cf delete-service-key "${SERVICE_INSTANCE_NAME}" "${KEY_NAME}";
