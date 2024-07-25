5cbd60c7a5cd00f04f20f1550bd2173ba8cb839e
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-07-25 08:39:20 -0400

    add cf target command

--- terraform/shared/modules/env/cors-script.sh
+++ terraform/shared/modules/env/cors-script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cf t -o "$1" -s "$2"
 SERVICE_INSTANCE_NAME=fac-public-s3;
 KEY_NAME=fac-public-s3-key;
 cf create-service-key "${SERVICE_INSTANCE_NAME}" "${KEY_NAME}";
@@ -9,6 +10,6 @@ export AWS_SECRET_ACCESS_KEY="$(echo "${S3_CREDENTIALS}" | jq -r .secret_access_
 export BUCKET_NAME="$(echo "${S3_CREDENTIALS}" | jq -r .bucket)";
 export AWS_DEFAULT_REGION="$(echo "${S3_CREDENTIALS}" | jq -r '.region')";
 echo "$BUCKET_NAME";
-aws s3api put-bucket-cors --bucket "$BUCKET_NAME" --cors-configuration "$1";
+aws s3api put-bucket-cors --bucket "$BUCKET_NAME" --cors-configuration "$3";
 aws s3api get-bucket-cors --bucket "$BUCKET_NAME";
 cf delete-service-key "${SERVICE_INSTANCE_NAME}" "${KEY_NAME}";
--- terraform/shared/modules/env/cors.tf
+++ terraform/shared/modules/env/cors.tf
@@ -4,7 +4,7 @@ resource "null_resource" "cors_script" {
   provisioner "local-exec" {
     working_dir = "${path.module}"
     interpreter = ["/bin/bash", "-c"]
-    command = "./cors-script.sh ${var.cors_json}"
+    command = "./cors-script.sh ${var.cf_org_name} ${var.cf_space_name} ${var.cors_json}"
   }
   depends_on = [module.s3-public]
 }
