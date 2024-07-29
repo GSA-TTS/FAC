c030d89a03eade10a8fa13e694007abfdccd7024
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-07-29 14:00:55 -0400

    Test the md5 change

--- terraform/shared/modules/cors/cors-script.sh
+++ terraform/shared/modules/cors/cors-script.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Modifying this file will cause terraform to update it in the system and re-apply the cors headers,
+# since the md5 will be changing.
+
 curl -L "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip && rm awscliv2.zip
 ./aws/install -i ~/usr -b ~/bin
