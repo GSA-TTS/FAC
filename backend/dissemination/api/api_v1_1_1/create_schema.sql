d5db1e6a42a70a1e803d9791be486ec5115ccfa6
James Person <jperson1@umbc.edu>
2024-08-15 10:39:34 -0400

    Add API version 1.1.1, including materialized view

--- backend/dissemination/api/api_v1_1_1/create_schema.sql
+++ backend/dissemination/api/api_v1_1_1/create_schema.sql
@@ -3,28 +3,28 @@ begin;
 do
 $$
 begin
-    DROP SCHEMA IF EXISTS api_v1_1_0 CASCADE;
-    DROP SCHEMA IF EXISTS api_v1_1_0_functions CASCADE;
+    DROP SCHEMA IF EXISTS api_v1_1_1 CASCADE;
+    DROP SCHEMA IF EXISTS api_v1_1_1_functions CASCADE;
 
-    if not exists (select schema_name from information_schema.schemata where schema_name = 'api_v1_1_0') then
-        create schema api_v1_1_0;
-        create schema api_v1_1_0_functions;
+    if not exists (select schema_name from information_schema.schemata where schema_name = 'api_v1_1_1') then
+        create schema api_v1_1_1;
+        create schema api_v1_1_1_functions;
         
-        grant usage on schema api_v1_1_0_functions to api_fac_gov;
+        grant usage on schema api_v1_1_1_functions to api_fac_gov;
 
         -- Grant access to tables and views
         alter default privileges
-            in schema api_v1_1_0
+            in schema api_v1_1_1
             grant select
         -- this includes views
         on tables
         to api_fac_gov;
 
         -- Grant access to sequences, if we have them
-        grant usage on schema api_v1_1_0 to api_fac_gov;
-        grant select, usage on all sequences in schema api_v1_1_0 to api_fac_gov;
+        grant usage on schema api_v1_1_1 to api_fac_gov;
+        grant select, usage on all sequences in schema api_v1_1_1 to api_fac_gov;
         alter default privileges
-            in schema api_v1_1_0
+            in schema api_v1_1_1
             grant select, usage
         on sequences
         to api_fac_gov;
@@ -35,8 +35,8 @@ $$
 
 -- https://postgrest.org/en/stable/references/api/openapi.html
 -- This is the title (version number) and description (text).
-COMMENT ON SCHEMA api_v1_1_0 IS
-$$v1.1.0
+COMMENT ON SCHEMA api_v1_1_1 IS
+$$v1.1.1
 
 A RESTful API that serves data from the SF-SAC.$$;
 
