e1605f29ec753eefd4236bb099bd1314a191f3c4
Phil Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2024-08-21 15:25:15 -0400

    Rerunning individual validations before cross validations and submission (#4203)
    * Running wb validations in pre-submission validation
    
    * Displaying generic error and moving to separate method
    
    * Lint
    
    * Lint
    
    * Lint
    
    * Returning empty object when no errors
    
    * Running wb validations during submission
    
    * Lint
    
    * Lint
    
    * Fixing test_post_redirect
    
    * Fixing test_ready_for_certification
    
    * Fixing test_submission
    
    * Fixing test_unlock_after_certification
    
    * Lint
    
    * Typo

--- backend/audit/test_views.py
+++ backend/audit/test_views.py
@@ -293,11 +293,12 @@ class SubmissionViewTests(TestCase):
             "audit_information": _fake_audit_information(),
             "federal_awards": _load_json(AUDIT_JSON_FIXTURES / awardsfile),
             "general_information": _load_json(AUDIT_JSON_FIXTURES / geninfofile),
-            "submission_status": STATUSES.AUDITEE_CERTIFIED,
+            "submission_status": STATUSES.IN_PROGRESS,  # Temporarily required for SAR creation below
         }
         sac_data["notes_to_sefa"]["NotesToSefa"]["accounting_policies"] = "Exhaustive"
         sac_data["notes_to_sefa"]["NotesToSefa"]["is_minimis_rate_used"] = "Y"
         sac_data["notes_to_sefa"]["NotesToSefa"]["rate_explained"] = "At great length"
+        sac_data["report_id"] = _mock_gen_report_id()
         user, sac = _make_user_and_sac(**sac_data)
 
         required_statuses = (
@@ -311,8 +312,10 @@ class SubmissionViewTests(TestCase):
             sac.transition_name.append(rs)
             sac.transition_date.append(datetime.now(timezone.utc))
 
-        sac.save()
+        baker.make(SingleAuditReportFile, sac=sac)
         baker.make(Access, user=user, sac=sac, role="certifying_auditee_contact")
+        sac.submission_status = STATUSES.AUDITEE_CERTIFIED
+        sac.save()
 
         response = _authed_post(
             Client(),
@@ -373,6 +376,9 @@ class SubmissionStatusTests(TransactionTestCase):
             "federal_awards": _load_json(AUDIT_JSON_FIXTURES / awardsfile),
             "general_information": _load_json(AUDIT_JSON_FIXTURES / geninfofile),
         }
+        sac_data["notes_to_sefa"]["NotesToSefa"]["accounting_policies"] = "Exhaustive"
+        sac_data["notes_to_sefa"]["NotesToSefa"]["is_minimis_rate_used"] = "Y"
+        sac_data["notes_to_sefa"]["NotesToSefa"]["rate_explained"] = "At great length"
 
         sac = SingleAuditChecklist.objects.get(report_id=report_id)
         for field, value in sac_data.items():
@@ -427,6 +433,9 @@ class SubmissionStatusTests(TransactionTestCase):
             "federal_awards": _load_json(AUDIT_JSON_FIXTURES / awardsfile),
             "general_information": _load_json(AUDIT_JSON_FIXTURES / geninfofile),
         }
+        sac_data["notes_to_sefa"]["NotesToSefa"]["accounting_policies"] = "Exhaustive"
+        sac_data["notes_to_sefa"]["NotesToSefa"]["is_minimis_rate_used"] = "Y"
+        sac_data["notes_to_sefa"]["NotesToSefa"]["rate_explained"] = "At great length"
 
         sac = SingleAuditChecklist.objects.get(report_id=report_id)
         for field, value in sac_data.items():
@@ -564,11 +573,12 @@ class SubmissionStatusTests(TransactionTestCase):
             "audit_information": _fake_audit_information(),
             "federal_awards": _load_json(AUDIT_JSON_FIXTURES / awardsfile),
             "general_information": _load_json(AUDIT_JSON_FIXTURES / geninfofile),
-            "submission_status": STATUSES.AUDITEE_CERTIFIED,
+            "submission_status": STATUSES.IN_PROGRESS,  # Temporarily required for SAR creation below
         }
         sac_data["notes_to_sefa"]["NotesToSefa"]["accounting_policies"] = "Exhaustive"
         sac_data["notes_to_sefa"]["NotesToSefa"]["is_minimis_rate_used"] = "Y"
         sac_data["notes_to_sefa"]["NotesToSefa"]["rate_explained"] = "At great length"
+        sac_data["report_id"] = _mock_gen_report_id()
         user, sac = _make_user_and_sac(**sac_data)
 
         required_statuses = (
@@ -582,9 +592,10 @@ class SubmissionStatusTests(TransactionTestCase):
             sac.transition_name.append(rs)
             sac.transition_date.append(datetime.now(timezone.utc))
 
-        sac.save()
-
+        baker.make(SingleAuditReportFile, sac=sac)
         baker.make(Access, sac=sac, user=user, role="certifying_auditee_contact")
+        sac.submission_status = STATUSES.AUDITEE_CERTIFIED
+        sac.save()
 
         kwargs = {"report_id": sac.report_id}
         _authed_post(self.client, user, "audit:Submission", kwargs=kwargs)
