30e9f34a5a94b2ac2e5475422c2060c6a704c881
James Person <jperson1@umbc.edu>
2024-08-27 11:04:09 -0400

    Explicitly bring everything down/up on API standup

--- backend/tools/api_standup.sh
+++ backend/tools/api_standup.sh
@@ -9,20 +9,39 @@ function api_standup {
     local d1=$?
     startup_log "CREATE_API_ACCESS_TABLES" "END"
 
+    # Bring the API down completely
+    startup_log "DROP_API_SCHEMA" "BEGIN"
+    python manage.py drop_api_schema
+    local d2=$?
+    startup_log "DROP_API_SCHEMA" "END"
+
     # Bring the API back, possibly installing a new API
     startup_log "CREATE_API_SCHEMA" "BEGIN"
     python manage.py create_api_schema
-    local d2=$?
+    local d3=$?
     startup_log "CREATE_API_SCHEMA" "END"
+
+    # Drop all views, if they already exist
+    startup_log "DROP_API_VIEWS" "BEGIN"
+    python manage.py drop_api_views &&
+    local d4=$?
+    startup_log "CREATE_API_VIEWS" "END"
+
+    # Create/recreate the materialized views. The 'combined' view is the important one for the API.
+    startup_log "CREATE_MATERIALIZED_VIEWS" "BEGIN"
+    python manage.py materialized_views --create &&
+    local d5=$?
+    startup_log "CREATE_MATERIALIZED_VIEWS" "END"
     
+    # Create the API views, which may rely on the 'combined' view from above.
     startup_log "CREATE_API_VIEWS" "BEGIN"
     python manage.py create_api_views &&
-    local d3=$?
+    local d6=$?
     startup_log "CREATE_API_VIEWS" "END"
 
     startup_log "API_STANDUP" "END"
 
-    result=$((($d1 + $d2) + $d3))
+    result=$(((((($d1 + $d2) + $d3) + $d4) + $d5) + $d6))
     # If these are all zero, we're all good.
     return $result
 }
