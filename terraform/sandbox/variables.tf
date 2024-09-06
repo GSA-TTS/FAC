60302f5278fabd9b64894e53c5cf57a4c4fcd7e4
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-09-06 10:04:50 -0400

    Initial sandbox prototype
    Services are built, postgrest is online, but having difficulties with
    the app, proxies, logshipper and scanner. Likely due to the fact that
    sandbox can't create multiple spaces for the egress. Considering putting
    them in the same sandbox space, and keeping everything on apps.internal

---
+++ terraform/sandbox/variables.tf
@@ -0,0 +1,32 @@
+variable "cf_org_name" {
+  type        = string
+  description = "name of the organization to configure"
+  default     = "sandbox-gsa"
+}
+
+variable "cf_space_name" {
+  type        = string
+  description = "name of the sandbox space (ex firstname.lastname)"
+}
+
+variable "cf_user" {
+  type        = string
+  description = "cloud.gov deployer account user"
+}
+
+variable "cf_password" {
+  type        = string
+  description = "secret; cloud.gov deployer account password"
+  sensitive   = true
+}
+
+variable "pgrst_jwt_secret" {
+  type        = string
+  description = "the JWT signing secret for validating JWT tokens from api.data.gov"
+}
+
+variable "new_relic_license_key" {
+  type        = string
+  description = "the license key to use when setting up the New Relic agent"
+}
+
