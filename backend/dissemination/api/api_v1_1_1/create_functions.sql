7aeeb4eddd67ab851adc5853aa05f2775af6c0af
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2024-09-09 11:12:00 -0400

    #3625 Moved request_file_access from admin support api to disseminati… (#4249)
    * #3625 Moved request_file_access function from admin support to dissemination
    
    * Missed to rename these references
    
    * #3625 Updated code to match recent change in main

--- backend/dissemination/api/api_v1_1_1/create_functions.sql
+++ backend/dissemination/api/api_v1_1_1/create_functions.sql
@@ -56,4 +56,51 @@ END;
 $has_tribal_data_access$ LANGUAGE plpgsql;
 
 
+CREATE OR REPLACE FUNCTION api_v1_1_1.request_file_access(
+    report_id TEXT
+) RETURNS JSON LANGUAGE plpgsql AS
+$$
+DECLARE
+    v_uuid_header TEXT;
+    v_access_uuid VARCHAR(200);
+    v_key_exists BOOLEAN;
+    v_key_added_date DATE;
+BEGIN
+    
+    SELECT api_v1_1_1_functions.get_api_key_uuid() INTO v_uuid_header;
+
+    -- Check if the provided API key exists in public.dissemination_TribalApiAccessKeyIds
+    SELECT 
+        EXISTS(
+            SELECT 1
+            FROM public.dissemination_TribalApiAccessKeyIds
+            WHERE key_id = v_uuid_header
+        ) INTO v_key_exists;
+    
+
+    -- Get the added date of the key from public.dissemination_TribalApiAccessKeyIds
+    SELECT date_added
+    INTO v_key_added_date
+    FROM public.dissemination_TribalApiAccessKeyIds
+    WHERE key_id = v_uuid_header;
+    
+
+    -- Check if the key is less than 6 months old
+    IF v_uuid_header IS NOT NULL AND v_key_exists AND v_key_added_date >= CURRENT_DATE - INTERVAL '6 months' THEN
+        -- Generate UUID (using PostgreSQL's gen_random_uuid function)
+        SELECT gen_random_uuid() INTO v_access_uuid;  
+              
+        -- Inserting data into the one_time_access table
+        INSERT INTO public.dissemination_onetimeaccess (uuid, api_key_id, timestamp, report_id)
+        VALUES (v_access_uuid::UUID, v_uuid_header, CURRENT_TIMESTAMP, report_id);
+
+        -- Return the UUID to the user
+        RETURN json_build_object('access_uuid', v_access_uuid);
+    ELSE
+        -- Return an error for unauthorized access
+        RETURN json_build_object('error', 'Unauthorized access or key older than 6 months')::JSON;
+    END IF;
+END;
+$$;
+
 NOTIFY pgrst, 'reload schema';
