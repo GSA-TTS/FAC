1c76c5c0024d80b3df1c9727e36861b82b48d690
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-09-11 09:32:04 -0400

    Modified setup to try and get a running instance
    Due to the error presented (Which we expect)
    2024-09-11T09:24:17.29-0400 [APP/PROC/WEB/0] OUT Invoking pre-start scripts.
       2024-09-11T09:24:17.47-0400 [APP/PROC/WEB/0] OUT STARTUP LOCAL_ENV Environment set as: SANDBOX
       2024-09-11T09:24:17.72-0400 [APP/PROC/WEB/0] OUT STARTUP STARTUP_CHECK setup_env PASS
       2024-09-11T09:24:17.72-0400 [APP/PROC/WEB/0] OUT STARTUP API_TEARDOWN BEGIN
       2024-09-11T09:24:17.72-0400 [APP/PROC/WEB/0] OUT STARTUP DROP_DEPRECATED_API_SCHEMA_AND_VIEWS BEGIN
    
       2024-09-11T09:24:18.19-0400 [APP/PROC/WEB/0] ERR show_deprecation_warning()
       2024-09-11T09:24:19.43-0400 [APP/PROC/WEB/0] ERR {"message": "RSA key format is not supported"}
       2024-09-11T09:24:19.43-0400 [APP/PROC/WEB/0] ERR {"message": "Key jar preparation failed for https://idp.int.identitysandbox.gov"}
       2024-09-11T09:24:19.43-0400 [APP/PROC/WEB/0] ERR {"message": "[******] The system broke while trying to log you in."}
       2024-09-11T09:24:19.43-0400 [APP/PROC/WEB/0] ERR {"message": "Unable to configure OpenID Connect provider. Users cannot log in."}
    
    This will require some dev intervention

--- terraform/shared/modules/app/prepare_app.sh
+++ terraform/shared/modules/app/prepare_app.sh
@@ -13,11 +13,12 @@ tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 cd "$tmpdir"
 
 # Grab a copy of the zip file for the specified ref
-curl -s -L https://github.com/gsa-tts/fac/archive/${GITREF}.zip --output local.zip
+curl -s -L "https://github.com/gsa-tts/fac/archive/${GITREF}.zip" --output local.zip
+branch=$(echo "$GITREF" | cut -f3 -d"/")
 
 # Zip up just the FAC-main/ subdirectory for pushing
-unzip -q -u local.zip \*"FAC-main/backend/*"\*
-cd "${tmpdir}/FAC-main/backend/" && zip -r -o -X "${popdir}/app.zip" ./ > /dev/null
+unzip -q -u local.zip \*"FAC-$branch/backend/*"\*
+cd "${tmpdir}/FAC-$branch/backend/" && zip -r -o -X "${popdir}/app.zip" ./ > /dev/null
 # zip -q -j -r ${popdir}/app.zip fac-*/backend
 
 # Tell Terraform where to find it
