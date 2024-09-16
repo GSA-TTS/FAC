bc3cbb09732272f058504752f5b95fd0b26631b2
Hassan D. M. Sambo <hassandeme.mamasambo@gsa.gov>
2024-09-16 19:29:10 -0400

    Added logic to validate finding reference uniqueness (#4283)
    * Added logic to validate finding reference uniqueness
    
    * #3948 Moved invalid historic record to the 'invalid historic records' section to ensure it passes with the migration flag
    
    * #3948 Improved error message and added invalid test workbook

---
+++ backend/audit/intakelib/checks/check_finding_uniqueness.py
@@ -0,0 +1,67 @@
+from ..common.util import build_cell_error_tuple, get_message, get_range_start_row
+from ..intermediate_representation import get_range_by_name, get_range_values_by_name
+
+
+def check_finding_uniqueness(ir, is_gsa_migration=False):
+    """
+    Check the uniqueness of finding associated with the same finding reference number.
+    """
+
+    errors = []
+    findings_by_reference = {}
+
+    if is_gsa_migration:
+        return errors
+
+    modified_opinion = get_range_values_by_name(ir, "modified_opinion")
+    other_matters = get_range_values_by_name(ir, "other_matters")
+    material_weakness = get_range_values_by_name(ir, "material_weakness")
+    significant_deficiency = get_range_values_by_name(ir, "significant_deficiency")
+    other_findings = get_range_values_by_name(ir, "other_findings")
+    finding_references = get_range_values_by_name(ir, "reference_number")
+    compliance_requirements = get_range_values_by_name(ir, "compliance_requirement")
+    questioned_costs = get_range_values_by_name(ir, "questioned_costs")
+    repeat_prior_reference = get_range_values_by_name(ir, "repeat_prior_reference")
+    prior_references = get_range_values_by_name(ir, "prior_references")
+
+    reference_number_range = get_range_by_name(ir, "reference_number")
+    range_start = int(get_range_start_row(reference_number_range))
+    # Iterate through the data rows
+    for ndx, (fr, cr, mo, om, mw, sd, of, qc, rr, pr) in enumerate(
+        zip(
+            finding_references,
+            compliance_requirements,
+            modified_opinion,
+            other_matters,
+            material_weakness,
+            significant_deficiency,
+            other_findings,
+            questioned_costs,
+            repeat_prior_reference,
+            prior_references,
+        )
+    ):
+
+        finding_set = (fr, cr, mo, om, mw, sd, of, qc, rr, pr)
+
+        if fr in findings_by_reference:
+            if findings_by_reference[fr]["values"] != finding_set:
+                previous_row = findings_by_reference[fr]["row"]
+                current_row = ndx
+                errors.append(
+                    build_cell_error_tuple(
+                        ir,
+                        reference_number_range,
+                        ndx,
+                        get_message("check_finding_uniqueness").format(
+                            range_start + previous_row,
+                            f'|{" | ".join(findings_by_reference[fr]["values"])}|',
+                            range_start + current_row,
+                            f'|{" | ".join(finding_set)}|',
+                        ),
+                    )
+                )
+        else:
+            findings_by_reference[fr] = {"values": finding_set, "row": ndx}
+
+    return errors
