4043374ad01d8371fc90c9e03d08488c85fcb9e8
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-09-17 14:02:37 -0400

    Add fac-key-service

--- terraform/sandbox/sandbox.tf
+++ terraform/sandbox/sandbox.tf
@@ -1,8 +1,12 @@
 module "sandbox" {
-  source                = "../shared/modules/sandbox"
-  cf_space_name         = var.cf_space_name
-  pgrst_jwt_secret      = var.pgrst_jwt_secret
-  new_relic_license_key = var.new_relic_license_key
+  source                  = "../shared/modules/sandbox"
+  cf_space_name           = var.cf_space_name
+  pgrst_jwt_secret        = var.pgrst_jwt_secret
+  new_relic_license_key   = var.new_relic_license_key
+  django_secret_login_key = var.django_secret_login_key
+  sam_api_key             = var.sam_api_key
+  login_client_id         = var.login_client_id
+  login_secret_key        = var.login_secret_key
 
   database_plan         = "medium-gp-psql"
   https_proxy_instances = 1
