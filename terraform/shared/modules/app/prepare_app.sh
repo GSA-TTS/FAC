ea62c23187b7828c998da95862f6dc6ea8a0c4b8
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-09-25 12:50:21 -0400

    Ensure we have compiled assets

--- terraform/shared/modules/app/prepare_app.sh
+++ terraform/shared/modules/app/prepare_app.sh
@@ -3,7 +3,13 @@
 # Exit if any step fails
 set -e
 
+# Comment out the above and use this to see what the script does
+# set -ex
+
 eval "$(jq -r '@sh "GITREF=\(.gitref)"')"
+
+# Useful for debugging the script. Comment out the eval if running for debugging purposes
+# GITREF="refs/heads/<branch-name>"
 
 popdir=$(pwd)
 
@@ -17,12 +23,12 @@ curl -s -L "https://github.com/gsa-tts/fac/archive/${GITREF}.zip" --output local
 branch=$(echo "$GITREF" | cut -f3 -d"/")
 
 # Zip up just the FAC-main/ subdirectory for pushing
+# Before zip stage, run [ npm ci --production | npm run build ] in /backend/ to get the compiled assets for the site in /static/compiled/
 unzip -q -u local.zip \*"FAC-$branch/backend/*"\*
-cd "${tmpdir}/FAC-$branch/backend/" && zip -r -o -X "${popdir}/app.zip" ./ > /dev/null
+cd "${tmpdir}/FAC-$branch/backend/" && npm ci --production --silent && npm install build > '/dev/null' 2>&1 && zip -r -o -X "${popdir}/app.zip" ./ > /dev/null
 # zip -q -j -r ${popdir}/app.zip fac-*/backend
 
 # Tell Terraform where to find it
 cat << EOF
 { "path": "app.zip" }
 EOF
-
