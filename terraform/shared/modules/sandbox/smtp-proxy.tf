1345b804dad4cf6b799c01b8f727fbdd31b05e64
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-09-25 12:51:04 -0400

    Disable smtp-proxy (for now)

--- terraform/shared/modules/sandbox/smtp-proxy.tf
+++ terraform/shared/modules/sandbox/smtp-proxy.tf
@@ -1,12 +1,12 @@
-module "smtp-proxy" {
-  source        = "../stream-proxy"
-  name          = "smtp-proxy"
-  cf_org_name   = var.cf_org_name
-  cf_space_name = "${var.cf_space_name}-egress"
-  client_space  = var.cf_space_name
-  instances     = var.smtp_proxy_instances
+# module "smtp-proxy" {
+#   source        = "../stream-proxy"
+#   name          = "smtp-proxy"
+#   cf_org_name   = var.cf_org_name
+#   cf_space_name = "${var.cf_space_name}-egress"
+#   client_space  = var.cf_space_name
+#   instances     = var.smtp_proxy_instances
 
-  upstream = "smtp-relay.gmail.com:587"
-  clients  = ["gsa-fac"]
-  depends_on = [ module.fac-app ]
-}
+#   upstream = "smtp-relay.gmail.com:587"
+#   clients  = ["gsa-fac"]
+#   depends_on = [ module.fac-app ]
+# }
