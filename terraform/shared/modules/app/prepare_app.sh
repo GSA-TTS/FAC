48b257cc133318759c8f2cf0125cdb5235baf31a
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-09-26 12:40:06 -0400

    Fix command

--- terraform/shared/modules/app/prepare_app.sh
+++ terraform/shared/modules/app/prepare_app.sh
@@ -25,7 +25,10 @@ branch=$(echo "$GITREF" | cut -f3 -d"/")
 # Zip up just the FAC-main/ subdirectory for pushing
 # Before zip stage, run [ npm ci --production | npm run build ] in /backend/ to get the compiled assets for the site in /static/compiled/
 unzip -q -u local.zip \*"FAC-$branch/backend/*"\*
-cd "${tmpdir}/FAC-$branch/backend/" && npm ci --production --silent && npm install build > '/dev/null' 2>&1 && zip -r -o -X "${popdir}/app.zip" ./ > /dev/null
+cd "${tmpdir}/FAC-$branch/backend/" &&
+npm ci --production --silent &&
+npm run build > '/dev/null' 2>&1 &&
+zip -r -o -X "${popdir}/app.zip" ./ > /dev/null
 # zip -q -j -r ${popdir}/app.zip fac-*/backend
 
 # Tell Terraform where to find it
