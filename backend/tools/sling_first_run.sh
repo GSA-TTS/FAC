3539ffcfa01fd38c496d871a8a5aafe3fa66a4f9
Matt Jadud <matthew.jadud@gsa.gov>
2024-10-10 12:34:25 -0400

    Still... fixing return values...

--- backend/tools/sling_first_run.sh
+++ backend/tools/sling_first_run.sh
@@ -20,7 +20,7 @@ function sling_first_run() {
     # If the metadata table exists, it means sling has run to completion.
     check_table_exists $FAC_SNAPSHOT_URI 'public_data_v1_0_0' 'metadata'
     local is_metadata_table=$?
-    echo "check_table_exists public_data_v1_0_0.metadata $is_metadata_table"
+    echo "SLING_FIRST_RUN public_data_v1_0_0.metadata = $is_metadata_table"
     
     # We need to load some functions for sling to complete, because
     # we use those functions as part of the metadata table generation.
--- backend/tools/util_startup.sh
+++ backend/tools/util_startup.sh
@@ -18,36 +18,44 @@ function gonogo {
   fi
 }
 
-function check_table_exists() {
-    local db_uri="$1"
-    local schema="$2"
-    local table="$3"
+# 2024-10-10T12:28:29.61-0400 [APP/PROC/WEB/0] OUT CHECK_TABLE_EXISTS START: public.dissemination_general
+# 2024-10-10T12:28:29.65-0400 [APP/PROC/WEB/0] OUT CHECK_TABLE_EXISTS END: public.dissemination_general = t
+# 2024-10-10T12:28:29.65-0400 [APP/PROC/WEB/0] OUT CHECK_TABLE_EXISTS: public_data_v1_0_0.metadata
+# 2024-10-10T12:28:29.68-0400 [APP/PROC/WEB/0] OUT CHECK_TABLE_EXISTS public_data_v1_0_0.metadata: f
+# 2024-10-10T12:28:29.68-0400 [APP/PROC/WEB/0] OUT Exit status 1
+# 2024-10-10T12:28:29.68-0400 [CELL/SSHD/0] OUT Exit status 0
 
-    echo "CHECK_TABLE_EXISTS: $schema.$table"
-    # >/dev/null 2>&1
-    # The qtAX incantation lets us pass the PSQL result value back to bash.
-    result=`$PSQL_EXE $db_uri -qtAX -c "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = '$schema' AND tablename = '$table');"`
-    echo "CHECK_TABLE_EXISTS $schema.$table: $result"
-    # Flip TRUE to a 0, because UNIX considers a 0 exit code to be good. 
-    if [ "$result" = "t" ]; then
-      return 0;
-    else
-      return 1;
-    fi;
+function check_table_exists() {
+  local db_uri="$1"
+  local schema="$2"
+  local table="$3"
+
+  echo "CHECK_TABLE_EXISTS START: $schema.$table"
+  # >/dev/null 2>&1
+  # The qtAX incantation lets us pass the PSQL result value back to bash.
+  result=`$PSQL_EXE $db_uri -qtAX -c "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = '$schema' AND tablename = '$table');"`
+  # Flip TRUE to a 0, because UNIX considers a 0 exit code to be good. 
+  if [ "$result" = "t" ]; then
+    echo "CHECK_TABLE_EXISTS END: $schema.$table = 0"
+    return 0
+  else
+    echo "CHECK_TABLE_EXISTS END: $schema.$table = 1"
+    return 1
+  fi
 }
 
 function check_schema_exists () {
-    local db_uri="$1"
-    local schema_name="$2"
-    echo "CHECK_SCHEMA_EXISTS $schema_name"
-    result=`$PSQL_EXE $db_uri -qtAX -c "SELECT EXISTS(SELECT 1 FROM information_schema.schemata WHERE schema_name = '$schema_name');"`
-    echo "CHECK_SCHEMA_EXISTS $schema_name: $result"
-    # Flip TRUE to a 0, because UNIX considers a 0 exit code to be good. 
-    if [ "$result" = "t" ]; then
-      return 0;
-    else
-      return 1;
-    fi
+  local db_uri="$1"
+  local schema_name="$2"
+  echo "CHECK_SCHEMA_EXISTS $schema_name"
+  result=`$PSQL_EXE $db_uri -qtAX -c "SELECT EXISTS(SELECT 1 FROM information_schema.schemata WHERE schema_name = '$schema_name');"`
+  echo "CHECK_SCHEMA_EXISTS $schema_name: $result"
+  # Flip TRUE to a 0, because UNIX considers a 0 exit code to be good. 
+  if [ "$result" = "t" ]; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 function run_sql () {
