ca39751ce60be234431f726a9caab1072e56f09b
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-10-21 11:51:53 -0400

    revert

--- backend/tools/migrate_app_tables.sh
+++ backend/tools/migrate_app_tables.sh
@@ -2,7 +2,6 @@ source tools/util_startup.sh
 
 function migrate_app_tables {
     startup_log "MIGRATE_APP_TABLES" "BEGIN"
-    python manage.py makemigrations --merge
     python manage.py migrate
     local result=$?
     startup_log "MIGRATE_APP_TABLES" "END"
