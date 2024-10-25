3aa6266d83d122468434db5dccf5c9ca4abd7380
Matt Jadud <matthew.jadud@gsa.gov>
2024-10-25 16:12:43 -0400

    Updated the sling script for bulk data
    This is a proof-of-concept included with the work. It's in the tree, and
    I've taken it out of active use/an active part of startup.
    
    However, we might consider working it into the nightly, to start testing
    it.

--- backend/dissemination/sql/sling/bulk_data_export/create_stream_yaml.py
+++ backend/dissemination/sql/sling/bulk_data_export/create_stream_yaml.py
@@ -6,6 +6,7 @@
 
 import yaml
 from pprint import pprint
+from datetime import datetime
 
 obj = {"streams": dict()}
 
@@ -35,6 +36,10 @@ tables = [
     "secondary_auditors",
 ]
 
+# Although this attempts to generate files all the way out to 2030,
+# it will not generate anything where data does not exist.
+# This future-proofs us for a year or two, so we don't have to worry
+# about updating the `sling` script that is created.
 years = range(2016, 2031)
 
 
@@ -42,10 +47,18 @@ for t in tables:
     ndx = 0
     for y in years:
         obj["streams"][f"{schema}.{t}.{ndx}"] = {
-            "object": f"bulk_export/{{MM}}/{t}_{y}.csv",
+            "object": f"bulk_export/{{MM}}/{y}_{t}.csv",
             "sql": f"SELECT * FROM {schema}.{t} WHERE audit_year = '{y}'",
             "mode": "full-refresh",
+            "target_options": {
+                "format": "csv",
+            },
         }
         ndx += 1
 
+today = datetime.today().strftime("%Y-%m-%d")
+print("# DO NOT EDIT; THIS IS A GENERATED FILE")
+print("# python create_stream_yaml.py > public_data_v1_0_0_single_csv.yaml")
+print(f"# Last generated {today}")
+print()
 print(yaml.dump(obj))
