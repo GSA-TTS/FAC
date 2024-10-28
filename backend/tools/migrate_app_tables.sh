edf2c1fbcef682c2d6c1eb354aeaf7a4a0772182
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-10-28 15:21:33 -0400

    Fake audit, test if migrations will go through

--- backend/tools/migrate_app_tables.sh
+++ backend/tools/migrate_app_tables.sh
@@ -2,7 +2,8 @@ source tools/util_startup.sh
 
 function migrate_app_tables {
     startup_log "MIGRATE_APP_TABLES" "BEGIN"
-    python manage.py migrate
+    # python manage.py migrate
+    python manage.py migrate --fake audit
     local result=$?
     startup_log "MIGRATE_APP_TABLES" "END"
     return $result
