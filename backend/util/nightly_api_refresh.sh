04970aaebaef9c83b0d2c0961c9e8f9a7180a1ed
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-10-29 15:33:46 -0400

    Use pushd & popd to put us where we need to be for source & other utils

--- backend/util/nightly_api_refresh.sh
+++ backend/util/nightly_api_refresh.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
-source tools/util_startup.sh
-source tools/setup_env.sh
-source tools/sql_pre_post.sh
-setup_env
+pushd ..
+    source tools/util_startup.sh
+    source tools/setup_env.sh
+    source tools/sql_pre_post.sh
+    setup_env
 
-# Run an RDS backup, to refresh dissemination_* in FAC_SNAPSHOT from FAC_DB
-#./../fac-backup-util.sh "v0.1.8" "rds_backup"
-./../fac-backup-util.sh "$1" "$2"
-gonogo "fac-backup-util"
+    # Run an RDS backup, to refresh dissemination_* in FAC_SNAPSHOT from FAC_DB
+    #./../fac-backup-util.sh "v0.1.8" "rds_backup"
+    ./fac-backup-util.sh "$1" "$2"
+    gonogo "fac-backup-util"
 
+popd
 # Run the pre/post.
 sql_pre_fac_snapshot_db
 gonogo "sql_pre_fac_snapshot_db"
