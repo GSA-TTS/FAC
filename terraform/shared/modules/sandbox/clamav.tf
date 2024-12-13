6b8680ee3d4f919f5c697f8f01aa543f5b7125b9
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-12-13 09:16:26 -0500

    ClamAV version 2.0.0
    - Removes app_name_or_id for network policies
    - Reintroduces external network policy for clamav -> proxy
    - Deployed successfully around 9a with v2.0.0 clamav module and was able to successfully
    validate a file at around 916a without any issues

--- terraform/shared/modules/sandbox/clamav.tf
+++ terraform/shared/modules/sandbox/clamav.tf
@@ -7,32 +7,31 @@ data "docker_registry_image" "clamav" {
 }
 
 module "clamav" {
-  source = "github.com/gsa-tts/terraform-cloudgov//clamav?ref=v1.1.0"
+  source = "github.com/gsa-tts/terraform-cloudgov//clamav?ref=v2.0.0"
 
   # This generates eg "fac-av-staging.apps.internal", avoiding collisions with routes for other projects and spaces
-  name           = local.clam_name
-  app_name_or_id = "gsa-fac"
+  name = local.clam_name
 
   cf_org_name   = var.cf_org_name
   cf_space_name = var.cf_space_name
+
   clamav_image  = "ghcr.io/gsa-tts/fac/clamav@${data.docker_registry_image.clamav.sha256_digest}"
+  clamav_memory = var.clamav_memory
   max_file_size = "30M"
   instances     = var.clamav_instances
-  clamav_memory = var.clamav_memory
 
   proxy_server   = module.https-proxy.domain
   proxy_port     = module.https-proxy.port
   proxy_username = module.https-proxy.username
   proxy_password = module.https-proxy.password
-  # depends_on     = [module.fac-app.app_id]
-  # depends_on     = [ module.https-proxy.https_proxy ]
 }
 
-# resource "cloudfoundry_network_policy" "clamav-network-policy" {
-#   policy {
-#     source_app      = module.clamav.app_id
-#     destination_app = module.https-proxy.app_id
-#     port            = "61443"
-#     protocol        = "tcp"
-#   }
-# }
+resource "cloudfoundry_network_policy" "clamav-network-policy" {
+  provider = cloudfoundry-community
+  policy {
+    source_app      = module.clamav.app_id
+    destination_app = module.https-proxy.app_id
+    port            = "61443"
+    protocol        = "tcp"
+  }
+}
