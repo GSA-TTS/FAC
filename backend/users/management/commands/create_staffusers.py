cbc39b3a02b8de0a316268adf2fc60b45792bbaa
Bobby Novak <176936850+rnovak338@users.noreply.github.com>
2024-12-24 10:17:50 -0500

    Fix duplicate user issue
    Fixes the scenario where there are 2 users of the same email in the DB (`.get` only works with one record).

--- backend/users/management/commands/create_staffusers.py
+++ backend/users/management/commands/create_staffusers.py
@@ -90,9 +90,10 @@ class Command(BaseCommand):
                         ).save()
 
                         # attempt to update the user.
-                        try:
-                            user = User.objects.get(email=email, is_staff=True)
+                        user = User.objects.filter(email=email, is_staff=True)
 
+                        if user.exists():
+                            user = user.first()
                             user.groups.clear()
                             match role:
                                 case "readonly":
@@ -106,9 +107,9 @@ class Command(BaseCommand):
                             user.save()
                             logger.info(f"Synced {email} to a StaffUser role.")
 
-                        # for whatever reason, this failed. Revert staffuser creation.
-                        except User.DoesNotExist:
+                        else:
                             transaction.set_rollback(True)
                             logger.warning(
                                 f"StaffUser not created for {email}, they have not logged in yet."
                             )
+                            
