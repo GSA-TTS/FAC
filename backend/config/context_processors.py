05434861a3885825d613e558b98993407f9c557b
Bobby Novak <176936850+rnovak338@users.noreply.github.com>
2025-01-23 11:22:28 -0500

    Re-position context processor

--- backend/config/context_processors.py
+++ backend/config/context_processors.py
@@ -10,6 +10,57 @@ def static_site_url(request):
     return {"STATIC_SITE_URL": settings.STATIC_SITE_URL}
 
 
+def omb_num_exp_date(request):
+    """
+    Returns the OMB_NUMBER (str) and OMB_EXP_DATE (str) in template context form.
+    Displayed as a legal requirement on the header of every page.
+    """
+    return {"OMB_NUMBER": settings.OMB_NUMBER, "OMB_EXP_DATE": settings.OMB_EXP_DATE}
+
+
+def current_environment(request):
+    """
+    Returns the ENVIRONMENT (str) in template context form.
+    Used in determining the display of the TEST SITE Banner.
+    """
+    return {"ENVIRONMENT": settings.ENVIRONMENT}
+
+
+def maintenance_banner(request):
+    """
+    Returns maintenance banner template context.
+    MAINTENANCE_BANNER is True if the banner should be displaying and False if not, based on settings.MAINTENANCE_BANNER_DATES.
+    MAINTENANCE_BANNER_START_TIME and MAINTENANCE_BANNER_END_TIME are None if the banner does not display.
+    MAINTENANCE_BANNER_MESSAGE is included if it exists alongside the banner dates.
+    """
+    current_time = datetime.now(timezone.utc)
+    context = {
+        "MAINTENANCE_BANNER": False,
+    }
+
+    # For every designated date range:
+    # If any start or end time is unavailable, something is misconfigured. So, disable the banner.
+    # If we are within the specified timeframes, enable the banner.
+    for date_range in settings.MAINTENANCE_BANNER_DATES:
+        start_time = date_range.get("start")
+        end_time = date_range.get("end")
+
+        if not start_time or not end_time:
+            return context
+
+        if current_time > start_time and current_time < end_time:
+            context["MAINTENANCE_BANNER"] = True
+            context = context | {
+                "MAINTENANCE_BANNER_START_TIME": start_time,
+                "MAINTENANCE_BANNER_END_TIME": end_time,
+                "MAINTENANCE_BANNER_MESSAGE": date_range.get("message", ""),
+            }
+            return context
+
+    # Base case - we are not within any of the given timeframes. Disable the banner.
+    return context
+
+
 def navigation_content(request):
     """
     Returns the tree of navigation links. This is added to the context of every template
@@ -131,54 +182,3 @@ def navigation_content(request):
             },
         ]
     }
-
-
-def omb_num_exp_date(request):
-    """
-    Returns the OMB_NUMBER (str) and OMB_EXP_DATE (str) in template context form.
-    Displayed as a legal requirement on the header of every page.
-    """
-    return {"OMB_NUMBER": settings.OMB_NUMBER, "OMB_EXP_DATE": settings.OMB_EXP_DATE}
-
-
-def current_environment(request):
-    """
-    Returns the ENVIRONMENT (str) in template context form.
-    Used in determining the display of the TEST SITE Banner.
-    """
-    return {"ENVIRONMENT": settings.ENVIRONMENT}
-
-
-def maintenance_banner(request):
-    """
-    Returns maintenance banner template context.
-    MAINTENANCE_BANNER is True if the banner should be displaying and False if not, based on settings.MAINTENANCE_BANNER_DATES.
-    MAINTENANCE_BANNER_START_TIME and MAINTENANCE_BANNER_END_TIME are None if the banner does not display.
-    MAINTENANCE_BANNER_MESSAGE is included if it exists alongside the banner dates.
-    """
-    current_time = datetime.now(timezone.utc)
-    context = {
-        "MAINTENANCE_BANNER": False,
-    }
-
-    # For every designated date range:
-    # If any start or end time is unavailable, something is misconfigured. So, disable the banner.
-    # If we are within the specified timeframes, enable the banner.
-    for date_range in settings.MAINTENANCE_BANNER_DATES:
-        start_time = date_range.get("start")
-        end_time = date_range.get("end")
-
-        if not start_time or not end_time:
-            return context
-
-        if current_time > start_time and current_time < end_time:
-            context["MAINTENANCE_BANNER"] = True
-            context = context | {
-                "MAINTENANCE_BANNER_START_TIME": start_time,
-                "MAINTENANCE_BANNER_END_TIME": end_time,
-                "MAINTENANCE_BANNER_MESSAGE": date_range.get("message", ""),
-            }
-            return context
-
-    # Base case - we are not within any of the given timeframes. Disable the banner.
-    return context
