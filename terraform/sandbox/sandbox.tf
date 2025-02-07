88e085008594fba1938814d10d495ad8bea73713
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-02-07 11:49:10 -0500

    Upgrade resources

--- terraform/sandbox/sandbox.tf
+++ terraform/sandbox/sandbox.tf
@@ -9,6 +9,7 @@ module "sandbox" {
   login_secret_key        = var.login_secret_key
   branch_name             = var.branch_name
   backups_s3_id           = module.sandbox-backups-bucket.bucket_id
+  cf_space_id             = data.cloudfoundry_space.space.id
 
   database_plan         = "medium-gp-psql"
   https_proxy_instances = 1
@@ -20,11 +21,45 @@ module "sandbox" {
 }
 
 module "sandbox-backups-bucket" {
-  source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v1.1.0"
+  source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v2.1.0"
 
-  cf_org_name   = var.cf_org_name
-  cf_space_name = "sandbox"
-  name          = "backups"
-  s3_plan_name  = "basic"
-  tags          = ["s3"]
+  cf_space_id  = data.cloudfoundry_space.space.id
+  name         = "backups"
+  s3_plan_name = "basic"
+  tags         = ["s3"]
+}
+
+# imports
+import {
+  to = module.sandbox.module.snapshot-database.cloudfoundry_service_instance.rds
+  id = "c5f6bddd-77a8-4af1-a1c1-157c24a0920b"
+}
+import {
+  to = module.sandbox.module.database.cloudfoundry_service_instance.rds
+  id = "4f22d420-7be5-43b5-b298-bb73760b6aa8"
+}
+
+import {
+  to = module.sandbox.module.s3-private.cloudfoundry_service_instance.bucket
+  id = "d4da0886-ab87-40aa-a2c3-ce415e298752"
+}
+
+import {
+  to = module.sandbox.module.s3-public.cloudfoundry_service_instance.bucket
+  id = "24ab1bca-286e-4a4f-ae5d-3b46d7e6547c"
+}
+
+import {
+  to = module.sandbox-backups-bucket.cloudfoundry_service_instance.bucket
+  id = "a9785caf-cc2b-4135-a79f-819022933e00"
+}
+
+# Stuff used for apps in this space
+data "cloudfoundry_org" "org" {
+  name = var.cf_org_name
+}
+
+data "cloudfoundry_space" "space" {
+  name = "sandbox"
+  org  = data.cloudfoundry_org.org.id
 }
