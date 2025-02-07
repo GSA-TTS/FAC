d3be6a87c0abc856e5f273246acbe374cc63d050
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-02-07 11:49:10 -0500

    Upgrade resources

--- terraform/shared/modules/sandbox/env.tf
+++ terraform/shared/modules/sandbox/env.tf
@@ -1,7 +1,7 @@
 module "database" {
   source = "github.com/gsa-tts/terraform-cloudgov//database?ref=v2.1.0"
 
-  cf_space_id   = data.cloudfoundry_space.space.id
+  cf_space_id   = var.cf_space_id
   name          = "fac-db"
   tags          = ["rds"]
   rds_plan_name = var.database_plan
@@ -11,7 +11,7 @@ module "database" {
 module "snapshot-database" {
   source = "github.com/gsa-tts/terraform-cloudgov//database?ref=v2.1.0"
 
-  cf_space_id   = data.cloudfoundry_space.space.id
+  cf_space_id   = var.cf_space_id
   name          = "fac-snapshot-db"
   tags          = ["rds"]
   rds_plan_name = var.database_plan
@@ -19,35 +19,25 @@ module "snapshot-database" {
 }
 
 module "s3-public" {
-  source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v1.1.0"
+  source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v2.1.0"
 
-  cf_org_name   = var.cf_org_name
-  cf_space_name = var.cf_space_name
-  name          = "fac-public-s3"
-  s3_plan_name  = "basic-public"
-  tags          = ["s3"]
+  cf_space_id  = var.cf_space_id
+  name         = "fac-public-s3"
+  s3_plan_name = "basic-public"
+  tags         = ["s3"]
 }
 
 module "s3-private" {
-  source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v1.1.0"
+  source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v2.1.0"
 
-  cf_org_name   = var.cf_org_name
-  cf_space_name = var.cf_space_name
-  name          = "fac-private-s3"
-  s3_plan_name  = "basic"
-  tags          = ["s3"]
+  cf_space_id  = var.cf_space_id
+  name         = "fac-private-s3"
+  s3_plan_name = "basic"
+  tags         = ["s3"]
 }
+
 
 # Stuff used for apps in this space
-data "cloudfoundry_org" "org" {
-  name = var.cf_org_name
-}
-
-data "cloudfoundry_space" "space" {
-  name = var.cf_space_name
-  org  = data.cloudfoundry_org.org.id
-}
-
 data "cloudfoundry_space" "apps" {
   provider = cloudfoundry-community
   org_name = var.cf_org_name
