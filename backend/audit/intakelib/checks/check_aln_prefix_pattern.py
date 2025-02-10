bed6ceaea3fd7cdb013bf8f88ce8bcbc046aceea
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2025-02-10 14:38:10 -0500

    Reverting check_aln_prefix_pattern.py

--- backend/audit/intakelib/checks/check_aln_prefix_pattern.py
+++ backend/audit/intakelib/checks/check_aln_prefix_pattern.py
@@ -1,14 +1,12 @@
-import json
 import logging
 import re
-
 from django.conf import settings
+
 from django.core.exceptions import ValidationError
-
-from audit.fixtures.excel import FEDERAL_AWARDS_JSONSCHEMA
+from ..intermediate_representation import (
+    get_range_by_name,
+)
 from ..common import get_message, build_cell_error_tuple
-from ..intermediate_representation import get_range_by_name
-
 
 logger = logging.getLogger(__name__)
 
@@ -17,11 +15,6 @@ logger = logging.getLogger(__name__)
 # The ALN prefix represents a two-digit number ##
 # TESTED BY has_bad_aln_prefix.xlsx
 def aln_agency_prefix(ir):
-    json_input = json.loads(FEDERAL_AWARDS_JSONSCHEMA.read_text(encoding="utf-8"))
-    print("#########")
-    print(json_input["filename"])
-    print(json_input["sheets"][3]["name"])
-    print("#########")
     prefixes = get_range_by_name(ir, "federal_agency_prefix")
     errors = []
     for index, prefix in enumerate(prefixes["values"]):
