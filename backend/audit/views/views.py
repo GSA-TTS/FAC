ac035c14f9b4c1859e033de0ed8435f1bb8f9c97
Jason Rothacker <jason.rothacker@gsa.gov>
2025-02-12 14:18:18 -0800

    Updating the migration script, generated fields

--- backend/audit/views/views.py
+++ backend/audit/views/views.py
@@ -45,6 +45,7 @@ from audit.validators import (
 from audit.verify_status import verify_status
 
 from audit.context import set_sac_to_context
+from dissemination.docs import major_program
 from dissemination.remove_workbook_artifacts import remove_workbook_artifacts
 from dissemination.file_downloads import get_download_url, get_filename
 from dissemination.models import General
@@ -779,48 +780,117 @@ class SubmissionView(CertifyingAuditeeRequiredMixin, generic.View):
                 return redirect(reverse("audit:MySubmissions"))
             raise
 
-# TODO: We'll want to calculate the cog/oversite for the audit same way as sac, for now just use the sac one
+# TODO:
+#    1) We'll want to calculate the cog/oversite for the audit same way as sac, for now just use the sac one
+#    2) Move this somewhere else... this file huge
 def _compute_additional_audit_fields(audit, sac):
     audit_year, fy_end_month, _ = audit.audit["general_information"]["auditee_fiscal_period_end"].split("-")
     cognizant_agency = sac.cognizant_agency
     oversight_agency = sac.oversight_agency
 
-    passthrough = _load_passthrough(audit.audit["federal_awards"]["awards"])
-    program_names = [fa["program"]["program_name"] for fa in audit.audit["federal_awards"]["awards"]]
-    findings = 0
-    for finding in audit.audit.get("findings_uniform_guidance", []):
-        for mask in FINDINGS_FIELD_TO_BITMASK:
-            if finding.get(mask, "N") == "Y":
-                findings &= mask.mask
-        # TODO: Repeat is a bit different
-
     is_public = audit.audit["general_information"]["user_provided_organization_type"] != "tribal" or \
                 audit.audit["tribal_data_consent"]["is_tribal_information_authorized_to_be_public"]
-
+    awards_indexes = _index_awards(audit.audit)
+    findings_indexes = _index_findings(audit.audit)
+    general_indexes = _index_general(audit.audit)
+    search_indexes = {
+        **awards_indexes,
+        **findings_indexes,
+        **general_indexes
+    }
     audit.audit.update({
-        "audit_year": audit_year,
-        "cognizant_agency": cognizant_agency,
-        "oversight_agency": oversight_agency,
-        "fy_end_month": fy_end_month,
-        "passthrough": passthrough,
-        "program_names": program_names,
-        "is_public": is_public,
-        "findings_summary": findings,
+            "audit_year": audit_year,
+            "cognizant_agency": cognizant_agency,
+            "oversight_agency": oversight_agency,
+            "fy_end_month": fy_end_month,
+            "is_public": is_public,
+
+        "search_indexes": search_indexes
     })
     audit.save()
 
-def _load_passthrough(federal_awards):
+def _index_findings(audit_data):
+    findings = 0
+    compliance_requirements = set()
+    for finding in audit_data.get("findings_uniform_guidance", []):
+        for mask in FINDINGS_FIELD_TO_BITMASK:
+            if finding.get(mask, "N") == "Y":
+                findings &= mask.mask
+        if finding.get("finding", {}).get("repeat_prior_reference", "N") == "Y":
+            findings &= FINDINGS_BITMASK.REPEAT_FINDING
 
-    pass_objects = []
-    for entry in federal_awards:
-        entities = entry.get("direct_or_indirect_award", {}).get("entities", [])
-        for entity in entities:
-            passthrough = {
-                "award_reference" : entry["award_reference"],
-                "passthrough_id" : entity.get("passthrough_identifying_number", ""),
-                "passthrough_name": entity["passthrough_name"],
-            }
-            pass_objects.append(passthrough)
+        compliance_requirement = finding.get("program", {}).get("compliance_requirement", "")
+        compliance_requirements.add(compliance_requirement)
 
-    return pass_objects
+    return {
+        "findings_summary": findings,
+        "compliance_requirements": list(compliance_requirements),
+    }
+
+def _index_awards(audit_data):
+    """
+    Method for pulling out all the data from awards that we search on, to improve
+    search performance.
+    """
+    program_names = []
+    passthrough_names = set()
+    agency_prefixes = set()
+    agency_extensions = set()
+    has_direct_funding = False
+    has_indirect_funding = False
+    is_major_program = False
+
+    for award in audit_data.get("federal_awards", {}).get("awards", []):
+        program = award.get("program", {})
+        if program.get("program_name", ""):
+            program_names.append(award["program"]["program_name"])
+        if program.get("is_major", "N") == "Y":
+            is_major_program = True
+        agency_prefixes.add(program.get("federal_agency_prefix", ""))
+        agency_extensions.add(program.get("three_digit_extension", ""))
+
+        if award.get("direct_or_indirect_award", {}).get("is_direct", "") == 'Y':
+            has_direct_funding = True
+        elif award.get("direct_or_indirect_award", {}).get("is_direct", "") == 'N':
+            has_indirect_funding = True
+        passthrough_names.update([entity.get("passthrough_name", None) for entity in
+                                  award.get("direct_or_indirect_award",
+                                            {}).get("entities", [])])
+
+    return {
+        "program_names": program_names,
+        "has_direct_funding": has_direct_funding,
+        "has_indirect_funding": has_indirect_funding,
+        "is_major_program": is_major_program,
+        "passthrough_names": list(passthrough_names),
+        "agency_extensions": list(agency_extensions),
+        "agency_prefixes": list(agency_prefixes)
+    }
+
+def _index_general(audit_data):
+
+    general_information = audit_data.get("general_information", {})
+
+    search_names = set()
+    general_fields = ["auditee_contact_name", "auditee_email", "auditee_name", "auditor_contact_name", "auditor_email", "auditor_firm_name"]
+    for field in general_fields:
+        search_names.add(general_information.get(field, ""))
+
+    # Also search over certification
+    auditee_certify_name = audit_data.get(
+        "auditee_certification", {}).get(
+        "auditee_signature",{}).get(
+        "auditee_name", "")
+    auditor_certify_name = audit_data.get(
+        "auditor_certification", {}).get(
+        "auditor_signature",{}).get(
+        "auditor_name", "")
+
+    search_names.add(auditee_certify_name)
+    search_names.add(auditor_certify_name)
+
+    return {
+        "search_names": list(search_names),
+    }
+
 # 2023-08-22 DO NOT ADD ANY FURTHER CODE TO THIS FILE; ADD IT IN viewlib AS WITH UploadReportView
--- backend/dissemination/search_utils.py
+++ backend/dissemination/search_utils.py
@@ -3,15 +3,14 @@ import re
 from enum import Enum
 from collections import namedtuple as NT
 
-from django.db.models import Q, Func
-from django.forms import BooleanField
+from django.db.models import Q, F
 
 from audit.models.constants import FINDINGS_FIELD_TO_BITMASK, FINDINGS_BITMASK
 from dissemination.searchlib.search_constants import text_input_delimiters
 
 logger = logging.getLogger(__name__)
 
-# TODO: Clean-up
+# TODO: Clean-up: Split all the search helpers into separate files?
 class SEARCH_FIELDS(Enum):
     AUDIT_YEARS = "audit_years"
     AUDITEE_STATE = "auditee_state"
@@ -31,27 +30,8 @@ class SEARCH_FIELDS(Enum):
     PASSTHROUGH_NAME = "passthrough_name"
     TYPE_REQUIREMENT = "type_requirement"
 
-class JSONBitwiseOr(Func):
-    function = 'jsonb_bitwise_or'
-    arity = 4
-    output_field = BooleanField()
-
 def _search_names(params):
-    names_list = params.get(SEARCH_FIELDS.NAMES)
-    # TODO: Confirm
-    name_fields = [
-        # "auditee_city",
-        "general_information__auditee_contact_name",
-        "auditee_certification__auditee_name",
-        "general_information__auditee_contact_name__auditee_email",
-        "general_information__auditee_name",
-        "general_information__auditor_contact_name",
-        "auditor_certification__auditor_signature__auditor_name",
-        "general_information__auditor_email",
-        "general_information__auditor_firm_name",
-    ]
-
-    names_match = Q()
+    names_list = params.get(SEARCH_FIELDS.NAMES.value)
 
     # The search terms are coming in as a string in a list.
     # E.g. the search text "college berea" returns nothing,
@@ -65,29 +45,17 @@ def _search_names(params):
         for sub in term.split():
             flattened.append(sub)
 
-    # Now, for each field (e.g. "auditee_contact_name")
-    # build up an AND over the terms. We want something where all of the
-    # terms appear.
-    # Then, do an OR over all of the fields. If that combo appears in
-    # any of the fields, we want to return it.
-    for field in name_fields:
-        field_q = Q()
-        for name in flattened:
-            field_q.add(Q(**{f"audit__{field}__icontains": name}), Q.AND)
-        names_match.add(field_q, Q.OR)
-
-    # Now, "college berea" and "university state ohio" return
-    # the appropriate terms. It is also significantly faster than what
-    # we had before.
-    return names_match
+    # TODO: This is probably wrong...
+    return Q(search_names__overlap=flattened) if flattened else Q()
 
 def _search_uei_ein(params):
-    uei_ein = params.get(SEARCH_FIELDS.UEI_EIN)
-    return Q(audit__general_information__auditee_ein__in=uei_ein) | \
-            Q(audit__general_information__auditee_uei__in=uei_ein) | \
-            Q(audit__additional_eins__contains=uei_ein) | \
-            Q(audit__additional_ueis__contains=uei_ein)
+    uei_ein = params.get(SEARCH_FIELDS.UEI_EIN.value)
+    return Q(auditee_ein__in=uei_ein) | \
+            Q(auditee_uei__in=uei_ein) | \
+            Q(additional_eins__contains=uei_ein) | \
+            Q(additional_ueis__contains=uei_ein)
 
+# TODO: Generated Field Exists... Update search
 def _search_alns(params):
     full_alns = _get_full_alns(params)
     agency_numbers = _get_agency_numbers(params)
@@ -115,8 +83,8 @@ def _search_cog_or_oversight(params):
     if cog_or_oversight.lower() in ["", "either"]:
         if agency_name:
             return Q(
-                Q(audit__cognizant_agency__in=[agency_name])
-                | Q(audit__oversight_agency__in=[agency_name])
+                Q(cognizant_agency__in=[agency_name])
+                | Q(oversight_agency__in=[agency_name])
             )
         else:
             # Every submission should have a value for either cog or over, so
@@ -124,16 +92,16 @@ def _search_cog_or_oversight(params):
             return Q()
     elif cog_or_oversight.lower() == "cog":
         if agency_name:
-            return Q(audit__cognizant_agency__in=[agency_name])
+            return Q(cognizant_agency__in=[agency_name])
         else:
             # Submissions that have any cog
-            return Q(audit__cognizant_agency__isnull=False)
+            return Q(cognizant_agency__isnull=False)
     elif cog_or_oversight.lower() == "oversight":
         if agency_name:
-            return Q(audit__oversight_agency__in=[agency_name])
+            return Q(oversight_agency__in=[agency_name])
         else:
             # Submissions that have any over
-            return Q(audit__oversight_agency__isnull=False)
+            return Q(oversight_agency__isnull=False)
 
 def _search_federal_program_name(params):
     query = Q()
@@ -143,7 +111,7 @@ def _search_federal_program_name(params):
         rsplit = re.compile("|".join(text_input_delimiters)).split
 
         for sub in [s.strip() for s in rsplit(name)]:
-            sub_query.add(Q(audit__program_names__icontains=sub), Q.AND)
+            sub_query.add(Q(program_names__icontains=sub), Q.AND)
 
         query.add(sub_query, Q.OR)
     return query
@@ -158,9 +126,9 @@ def _search_findings(params):
         for mask in FINDINGS_FIELD_TO_BITMASK:
             if mask.search_param in findings_fields:
                 findings_mask = findings_mask | mask.mask
-    # TODO: Figure out how to get the query to work on the generated field
+    # TODO: Found on stackoverflow, still not certain it is correct...
     # where findings_summary & findings_mask > 0
-    return Q()
+    return Q(findings_summary__gt=0) & Q(findings_summary__lt=F('findings_summary').bitor(findings_mask)) if findings_mask else Q()
 
 def _search_direct_funding(params):
     q = Q()
@@ -168,20 +136,11 @@ def _search_direct_funding(params):
     for field in direct_funding_fields:
         match field:
             case "direct_funding":
-                q |= Q(audit__federal_awards__awards__contains=[{"direct_or_indirect_award": {"is_direct":"Y"}}])
+                q |= Q(has_direct_funding__eq=True)
             case "passthrough_funding":
-                q |= Q(audit__federal_awards__awards__contains=[{"direct_or_indirect_award": {"is_direct":"N"}}])
+                q |= Q(has_indirect_funding__eq=False)
             case _:
                 pass
-    return q
-
-def _search_major_program(params):
-    q = Q()
-    major_program_fields = params.get("major_program")
-    if "True" in major_program_fields:
-        q |= Q(audit__federal_awards__awards__contains=[{"program": {"is_major": "Y"}}])
-    elif "False" in major_program_fields:
-        q |= Q(audit__federal_awards__awards__contains=[{"program": {"is_major": "N"}}])
     return q
 
 def _search_passthrough_name(params):
@@ -190,16 +149,17 @@ def _search_passthrough_name(params):
     for term in passthrough_names:
         q_sub = Q()
         for sub in term.split():
-            q_sub.add(Q(audit__passthrough__contains=[{"passthrough_name": sub}]), Q.AND)
+            q_sub.add(Q(passthrough__contains=[{"passthrough_name": sub}]), Q.AND)
         q.add(q_sub, Q.OR)
     return q
 
-def _search_type_requirement(params):
+def _search_major_program(params):
     q = Q()
-    type_requirements = params.get("type_requirement")
-    for tr in type_requirements:
-        q |= Q(audit__findings_uniform_guidance__contains=[{"program": { "compliance_requirement": tr}}])
-
+    major_program_fields = params.get("major_program")
+    if "True" in major_program_fields:
+        q |= Q(is_major_program=True)
+    elif "False" in major_program_fields:
+        q |= Q(is_major_program=False)
     return q
 
 ALN = NT("ALN", "prefix, program")
@@ -228,12 +188,11 @@ def _get_full_alns(params):
                 split_alns.update([ALN(split_aln[0], split_aln[1])])
     return split_alns
 
-# TODO: Update to use generated fields
 SEARCH_QUERIES = {
     SEARCH_FIELDS.AUDIT_YEARS: lambda params : Q(audit_year__in=params.get(SEARCH_FIELDS.AUDIT_YEARS.value)),
     SEARCH_FIELDS.AUDITEE_STATE: lambda params : Q(auditee_state__in=[params.get(SEARCH_FIELDS.AUDITEE_STATE.value)]),
     SEARCH_FIELDS.NAMES: _search_names,
-    SEARCH_FIELDS.UEI_EIN: lambda params : _search_uei_ein,
+    SEARCH_FIELDS.UEI_EIN: _search_uei_ein,
     SEARCH_FIELDS.START_DATE: lambda params : Q(fac_accepted_date__date__gte=params.get(SEARCH_FIELDS.START_DATE.value)),
     SEARCH_FIELDS.END_DATE: lambda params : Q(fac_accepted_date__date__lte=params.get(SEARCH_FIELDS.END_DATE.value)),
     SEARCH_FIELDS.FY_END_MONTH: lambda params : Q(fy_end_month=params.get(SEARCH_FIELDS.FY_END_MONTH.value)),
@@ -247,31 +206,5 @@ SEARCH_QUERIES = {
     SEARCH_FIELDS.DIRECT_FUNDING: _search_direct_funding,
     SEARCH_FIELDS.MAJOR_PROGRAM: _search_major_program,
     SEARCH_FIELDS.PASSTHROUGH_NAME: _search_passthrough_name,
-    SEARCH_FIELDS.TYPE_REQUIREMENT: _search_type_requirement,
+    SEARCH_FIELDS.TYPE_REQUIREMENT: lambda params : Q(compliance_requirements__overlap=params.get(SEARCH_FIELDS.TYPE_REQUIREMENT.value)) if params.get(SEARCH_FIELDS.TYPE_REQUIREMENT.value) else Q()
 }
-
-## TODO: Fields to Index
-# audit__audit_year
-# audit__general_information__auditee_state
-# audit__fac_accepted_date\
-# audit__fy_end_month
-# audit__general_information__user_provided_organization_type
-# report_id
-# general_information__auditee_contact_name,
-# auditee_certification__auditee_name,
-# general_information__auditee_contact_name__auditee_email,
-# general_information__auditee_name,
-# general_information__auditor_contact_name,
-# auditor_certification__auditor_signature__auditor_name,
-# general_information__auditor_email,
-# general_information__auditor_firm_name
-# audit__general_information__auditee_ein
-# audit__general_information__auditee_uei
-# audit__additional_eins
-# audit__additional_ueis
-# audit__federal_awards__awards__program__federal_agency_prefix
-# audit__federal_awards__awards__program__is_major
-# audit__federal_awards__awards__direct_or_indirect_award__is_direct
-# audit__oversight_agency
-# audit__cognizant_agency
-# audit_program_names
