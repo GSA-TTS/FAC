a8302d1038e590a435b7ca265977258de27620a5
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-02-14 10:08:29 -0500

    Meta Provider Update

--- terraform/production/providers-managed.tf
+++ terraform/production/providers-managed.tf
@@ -6,6 +6,10 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     cloudfoundry = {
+      source  = "cloudfoundry/cloudfoundry"
+      version = "1.1.0"
+    }
+    cloudfoundry-community = {
       source  = "cloudfoundry-community/cloudfoundry"
       version = "~>0.53.1"
     }
@@ -23,6 +27,14 @@ terraform {
 }
 
 provider "cloudfoundry" {
+  api_url  = "https://api.fr.cloud.gov"
+  user     = var.cf_user
+  password = var.cf_password
+}
+
+# Backwards compatability provider
+# https://github.com/GSA-TTS/terraform-cloudgov/blob/main/UPGRADING.md#using-v1-and-v2-together
+provider "cloudfoundry-community" {
   api_url  = "https://api.fr.cloud.gov"
   user     = var.cf_user
   password = var.cf_password
