2eb468ec970610f7d2e1b67d9211297f829f8533
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-02-20 14:31:33 -0500

    Move sandbox imports

---
+++ terraform/sandbox/imports.tf
@@ -0,0 +1,23 @@
+import {
+  to = module.sandbox.module.snapshot-database.cloudfoundry_service_instance.rds
+  id = "c5f6bddd-77a8-4af1-a1c1-157c24a0920b"
+}
+import {
+  to = module.sandbox.module.database.cloudfoundry_service_instance.rds
+  id = "4f22d420-7be5-43b5-b298-bb73760b6aa8"
+}
+
+import {
+  to = module.sandbox.module.s3-private.cloudfoundry_service_instance.bucket
+  id = "d4da0886-ab87-40aa-a2c3-ce415e298752"
+}
+
+import {
+  to = module.sandbox.module.s3-public.cloudfoundry_service_instance.bucket
+  id = "24ab1bca-286e-4a4f-ae5d-3b46d7e6547c"
+}
+
+import {
+  to = module.sandbox-backups-bucket.cloudfoundry_service_instance.bucket
+  id = "a9785caf-cc2b-4135-a79f-819022933e00"
+}
