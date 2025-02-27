de1491a78d4f2bb7d1d786f000395eba8cc9cbb1
James Person <jperson1@umbc.edu>
2025-02-27 12:42:35 -0500

    Add more audit endpoints to newrelic monitoring.
    Restructure endpoints template for more control over endpoint and method
    
    * Homepage
    * Audit submissions homepage
    * All excel uploads (not downloads)
    * PDF Uploads (not downloads)
    * Cross val (just the actual validation from POST)
    * Single summary report downloads (not multiple from search)

--- terraform/shared/modules/newrelic/monitoring.tf
+++ terraform/shared/modules/newrelic/monitoring.tf
@@ -14,16 +14,60 @@ locals {
     endpoint_config = [
       { name             = "UEI Validation"
         uri              = "/api/sac/ueivalidation"
+        method           = "POST"
         transactions_sla = { critical = 1, warning = 5 }         # Number of Transactions per hour
         success_rate_sla = { critical = 0.975, warning = 0.985 } # Success Rate Percentage
         latency_sla      = { critical = 1000, warning = 800 }    # Average Latency over a week, in ms
       },
       { name             = "Submit Audit"
-        uri              = "/audit/submission"
-        transactions_sla = { critical = 100, warning = 250 }    # Number of Transactions per hour
+        uri              = "/audit/submission/%"                # '/audit/submission%' was also catching '/audit/submission-progress'
+        method           = "POST"
+        transactions_sla = { critical = 5, warning = 8 }        # Number of Transactions per hour
         success_rate_sla = { critical = 0.99, warning = 0.995 } # Success Rate Percentage
-        latency_sla      = { critical = 500, warning = 450 }    # Average Latency over a week, in ms
-      }
+        latency_sla      = { critical = 1500, warning = 1200 }  # Average Latency over a week, in ms
+      },
+      { name             = "Homepage"
+        uri              = "/"
+        method           = "GET"
+        transactions_sla = { critical = 100, warning = 150 }    # Number of Transactions per hour
+        success_rate_sla = { critical = 0.99, warning = 0.995 } # Success Rate Percentage
+        latency_sla      = { critical = 350, warning = 250 }    # Average Latency over a week, in ms
+      },
+      { name             = "Audit Submissions Homepage"
+        uri              = "/audit/"
+        method           = "GET"
+        transactions_sla = { critical = 50, warning = 70 }      # Number of Transactions per hour
+        success_rate_sla = { critical = 0.99, warning = 0.995 } # Success Rate Percentage
+        latency_sla      = { critical = 1500, warning = 1200 }  # Average Latency over a week, in ms
+      },
+      { name             = "All Excel Uploads"
+        uri              = "/audit/excel/%"
+        method           = "POST"
+        transactions_sla = { critical = 20, warning = 25 }       # Number of Transactions per hour
+        success_rate_sla = { critical = 0.99, warning = 0.995 }  # Success Rate Percentage
+        latency_sla      = { critical = 12000, warning = 10000 } # Average Latency over a week, in ms
+      },
+      { name             = "PDF Uploads"
+        uri              = "/audit/upload-report/%"
+        method           = "POST"
+        transactions_sla = { critical = 10, warning = 20 }       # Number of Transactions per hour
+        success_rate_sla = { critical = 0.99, warning = 0.995 }  # Success Rate Percentage
+        latency_sla      = { critical = 15000, warning = 12000 } # Average Latency over a week, in ms
+      },
+      { name             = "Cross Validation"
+        uri              = "/audit/cross-validation/%"
+        method           = "POST"
+        transactions_sla = { critical = 8, warning = 10 }       # Number of Transactions per hour
+        success_rate_sla = { critical = 0.99, warning = 0.995 } # Success Rate Percentage
+        latency_sla      = { critical = 800, warning = 650 }    # Average Latency over a week, in ms
+      },
+      { name             = "Single Summary Report Download"
+        uri              = "/dissemination/summary-report/xlsx%"
+        method           = "GET"
+        transactions_sla = { critical = 70, warning = 80 }       # Number of Transactions per hour
+        success_rate_sla = { critical = 0.99, warning = 0.995 }  # Success Rate Percentage
+        latency_sla      = { critical = 800, warning = 700 }     # Average Latency over a week, in ms
+      },
     ]
   })
 }
