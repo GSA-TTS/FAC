cd323b96c2710825206238155ba3e6157af95fd9
Matt Jadud <matthew.jadud@gsa.gov>
2025-03-12 09:24:06 -0400

    Initial data load config
    Only loads dissemination data.

---
+++ backend/util/load_public_dissem_data/README.md
@@ -0,0 +1,77 @@
+# loading public data
+
+This provides a containerized data loading process that sets up your local FAC in a manner that duplicates the live/production app.
+
+The data we are using is public, historic data. It can be replaced, at a later point, with data that is more current.
+
+## Full clean
+
+You might want a completely clean local stack to start. It is not strictly necessary. If you get key conflicts, it means you already have some of this historic data loaded.
+
+### Wipe the stack
+
+From the backend folder
+
+```
+make -i docker-full-clean
+```
+
+Note the `-i` flag. This means `make` should ignore errors. You want it to, so it will keep going and wipe everything.
+
+```
+make docker-first-run
+```
+
+and then
+
+```
+docker compose up
+```
+
+We need the stack running for this whole process.
+
+## Grab the ZIP
+
+You'll need to grab
+
+https://drive.google.com/drive/folders/1gUsqD31Pkd17CruE4PWwwPKJVUssYNnI
+
+which is cleaned/historic public data, fit for our dissem_* tables.
+
+Compressed, it is 330MB. Uncompressed, around 3GB.
+
+Put it in dissemination/tools/load_public_dissem_data/data (a child of this directory)
+
+## Build the container
+
+This is containerized, so it should work on all platforms. To build the container, run
+
+```
+make build
+```
+
+Then, to run the container,
+
+```
+make run
+```
+
+You need to run this from the current directory.
+
+NOTE: The docker command in the Makefile uses the `--network` flag. The `--network` flag tells Docker to run this container on the same network as your currently running stack. It assumes that you did a `docker compose up` on the FAC stack, and that the web container has the default name of `backend-web-1`. If this does not work, you will need to...
+
+```
+make NETWORK=<container-name> run
+```
+
+where `<container-name>` is the name of your web container. This should allow this container to correctly talk to our databases.
+
+## When to rebuild this container
+
+Note this is pinned to v0.1.9 of the cgov-util.
+
+https://github.com/GSA-TTS/fac-backup-utility
+
+If that gets updated, you'll need to update the dockerfile.
+
+It also copies in the YAML for sling from `dissemination/sql/sling`. If that changes, you'll want to
---
+++ backend/util/load_public_dissem_data/load_public_data_locally.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+export CGOV_UTIL_VERSION=v0.1.9
+
+echo "FAC_DB_URI is [" $1 "]"
+
+if [ -z "$1" ]
+then
+    export FAC_DB_URI="postgresql://postgres@db:5432/postgres?sslmode=disable"
+else
+    export FAC_DB_URI="$1"
+    export RUNNING_LOCAL=1
+fi
+
+echo "FAC_DB_URI is now [" $FAC_DB_URI "] local [" $RUNNING_LOCAL "]"
+
+export FAC_SNAPSHOT_URI="postgresql://postgres@db2:5432/postgres?sslmode=disable"
+
+
+function check_table_exists() {
+    local db_uri="$1"
+    local dbname="$2"
+    psql $db_uri -c "SELECT '$dbname'::regclass"  >/dev/null 2>&1
+    result=$?
+    return $result
+}
+
+echo "This will unzip ~3.3GB of data, and load it into a local FAC."
+echo "Make sure the FAC is running."
+echo "Make sure you have disk space."
+echo "Sleeping for 4 seconds..."
+sleep 4
+
+# First, we cleanup the local filesystem.
+# This removes any temporary files from any
+# previous data loads
+if [ "$RUNNING_LOCAL" -eq "1" ]; then
+    export BASE_PATH="."
+else
+    export BASE_PATH="/app"
+fi
+
+rm -f $BASE_PATH/data/db_dissem_dump
+rm -rf $BASE_PATH/data/__MACOSX
+
+# # Next, we drop the public_data schema.
+# # This is because we want to make sure it is
+# # regenerated fresh.
+# psql $FAC_SNAPSHOT_URI -c "DROP SCHEMA IF EXISTS public_data_v1_0_0 CASCADE"
+
+# # Now, the schema for the public_data is
+# # created. This provies a place for the tables to
+# # land when we run sling
+# psql $FAC_SNAPSHOT_URI -c "CREATE SCHEMA IF NOT EXISTS public_data_v1_0_0"
+# psql $FAC_SNAPSHOT_URI -c "CREATE SEQUENCE IF NOT EXISTS public_data_v1_0_0.seq_combined START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE"
+
+# Unzip the compressed historical data dump.
+
+pushd $BASE_PATH/data
+echo "Unzipping data."
+unzip db_dissem_dump.zip
+popd
+
+# Truncate the dissemination_* tables if they exist.
+# CASCADE as well. This makes sure we don't duplicate data,
+# which causes PK/FK problems.
+echo "select 'TRUNCATE TABLE '||tablename||' CASCADE;' FROM pg_tables WHERE tablename LIKE 'dissemination_%'" | \
+    psql $FAC_DB_URI -t | \
+    psql $FAC_DB_URI
+
+# Load the large historic dataset.
+psql $FAC_DB_URI -v ON_ERROR_STOP=on < $BASE_PATH/data/db_dissem_dump
+result=$?
+if [ $result -ne 0 ]; then
+    echo "Something went wrong."
+    exit -1
+else
+    echo "Loaded lots of data without error, apparently."
+fi
+
+echo "Expanding the big audit table .tar file"
+pushd data
+    tar xvf audit_table.tar
+popd
+
+# psql $FAC_DB_URI -v ON_ERROR_STOP=on < $BASE_PATH/data/audit_table.tar
