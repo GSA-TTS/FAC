f6426518f426d670b13508e2554083b996e30ca4
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-14 12:18:04 -0400

    Terraform module updates (#4692)
    * Initial Legacy Provider Update
    
    Sets the new provider into the terraform providers.tf and runs a dual provider setup,
    forcing any resource that does not natively interact with the new provider using:
    
    provider = cloudfoundry-community
    
    * ClamAV version 2.0.0
    
    - Removes app_name_or_id for network policies
    - Reintroduces external network policy for clamav -> proxy
    - Deployed successfully around 9a with v2.0.0 clamav module and was able to successfully
    validate a file at around 916a without any issues
    
    * Clamav Module Update
    
    * Preliminary setup for v2.1.0 upgrade
    
    * Upgrade resources
    
    * Add upgrade docs
    
    * Add app -> clamav network policy
    
    * Update gitignore
    
    * Update gitignore
    
    * Update prepare-proxy.sh
    
    * Meta Provider Update
    
    * Update managed providers
    
    * prevent globbing
    
    * Move sandbox imports
    
    * Live commands
    
    * Documentation on all commands
    
    * Meta Module Upgrade
    
    * Dev and Preview Module Upgrade
    
    * Add routes.tf
    
    * Staging Module Upgrade
    
    * Production Module Upgrade
    
    * Update prod instances
    
    * Update bootstrap provider version
    
    * Update sandbox provider version
    
    * Upgrade sandbox shared modules provider

--- terraform/dev/dev.tf
+++ terraform/dev/dev.tf
@@ -1,6 +1,7 @@
 module "dev" {
   source                = "../shared/modules/env"
   cf_space_name         = "dev"
+  cf_space_id           = data.cloudfoundry_space.space.id
   new_relic_license_key = var.new_relic_license_key
   new_relic_account_id  = var.new_relic_account_id
   new_relic_api_key     = var.new_relic_api_key
@@ -13,7 +14,7 @@ module "dev" {
   https_proxy_instances = 1
   smtp_proxy_instances  = 1
   clamav_instances      = 1
-  clamav_memory         = 2048
+  clamav_memory         = "2048M"
   clamav_fs_instances   = 1
   json_params = jsonencode(
     {
@@ -23,11 +24,19 @@ module "dev" {
 }
 
 module "dev-backups-bucket" {
-  source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v1.1.0"
+  source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v2.2.0"
 
-  cf_org_name   = var.cf_org_name
-  cf_space_name = "dev"
-  name          = "backups"
-  s3_plan_name  = "basic"
-  tags          = ["s3"]
+  cf_space_id  = data.cloudfoundry_space.space.id
+  name         = "backups"
+  s3_plan_name = "basic"
+  tags         = ["s3"]
+}
+
+data "cloudfoundry_org" "org" {
+  name = var.cf_org_name
+}
+
+data "cloudfoundry_space" "space" {
+  name = "dev"
+  org  = data.cloudfoundry_org.org.id
 }
