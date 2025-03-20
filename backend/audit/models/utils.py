2d0fcc7711bd827bd6ced4eccf66c76dbe972084
Anastasia Gradova <anastasia.gradova@gsa.gov>
2025-03-20 12:08:09 -0600

    Updated error formatting for alternative values

--- backend/audit/models/utils.py
+++ backend/audit/models/utils.py
@@ -283,6 +283,15 @@ def validate_audit_consistency(audit_instance):
                         }
                     )
 
+                elif result.get("found"):
+                    differences.append({
+                        "field": field,
+                        "sac_path": sac_path,
+                        "sac_value": sac_value,
+                        # "audit_path": result["audit_path"],
+                        **result
+                    })
+
     return len(differences) == 0, differences
 
 
@@ -319,9 +328,11 @@ def value_exists_in_audit(sac_path, sac_value, audit_data):
 
         norm_audit_field = normalize_key(audit_field)
 
-        if sac_norm_field == norm_audit_field and (
-            sac_value == audit_value or compare_values(sac_value, audit_value)
-        ):
+        # if sac_norm_field == norm_audit_field and (
+        #     sac_value == audit_value or compare_values(sac_value, audit_value)
+        # ):
+
+        if sac_norm_field == norm_audit_field and sac_value == audit_value:
             if sac_field != audit_field:
                 return {
                     "found": True,
@@ -333,9 +344,24 @@ def value_exists_in_audit(sac_path, sac_value, audit_data):
             return {
                 "found": True,
             }
+        elif compare_values(sac_value, audit_value).get("found"):
+            comp_vals = compare_values(sac_value, audit_value)
+            if sac_field != audit_field:
+                return {
+                    "found": True,
+                    "found_with_different_key": True,
+                    "sac_field": sac_field,
+                    "audit_path": audit_path,
+                    "value": audit_value,
+                    **comp_vals
+                }
+            else:
+                return comp_vals
+        else:
+            return {"found": False}
 
     for audit_path, audit_value in audit_data.items():
-        if sac_value == audit_value or compare_values(sac_value, audit_value):
+        if sac_value == audit_value:
             return {
                 "found": True,
                 "found_with_different_key": True,
@@ -343,10 +369,23 @@ def value_exists_in_audit(sac_path, sac_value, audit_data):
                 "audit_path": audit_path,
                 "value": audit_value,
             }
-
-    return {
-        "found": False,
-    }
+        elif compare_values(sac_value, audit_value).get("found"):
+            comp_vals = compare_values(sac_value, audit_value)
+            return {
+                "found": True,
+                "found_with_different_key": True,
+                "sac_field": sac_field,
+                "audit_path": audit_path,
+                "value": audit_value,
+                **comp_vals
+            }
+        else:
+            return {
+                "found": False,
+            }
+    # return {
+    #     "found": False,
+    # }
 
 
 # Do we need this?
@@ -362,26 +401,39 @@ def normalize_key(key):
 def compare_values(value1, value2):
     """Generate other format types for obj, str, list, dict"""
 
-    if str(value1) == str(value2):
-        return True
+    # if str(value1) == str(value2):
+    #     return {"found": True, "result": "String conversion match"}
+    #     # return True
 
     if isinstance(value1, list) and value2 in value1:
-        return True
+        return {"found": True, "error": f"{value1} is list, found {value2}"}
+        # return True
 
     if isinstance(value2, list) and value1 in value2:
-        return True
+        return {"found": True, "error": f"{value2} is list, found {value1}"}
+        # return True
 
     if isinstance(value1, dict) and value2 in value1.values():
-        return True
+        return {"found": True, "error": f"{value1} is dict, found {value2}"}
+        # return True
     if isinstance(value2, dict) and value1 in value2.values():
-        return True
+        return {"found": True, "error": f"{value2} is dict, found {value1}"}
+        # return True
 
     try:
         if isinstance(value1, (int, float)) and isinstance(value2, str):
-            return value1 == float(value2)
+            if value1 == float(value2):
+                return {
+                    "found": True,
+                    "error": f"{value1} is int/float, found {value2} as string",
+                }
         if isinstance(value2, (int, float)) and isinstance(value1, str):
-            return value2 == float(value1)
+            if value2 == float(value1):
+                return {
+                    "found": True,
+                    "error": f"{value2} is int/float, found {value1} as string",
+                }
     except (ValueError, TypeError):
         pass
 
-    return False
+    return {"found": False}
