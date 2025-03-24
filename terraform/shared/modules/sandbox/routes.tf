eb80be87af86d67a1b0528a7f38af7aa225a283a
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-24 16:07:47 -0400

    Initial Sandbox Overhaul

--- terraform/shared/modules/sandbox/routes.tf
+++ terraform/shared/modules/sandbox/routes.tf
@@ -1,3 +1,12 @@
+# The following use the community provider as these have not been moved to the official provider.
+# In the event that test items do not get moved, the following will likely break
+# and need to be rebuilt in a different method. In the event the v2 api gets an extended depreciation,
+# these may continue to be used until the provider adds this functionality, in which case, should be
+# upgraded as soon as possible.
+
+# This is responsible for creating the client routing for the proxy which is not done
+# in the module https://github.com/GSA-TTS/terraform-cloudgov/blob/main/egress_proxy/main.tf
+
 locals {
   # Make a clean list of the client apps for iteration purposes
   clients      = toset(keys(merge(var.allowlist, var.denylist)))
