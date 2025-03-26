0b72130caf8c8e9ebb317f5b3ae1812f9bd7b04b
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-26 10:10:58 -0400

    File Scanner Overhaul

--- terraform/sandbox/helper_scripts/plan.sh
+++ terraform/sandbox/helper_scripts/plan.sh
@@ -5,6 +5,7 @@ dir=$(pwd)
 cd ..
 cf delete-route app.cloud.gov --hostname fac-sandbox-postgrest -f
 cf delete-route app.cloud.gov --hostname fac-sandbox -f
+cf delete-route apps.internal --hostname fac-file-scanner-fs -f
 terraform plan \
   -var-file="../shared/config/sandbox.tfvars" \
   -out sandbox.tfplan
--- terraform/shared/modules/sandbox/clamav.tf
+++ terraform/shared/modules/sandbox/clamav.tf
@@ -1,5 +1,6 @@
 locals {
-  clam_name = "fac-av-${var.cf_space_name}"
+  clam_name    = "fac-av-${var.cf_space_name}"
+  fs_clam_name = "fac-av-${var.cf_space_name}-fs"
 }
 
 data "docker_registry_image" "clamav" {
@@ -26,15 +27,42 @@ module "clamav" {
   proxy_password = module.https-proxy.password
 }
 
+module "file_scanner_clamav" {
+  source = "github.com/gsa-tts/terraform-cloudgov//clamav?ref=v2.2.0"
+
+  # This generates eg "fac-av-staging-fs.apps.internal", avoiding collisions with routes for other projects and spaces
+  name = local.fs_clam_name
+
+  cf_org_name   = var.cf_org_name
+  cf_space_name = var.cf_space_name
+  clamav_image  = "ghcr.io/gsa-tts/fac/clamav@${data.docker_registry_image.clamav.sha256_digest}"
+  max_file_size = "30M"
+  instances     = var.clamav_fs_instances
+  clamav_memory = var.clamav_memory
+
+  proxy_server   = module.https-proxy.domain
+  proxy_port     = module.https-proxy.https_port
+  proxy_username = module.https-proxy.username
+  proxy_password = module.https-proxy.password
+}
+
 # The following use the community provider as these have not been moved to the official provider.
 # In the event that test items do not get moved, the following will likely break
 # and need to be rebuilt in a different method. In the event the v2 api gets an extended depreciation,
 # these may continue to be used until the provider adds this functionality, in which case, should be
 # upgraded as soon as possible.
+
 resource "cloudfoundry_network_policy" "clamav-network-policy" {
   provider = cloudfoundry-community
   policy {
     source_app      = module.clamav.app_id
+    destination_app = module.https-proxy.app_id
+    port            = "61443"
+    protocol        = "tcp"
+  }
+
+  policy {
+    source_app      = module.file_scanner_clamav.app_id
     destination_app = module.https-proxy.app_id
     port            = "61443"
     protocol        = "tcp"
