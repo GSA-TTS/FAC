db99204c29d700c65a386044e1e9e109c109c1e5
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-27 15:27:08 -0400

    Cleanup Comments

--- terraform/shared/modules/sandbox/https-proxy.tf
+++ terraform/shared/modules/sandbox/https-proxy.tf
@@ -20,7 +20,7 @@ module "https-proxy" {
 }
 
 # The following use the community provider as these have not been moved to the official provider.
-# In the event that test items do not get moved, the following will likely break
+# In the event that these resources do not get moved, the following will likely break
 # and need to be rebuilt in a different method. In the event the v2 api gets an extended depreciation,
 # these may continue to be used until the provider adds this functionality, in which case, should be
 # upgraded as soon as possible.
