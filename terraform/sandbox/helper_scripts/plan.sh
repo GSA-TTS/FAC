2f3b00808f7faeb3e6951f590d5375f3c105a399
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-27 12:22:29 -0400

    1.4.0 Provider Upgrade

--- terraform/sandbox/helper_scripts/plan.sh
+++ terraform/sandbox/helper_scripts/plan.sh
@@ -3,10 +3,10 @@
 set -e
 dir=$(pwd)
 cd ..
-cf delete-route apps.internal --hostname fac-file-scanner-sandbox -f
-cf delete-route app.cloud.gov --hostname fac-sandbox-postgrest -f
-cf delete-route app.cloud.gov --hostname fac-sandbox -f
-cf delete-route app.cloud.gov --hostname fac-sandbox-logshipper -f
+# cf delete-route apps.internal --hostname fac-file-scanner-sandbox -f
+# cf delete-route app.cloud.gov --hostname fac-sandbox-postgrest -f
+# cf delete-route app.cloud.gov --hostname fac-sandbox -f
+# cf delete-route app.cloud.gov --hostname fac-sandbox-logshipper -f
 terraform plan \
   -var-file="../shared/config/sandbox.tfvars" \
   -out sandbox.tfplan
