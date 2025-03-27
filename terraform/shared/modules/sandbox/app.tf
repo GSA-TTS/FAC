38062844d1b1ab593f8d351fa2fc8b46c5c8134e
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-27 15:48:09 -0400

    Remove policies from main files

--- terraform/shared/modules/sandbox/app.tf
+++ terraform/shared/modules/sandbox/app.tf
@@ -38,25 +38,3 @@ module "fac-app" {
   }
   depends_on = [cloudfoundry_service_instance.newrelic_creds, module.https-proxy, module.logshipper]
 }
-
-# The following use the community provider as these have not been moved to the official provider.
-# In the event that these resources do not get moved, the following will likely break
-# and need to be rebuilt in a different method. In the event the v2 api gets an extended depreciation,
-# these may continue to be used until the provider adds this functionality, in which case, should be
-# upgraded as soon as possible.
-resource "cloudfoundry_network_policy" "app-network-policy" {
-  provider = cloudfoundry-community
-
-  policy {
-    source_app      = module.fac-app.app_id
-    destination_app = module.https-proxy.app_id
-    port            = "61443"
-    protocol        = "tcp"
-  }
-  policy {
-    source_app      = module.fac-app.app_id
-    destination_app = module.clamav.app_id
-    port            = "61443"
-    protocol        = "tcp"
-  }
-}
