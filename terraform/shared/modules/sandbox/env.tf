5d0684666161bddfc84dceb24755f373659dd373
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-27 15:27:39 -0400

    Cleanup Space Var

--- terraform/shared/modules/sandbox/env.tf
+++ terraform/shared/modules/sandbox/env.tf
@@ -1,7 +1,7 @@
 module "database" {
   source = "github.com/gsa-tts/terraform-cloudgov//database?ref=v2.2.0"
 
-  cf_space_id   = var.cf_space_id
+  cf_space_id   = var.cf_space.id
   name          = "fac-db"
   tags          = ["rds"]
   rds_plan_name = var.database_plan
@@ -11,7 +11,7 @@ module "database" {
 module "snapshot-database" {
   source = "github.com/gsa-tts/terraform-cloudgov//database?ref=v2.2.0"
 
-  cf_space_id   = var.cf_space_id
+  cf_space_id   = var.cf_space.id
   name          = "fac-snapshot-db"
   tags          = ["rds"]
   rds_plan_name = var.database_plan
@@ -21,7 +21,7 @@ module "snapshot-database" {
 module "s3-public" {
   source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v2.2.0"
 
-  cf_space_id  = var.cf_space_id
+  cf_space_id  = var.cf_space.id
   name         = "fac-public-s3"
   s3_plan_name = "basic-public"
   tags         = ["s3"]
@@ -30,12 +30,11 @@ module "s3-public" {
 module "s3-private" {
   source = "github.com/gsa-tts/terraform-cloudgov//s3?ref=v2.2.0"
 
-  cf_space_id  = var.cf_space_id
+  cf_space_id  = var.cf_space.id
   name         = "fac-private-s3"
   s3_plan_name = "basic"
   tags         = ["s3"]
 }
-
 
 # Stuff used for apps in this space
 data "cloudfoundry_org" "org" {
@@ -43,12 +42,12 @@ data "cloudfoundry_org" "org" {
 }
 
 data "cloudfoundry_space" "space" {
-  name = var.cf_space_name
+  name = var.cf_space.name
   org  = data.cloudfoundry_org.org.id
 }
 
 data "cloudfoundry_space" "egress_space" {
-  name = "${var.cf_space_name}-egress"
+  name = "${var.cf_space.name}-egress"
   org  = data.cloudfoundry_org.org.id
 }
 
