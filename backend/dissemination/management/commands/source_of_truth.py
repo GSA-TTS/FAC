44a1f74194d38c7961ae9455a5468ad72cf7f099
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2025-04-07 14:09:30 -0400

    Adding limit option

--- backend/dissemination/management/commands/source_of_truth.py
+++ backend/dissemination/management/commands/source_of_truth.py
@@ -30,22 +30,25 @@ class Command(BaseCommand):
         parser.add_argument("--start", type=str, required=False, help="YYYYMMDD start fac_accepted_date")
         parser.add_argument("--end", type=str, required=False, help="YYYYMMDD end fac_accepted_date")
         parser.add_argument("--report_id", type=str, required=False, help="A report_id")
+        parser.add_argument("--limit", type=int, required=False, help="Limit the number of audits to test")
 
     def handle(self, *args, **kwargs):
         report_id = kwargs.get("report_id", None)
         if report_id:
             logger.info(f"Testing on report_id {report_id}")
             query = Q(report_id=report_id)
+            limit = 1
         else:
             start = kwargs["start"]
             end = kwargs["end"]
             logger.info(f"Testing on date range {start}-{end}")
             query = Q(fac_accepted_date__gte=start) | Q(fac_accepted_date__lte=end)
+            limit = kwargs.get("limit", None)
 
-        sot_audits_query = Audit.objects.filter(query) # also search for dissem status?
+        sot_audits_query = Audit.objects.filter(query | Q(submission_status='disseminated')).order_by('report_id')[:limit]
         sot_sorted_report_ids = self._get_sorted_report_ids(sot_audits_query)
 
-        gen_audits_query = General.objects.filter(query)
+        gen_audits_query = General.objects.filter(query).order_by('report_id')[:limit]
         sac_sorted_report_ids = self._get_sorted_report_ids(gen_audits_query)
 
         logger.info(f"SOT report_ids: {sot_sorted_report_ids}")
@@ -65,14 +68,15 @@ class Command(BaseCommand):
             is_consistent, differences = validate_audit_consistency(sot_audits_by_report_id[report_id])
 
             if not is_consistent:
+                logger.error(f"Differences found for {report_id}:")
                 logger.error(differences)
 
     def _get_sorted_report_ids(self, queryset):
+        # The query results are already ORDER_BYed report_id, so no need to
+        # sort again
         report_ids = []
         for audit in queryset:
             report_ids.append(audit.report_id)
-
-        report_ids.sort()
 
         return report_ids
 
