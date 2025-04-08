036cc17f40e5bdf975a35d4091682e0f48686e1c
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2025-04-08 09:00:30 -0400

    Fixing unit tests

--- backend/audit/models/utils.py
+++ backend/audit/models/utils.py
@@ -261,10 +261,13 @@ def validate_audit_consistency(audit_instance):
         audit_field_data = audit_instance.audit.get(field)
 
         # Ignore SAC fields that only contain metadata
-        if field == "findings_text" and not sac_data.get("FindingsText").get("findings_text_entries"):
-            sac_data = None
-        elif field == "corrective_action_plan" and not sac_data.get("CorrectiveActionPlan").get("corrective_action_plan_entries"):
-            sac_data = None
+        if sac_data:
+            if field == "findings_text" \
+                and not sac_data.get("FindingsText", {}).get("findings_text_entries"):
+                sac_data = None
+            elif field == "corrective_action_plan" \
+                and not sac_data.get("CorrectiveActionPlan", {}).get("corrective_action_plan_entries"):
+                sac_data = None
 
         if sac_data is not None and audit_field_data in [None, {}]:
             differences.append(
