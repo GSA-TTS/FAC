dcf3d616954d5c44c1573bdd05cc3d61d2297127
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2025-04-11 14:12:42 -0400

    Updating json_array_to_text_array to use jsonb_array_elements_text

---
+++ backend/audit/migrations/0019_update_json_array_to_text_array.py
@@ -0,0 +1,30 @@
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("audit", "0018_auditflow_rename_audit_history_event_data_and_more"),
+    ]
+
+    operations = [
+        # This custom function is needed for the generated columns.
+        # Updating it to use jsonb_array_elements_text instead of
+        # jsonb_array_elements to fix double quotes issue.
+        migrations.RunSQL(
+            """
+                    CREATE OR REPLACE FUNCTION json_array_to_text_array (data jsonb)
+                        RETURNS text[]
+                        AS $CODE$
+                    BEGIN
+                        RETURN ARRAY (
+                            SELECT
+                                jsonb_array_elements_text(data));
+                    END
+                    $CODE$
+                    LANGUAGE plpgsql
+                    IMMUTABLE;
+            """
+        ),
+    ]
