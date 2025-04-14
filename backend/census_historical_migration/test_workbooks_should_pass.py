473324eb8c60e76ed7a4dc7a79af01af6dac6dbd
Jason Rothacker <jason.rothacker@gsa.gov>
2025-04-14 14:06:05 -0700

    test_workbooks

--- backend/census_historical_migration/test_workbooks_should_pass.py
+++ backend/census_historical_migration/test_workbooks_should_pass.py
@@ -5,9 +5,9 @@ import logging
 from django.test import TestCase
 import os
 from functools import reduce
-from audit.context import set_sac_to_context
+from audit.context import set_audit_to_context
 from model_bakery import baker
-from audit.models import SingleAuditChecklist
+from audit.models import Audit
 from audit.test_workbooks_should_pass import process_workbook_set
 
 logger = logging.getLogger(__name__)
@@ -19,8 +19,8 @@ class PassingWorkbooks(TestCase):
             os.path.join,
             ["census_historical_migration", "fixtures", "workbooks", "should_pass"],
         )
-        sac = baker.make(SingleAuditChecklist)
-        with set_sac_to_context(sac):
+        sac = baker.make(Audit, version=0)
+        with set_audit_to_context(sac):
             for dirpath, dirnames, _ in os.walk(workbook_sets):
                 for workbook_set in dirnames:
                     logger.info("Walking ", workbook_set)
