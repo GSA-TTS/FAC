f10a30837c22c18d1a29ea089229c369708f2f67
Bobby Novak <176936850+rnovak338@users.noreply.github.com>
2025-04-16 15:38:14 -0400

    Gently handle version mismatch
    - No longer raising an exception on a version mismatch in Audit.
    - Instead, we are logging on NR.

--- backend/audit/models/audit.py
+++ backend/audit/models/audit.py
@@ -27,8 +27,10 @@ from audit.models.utils import (
 from itertools import chain
 from audit.cross_validation import functions as cross_validation_functions
 from audit.utils import FORM_SECTION_HANDLERS
+import logging
 
 User = get_user_model()
+logger = logging.getLogger(__name__)
 
 
 class AuditManager(models.Manager):
@@ -327,14 +329,15 @@ class Audit(CreatedMixin, UpdatedMixin):
         previous_version = self.version
 
         self.updated_by = self.updated_by if self.updated_by else event_user
-        self.version = previous_version + 1
 
         with transaction.atomic():
             current_version = Audit.objects.get_current_version(report_id)
             if previous_version != current_version:
-                raise Exception(
+                logger.error(
                     f"Version Mismatch: Expected {previous_version} Got {current_version}"
                 )  # TODO
+
+            self.version = previous_version + 1
 
             if event_type and event_user:
                 History.objects.create(
