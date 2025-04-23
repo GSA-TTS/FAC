8901440abcdfc1d0b4d3603f3f94e82cbff080b4
Dan Swick <2365503+danswick@users.noreply.github.com>
2025-04-23 05:40:07 -0700

    DRP2 offboarding (#4939)

--- terraform/shared/modules/newrelic/alerts.tf
+++ terraform/shared/modules/newrelic/alerts.tf
@@ -15,7 +15,7 @@ resource "newrelic_notification_destination" "email_destination" {
 
   property {
     key   = "email"
-    value = "daniel.swick@gsa.gov, matthew.jadud@gsa.gov, alexander.steel@gsa.gov"
+    value = "matthew.jadud@gsa.gov, alexander.steel@gsa.gov"
   }
 }
 
