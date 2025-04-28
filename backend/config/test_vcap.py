34dd03852c63827ec99b7f660eb63a2ea0be3648
Matt Jadud <matthew.jadud@gsa.gov>
2025-04-28 09:09:20 -0400

    Tests the VCAP reading code for the CSV dumper
    This code could be used more generally in the future, if needed/desired.

--- backend/config/test_vcap.py
+++ backend/config/test_vcap.py
@@ -1,6 +1,9 @@
 import json
 import os
-from backend.config.vcap import GET, FIND, get_vcap_services
+from config.vcap import GET, FIND, get_vcap_services
+
+from django.test import TestCase
+
 
 vcap_1 = {"key": "value"}
 vcap_2 = {"key": "value", "key2": "value2"}
@@ -29,41 +32,37 @@ vcap_4 = {
 }
 
 
-def _simple(vcap, cmds, result):
+def _simple(self, vcap, cmds, result):
     os.environ["VCAP_SERVICES"] = json.dumps(vcap)
-    assert get_vcap_services(cmds) == result  # nosec
+    self.assertEqual(get_vcap_services(cmds), result)
 
 
-def test_simple_vcap1():
-    cmds = [GET("key")]
-    _simple(vcap_1, cmds, "value")
+class VcapTestCase(TestCase):
 
+    def test_simple_vcap1(self):
+        cmds = [GET("key")]
+        _simple(self, vcap_1, cmds, "value")
 
-def test_simple_vcap2():
-    cmds = [GET("key2")]
-    _simple(vcap_2, cmds, "value2")
+    def test_simple_vcap2(self):
+        cmds = [GET("key2")]
+        _simple(self, vcap_2, cmds, "value2")
 
+    def test_list_3a(self):
+        cmds = [GET("key"), FIND("name", "alice"), GET("a"), GET("k1")]
+        _simple(self, vcap_3, cmds, "v1")
 
-def test_list_3a():
-    cmds = [GET("key"), FIND("name", "alice"), GET("a"), GET("k1")]
-    _simple(vcap_3, cmds, "v1")
+    def test_list_3b(self):
+        cmds = [GET("key"), FIND("name", "bob"), GET("c"), GET("k3")]
+        _simple(self, vcap_3, cmds, "v3")
 
+    def test_list_3c(self):
+        cmds = [GET("key"), FIND("name", "alice"), GET("b"), GET("k2")]
+        _simple(self, vcap_3, cmds, "v2")
 
-def test_list_3b():
-    cmds = [GET("key"), FIND("name", "bob"), GET("c"), GET("k3")]
-    _simple(vcap_3, cmds, "v3")
+    def test_list_4_1(self):
+        cmds = [GET("key"), FIND("name", "clarice"), GET("c"), GET("k3"), GET("nk1")]
+        _simple(self, vcap_4, cmds, "nv1")
 
-
-def test_list_3c():
-    cmds = [GET("key"), FIND("name", "alice"), GET("b"), GET("k2")]
-    _simple(vcap_3, cmds, "v2")
-
-
-def test_list_4_1():
-    cmds = [GET("key"), FIND("name", "clarice"), GET("c"), GET("k3"), GET("nk1")]
-    _simple(vcap_4, cmds, "nv1")
-
-
-def test_list_4_2():
-    cmds = [GET("key"), FIND("name", "daryl"), GET("c"), GET("k3"), GET("nk1")]
-    _simple(vcap_4, cmds, "nv5")
+    def test_list_4_2(self):
+        cmds = [GET("key"), FIND("name", "daryl"), GET("c"), GET("k3"), GET("nk1")]
+        _simple(self, vcap_4, cmds, "nv5")
