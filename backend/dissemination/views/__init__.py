e8cef3d1c0479038363b1fd986646ade12a11c30
James Person <jperson1@umbc.edu>
2025-06-03 10:48:17 -0400

    A sample doughnut chart for entity type spending

--- backend/dissemination/views/__init__.py
+++ backend/dissemination/views/__init__.py
@@ -1,3 +1,4 @@
+from dissemination.views.dashboard import DashboardView
 from dissemination.views.download import (
     PdfDownloadView,
     XlsxDownloadView,
@@ -11,6 +12,7 @@ from dissemination.views.search import AdvancedSearch, Search, AuditSearch
 from dissemination.views.summary import AuditSummaryView
 
 views_list = [
+    DashboardView,
     PdfDownloadView,
     XlsxDownloadView,
     PublicDataDownloadView,
---
+++ backend/dissemination/views/dashboard.py
@@ -0,0 +1,40 @@
+from django.db.models import BigIntegerField, Sum, Q
+from django.db.models.functions import Cast
+from django.shortcuts import render
+from django.views.generic import View
+
+from audit.models import Audit
+from audit.models.constants import STATUS, ORGANIZATION_TYPE
+
+
+class DashboardView(View):
+    def get(self, request):
+        context = {}
+
+        query = Q(submission_status=STATUS.DISSEMINATED)
+
+        types = [
+            ORGANIZATION_TYPE.STATE,
+            ORGANIZATION_TYPE.LOCAL,
+            ORGANIZATION_TYPE.TRIBAL,
+            ORGANIZATION_TYPE.HIGHER_ED,
+            ORGANIZATION_TYPE.NON_PROFIT,
+        ]
+
+        # Funding by entity type, as a starter
+        context['dashboard_data'] = []
+        for organization_type in types:
+            sum = (
+                Audit.objects.filter(query)
+                .filter(organization_type=organization_type)
+                .annotate(
+                    total_amount_expended_int=Cast(
+                        "audit__federal_awards__total_amount_expended", BigIntegerField()
+                    )
+                )
+                .aggregate(sum=Sum("total_amount_expended_int"))
+                ['sum']
+            )
+            context['dashboard_data'].append({"entity_type": organization_type, "sum": sum})
+
+        return render(request, "dashboard.html", context)
