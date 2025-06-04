864cd80ff11e9c19d4432a84c79e11fb4e3e9895
Bobby Novak <176936850+rnovak338@users.noreply.github.com>
2025-06-04 14:22:27 -0400

    Linting
    Signed-off-by: Bobby Novak <176936850+rnovak338@users.noreply.github.com>

--- backend/dissemination/views/analytics.py
+++ backend/dissemination/views/analytics.py
@@ -1,20 +1,35 @@
 import logging
 
 from dissemination.analytics.state import DisseminationStateAnalytics
-from django.shortcuts import render
+from dissemination.analytics.trends import DisseminationTrendAnalytics
+from django.http import JsonResponse
 from django.views.generic import View
 
 logger = logging.getLogger(__name__)
+
 
 # TODO: Analytics Dashboard
 # An example of how we can pull analytics data to the template.
 class AnalyticsView(View):
     def get(self, request):
-        analytics = DisseminationStateAnalytics(request.GET['year'], request.GET['state'])
+        analytics = DisseminationStateAnalytics(
+            request.GET.get("year"), request.GET.get("state")
+        )
+        trend_analytics = DisseminationTrendAnalytics(["2022", "2023", "2024"])
 
-        return render(request, "my_html_page.html", {
+        output_state = {
+            # STATE analytics.
             "total": analytics.single_dissemination_count(),
             "programs_with_repeated_findings": analytics.programs_with_repeated_findings(),
             "top_programs": analytics.top_programs(),
             "funding_by_entity_type": analytics.funding_by_entity_type(),
-        })
+            # TREND analytics.
+            "total_submissions": trend_analytics.total_submissions(),
+            "total_award_volume": trend_analytics.total_award_volume(),
+            "total_findings": trend_analytics.total_findings(),
+            "total_submissions_with_findings": trend_analytics.submissions_with_findings(),
+            "auditee_risk_profile": trend_analytics.auditee_risk_profile(),
+            "risk_profile_vs_findings": trend_analytics.risk_profile_vs_findings(),
+        }
+
+        return JsonResponse(data=output_state, safe=False)
