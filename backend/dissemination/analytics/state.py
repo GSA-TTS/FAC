30bbc858dcf1d1b0cbc8eaf62d8caefaba313856
Bobby Novak <176936850+rnovak338@users.noreply.github.com>
2025-06-04 14:22:27 -0400

    Linting

--- backend/dissemination/analytics/state.py
+++ backend/dissemination/analytics/state.py
@@ -4,12 +4,8 @@ from dissemination.models import (
     FederalAward,
     General,
 )
-from django.db.models import (
-    Count,
-    F,
-    Sum,
-    Q
-)
+from django.db.models import Count, F, Sum, Q
+
 
 class DisseminationStateAnalytics:
 
@@ -22,7 +18,7 @@ class DisseminationStateAnalytics:
         self.awards = self._get_awards_by_state_and_year()
 
     def _get_records_by_state_and_year(self):
-        """ Get all disseminated records for a specified year and state. """
+        """Get all disseminated records for a specified year and state."""
         records = General.objects.filter(is_public=True)
 
         if self.state:
@@ -33,56 +29,51 @@ class DisseminationStateAnalytics:
         return records
 
     def _get_awards_by_state_and_year(self):
-        """ Get federal awards based on all the records that were disseminated for a specific state and year. """
-        record_ids = self.records.values('report_id')
+        """Get federal awards based on all the records that were disseminated for a specific state and year."""
+        record_ids = self.records.values("report_id")
         return FederalAward.objects.filter(report_id__in=record_ids)
 
     def single_dissemination_count(self):
-        out = {
-            "total": self.records.count()
-        }
+        out = {"total": self.records.count()}
         return out
 
     def top_programs(self):
-        """ Top funded federal programs. """
+        """Top funded federal programs."""
         out = list(
-            self.awards
-            .values('federal_program_name')
-            .annotate(total_expended=Sum('amount_expended'))
-            .order_by('-total_expended')
+            self.awards.values("federal_program_name")
+            .annotate(total_expended=Sum("amount_expended"))
+            .order_by("-total_expended")
         )
         return out
 
     def funding_by_entity_type(self):
-        """ Top funded federal programs based on entity type. """
+        """Top funded federal programs based on entity type."""
         out = list(
-            self.awards
-            .values(entity_type=F('report_id__entity_type'))
-            .annotate(total_expended=Sum('amount_expended'))
-            .order_by('-total_expended')
+            self.awards.values(entity_type=F("report_id__entity_type"))
+            .annotate(total_expended=Sum("amount_expended"))
+            .order_by("-total_expended")
         )
         return out
 
     def funding_by_county(self):
-        """ Top funded federal programs based on county. """
+        """Top funded federal programs based on county."""
         # TODO: Analytics Dashboard
         # How do we get counties?
         pass
 
     def programs_with_repeated_findings(self):
-        """ Top federal programs with repeated findings. """
-        awards = self.awards.filter(report_id__finding__is_repeat_finding='Y')
+        """Top federal programs with repeated findings."""
+        awards = self.awards.filter(report_id__finding__is_repeat_finding="Y")
         out = list(
-            awards
-            .values('federal_program_name')
+            awards.values("federal_program_name")
             .annotate(
                 repeat_findings=Count(
-                    'report_id__finding',
-                    filter=Q(report_id__finding__is_repeat_finding='Y'),
-                    distinct=True
+                    "report_id__finding",
+                    filter=Q(report_id__finding__is_repeat_finding="Y"),
+                    distinct=True,
                 )
             )
-            .order_by('-repeat_findings')
+            .order_by("-repeat_findings")
         )
         return out
 
