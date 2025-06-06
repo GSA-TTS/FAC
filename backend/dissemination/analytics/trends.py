ce21a34e433a6ff8204220a0ad50eb91dd62dbc5
Bobby Novak <176936850+rnovak338@users.noreply.github.com>
2025-06-06 16:08:21 -0400

    Initial Commit

---
+++ backend/dissemination/analytics/trends.py
@@ -0,0 +1,144 @@
+# Analytics for disseminated records on a yearly basis.
+
+from dissemination.models import (
+    FederalAward,
+    Finding,
+    General,
+)
+from django.db.models import Sum
+
+
+class DisseminationTrendAnalytics:
+
+    def __init__(self, years):
+
+        # list of years.
+        self.years = years
+
+        # preload queries for faster performance.
+        self.records = self._get_records_by_years()
+        self.awards = self._get_awards_by_years()
+        self.findings = self._get_findings_by_years()
+
+    def _get_records_by_years(self):
+        """Get all disseminated records for a specified range of years."""
+        return General.objects.filter(
+            is_public=True, fac_accepted_date__year__in=self.years
+        )
+
+    def _get_awards_by_years(self):
+        """Get federal awards based on all the records that were disseminated for a specific range of years."""
+        record_ids = self.records.values("report_id")
+        return FederalAward.objects.filter(report_id__in=record_ids)
+
+    def _get_findings_by_years(self):
+        """Get findings based on all the records that were disseminated for a specific range of years."""
+        record_ids = self.records.values("report_id")
+        return Finding.objects.filter(report_id__in=record_ids)
+
+    def _get_records_by_year(self, year):
+        """Get a record set from a specific year within self.records"""
+        return self.records.filter(fac_accepted_date__year=year)
+
+    def total_submissions(self):
+        out = []
+        for year in self.years:
+            out.append(
+                {
+                    "year": year,
+                    "total": self._get_records_by_year(year).count(),
+                }
+            )
+        return out
+
+    def total_award_volume(self):
+        out = []
+        for year in self.years:
+            out.append(
+                {
+                    "year": year,
+                    "total": self.awards.filter(
+                        report_id__fac_accepted_date__year=year
+                    ).aggregate(Sum("amount_expended"))["amount_expended__sum"],
+                }
+            )
+        return out
+
+    def total_findings(self):
+        out = []
+        for year in self.years:
+            out.append(
+                {
+                    "year": year,
+                    "total": self.findings.filter(
+                        report_id__fac_accepted_date__year=year
+                    ).count(),
+                }
+            )
+        return out
+
+    def submissions_with_findings(self):
+        out = []
+        for year in self.years:
+            audits_count = self._get_records_by_year(year).count()
+            findings_count = (
+                self._get_records_by_year(year).exclude(finding=None).count()
+            )
+            out.append(
+                {
+                    "year": year,
+                    "total": (
+                        (findings_count / audits_count) * 100 if audits_count > 0 else 0
+                    ),
+                }
+            )
+        return out
+
+    def auditee_risk_profile(self):
+        out = []
+        for year in self.years:
+            total = self._get_records_by_year(year).count()
+            low_risk_count = (
+                self._get_records_by_year(year)
+                .filter(is_low_risk_auditee="Yes")
+                .count()
+            )
+            not_low_risk_count = total - low_risk_count
+
+            out.append(
+                {
+                    "year": year,
+                    "low_risk": low_risk_count,
+                    "low_risk_percent": (
+                        (low_risk_count / total) * 100 if total > 0 else 0
+                    ),
+                    "not_low_risk": not_low_risk_count,
+                    "not_low_risk_percent": (
+                        (not_low_risk_count / total) * 100 if total > 0 else 0
+                    ),
+                }
+            )
+        return out
+
+    def risk_profile_vs_findings(self):
+        out = []
+        for year in self.years:
+            not_low_risk_count = (
+                self._get_records_by_year(year).filter(is_low_risk_auditee="No").count()
+            )
+            findings_count = (
+                self._get_records_by_year(year).exclude(finding=None).count()
+            )
+            total = self._get_records_by_year(year).count()
+            out.append(
+                {
+                    "year": year,
+                    "not_low_risk": (
+                        (not_low_risk_count / total) * 100 if total > 0 else 0
+                    ),
+                    "audits_with_findings": (
+                        (findings_count / total) * 100 if total > 0 else 0
+                    ),
+                }
+            )
+        return out
---
+++ backend/dissemination/views/analytics.py
@@ -0,0 +1,37 @@
+import logging
+
+from dissemination.analytics.state import DisseminationStateAnalytics
+from dissemination.analytics.trends import DisseminationTrendAnalytics
+from django.http import JsonResponse
+from django.views.generic import View
+
+logger = logging.getLogger(__name__)
+
+
+# TODO: Analytics Dashboard
+# An example of how we can pull analytics data to the template.
+class AnalyticsView(View):
+    def get(self, request):
+        analytics = DisseminationStateAnalytics(
+            request.GET.get("year"), request.GET.get("state")
+        )
+        trend_analytics = DisseminationTrendAnalytics(["2022", "2023", "2024"])
+
+        output_state = {
+            # STATE analytics.
+            "total": analytics.single_dissemination_count(),
+            "programs_with_repeated_findings": analytics.programs_with_repeated_findings(
+                limit=10
+            ),
+            "top_programs": analytics.top_programs(limit=10),
+            "funding_by_entity_type": analytics.funding_by_entity_type(limit=10),
+            # TREND analytics.
+            "total_submissions": trend_analytics.total_submissions(),
+            "total_award_volume": trend_analytics.total_award_volume(),
+            "total_findings": trend_analytics.total_findings(),
+            "total_submissions_with_findings": trend_analytics.submissions_with_findings(),
+            "auditee_risk_profile": trend_analytics.auditee_risk_profile(),
+            "risk_profile_vs_findings": trend_analytics.risk_profile_vs_findings(),
+        }
+
+        return JsonResponse(data=output_state, safe=False)
