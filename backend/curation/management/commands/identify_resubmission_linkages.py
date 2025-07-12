d0ea046afceb8c1e92522d1fd9a88c4634de3e01
Matt Jadud <matthew.jadud@gsa.gov>
2025-07-12 16:53:12 -0400

    Analyzing resubmissions
    In order to implement resubmission, we'll have to curate the existing
    database.
    
    That implies we know which audits were "resubmissions" of each-other in
    the past.
    
    This is a clustering algorithm that introduces an "audit_distance" to
    determine if audits are similar enough to be considered resubmissions of
    each-other.

---
+++ backend/curation/management/commands/identify_resubmission_linkages.py
@@ -0,0 +1,317 @@
+from django.core.management.base import BaseCommand
+from django.db.models import Count
+from django.db import connection
+
+from audit.models import SingleAuditChecklist
+import logging
+from django.db.models import Q
+
+# from difflib import *
+from prettytable import PrettyTable
+from pprint import pprint
+
+from Levenshtein import distance
+
+# 'difflib' is not currently part of the FAC build.
+
+logger = logging.getLogger(__name__)
+
+"""
+This is a dynamic programming solution to a complex problem.
+
+https://github.com/GSA-TTS/FAC/issues/5102
+
+Previous audits that were "resubmitted" want to be linked using our
+new metadata. However, many people misuse UEIs and EINs. 
+
+This is a clustering algorithm designed to build the linkages 
+between previously "resubmitted" audits. It uses a novel
+distance function to calculate the "distance" between two SF-SAC
+records and cluster them for linking based on the similarity between 
+those records.
+"""
+
+
+def edit_dist(junk, a, b):
+    # This does *sequences*. That means big changes can have a
+    # distance of 2.
+    # seq = SequenceMatcher(isjunk=junk, a=a, b=b)
+    # # Equal strings have one opcode. So, subtract one.
+    # # [('equal', 0, 12, 0, 12)]
+    # return len(seq.get_opcodes()) - 1
+    return distance(a, b)
+
+
+def prep(s):
+    return s.lower()
+
+
+def get_audit_year(r):
+    return int(r.general_information["auditee_fiscal_period_end"].split("-")[0])
+
+
+def ay_dist(r1, r2, scale=11):
+    return abs(get_audit_year(r2) - get_audit_year(r1)) * scale
+
+
+def uei_dist(r1, r2, scale=8):
+    return (
+        edit_dist(
+            None,
+            prep(r1.general_information["auditee_uei"]),
+            prep(r2.general_information["auditee_uei"]),
+        )
+        * scale
+    )
+
+
+def ein_dist(r1, r2, scale=3):
+    return (
+        edit_dist(
+            None,
+            prep(r1.general_information["ein"]),
+            prep(r2.general_information["ein"]),
+        )
+        * scale
+    )
+
+
+def auditee_email_dist(r1, r2, scale=1):
+    return (
+        edit_dist(
+            None,
+            prep(r1.general_information["auditee_email"]),
+            prep(r2.general_information["auditee_email"]),
+        )
+        * scale
+    )
+
+
+def auditee_name_dist(r1, r2, scale=3):
+    return (
+        edit_dist(
+            None,
+            prep(r1.general_information["auditee_name"]),
+            prep(r2.general_information["auditee_name"]),
+        )
+        * scale
+    )
+
+
+def auditee_state_dist(r1, r2, scale=8):
+    s1 = r1.general_information["auditee_state"]
+    s2 = r2.general_information["auditee_state"]
+    return scale if s1 != s2 else 0
+
+
+def audit_distance(r1, r2):
+    # Calculate the distance
+    d = 0
+    for dist_fun in [
+        ay_dist,
+        uei_dist,
+        ein_dist,
+        auditee_email_dist,
+        auditee_name_dist,
+        auditee_state_dist,
+    ]:
+        # print("\t", dist_fun, dist_fun(r1, r2))
+        d = d + dist_fun(r1, r2)
+    return d
+
+
+class MinDist:
+    pass
+
+
+def set_distance(r1, s, ndx):
+    dist = 0
+    for r2 in s:
+        dist += audit_distance(r1, r2)
+    # Do not average the distances.
+    # if len(s) > 0:
+    #     dist = dist / len(s)
+    # print(f"{dist} {r1.report_id} <- set({ndx})")
+    return dist
+
+
+def order_reports_key(r):
+    for ndx, tn in enumerate(list(reversed(r.transition_name))):
+        if tn == "submitted":
+            break
+    return list(reversed(r.transition_date))[ndx]
+
+
+def print_sets(AY, sets):
+    with open(f"tables-{AY}-{len(sets)}.txt", "w") as table_file:
+        for ndx, s in enumerate(sets):
+            if len(s) > 1:
+                table = PrettyTable()
+                table.align = "l"
+                table.field_names = ["field", "value"]
+
+                table_file.write(f"\n===============\n")
+                table_file.write(f"SET {ndx}\n")
+                table_file.write(f"===============\n")
+
+                for r in sorted(s, key=order_reports_key):
+                    table.add_row(["report_id", r.report_id])
+                    table.add_row(["distance", r.distance])
+                    table.add_row(["order", r.order])
+                    table.add_row(["audit year", get_audit_year(r)])
+                    table.add_row(
+                        [
+                            "accepted date",
+                            order_reports_key(r).strftime("%Y-%m-%d %H:%M"),
+                        ]
+                    )
+                    table.add_row(["uei", r.general_information["auditee_uei"]])
+                    table.add_row(["ein", r.general_information["ein"]])
+                    table.add_row(
+                        ["email", prep(r.general_information["auditee_email"])]
+                    )
+                    table.add_row(["name", prep(r.general_information["auditee_name"])])
+                    table.add_row(
+                        ["state", prep(r.general_information["auditee_state"])]
+                    )
+                    table.add_divider()
+
+                table_file.write(str(table))
+                table_file.write("\n\n")
+
+
+import time
+import csv
+from django.db.models import F
+from datetime import datetime
+
+
+def export_sets_as_csv(AY, sets):
+    with open(f"sets-{AY}-{len(sets)}.csv", "w") as csv_file:
+        wr = csv.writer(csv_file)  # , quoting=csv.QUOTE_ALL
+        wr.writerow(
+            [
+                "set_index",
+                "set_distance",
+                "set_order",
+                "report_id",
+                "audit_year",
+                "fac_accepted_date",
+                "auditee_uei",
+                "auditee_ein",
+                "auditee_email",
+                "auditee_name",
+                "auditee_state",
+            ]
+        )
+        for ndx, s in enumerate(sets):
+            if len(s) > 1:
+                for r in sorted(s, key=order_reports_key):
+                    wr.writerow(
+                        [
+                            ndx,
+                            r.distance,
+                            r.order,
+                            r.report_id,
+                            get_audit_year(r),
+                            order_reports_key(r).strftime("%Y-%m-%d %H:%M:%S"),
+                            r.general_information["auditee_uei"],
+                            r.general_information["ein"],
+                            prep(r.general_information["auditee_email"]),
+                            prep(r.general_information["auditee_name"]),
+                            prep(r.general_information["auditee_state"]),
+                        ]
+                    )
+
+
+def generate_clusters(AY):
+    print("\n-=-=-=-=-=-=-=-=-=-")
+    DUPLICATION_COUNT = 2
+    AUDIT_YEAR = AY
+
+    # Begin with a small set.
+    # Take a full year, grab the UEIs, and look for duplicated UEIs.
+    base = SingleAuditChecklist.objects.filter(
+        general_information__auditee_fiscal_period_end__startswith=AUDIT_YEAR,
+        submission_status="disseminated",
+    )
+    print(f"{len(base)} in AY{AUDIT_YEAR}")
+
+    ueis = (
+        base.values("general_information__auditee_uei")
+        .annotate(uei=F("general_information__auditee_uei"))
+        .annotate(uei_duplication_count=Count("report_id"))
+    ).filter(uei_duplication_count__gte=DUPLICATION_COUNT)
+
+    print("----")
+    print(f"{ueis.count()} duplicated UEIs (dupe count >= {DUPLICATION_COUNT})")
+    # print(ueis[:2])
+
+    # Now, I want the records for those UEIs
+    # Order by report_id, so that the first in a set goes in first.
+    # We'll have a strict ordering on the report ID that helps out here.
+    records = base.filter(
+        general_information__auditee_uei__in=ueis.values("uei"),
+    ).order_by("report_id")
+
+    print("-----")
+    print(f"{records.count()} records for the duplicated UEIs")
+    time.sleep(3)
+
+    # print("----")
+    # print(records[:2])
+
+    # For each record, compute its distance to the existing sets.
+    # If it is below the threshold, insert it into an existing set.
+    # Otherwise, insert into a new set.
+    sets = []
+    THRESHOLD = 3
+
+    for rndx, r in enumerate(records):
+        print(
+            f"Processing {rndx} of {records.count()}: {r.report_id} sets: {len(sets)}"
+        )
+        # Start infinitely far apart
+        md = MinDist()
+        md.distance = float("inf")
+        md.set_index = -1
+
+        for ndx, s in enumerate(sets):
+            d = set_distance(r, s, ndx)
+
+            if d < md.distance:
+                md.distance = d
+                md.set_index = ndx
+
+        r.distance = md.distance
+
+        if md.distance < THRESHOLD:
+            # print(f"Adding {r} to {md.set_index}")
+            r.order = len(sets[md.set_index])
+            sets[md.set_index].add(r)
+        else:
+            # print(f"Adding {r} to new set ({r.__hash__()})")
+            new_s = set()
+            r.order = 0
+            new_s.add(r)
+            sets.append(new_s)
+
+    sorted_sets = sorted(sets, key=lambda s: get_audit_year(next(iter(s))))
+    print_sets(AUDIT_YEAR, sorted_sets)
+    export_sets_as_csv(AUDIT_YEAR, sorted_sets)
+    return sorted_sets
+
+
+class Command(BaseCommand):
+    """Clusters audits for resubmission linking"""
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--audit_year",
+            type=str,
+            required=True,
+            help="Audit year to process",
+        )
+
+    def handle(self, *args, **options):
+        generate_clusters(options["audit_year"])
---
+++ backend/curation/test_audit_linkages.py
@@ -0,0 +1,157 @@
+from .management.commands.identify_resubmission_linkages import generate_clusters
+from model_bakery import baker
+from django.test import TestCase
+from audit.models import SingleAuditChecklist
+
+sac_01 = {
+    "audit_year": "2022",
+    "report_id": "2022-42-MAGIC-0000000001",
+    "submission_status": "disseminated",
+    "transition_name": [
+        "ready_for_certification",
+        "auditor_certified",
+        "auditee_certified",
+        "certified",
+        "submitted",
+        "disseminated",
+    ],
+    "transition_date": [
+        "2023-09-26T00:00:00.000Z",
+        "2023-09-26T10:11:52.000Z",
+        "2023-09-26T13:19:56.000Z",
+        "2023-09-26T00:00:00.000Z",
+        "2023-09-26T00:00:00.000Z",
+        "2024-01-20T01:12:35.065Z",
+    ],
+    "general_information": {
+        "ein": "237399677",
+        "auditee_uei": "GNU9RNVE6J68",
+        "auditee_zip": "15942",
+        "auditor_ein": "251390233",
+        "auditee_city": "MINERAL POINT",
+        "auditee_name": "JACKSON TOWNSHIP VOLUNTEER FIRE COMPANY",
+        "auditee_email": "MS74CCHS@ATLANTICBB.NET",
+        "auditee_state": "PA",
+        "auditee_fiscal_period_end": "2022-12-31",
+    },
+}
+
+
+class ClusteringTests(TestCase):
+    def test_no_clusters(self):
+        # Test on a single audit.
+        baker.make(
+            SingleAuditChecklist,
+            report_id=sac_01["report_id"],
+            submission_status=sac_01["submission_status"],
+            transition_name=sac_01["transition_name"],
+            transition_date=sac_01["transition_date"],
+            general_information=sac_01["general_information"],
+        )
+        sorted_sets = generate_clusters("2022")
+        # No audits should be clustered. There is only one.
+        self.assertEqual(len(sorted_sets), 0)
+
+    def test_one_cluster(self):
+        # Two identical audits should yield a cluster
+        baker.make(
+            SingleAuditChecklist,
+            report_id=sac_01["report_id"],
+            submission_status=sac_01["submission_status"],
+            transition_name=sac_01["transition_name"],
+            transition_date=sac_01["transition_date"],
+            general_information=sac_01["general_information"],
+        )
+        baker.make(
+            SingleAuditChecklist,
+            report_id=sac_01["report_id"][:-1] + "2",
+            submission_status=sac_01["submission_status"],
+            transition_name=sac_01["transition_name"],
+            transition_date=sac_01["transition_date"],
+            general_information=sac_01["general_information"],
+        )
+        sorted_sets = generate_clusters("2022")
+        # No audits should be clustered. There is only one.
+        self.assertEqual(len(sorted_sets), 1)
+
+    def test_email_difference(self):
+        # A single-character difference in the email
+        # should yield a single cluster.
+        baker.make(
+            SingleAuditChecklist,
+            report_id=sac_01["report_id"],
+            submission_status=sac_01["submission_status"],
+            transition_name=sac_01["transition_name"],
+            transition_date=sac_01["transition_date"],
+            general_information=sac_01["general_information"],
+        )
+
+        # Add a single character typo to the email address.
+        # We should still get a cluster.
+        gi = sac_01["general_information"]
+        gi["auditee_email"] = gi["auditee_email"][:-1] + "x"
+
+        baker.make(
+            SingleAuditChecklist,
+            report_id=sac_01["report_id"][:-1] + "2",
+            submission_status=sac_01["submission_status"],
+            transition_name=sac_01["transition_name"],
+            transition_date=sac_01["transition_date"],
+            general_information=gi,
+        )
+        sorted_sets = generate_clusters("2022")
+        # No audits should be clustered. There is only one.
+        self.assertEqual(len(sorted_sets), 1)
+
+    def test_different_state(self):
+        # Different states should force the audits
+        # into different clusters for the same UEI.
+        rid_count = 0
+        for state in ["PA", "ME"]:
+            rid_count += 1
+            gi = sac_01["general_information"]
+            gi["auditee_state"] = state
+            baker.make(
+                SingleAuditChecklist,
+                report_id=sac_01["report_id"][:-1] + f"{rid_count}",
+                submission_status=sac_01["submission_status"],
+                transition_name=sac_01["transition_name"],
+                transition_date=sac_01["transition_date"],
+                general_information=gi,
+            )
+        sorted_sets = generate_clusters("2022")
+        # I expect each audit to be in its own cluster.
+        self.assertEqual(len(sorted_sets), 2)
+
+    def test_four_audits_two_eins(self):
+        # Four audits.
+        # RID1, UEI1, EIN1
+        # RID2, UEI1, EIN1
+        # RID3, UEI2, EIN2
+        # RID4, UEI2, EIN2
+        # Two clusters of size two.
+        rid_count = 0
+        for i in range(2):
+            for ein in ["123456789", "123123123"]:
+                rid_count += 1
+
+                gi = sac_01["general_information"]
+                gi["ein"] = ein
+
+                rid = sac_01["report_id"][:-1] + f"{rid_count}"
+
+                baker.make(
+                    SingleAuditChecklist,
+                    report_id=rid,
+                    submission_status=sac_01["submission_status"],
+                    transition_name=sac_01["transition_name"],
+                    transition_date=sac_01["transition_date"],
+                    general_information=gi,
+                )
+
+        sorted_sets = generate_clusters("2022")
+        # I expect two clusters, one for each UEI, and each
+        # set to be of size two.
+        self.assertEqual(len(sorted_sets), 2)
+        for s in sorted_sets:
+            self.assertEqual(len(s), 2)
