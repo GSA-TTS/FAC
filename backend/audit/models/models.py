3d10401e65928ff885d23c80b2ecca6d69862eb6
ranyem <rmclendon@fearless.tech>
2025-07-14 12:01:16 -0400

    linting issue

--- backend/audit/models/models.py
+++ backend/audit/models/models.py
@@ -427,9 +427,7 @@ class SingleAuditChecklist(models.Model, GeneralInformationMixin):  # type: igno
 
     # Resubmission Meta
     resubmission_meta = models.JSONField(
-        blank=True,
-        null=True,
-        help_text="Resubmission JSON structure"
+        blank=True, null=True, help_text="Resubmission JSON structure"
     )
 
     # Resubmission Status
@@ -437,13 +435,13 @@ class SingleAuditChecklist(models.Model, GeneralInformationMixin):  # type: igno
         max_length=30,
         blank=True,
         null=True,
-        help_text="Current status of the resubmission (e.g. ORIGINAL_SUBMISSION, MOST_RECENT, DEPRECATED_VIA_RESUBMISSION)."
+        help_text="Current status of the resubmission (e.g. ORIGINAL_SUBMISSION, MOST_RECENT, DEPRECATED_VIA_RESUBMISSION).",
     )
 
     # Resubmission Version
     resubmission_version = models.BigIntegerField(
         default=0,
-        help_text="Version counter of how many times this SAC was resubmitted."
+        help_text="Version counter of how many times this SAC was resubmitted.",
     )
 
     def validate_full(self):
