acae3c560048b7856c24b50b16e1a6c9788781c2
Matthew Jadud <matthew.jadud@gsa.gov>
2025-07-22 06:52:54 -0400

    Update UEIs or EINs for submitted audits (#5108)
    * First steps on UEI update
    
    This is a start. I need to refresh my local before going further.
    
    1. Entries validated.
    2. Changes made.
    3. Save implemented.
    
    * Adds "redisseminate()" and moves closer to complete
    
    This now will relplace UEI or EIN records, and adds `redisseminate()` to
    the SAC, because we'll need it. (And, it is safer to put there than
    here.)
    
    Some more work to do, but I wanted to get it pushed in an interim state.
    
    Many thanks to Phil for working through the logic in `.save()`.
    
    * Adding hashing, __eq__
    
    In order to add things to set()s, I needed to add a __hash__ function to
    the model. I hash the string representation. (This will not impact
    anything in production.)
    
    * Updating tool, adding tests
    
    This adds some unit tests, and also breaks things apart (mostly for
    testing).
    
    * More tests, validations on inputs
    
    This now defends nicely against bad inputs, and only works if the audit
    is already dissemianted.
    
    * Adding in code for supporting redissemination
    
    Adding a DISSEMINATED->DISSEMINATED transition, so that we can extend
    the transition_name and transition_date fields to include additional
    dissemination notices when we... redisseminate a record.
    
    * Final changes before merge.
    
    * Updating throws, linting
    
    `make lint` was run inside the container.
    
    Lets see if that aligns with what we do in the cloud.
    
    * Moving atomic()
    
    Making sure that the curation tracking happens atomically.
    
    * Adding in docs
    
    We'll want to update this for other commands. I don't think we have any
    notes anywhere about our management commands.
    
    * The ordering of curation tracking matters
    
    We cannot
    
    atomic.transaction():
      with curation_tracking():
        ...
    
    Why? Because curation tracking creates AND destroys triggers.
    
    As a result, doing that under a transaction causes the process/DB to
    lock.
    
    Instead, we MUST
    
    with curation_tracking():
      atomic.transaction():
        ...
    
    so that we enable the triggers, do the atomic work, and disable the
    triggers for tracking changes.
    
    * A new migration comes with this work
    
    Why?
    
    Becuase we are adding to the choices for a submission event. Therefore,
    those choices are reflected in the model.
    
    * Fixing .save()
    
    When updating the .save() in the SAC, we missed that we should ALWAYS
    create the submission event. Our unit tests saved us.
    
    All tests pass, comments improved the code, and we think we left it in a
    better state. HT to JamesP and PhilD.
    
    * Updates based on PD's suggestions
    
    Some renaming, a few comments removed, table name fix.
    
    * Quick linting fix for the review
    
    ---------
    
    Co-authored-by: Phil Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
    Co-authored-by: James Person <jperson1@umbc.edu>

---
+++ backend/curation/management/commands/update_uei_or_ein_for_submitted_audit.py
@@ -0,0 +1,167 @@
+from django.core.management.base import BaseCommand
+
+from audit.models import SingleAuditChecklist
+import logging
+from curation.curationlib.update_uei_or_ein import (
+    update_uei,
+    update_ein,
+    get_sac_with_uei,
+    get_sac_with_ein_to_update,
+    check_report_disseminated,
+)
+
+import sys
+from users.models import StaffUser
+from audit.validators import validate_uei
+from django.core.exceptions import ValidationError
+import re
+
+logger = logging.getLogger(__name__)
+
+
+def validate_uei_options(options):
+    try:
+        # We use a .get(), which will fail if there is more than one.
+        _ = get_sac_with_uei(options)
+        ok_old_uei = True
+    except Exception as e:
+        logger.error(e)
+        logger.error(f"could not fetch report_id {options['report_id']}")
+        ok_old_uei = False
+    # New UEI?
+    # Hm. The new UEI might not be in the database.
+    # We could validate against SAM here. For now, we'll make sure
+    # it is a valid-shaped UEI.
+    try:
+        ok_new_uei = validate_uei(options["new_uei"])
+    except ValidationError:
+        logger.error("new_uei is not valid")
+        ok_new_uei = False
+    return ok_old_uei, ok_new_uei
+
+
+def validate_ein_options(options):
+    try:
+        _ = get_sac_with_ein_to_update(options)
+        ok_old_ein = re.match("[0-9]{9}", options["old_ein"])
+        if not ok_old_ein:
+            logger.error(f"The EIN {options['old_ein']} is not nine digits.")
+    except Exception as e:
+        logger.error(e)
+        logger.error(f"could not fetch {options['report_id']}")
+        ok_old_ein = False
+
+    # All we can assert is an EIN is nine digits.
+    try:
+        ok_new_ein = re.match("[0-9]{9}", options["new_ein"])
+        if not ok_new_ein:
+            logger.error(f"The EIN {options['new_ein']} is not nine digits.")
+    except ValidationError:
+        logger.error("new_ein is not valid")
+        ok_new_ein = False
+    return ok_old_ein, ok_new_ein
+
+
+def validate_inputs(options):
+    # We have to check all of the options passed.
+    # Those checks need to be against the database.
+
+    # Does the report id exist?
+    try:
+        ok_report_id = SingleAuditChecklist.objects.get(report_id=options["report_id"])
+    except SingleAuditChecklist.DoesNotExist:
+        logger.error("report_id not found")
+        ok_report_id = False
+
+    # Is it disseminated?
+    is_disseminated = check_report_disseminated(options)
+    if not is_disseminated:
+        logger.error(f"The report {options['report_id']} is not disseminated. Exiting.")
+        return False
+
+    # And, did they provide a staff user email?
+    # (Note that they had to have privs in TF and be able to
+    # enable SSH inproduction in order to get here.)
+    try:
+        ok_staff_user = StaffUser.objects.get(staff_email=options["email"])
+    except StaffUser.DoesNotExist:
+        logger.error(f'Staff user {options["email"]} does not exist')
+        ok_staff_user = False
+
+    # We either need a pair of UEIs, or a pair of EINs.
+    # Do we have a pair of UEIs?
+    if options["old_uei"] is not None and options["new_uei"] is not None:
+        ok_old_uei, ok_new_uei = validate_uei_options(options)
+        return ok_report_id and (ok_old_uei and ok_new_uei) and ok_staff_user
+
+    # Do we have a pair of EINs?
+    elif options["old_ein"] is not None and options["new_ein"] is not None:
+        ok_old_ein, ok_new_ein = validate_ein_options(options)
+        return ok_report_id and (ok_old_ein and ok_new_ein) and ok_staff_user
+    # Did we mix-and-match between EIN and UEI?
+    elif options["old_ein"] is not None and options["new_uei"] is not None:
+        logger.error("You provided an old EIN and new UEI. Exiting.")
+        return False
+    # or the other way around?
+    elif options["old_uei"] is not None and options["new_ein"] is not None:
+        logger.error("You provided an old UEI and new EIN. Exiting.")
+        return False
+    # Otherwise, let the user know this won't work.
+    else:
+        logger.error("You must provide either an old/new UEI or old/new EIN")
+        return False
+
+
+class Command(BaseCommand):
+    """Updates the UEI for a previously submitted audit."""
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--report_id",
+            type=str,
+            required=True,
+            help="Report id that we will modify",
+        )
+
+        # Apparently I cannot build a mutex of groups?
+        # https://github.com/python/cpython/issues/101337
+        # So, I'll enforce the mutual exclusion in `validate_inputs`
+        parser.add_argument(
+            "--old_uei",
+            type=str,
+            help="The UEI that is currently in place",
+        )
+        parser.add_argument(
+            "--new_uei",
+            type=str,
+            help="The new UEI for this report",
+        )
+
+        parser.add_argument(
+            "--old_ein",
+            type=str,
+            help="The EIN that is currently in place",
+        )
+        parser.add_argument(
+            "--new_ein",
+            type=str,
+            help="The new EIN for this report",
+        )
+
+        parser.add_argument(
+            "--email",
+            type=str,
+            required=True,
+            help="The email of the FAC staff making this change",
+        )
+
+    def handle(self, *args, **options):
+        valid_inputs = validate_inputs(options)
+        if not valid_inputs:
+            logger.error("inputs were not valid")
+            sys.exit(-1)
+
+        if options["old_uei"] is not None and options["new_uei"] is not None:
+            update_uei(options)
+        elif options["old_ein"] is not None and options["new_ein"] is not None:
+            update_ein(options)
