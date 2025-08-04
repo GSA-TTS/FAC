8cee9894caebd77655b9febe7f1742aeecbfd04f
James Person <jperson1@umbc.edu>
2025-08-04 10:26:37 -0400

    Minor reorganizing and a docstring, appeasing the linter

--- backend/audit/check_resubmission_allowed.py
+++ backend/audit/check_resubmission_allowed.py
@@ -13,15 +13,31 @@ def get_last_transition_date(sac):
 
 def check_resubmission_allowed(
     sac: SingleAuditChecklist,
-) -> Tuple[bool, str]:  # noqa: C901
+) -> Tuple[bool, str]:
+    """
+    Given a SingleAuditChecklist, determine if resubmitting the record should be allowed.
+    Return a Tuple(boolean, string), where the boolean determines if resubmission is allowed
+    and the string is a justification.
+
+    A record cannot be resubmitted:
+    1. If it is not already public. As in, it is not already disseminated.
+    2. If essential data is missing.
+    3. If it has already been deprecated by another resubmission.
+    4. If we can reasonably assume that a sibling record is a more recent resubmission.
+    """
+    # JSON from SAC
     meta = sac.resubmission_meta or {}
     gi = sac.general_information or {}
 
-    uei = gi.get("auditee_uei")
-    end_date = gi.get("auditee_fiscal_period_end")
-    version = meta.get("version") if meta else sac.version
-    submission_status = sac.submission_status
+    # String variables pulled from SAC info
+    end_date = gi.get("auditee_fiscal_period_end", "")
     resub_status = meta.get("resubmission_status")
+    submission_status = sac.submission_status
+    auditee_uei = gi.get("auditee_uei")
+    version = meta.get("version") if meta else sac.version
+
+    # Further derived from string variables
+    audit_year = int(end_date.split("-")[0])
 
     # SAC Status must be DISSEMINATED
     if submission_status != STATUS.DISSEMINATED:
@@ -30,14 +46,8 @@ def check_resubmission_allowed(
             f"Resubmission is only allowed when the current submission is in '{STATUS.DISSEMINATED}' status. Current status: '{submission_status}'",
         )
 
-    # Audit Year
-    try:
-        audit_year = int(end_date.split("-")[0])
-    except Exception:
-        audit_year = None
-
     # Fallback check when version, year, uei is missing or incorrect data
-    if not all([uei, end_date, audit_year, sac.id]) or version is None:
+    if not all([auditee_uei, end_date, audit_year, sac.id]) or version is None:
         return (
             False,
             "Audit record is incomplete and cannot be evaluated for resubmission.",
@@ -53,7 +63,7 @@ def check_resubmission_allowed(
     # Legacy audit (meta = None) At this point, we assume data has been curated: valid submission_status, uei, year.
     if sac.resubmission_meta is None:
         siblings = SingleAuditChecklist.objects.filter(
-            general_information__auditee_uei=uei,
+            general_information__auditee_uei=auditee_uei,
             general_information__auditee_fiscal_period_end__startswith=str(audit_year),
             resubmission_meta__isnull=True,
         )
