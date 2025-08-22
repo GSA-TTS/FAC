8f23cbfdce4a907b5570a3429150c0e5e30ba530
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2025-08-22 13:45:31 -0400

    Adding resub perm checks to PDF download mixin

--- backend/dissemination/mixins.py
+++ backend/dissemination/mixins.py
@@ -5,6 +5,7 @@ from django.http import Http404
 
 from dissemination.models import General
 from users.permissions import can_read_tribal
+from audit.models.constants import STATUS, RESUBMISSION_STATUS
 
 
 logger = logging.getLogger(__name__)
@@ -15,6 +16,26 @@ class ReportAccessRequiredMixin:
         report_id = kwargs["report_id"]
         try:
             general = General.objects.get(report_id=report_id)
+
+            # If the resubmission_status is None
+            if not general.resubmission_status:
+                pass
+            # If the resubmission is a recent/current one
+            elif general.resubmission_status in [RESUBMISSION_STATUS.MOST_RECENT, RESUBMISSION_STATUS.ORIGINAL]:
+                pass
+            # If you have privileged access, you can see old/resubmitted reports
+            elif general.resubmission_status == RESUBMISSION_STATUS.DEPRECATED and request.user and can_read_tribal(request.user):
+                pass
+            # If you are not logged in, or not privileged, and it is deprecated,
+            # we need to deny.
+            elif general.resubmission_status == RESUBMISSION_STATUS.DEPRECATED:
+                raise PermissionDenied
+            # In case we add new statuses in the future, we want to fail here
+            # And, if we're wrong, it should be `pass`. However, the safe
+            # thing to do is to fail here; we should explicitly encode all of
+            # the conditions we 'pass' in case of resubmissions.
+            else:
+                raise PermissionDenied
 
             if general.is_public:
                 return super().dispatch(request, *args, **kwargs)
