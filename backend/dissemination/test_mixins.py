fdaeaf1d38be95c6ba13fcd2de15b057279f8dfa
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2025-08-26 12:16:49 -0400

    Actually adding unit tests

--- backend/dissemination/test_mixins.py
+++ backend/dissemination/test_mixins.py
@@ -7,6 +7,7 @@ from django.views.generic import View
 
 from dissemination.models import General
 from dissemination.mixins import ReportAccessRequiredMixin, FederalAccessRequiredMixin
+from audit.models.constants import RESUBMISSION_STATUS
 
 from users.models import Permission, UserPermission
 
@@ -16,95 +17,122 @@ User = get_user_model()
 
 
 class ReportAccessRequiredMixinTests(TestCase):
+    def setUp(self):
+        self.user = baker.make(User)
+        self.request = RequestFactory().get("/")
+        self.request.user = self.user
+
     class ViewStub(ReportAccessRequiredMixin, View):
         def get(self, request, *args, **kwargs):
             pass
 
-    def test_missing_report_id_raises(self):
-        request = RequestFactory().get("/")
+    def _make_permissioned(self):
+        permission = Permission.objects.get(slug=Permission.PermissionType.READ_TRIBAL)
+        baker.make(
+            UserPermission, user=self.user, email=self.user.email, permission=permission
+        )
 
-        self.assertRaises(KeyError, self.ViewStub().dispatch, request)
+    def test_missing_report_id_raises(self):
+        self.assertRaises(KeyError, self.ViewStub().dispatch, self.request)
 
     def test_nonexistent_report_raises(self):
-        request = RequestFactory().get("/")
-
         self.assertRaises(
             Http404,
             self.ViewStub().dispatch,
-            request,
+            self.request,
             report_id="not-a-real-report-id",
         )
 
     def test_public_report_passes(self):
-        request = RequestFactory().get("/")
-
         general = baker.make(General, is_public=True)
 
-        self.ViewStub().dispatch(request, report_id=general.report_id)
+        self.ViewStub().dispatch(self.request, report_id=general.report_id)
 
     def test_non_public_raises_for_anonymous(self):
-        request = RequestFactory().get("/")
-        request.user = None
+        self.request.user = None
 
         general = baker.make(General, is_public=False)
 
         self.assertRaises(
             PermissionDenied,
             self.ViewStub().dispatch,
-            request,
+            self.request,
             report_id=general.report_id,
         )
 
     def test_non_public_raises_for_unpermissioned(self):
-        request = RequestFactory().get("/")
-
-        user = baker.make(User)
-        request.user = user
-
         general = baker.make(General, is_public=False)
 
         self.assertRaises(
             PermissionDenied,
             self.ViewStub().dispatch,
-            request,
+            self.request,
             report_id=general.report_id,
         )
 
     def test_non_public_passes_for_permissioned(self):
-        request = RequestFactory().get("/")
-
-        user = baker.make(User)
-        request.user = user
-
-        permission = Permission.objects.get(slug=Permission.PermissionType.READ_TRIBAL)
-        baker.make(UserPermission, user=user, email=user.email, permission=permission)
+        self._make_permissioned()
 
         general = baker.make(General, is_public=False)
 
-        self.ViewStub().dispatch(request, report_id=general.report_id)
+        self.ViewStub().dispatch(self.request, report_id=general.report_id)
 
     def test_public_passes_for_unpermissioned(self):
-        request = RequestFactory().get("/")
-
-        user = baker.make(User)
-        request.user = user
-
         general = baker.make(General, is_public=True)
 
-        self.ViewStub().dispatch(request, report_id=general.report_id)
+        self.ViewStub().dispatch(self.request, report_id=general.report_id)
 
     def test_public_passes_for_permissioned(self):
-        request = RequestFactory().get("/")
-
-        user = baker.make(User)
-        request.user = user
-
-        permission = Permission.objects.get(slug=Permission.PermissionType.READ_TRIBAL)
-        baker.make(UserPermission, user=user, email=user.email, permission=permission)
+        self._make_permissioned()
 
         general = baker.make(General, is_public=True)
 
-        self.ViewStub().dispatch(request, report_id=general.report_id)
+        self.ViewStub().dispatch(self.request, report_id=general.report_id)
+
+    def test_resub_passes_for_permissioned(self):
+        self._make_permissioned()
+
+        general = baker.make(
+            General, is_public=True, resubmission_status=RESUBMISSION_STATUS.DEPRECATED
+        )
+
+        self.ViewStub().dispatch(self.request, report_id=general.report_id)
+
+    def test_resub_passes_for_null_status(self):
+        general = baker.make(General, is_public=True, resubmission_status=None)
+
+        self.ViewStub().dispatch(self.request, report_id=general.report_id)
+
+    def test_resub_passes_for_most_recent_status(self):
+        general = baker.make(
+            General, is_public=True, resubmission_status=RESUBMISSION_STATUS.MOST_RECENT
+        )
+
+        self.ViewStub().dispatch(self.request, report_id=general.report_id)
+
+    def test_resub_fails_for_unpermissioned(self):
+        general = baker.make(
+            General, is_public=True, resubmission_status=RESUBMISSION_STATUS.DEPRECATED
+        )
+
+        self.assertRaises(
+            PermissionDenied,
+            self.ViewStub().dispatch,
+            self.request,
+            report_id=general.report_id,
+        )
+
+    def test_resub_fails_for_unknown_status(self):
+        self._make_permissioned()
+
+        general = baker.make(General, is_public=True, resubmission_status="foobar")
+
+        self.assertRaises(
+            PermissionDenied,
+            self.ViewStub().dispatch,
+            self.request,
+            report_id=general.report_id,
+        )
 
 
 class FederalAccessRequiredMixinTests(TestCase):
@@ -146,3 +174,11 @@ class FederalAccessRequiredMixinTests(TestCase):
             self.ViewStub().dispatch(request)
         except PermissionDenied:
             self.fail("Authenticated users should have access.")
+
+    def test_no_resub_status(self):
+        """
+        An unauthenticated user should not be able to access the gated view.
+        """
+        request = RequestFactory().get("/")
+
+        self.assertRaises(PermissionDenied, self.ViewStub().dispatch, request)
