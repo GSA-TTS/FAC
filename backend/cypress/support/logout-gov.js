e8dc715eb2f51c957fa59a94bf3707087ab488eb
ranyem <rmclendon@fearless.tech>
2026-01-16 11:25:45 -0500

    logout cypress test

--- backend/cypress/support/logout-gov.js
+++ backend/cypress/support/logout-gov.js
@@ -1,11 +1,14 @@
 export function testLogoutGov() {
-  cy.get('.usa-menu-btn').contains('Menu').click();
-  cy.get('button').contains('Sign out').click();
-  cy.origin(
-    'https://idp.int.identitysandbox.gov/',
-    {},
-    () => {
-      cy.contains('Yes, sign out of Login.gov').click();
-    }
-  );
-};
+  cy.origin('http://localhost:8000', () => {
+    cy.get('.usa-menu-btn').contains('Menu').click();
+    cy.get('button').contains('Sign out').click();
+  });
+  
+  cy.origin('https://idp.int.identitysandbox.gov/', () => {
+    cy.contains('Yes, sign out of Login.gov').click();
+  });
+  
+  cy.origin('http://localhost:8000', () => {
+    cy.url().should('include', 'localhost:8000');
+  });
+}
