0faeaee844d317399f49ea773caf08a656405542
James Person <jperson1@umbc.edu>
2026-01-15 22:43:33 -0500

    LINTING

--- backend/audit/cross_validation/check_expenditure_threshold_met.py
+++ backend/audit/cross_validation/check_expenditure_threshold_met.py
@@ -30,8 +30,11 @@ def check_expenditure_threshold_met(
             award["loan_or_loan_guarantee"]["is_guaranteed"] == "Y"
         )
         if is_loan_or_loan_guarantee:
-            loan_balance = award["loan_or_loan_guarantee"]["loan_balance_at_audit_period_end"]
-            if isinstance(loan_balance, (int, float)):  # 20260115 - Hotfix. This is not guaranteed to be either empty or an integer.
+            loan_balance = award["loan_or_loan_guarantee"][
+                "loan_balance_at_audit_period_end"
+            ]
+            # 20260115 - Hotfix. This is not guaranteed to be either empty or an integer.
+            if isinstance(loan_balance, (int, float)):
                 abs_total += abs(loan_balance)
 
     fy_start_date = date.fromisoformat(
