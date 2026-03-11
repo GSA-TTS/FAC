b17b1a3b5ac8109fdf7dcbd8a6982b7e81c7fa55
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2026-03-11 12:52:44 -0400

    Linting

--- backend/dissemination/test_search_resub_tags.py
+++ backend/dissemination/test_search_resub_tags.py
@@ -10,7 +10,7 @@ from audit.models.constants import RESUBMISSION_STATUS, RESUBMISSION_TAGS
 
 class ResubmissionTagTests(TestCase):
     def test_v0_no_tag(self):
-        """ v0 audits don't get a tag """
+        """v0 audits don't get a tag"""
         rows = [
             baker.make(
                 General,
@@ -26,7 +26,7 @@ class ResubmissionTagTests(TestCase):
         self.assertEqual(rows[0].resubmission_tag, None)
 
     def test_v1_no_tag(self):
-        """ v1 most_recent audits don't get a tag """
+        """v1 most_recent audits don't get a tag"""
 
         rows = [
             baker.make(
@@ -43,7 +43,7 @@ class ResubmissionTagTests(TestCase):
         self.assertEqual(rows[0].resubmission_tag, None)
 
     def test_v2_most_recent(self):
-        """ v2 most_recent audits do get a tag """
+        """v2 most_recent audits do get a tag"""
 
         rows = [
             baker.make(
@@ -57,10 +57,12 @@ class ResubmissionTagTests(TestCase):
         ]
         add_resub_tag_data(rows)
 
-        self.assertEqual(rows[0].resubmission_tag, f"V2 ({RESUBMISSION_TAGS.MOST_RECENT})")
+        self.assertEqual(
+            rows[0].resubmission_tag, f"V2 ({RESUBMISSION_TAGS.MOST_RECENT})"
+        )
 
     def test_v1_resub(self):
-        """ v1 deprecated audits do get a tag """
+        """v1 deprecated audits do get a tag"""
 
         rows = [
             baker.make(
@@ -74,10 +76,12 @@ class ResubmissionTagTests(TestCase):
         ]
         add_resub_tag_data(rows)
 
-        self.assertEqual(rows[0].resubmission_tag, f"V1 ({RESUBMISSION_TAGS.DEPRECATED})")
+        self.assertEqual(
+            rows[0].resubmission_tag, f"V1 ({RESUBMISSION_TAGS.DEPRECATED})"
+        )
 
     def test_mixed(self):
-        """ Simple case of audits that get different tags """
+        """Simple case of audits that get different tags"""
 
         rows = [
             baker.make(
@@ -103,10 +107,14 @@ class ResubmissionTagTests(TestCase):
                 audit_year="2022",
                 resubmission_status=RESUBMISSION_STATUS.MOST_RECENT,
                 resubmission_version=2,
-            )
+            ),
         ]
         add_resub_tag_data(rows)
 
         self.assertEqual(rows[0].resubmission_tag, None)
-        self.assertEqual(rows[1].resubmission_tag, f"V1 ({RESUBMISSION_TAGS.DEPRECATED})")
-        self.assertEqual(rows[2].resubmission_tag, f"V2 ({RESUBMISSION_TAGS.MOST_RECENT})")
+        self.assertEqual(
+            rows[1].resubmission_tag, f"V1 ({RESUBMISSION_TAGS.DEPRECATED})"
+        )
+        self.assertEqual(
+            rows[2].resubmission_tag, f"V2 ({RESUBMISSION_TAGS.MOST_RECENT})"
+        )
