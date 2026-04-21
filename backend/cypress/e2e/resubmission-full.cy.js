e45479841de8f5ec67f00540d82a7d6ae9675fc4
Phil Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2026-04-21 12:31:08 -0400

    Fixes for resub meta population (#5571)
    * Fixes for resub meta population
    
    * 3 submission chain for resubmission-full
    
    * Whitespace
    
    * Linting

--- backend/cypress/e2e/resubmission-full.cy.js
+++ backend/cypress/e2e/resubmission-full.cy.js
@@ -3,16 +3,26 @@ import { testLoginGovLogin } from '../support/login-gov.js';
 
 describe('Full audit resubmission', () => {
   it('Non-tribal, public', () => {
-    testFullSubmission(false, true).then((previous_report_id) => {
+    testFullSubmission(false, true).then((previous_report_id_1) => {
       testLoginGovLogin();
 
       cy.visit('/audit/resubmission-start');
       cy.get('#id_material_change_reasons_0').check({ force: true });
-      cy.get('#report_id').type(previous_report_id);
+      cy.get('#report_id').type(previous_report_id_1);
       cy.get('#continue').click();
       cy.url().should('include', '/report_submission/eligibility/');
 
-      testFullSubmission(false, true, true);
+      testFullSubmission(false, true, true).then((previous_report_id_2) => {
+        testLoginGovLogin();
+
+        cy.visit('/audit/resubmission-start');
+        cy.get('#id_material_change_reasons_0').check({ force: true });
+        cy.get('#report_id').type(previous_report_id_2);
+        cy.get('#continue').click();
+        cy.url().should('include', '/report_submission/eligibility/');
+
+        testFullSubmission(false, true, true)
+      });
     });
   });
 });
