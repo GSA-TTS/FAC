6f9345ce053e7de3c16345847ba3368a73bc2fd2
Philip Dominguez <142051477+phildominguez-gsa@users.noreply.github.com>
2026-09-04 07:44:36 -0400

    Removing AdvancedSearchFormTests

--- backend/dissemination/test_search_forms.py
+++ backend/dissemination/test_search_forms.py
@@ -27,29 +27,3 @@ class SearchFormTests(TestCase):
             form.cleaned_data["uei_or_ein"],
             ["123456789", "987654321"],
         )
-
-
-class AdvancedSearchFormTests(TestCase):
-    def test_uei_or_ein_removes_dashes(self):
-        form = AdvancedSearchForm(data={"uei_or_ein": "12-3456789"})
-
-        self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data["uei_or_ein"], ["123456789"])
-
-    def test_uei_or_ein_separates_commas(self):
-        form = AdvancedSearchForm(data={"uei_or_ein": "123456789,987654321"})
-
-        self.assertTrue(form.is_valid())
-        self.assertEqual(
-            form.cleaned_data["uei_or_ein"],
-            ["123456789", "987654321"],
-        )
-
-    def test_uei_or_ein_separates_newlines(self):
-        form = AdvancedSearchForm(data={"uei_or_ein": "123456789\n987654321"})
-
-        self.assertTrue(form.is_valid())
-        self.assertEqual(
-            form.cleaned_data["uei_or_ein"],
-            ["123456789", "987654321"],
-        )
